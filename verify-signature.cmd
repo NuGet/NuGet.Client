d9a0f812bfc7fbc3fcc89f0dd9c86361f5d95382
juste <juste@microsoft.com>
2014-12-04 22:08:56 -0800

    Updating pack scripts

--- verify-signature.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-echo ---------DEBUG---------
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\x64\sn.exe" -Tp src\Versioning\bin\Debug\NuGet.Versioning.dll
-
-echo ---------RELEASE---------
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\x64\sn.exe" -Tp src\Versioning\bin\Release\NuGet.Versioning.dll
