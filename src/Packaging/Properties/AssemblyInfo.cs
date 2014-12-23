8b0369c2d78f7706ede8e126a97354c03876a7d8
unknown <daravind@DARAVINDMAIN.redmond.corp.microsoft.com>
2014-12-23 14:18:50 -0800

    Changed the target fx of NuGet.Packaging from PCL to net45 Eventually, it should target both net45 and CoreCLR

--- src/Packaging/Properties/AssemblyInfo.cs
+++ src/Packaging/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -9,12 +8,19 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Packaging")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft Corp.")]
 [assembly: AssemblyProduct("Packaging")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corp. 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60f1f764-1792-4fc6-b23d-2373ab328535")]
 
 // Version information for an assembly consists of the following four values:
 //
