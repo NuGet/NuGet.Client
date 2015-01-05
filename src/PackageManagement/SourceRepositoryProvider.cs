89c8438139bff6d810b9814e6ff6d8934dee2bab
juste <justin@emgarten.com>
2015-01-04 20:58:25 -0800

    Adding a basic SourceRepositoryProvider

---
+++ src/PackageManagement/SourceRepositoryProvider.cs
@@ -0,0 +1,66 @@
+﻿using NuGet.Client;
+using NuGet.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet.PackageManagement
+{
+    /// <summary>
+    /// SourceRepositoryProvider is the high level source for repository objects representing package sources.
+    /// </summary>
+    [Export]
+    public sealed class SourceRepositoryProvider
+    {
+        // TODO: add support for reloading sources when changes occur
+        private readonly IPackageSourceProvider _packageSourceProvider;
+        private IEnumerable<Lazy<INuGetResourceProvider, INuGetResourceProviderMetadata>> _resourceProviders;
+        private List<SourceRepository> _repositories;
+
+        // TODO: fix the settings here
+        [ImportingConstructor]
+        public SourceRepositoryProvider(IEnumerable<Lazy<INuGetResourceProvider, INuGetResourceProviderMetadata>> resourceProviders)
+            : this(new PackageSourceProvider(NullSettings.Instance), resourceProviders)
+        {
+
+        }
+
+        /// <summary>
+        /// Non-MEF constructor
+        /// </summary>
+        public SourceRepositoryProvider(IPackageSourceProvider packageSourceProvider, IEnumerable<Lazy<INuGetResourceProvider, INuGetResourceProviderMetadata>> resourceProviders)
+        {
+            _packageSourceProvider = packageSourceProvider;
+            _resourceProviders = resourceProviders;
+
+            Init();
+        }
+
+        /// <summary>
+        /// Retrieve repositories
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<SourceRepository> GetRepositories()
+        {
+            return _repositories;
+        }
+
+        private void Init()
+        {
+            _repositories = new List<SourceRepository>();
+
+            foreach (var source in _packageSourceProvider.LoadPackageSources())
+            {
+                if (source.IsEnabled)
+                {
+                    SourceRepository sourceRepo = new SourceRepository(source, _resourceProviders);
+                    _repositories.Add(sourceRepo);
+                }
+            }
+        }
+    }
+}
