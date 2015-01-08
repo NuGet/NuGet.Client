e83123c1e57b3eaa52f207a67ece22c84ccb9e2d
deepakaravindr <daravind@microsoft.com>
2015-01-08 00:09:55 -0800

    Added packageidentity tests

---
+++ test/Core.Test/PackageIdentityTests.cs
@@ -0,0 +1,19 @@
+﻿using NuGet.PackagingCore;
+using NuGet.Versioning;
+using Xunit;
+
+namespace Core.Test
+{
+    public class PackageIdentityTests
+    {
+        [Fact]
+        public void TestToString()
+        {
+            var packageIdentity = new PackageIdentity("packageA", new NuGetVersion("1.0.0"));
+            Assert.Equal("packageA.1.0.0", packageIdentity.ToString());
+
+            var formattedString = string.Format("This is package '{0}'", packageIdentity);
+            Assert.Equal("This is package 'packageA.1.0.0'", formattedString);
+        }
+    }
+}
