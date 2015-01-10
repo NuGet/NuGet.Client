1058a4b5af5008c5525bd2730db93a9c15aab389
deepakaravindr <daravind@microsoft.com>
2015-01-09 18:07:25 -0800

    Added IProductUpdateService and IPackageRestoreManager. And, Removed dependency on NuGet.TestUtility. Updated solution to latest NuGet.PackageManagement

---
+++ src/PackageManagement/IDE/IPackageRestoreManagercs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NuGet.PackageManagement
+{
+    public interface IPackageRestoreManager
+    {
+        /// <summary>
+        /// Gets a value indicating whether the current solution is configured for Package Restore mode.
+        /// </summary>
+        bool IsCurrentSolutionEnabledForRestore { get; }
+
+        /// <summary>
+        /// Configures the current solution for Package Restore mode.
+        /// </summary>
+        /// <param name="fromActivation">if set to <c>false</c>, the method will not show any error message, and will not set package restore consent.</param>
+        void EnableCurrentSolutionForRestore(bool fromActivation);
+
+        /// <summary>
+        /// Occurs when it is detected that the packages are missing or restored for the current solution.
+        /// </summary>
+        event EventHandler<PackagesMissingStatusEventArgs> PackagesMissingStatusChanged;
+
+        /// <summary>
+        /// Checks the current solution if there is any package missing.
+        /// </summary>
+        /// <returns></returns>
+        void CheckForMissingPackages();
+
+        /// <summary>
+        /// Restores the missing packages for the current solution.
+        /// </summary>
+        Task RestoreMissingPackages();
+    }
+
+    public class PackagesMissingStatusEventArgs : EventArgs
+    {
+        public bool PackagesMissing { get; private set; }
+
+        public PackagesMissingStatusEventArgs(bool packagesMissing)
+        {
+            PackagesMissing = packagesMissing;
+        }
+    }
+}
---
+++ src/PackageManagement/IDE/IProductUpdateService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NuGet.PackageManagement
+{
+    public interface IProductUpdateService
+    {
+        void CheckForAvailableUpdateAsync();
+        void Update();
+        void DeclineUpdate(bool doNotRemindAgain);
+        event EventHandler<ProductUpdateAvailableEventArgs> UpdateAvailable;
+    }
+
+    public class ProductUpdateAvailableEventArgs : EventArgs
+    {
+        internal ProductUpdateAvailableEventArgs(Version currentVersion, Version newVersion)
+        {
+            CurrentVersion = currentVersion;
+            NewVersion = newVersion;
+        }
+
+        public Version CurrentVersion { get; private set; }
+        public Version NewVersion { get; private set; }
+    }
+}
