a540c81c3acfe040eda6445f737eadb6256d4d90
zhili1208 <zhili128@outlook.com>
2015-01-12 16:30:29 -0800

    make encryption utility public

--- src/NuGet.Configuration/Utility/EncryptionUtility.cs
+++ src/NuGet.Configuration/Utility/EncryptionUtility.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace NuGet.Configuration
 {
-    internal static class EncryptionUtility
+    public static class EncryptionUtility
     {
         private static readonly byte[] _entropyBytes = Encoding.UTF8.GetBytes("NuGet");
 
-        internal static string EncryptString(string value)
+        public static string EncryptString(string value)
         {
             var decryptedByteArray = Encoding.UTF8.GetBytes(value);
             var encryptedByteArray = ProtectedData.Protect(decryptedByteArray, _entropyBytes, DataProtectionScope.CurrentUser);
@@ -16,7 +16,7 @@ namespace NuGet.Configuration
             return encryptedString;
         }
 
-        internal static string DecryptString(string encryptedString)
+        public static string DecryptString(string encryptedString)
         {
             var encryptedByteArray = Convert.FromBase64String(encryptedString);
             var decryptedByteArray = ProtectedData.Unprotect(encryptedByteArray, _entropyBytes, DataProtectionScope.CurrentUser);
