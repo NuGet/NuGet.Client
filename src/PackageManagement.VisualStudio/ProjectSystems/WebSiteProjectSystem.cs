bf84a128c91697f28941ec4ff5f043a4424e442d
zhili1208 <zhili128@outlook.com>
2015-01-14 13:06:21 -0800

    add website project system

---
+++ src/PackageManagement.VisualStudio/ProjectSystems/WebSiteProjectSystem.cs
@@ -0,0 +1,49 @@
+﻿using NuGet.PackageManagement.VisualStudio;
+using NuGet.ProjectManagement;
+using NuGet.PackageManagement.VisualStudio.Utility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTEProject = EnvDTE.Project;
+
+namespace NuGet.PackageManagement.VisualStudio
+{
+    public class WebSiteProjectSystem : VSMSBuildNuGetProjectSystem
+    {
+        public WebSiteProjectSystem(EnvDTEProject envDTEProject, INuGetProjectContext nuGetProjectContext)
+            : base(envDTEProject, nuGetProjectContext)
+        {
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "We want to catch all exceptions")]
+        public override void AddReference(string referencePath)
+        {
+            string name = Path.GetFileNameWithoutExtension(referencePath);
+            try
+            {
+                var root = EnvDTEProjectUtility.GetFullPath(EnvDTEProject);
+                EnvDTEProjectUtility.GetAssemblyReferences(EnvDTEProject).AddFromFile(PathUtility.GetAbsolutePath(root, referencePath));
+
+                // Always create a refresh file. Vs does this for us in most cases, however for GACed binaries, it resorts to adding a web.config entry instead.
+                // This may result in deployment issues. To work around ths, we'll always attempt to add a file to the bin.
+                RefreshFileUtility.CreateRefreshFile(root,PathUtility.GetAbsolutePath(EnvDTEProjectUtility.GetFullPath(EnvDTEProject), referencePath));
+
+                NuGetProjectContext.Log(MessageLevel.Debug, VsResources.Debug_AddReference, name, ProjectName);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException(String.Format(CultureInfo.CurrentCulture, VsResources.FailedToAddReference, name), e);
+            }
+        }
+
+        protected override void AddGacReference(string name)
+        {
+            EnvDTEProjectUtility.GetAssemblyReferences(EnvDTEProject).AddFromGAC(name);
+        }
+    }
+}
---
+++ src/PackageManagement.VisualStudio/Utility/PathUtility.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet.PackageManagement.VisualStudio.Utility
+{
+    public static class PathUtility
+    {
+        public static string GetAbsolutePath(string basePath, string relativePath)
+        {
+            if (basePath == null)
+            {
+                throw new ArgumentNullException("basePath");
+            }
+
+            if (relativePath == null)
+            {
+                throw new ArgumentNullException("relativePath");
+            }
+
+            Uri resultUri = new Uri(new Uri(basePath), new Uri(relativePath, UriKind.Relative));
+            return resultUri.LocalPath;
+        }
+
+        public static string EnsureTrailingSlash(string path)
+        {
+            return EnsureTrailingCharacter(path, Path.DirectorySeparatorChar);
+        }
+
+        public static string EnsureTrailingForwardSlash(string path)
+        {
+            return EnsureTrailingCharacter(path, '/');
+        }
+
+        private static string EnsureTrailingCharacter(string path, char trailingCharacter)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException("path");
+            }
+
+            // if the path is empty, we want to return the original string instead of a single trailing character.
+            if (path.Length == 0 || path[path.Length - 1] == trailingCharacter)
+            {
+                return path;
+            }
+
+            return path + trailingCharacter;
+        }
+
+        /// <summary>
+        /// Returns path2 relative to path1
+        /// </summary>
+        public static string GetRelativePath(string path1, string path2)
+        {
+            if (path1 == null)
+            {
+                throw new ArgumentNullException("path1");
+            }
+
+            if (path2 == null)
+            {
+                throw new ArgumentNullException("path2");
+            }
+
+            Uri source = new Uri(path1);
+            Uri target = new Uri(path2);
+
+            return UriUtility.GetPath(source.MakeRelativeUri(target));
+        }
+    }
+}
