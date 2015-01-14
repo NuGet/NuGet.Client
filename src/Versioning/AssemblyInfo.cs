73498005a788d1c6b9055161a1751fb36b14e9c4
Jeff Handley <jeff@jeffhandley.com>
2015-01-14 12:25:02 -0800

    Preparing for build server

--- src/Versioning/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("NuGet.Versioning")]
-[assembly: AssemblyDescription("NuGet.Versioning is the NuGet implementation of Semantic Versioning.")]
-[assembly: AssemblyVersion("1.0.7.0")]
-[assembly: AssemblyInformationalVersion("1.0.7")]
-[assembly: CLSCompliant(true)]
---
+++ src/Versioning/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+// Project-specific attributes
+[assembly: AssemblyTitle("NuGet.Versioning")]
+[assembly: AssemblyDescription("NuGet.Versioning is the NuGet implementation of Semantic Versioning.")]
+
+// Common attributes
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("\x00a9 Outercurve Foundation. All rights reserved.")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: CLSCompliant(true)]
+
+// When built on the build server, the NuGet release version is specified by the build.
+// When built locally, the NuGet release version is the values specified in this file.
+#if !FIXED_ASSEMBLY_VERSION
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-rc")]
+#endif
