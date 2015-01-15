fa1648c54eea1670170db63222e13be42cda550c
Jeff Handley <jeff@jeffhandley.com>
2015-01-15 14:14:47 -0800

    Preparing for 3.0.0-rc build

--- src/NuGet.Configuration/Properties/AssemblyInfo.cs
+++ src/NuGet.Configuration/Properties/AssemblyInfo.cs
@@ -1,10 +1,25 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+// Project-specific attributes
 [assembly: AssemblyTitle("NuGet.Configuration")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("NuGet.Configuration implements NuGet's configuration settings")]
+
+// Common attributes
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("Copyright Outercurve Foundation. All rights reserved.")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: CLSCompliant(true)]
+
+// When built on the build server, the NuGet release version is specified by the build.
+// When built locally, the NuGet release version is the values specified in this file.
+#if !FIXED_ASSEMBLY_VERSION
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-rc")]
+#endif
