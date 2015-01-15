2fa0ec11b63362f6884deceff678b800dc3aa207
Dan Liu <danliu@microsoft.com>
2015-01-14 16:43:26 -0800

    update namespace

--- src/PackageManagement.UI/Common/ProductUpdateBar.xaml.cs
+++ src/PackageManagement.UI/Common/ProductUpdateBar.xaml.cs
@@ -1,21 +1,10 @@
 ﻿using Microsoft.VisualStudio.Shell;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
-namespace NuGet.PackageManagement.UI.Common
+namespace NuGet.PackageManagement.UI
 {
     public partial class ProductUpdateBar : UserControl
     {
