8f042d964f69a893643d5d7f686fd59144393661
Jeff Handley <jeff@jeffhandley.com>
2015-01-19 13:36:04 -0800

    Updated assembly and package metadata

--- src/PackageManagement.UI/Properties/AssemblyInfo.cs
+++ src/PackageManagement.UI/Properties/AssemblyInfo.cs
@@ -1,11 +1,23 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("PackageManagement.UI")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Project-specific attributes
+[assembly: AssemblyTitle("NuGet's Package Management UI for Visual Studio")]
+[assembly: AssemblyDescription("NuGet's Package Management UI for Visual Studio")]
+
+// Common attributes
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("Copyright Outercurve Foundation. All rights reserved.")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: CLSCompliant(true)]
+
+// When built on the build server, the NuGet release version is specified by the build.
+// When built locally, the NuGet release version is the values specified in this file.
+#if !FIXED_ASSEMBLY_VERSION
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-rc")]
+#endif
--- src/PackageManagement/Properties/AssemblyInfo.cs
+++ src/PackageManagement/Properties/AssemblyInfo.cs
@@ -1,14 +1,23 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NuGet.PackageManagement")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Project-specific attributes
+[assembly: AssemblyTitle("NuGet's core package management features")]
+[assembly: AssemblyDescription("NuGet's core package management features")]
 
+// Common attributes
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("Copyright Outercurve Foundation. All rights reserved.")]
 
-//[assembly: InternalsVisibleTo("PackageManagement.Test,PublicKey=00240000048000009400000006020000002400005253413100040000010001002d0c36d1a681bd1990011cfb66553a4d5eac7c3886d749eeb56426de5e17fde4a3eebc62d5b03ac5c640d5438165fce38317a2134320685bc6ceb85e45bd6cc7bf7dc28f166b82d0b461e91351d8943a5a945ce8dcd5bfd670927db1821a5ea1087423547b3f903781330e9f20beae8a55df7337be8a2c9bef77b317abd86b99")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: CLSCompliant(true)]
+
+// When built on the build server, the NuGet release version is specified by the build.
+// When built locally, the NuGet release version is the values specified in this file.
+#if !FIXED_ASSEMBLY_VERSION
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-rc")]
+#endif
