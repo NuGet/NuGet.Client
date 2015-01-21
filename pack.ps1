83fc51f8fb255e872ff59203a52b2a3e29c0e936
Jeff Handley <jeff@jeffhandley.com>
2015-01-21 12:14:22 -0800

    Include the push target in the restore sources

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,5 @@
 param (
-    [string]$PushTarget,
+    [string]$PushTarget=$env:NUGET_PUSH_TARGET,
     [ValidateSet("debug", "release")][string]$Configuration="release",
     [switch]$SkipTests,
     [switch]$SkipBuild,
@@ -32,11 +32,6 @@ if (!$SkipBuild)
         {
             $env:NUGET_DELAYSIGN="true"
         }
-    }
-
-    if ($PushTarget)
-    {
-        & nuget restore -source "$PushTarget"
     }
 
     Write-Host "Building! configuration: $Configuration" -ForegroundColor Cyan
