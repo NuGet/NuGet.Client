c2ea68a8ce55f01055f9ac9b563f6c71d6718274
Ranjini Mathrubootham <ranjinim@microsoft.com>
2015-01-26 13:50:25 -0800

    PackageServiceProvider to handle V3 and V2 feeds

---
+++ test/NuGet.Configuration.Test/TestUtils.cs
@@ -0,0 +1,36 @@
+﻿﻿using System;
+using System.IO;
+
+namespace NuGet.Configuration.Test
+{
+    public static class TestFilesystemUtility
+    {
+        private static readonly string NuGetTestFolder = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "NuGetTestFolder");
+
+        public static string CreateRandomTestFolder()
+        {
+            var randomFolderName = Guid.NewGuid().ToString();
+            var path = Path.Combine(NuGetTestFolder, randomFolderName);
+            Directory.CreateDirectory(path);
+            return path;
+        }
+
+        public static void DeleteRandomTestFolders(params string[] randomTestPaths)
+        {
+            foreach (var randomTestPath in randomTestPaths)
+            {
+                DeleteRandomTestFolder(randomTestPath);
+            }
+        }
+
+        private static void DeleteRandomTestFolder(string randomTestPath)
+        {
+            if (Directory.Exists(randomTestPath))
+            {
+                Directory.Delete(randomTestPath, recursive: true);
+            }
+        }
+    }
+}
