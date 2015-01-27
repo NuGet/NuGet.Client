b4f7b9cddb149880321495a61a6be3c5082fcc99
Justin Emgarten <justin@emgarten.com>
2015-01-27 15:28:18 -0800

    Moving ISourceRepositoryProvider to the protocol

---
+++ src/NuGet.Client.V3.VisualStudio/ProtocolV3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet.Client.V3.VisualStudio
+{
+    public static class ProtocolV3
+    {
+
+        // TODO: static methods create source repositories
+
+    }
+}
