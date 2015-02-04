499efb5a84b1814dc5b0633c15e6b943bd003fcf
feiling <feiling@microsoft.com>
2015-02-04 14:34:55 -0800

    Ignore the autogenerated file DebugConstants.cs.

--- src/VsConsole/PowerShellHost/DebugConstants.cs
+++
@@ -1,8 +0,0 @@
-#if DEBUG
-using System.IO;
-namespace NuGetConsole.Host.PowerShell {
-    public static class DebugConstants {
-        internal static string TestModulePath = Path.Combine(@"C:\nuget\NuGet.VisualStudioExtension\src\VsConsole\PowerShellHost\..\..\..", @"test\EndToEnd\NuGet.Tests.psm1");
-    }
-}
-#endif
