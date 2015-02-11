d9830de1634b80df1a49ba150acb6405725ce2f0
Justin Emgarten <justin@emgarten.com>
2015-02-10 18:31:36 -0800

    VsPackageInstallerEvents hook up

--- src/VisualStudioAPI/Extensibility/VsPackageInstallerEvents.cs
+++ src/VisualStudioAPI/Extensibility/VsPackageInstallerEvents.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.Composition;
+﻿using NuGet.PackageManagement;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
 
 namespace NuGet.VisualStudio
 {
@@ -16,7 +18,52 @@ namespace NuGet.VisualStudio
 
         public event VsPackageEventHandler PackageReferenceAdded = delegate { };
 
-        public event VsPackageEventHandler PackageReferenceRemoved = delegate { }; 
+        public event VsPackageEventHandler PackageReferenceRemoved = delegate { };
+
+        private readonly PackageEvents _eventSource;
+
+        [ImportingConstructor]
+        public VsPackageInstallerEvents(IPackageEventsProvider eventProvider)
+        {
+            _eventSource = eventProvider.GetPackageEvents();
+
+            _eventSource.PackageInstalled += Source_PackageInstalled;
+            _eventSource.PackageInstalling += Source_PackageInstalling;
+            _eventSource.PackageReferenceAdded += Source_PackageReferenceAdded;
+            _eventSource.PackageReferenceRemoved += Source_PackageReferenceRemoved;
+            _eventSource.PackageUninstalled += Source_PackageUninstalled;
+            _eventSource.PackageUninstalling += Source_PackageUninstalling;
+        }
+
+        private void Source_PackageUninstalling(object sender, PackageEventArgs e)
+        {
+            NotifyUninstalling(new PackageOperationEventArgs(e.InstallPath, e.Identity, null));
+        }
+
+        private void Source_PackageUninstalled(object sender, PackageEventArgs e)
+        {
+            NotifyUninstalled(new PackageOperationEventArgs(e.InstallPath, e.Identity, null));
+        }
+
+        private void Source_PackageReferenceRemoved(object sender, PackageEventArgs e)
+        {
+            NotifyReferenceRemoved(new PackageOperationEventArgs(e.InstallPath, e.Identity, null));
+        }
+
+        private void Source_PackageReferenceAdded(object sender, PackageEventArgs e)
+        {
+            NotifyReferenceAdded(new PackageOperationEventArgs(e.InstallPath, e.Identity, null));
+        }
+
+        private void Source_PackageInstalling(object sender, PackageEventArgs e)
+        {
+            NotifyInstalling(new PackageOperationEventArgs(e.InstallPath, e.Identity, null));
+        }
+
+        private void Source_PackageInstalled(object sender, PackageEventArgs e)
+        {
+            NotifyInstalled(new PackageOperationEventArgs(e.InstallPath, e.Identity, null));
+        }
 
         internal void NotifyInstalling(PackageOperationEventArgs e)
         {
