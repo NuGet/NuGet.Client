af02234452deea13a3322cd99544690942cf50d7
Maarten Balliauw <maarten@maartenballiauw.be>
2015-02-19 16:47:40 +0100

    Updated frontpage stats resource type

--- test/Client.V3Test/V3StatsResourceTests.cs
+++ test/Client.V3Test/V3StatsResourceTests.cs
@@ -12,8 +12,8 @@ namespace Client.V3Test
         public async Task GetTotalStatsHasExpectedProperties()
         {
             //var resource = await SourceRepository.GetResourceAsync<V3StatsResource>();
-            var resource = new V3StatsResource(DataClient, new Uri("https://nugetgallery.blob.core.windows.net/v3-stats0/")); // todo: remove and replace with line above when the index.json has this service type listed
-
+            var resource = new V3StatsResource(DataClient, new Uri("https://api.nuget.org/v3/stats0/")); // todo: remove and replace with line above when the index.json has this service type listed
+            
             var res = await resource.GetTotalStats(CancellationToken.None);
 
             Assert.NotNull(res);
