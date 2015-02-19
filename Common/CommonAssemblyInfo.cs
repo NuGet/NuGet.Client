e2e7c38854c3fcb5b09cb3191e6c2c0a30ffcee2
feiling <feiling@microsoft.com>
2015-02-19 15:55:39 -0800

    Fix the version of VisualStudio.Interop so that it's always 1.0.0.0.

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -13,6 +13,10 @@ using System.Runtime.InteropServices;
 // When built on the build server, the NuGet release version is specified in
 // Build\Build.proj.
 // When built locally, the NuGet release version is the values specified in this file.
+
+// Note that FIXED_ASSEMBLY_VERSION is defined in project VisualStudio.Interop so that
+// the version defined here is not used for VisualStudio.Interop.Dll. Instead, the version
+// of that DLL is always "1.0.0.0".
 #if !FIXED_ASSEMBLY_VERSION
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyInformationalVersion("3.0.0-beta")]
--- src/VisualStudio.Interop/Properties/AssemblyInfo.cs
+++ src/VisualStudio.Interop/Properties/AssemblyInfo.cs
@@ -2,3 +2,6 @@
 
 [assembly: AssemblyTitle("NuGet.VisualStudio.Interop")]
 [assembly: AssemblyDescription("NuGet/Visual Studio interop assembly")]
+
+// The version of this assembly should never be changed.
+[assembly: AssemblyVersion("1.0.0.0")]
