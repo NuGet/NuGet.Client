f69278d90a07ca179bae1a8d1c7b2446f810701d
anurse <andrew@andrewnurse.net>
2015-02-23 15:23:55 -0800

    remove unnecessary ToDictionary helper

--- src/NuGet.ProjectModel/JTokenExtensions.cs
+++ src/NuGet.ProjectModel/JTokenExtensions.cs
@@ -33,42 +33,5 @@ namespace NuGet.ProjectModel
 
             return obj.Value<T>();
         }
-        public static IDictionary<string, object> ToDictionary(this JObject obj)
-        {
-            return obj.Properties()
-                .ToDictionary(
-                    p => p.Name,
-                    p => ConvertJsonValue(p.Value));
-        }
-
-        private static object ConvertJsonValue(JToken token)
-        {
-            switch (token.Type)
-            {
-                case JTokenType.Object:
-                    return ((JObject)token).ToDictionary();
-                case JTokenType.Array:
-                    return ((JArray)token).Select(ConvertJsonValue).ToArray();
-                case JTokenType.Integer:
-                    return (long)token;
-                case JTokenType.Float:
-                    return (double)token;
-                case JTokenType.Boolean:
-                    return (bool)token;
-
-                // Dump all the string-derived types that JSON.NET tries to sniff out back to strings
-                case JTokenType.String:
-                case JTokenType.Date:
-                case JTokenType.Bytes:
-                case JTokenType.Guid:
-                case JTokenType.Uri:
-                case JTokenType.TimeSpan:
-                    return (string)token;
-                default:
-                    // This covers BOTH the case where the property value is null
-                    // AND the case where the property value is of unknown type
-                    return null;
-            }
-        }
     }
 }
