fdc5e172193da0ba5e32f1c6ac31f937838438b6
anurse <andrew@andrewnurse.net>
2015-03-04 11:28:13 -0800

    remove unused namespace

--- src/NuGet.Frameworks/NuGetFramework.cs
+++ src/NuGet.Frameworks/NuGetFramework.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Diagnostics;
-using System.Collections;
 
 namespace NuGet.Frameworks
 {
