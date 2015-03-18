3460928f2431b93ecd56d90eadf21e3261f21297
anurse <andrew@andrewnurse.net>
2015-03-17 17:39:00 -0700

    Have GetTargetFramework try an exact match first to slightly improve performance

--- src/NuGet.ProjectModel/PackageSpecExtensions.cs
+++ src/NuGet.ProjectModel/PackageSpecExtensions.cs
@@ -1,4 +1,5 @@
-﻿using NuGet.Frameworks;
+﻿using System.Linq;
+using NuGet.Frameworks;
 
 namespace NuGet.ProjectModel
 {
@@ -6,9 +7,13 @@ namespace NuGet.ProjectModel
     {
         public static TargetFrameworkInformation GetTargetFramework(this PackageSpec project, NuGetFramework targetFramework)
         {
-            var frameworkInfo = NuGetFrameworkUtility.GetNearest(project.TargetFrameworks, 
-                                                                 targetFramework, 
+            var frameworkInfo = project.TargetFrameworks.FirstOrDefault(f => f.FrameworkName.Equals(targetFramework));
+            if (frameworkInfo == null)
+            {
+                frameworkInfo = NuGetFrameworkUtility.GetNearest(project.TargetFrameworks,
+                                                                 targetFramework,
                                                                  item => item.FrameworkName);
+            }
 
             return frameworkInfo ?? new TargetFrameworkInformation();
         }
