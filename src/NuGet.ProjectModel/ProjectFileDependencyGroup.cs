a7650bc8306b4f9df7ac60e2f60834db2fafce77
Wei Wang <chengtian.wang@gmail.com>
2015-04-13 00:11:14 -0700

    Fix a file name

--- src/NuGet.ProjectModel/ProjectFileDependencyGroup.cs
+++ src/NuGet.ProjectModel/ProjectFileDependencyGroup.cs
@@ -1,9 +1,9 @@
-// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
 
-namespace Microsoft.Framework.Runtime.DependencyManagement
+namespace NuGet.ProjectModel
 {
     public class ProjectFileDependencyGroup
     {
