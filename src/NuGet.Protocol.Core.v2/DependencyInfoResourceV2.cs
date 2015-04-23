85ac898f8b6219d68fbe2e697cc296dcb148475f
Justin Emgarten <justin@emgarten.com>
2015-04-23 12:09:27 -0700

    Removing multiple fetches per package id from DependencyInfoResourceV2

--- src/NuGet.Protocol.Core.v2/DependencyInfoResourceV2.cs
+++ src/NuGet.Protocol.Core.v2/DependencyInfoResourceV2.cs
@@ -1,19 +1,13 @@
-﻿using NuGet;
-using NuGet.Versioning;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
-using NuGet.Packaging.Core;
-using NuGet.Frameworks;
-using System.Runtime.Versioning;
-using System.Threading;
+﻿using System;
 using System.Collections.Concurrent;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Frameworks;
+using NuGet.Packaging.Core;
 using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
 
 namespace NuGet.Protocol.Core.v2
 {
@@ -31,7 +25,6 @@ namespace NuGet.Protocol.Core.v2
         private readonly IVersionComparer _versionComparer;
         private readonly IVersionRangeComparer _versionRangeComparer;
         private static readonly VersionRange EmptyRange = VersionRange.None;
-        private readonly bool _useFindById;
 
         public DependencyInfoResourceV2(IPackageRepository repo)
         {
@@ -43,8 +36,6 @@ namespace NuGet.Protocol.Core.v2
             _packageDepComparer = new PackageDependencyComparer();
             _versionComparer = VersionComparer.VersionRelease;
             _versionRangeComparer = VersionRangeComparer.VersionRelease;
-
-            _useFindById = !(repo is DataServicePackageRepository);
         }
 
         public DependencyInfoResourceV2(V2Resource resource)
@@ -169,86 +160,28 @@ namespace NuGet.Protocol.Core.v2
             {
                 VersionRange alreadySearched = null;
 
+                // Fetch by range is no longer supported, all packages will be fetched
                 if (!_rangeSearched.TryGetValue(target.Id, out alreadySearched))
                 {
-                    alreadySearched = EmptyRange;
-                }
+                    // server search
+                    IEnumerable<IPackage> repoPackages = V2Client.FindPackagesById(target.Id);
 
-                if (alreadySearched == null || !target.VersionRange.IsSubSetOrEqualTo(alreadySearched))
-                {
-                    // find what we haven't checked already
-                    var needed = NeededRange(alreadySearched, target.VersionRange);
+                    _rangeSearched.AddOrUpdate(target.Id, VersionRange.All, (k, v) => VersionRange.All);
 
-                    // adjust prerelease, is this needed?
-                    needed = ModifyRange(needed, includePrerelease);
+                    HashSet<PackageDependencyInfo> foundPackages = null;
 
-                    if (!_versionRangeComparer.Equals(needed, EmptyRange))
+                    // add everything to found
+                    if (!_found.TryGetValue(target.Id, out foundPackages))
                     {
-                        // server search
-                        IEnumerable<IPackage> repoPackages = null;
-
-                        if (_useFindById)
-                        {
-                            // Ranges fail in some cases for local repos, to work around this just collect every
-                            // version of the package to filter later
-                            repoPackages = V2Client.FindPackagesById(target.Id);
-                        }
-                        else
-                        {
-                            // DataService Repository
-                            repoPackages = V2Client.FindPackages(target.Id, GetVersionSpec(needed), includePrerelease, false);
-                        }
-
-                        List<VersionRange> currentRanges = new List<VersionRange>();
-                        currentRanges.Add(target.VersionRange);
-
-                        if (alreadySearched != null)
-                        {
-                            currentRanges.Add(alreadySearched);
-                        }
-
-                        // update the already searched range
-                        VersionRange combined = null;
-
-                        if (_useFindById)
-                        {
-                            // for local repos we found all possible versions
-                            combined = VersionRange.All;
-                        }
-                        else
-                        {
-                            // for non-local repos find the real range
-                            combined = VersionRange.Combine(currentRanges);
-                        }
-
-                        _rangeSearched.AddOrUpdate(target.Id, combined, (k, v) => combined);
-
-                        HashSet<PackageDependencyInfo> foundPackages = null;
-
-                        // add everything to found
-                        if (!_found.TryGetValue(target.Id, out foundPackages))
-                        {
-                            foundPackages = new HashSet<PackageDependencyInfo>(PackageIdentity.Comparer);
-                            _found.TryAdd(target.Id, foundPackages);
-                        }
-
-                        // add current packages to found
-                        IEnumerable<PackageDependencyInfo> packageVersions = repoPackages.Select(p => CreateDependencyInfo(p, projectFramework));
-                        foundPackages.UnionWith(packageVersions);
+                        foundPackages = new HashSet<PackageDependencyInfo>(PackageIdentity.Comparer);
+                        _found.TryAdd(target.Id, foundPackages);
                     }
+
+                    // add current packages to found
+                    IEnumerable<PackageDependencyInfo> packageVersions = repoPackages.Select(p => CreateDependencyInfo(p, projectFramework));
+                    foundPackages.UnionWith(packageVersions);
                 }
             }
-        }
-
-        // Modify a range to have the correct prerelease settings
-        private VersionRange ModifyRange(VersionRange range, bool includePrelease)
-        {
-            if (range.IncludePrerelease != includePrelease)
-            {
-                range = new VersionRange(range.MinVersion, range.IsMinInclusive, range.MaxVersion, range.IsMaxInclusive, includePrelease);
-            }
-
-            return range;
         }
 
         private PackageDependencyInfo CreateDependencyInfo(IPackage packageVersion, NuGetFramework projectFramework)
@@ -271,74 +204,6 @@ namespace NuGet.Protocol.Core.v2
             return new PackageDependencyInfo(identity, deps);
         }
 
-        private VersionRange NeededRange(VersionRange alreadySearched, VersionRange possibleSubSet)
-        {
-            if (alreadySearched == null || _versionRangeComparer.Equals(alreadySearched, EmptyRange))
-            {
-                return possibleSubSet;
-            }
-
-            if (_versionRangeComparer.Equals(possibleSubSet, EmptyRange))
-            {
-                return EmptyRange;
-            }
-
-            // full overlap scenarios
-            if (possibleSubSet.IsSubSetOrEqualTo(alreadySearched))
-            {
-                return EmptyRange;
-            }
-            else if (possibleSubSet.IsSubSetOrEqualTo(alreadySearched))
-            {
-                return possibleSubSet;
-            }
-
-            // we need a partial range
-            //  [  ]
-            //    [    ]
-            if (possibleSubSet.HasLowerBound && alreadySearched.Satisfies(possibleSubSet.MinVersion))
-            {
-                // already searched the lower set
-                return new VersionRange(possibleSubSet.MinVersion, possibleSubSet.IsMinInclusive,
-                    alreadySearched.MaxVersion, alreadySearched.IsMaxInclusive,
-                    possibleSubSet.IncludePrerelease || alreadySearched.IncludePrerelease);
-            }
-            else if (possibleSubSet.HasUpperBound && alreadySearched.Satisfies(possibleSubSet.MaxVersion))
-            {
-                // already searched the higher set
-                return new VersionRange(alreadySearched.MinVersion, alreadySearched.IsMinInclusive,
-                    possibleSubSet.MaxVersion, possibleSubSet.IsMaxInclusive,
-                    possibleSubSet.IncludePrerelease || alreadySearched.IncludePrerelease);
-            }
-            else
-            {
-                // TODO: improve this
-                return VersionRange.Combine(new VersionRange[] { alreadySearched, possibleSubSet });
-            }
-        }
-
-        #region PrivateHelpers
-
-        private IVersionSpec GetVersionSpec(VersionRange range)
-        {
-            return new VersionSpec()
-            {
-                IsMinInclusive = range.IsMinInclusive,
-                IsMaxInclusive = range.IsMaxInclusive,
-                MinVersion = range.HasLowerBound ? SemanticVersion.Parse(range.MinVersion.ToString()) : null,
-                MaxVersion = range.HasUpperBound ? SemanticVersion.Parse(range.MaxVersion.ToString()) : null,
-            };
-        }
-
-        private Tuple<string, IVersionSpec> GetIdAndVersionSpec(PackageDependency item)
-        {
-            return new Tuple<string, IVersionSpec>(item.Id, item.VersionSpec);
-        }
-        private Tuple<string, IVersionSpec> GetIdAndVersionSpec(PackageIdentity item)
-        {
-            return new Tuple<string, IVersionSpec>(item.Id, new VersionSpec(new SemanticVersion(item.Version.ToNormalizedString())));
-        }
-
         private NuGetFramework GetNuGetFramework(PackageDependencySet dependencySet)
         {
             NuGetFramework fxName = NuGetFramework.AnyFramework;
@@ -353,7 +218,5 @@ namespace NuGet.Protocol.Core.v2
             VersionRange versionRange = dependency.VersionSpec == null ? null : VersionRange.Parse(dependency.VersionSpec.ToString());
             return new NuGet.Packaging.Core.PackageDependency(id, versionRange);
         }
-
-        #endregion PrivateHelpers
     }
 }
