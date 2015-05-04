811d981e1cdb2e440a3e23f7658d97da18b1e4f9
Xavier Decoster <xavier.decoster@outlook.com>
2015-05-04 09:58:29 +0200

    update copyright info

--- src/NuGet.Resolver/Properties/AssemblyInfo.cs
+++ src/NuGet.Resolver/Properties/AssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 
 // Common attributes
-[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyCompany(".NET Foundation")]
 [assembly: AssemblyProduct("NuGet")]
-[assembly: AssemblyCopyright("Copyright Outercurve Foundation. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright .NET Foundation. All rights reserved.")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
