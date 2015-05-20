d19386e999feb09809d7930210f048ce3e49338f
feiling <feiling@microsoft.com>
2015-05-20 14:42:52 -0700

    Fixes the text of the framework name on the UI.
    If the framework is Any, then do not show anything;
    If the framework is special, such as Agnostic, then just show the framework name;
    Otherwise, show the full name, i.e. framework name + version.
    
    Fix NuGet/Home#222

---
+++ src/PackageManagement.UI/Converters/NuGetFrameworkToStringConverter.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+using NuGet.Frameworks;
+
+namespace NuGet.PackageManagement.UI
+{
+    public class NuGetFrameworkToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var framework = value as NuGetFramework;
+            if (framework == null)
+            {
+                return string.Empty;
+            }
+
+            if (framework.IsSpecificFramework)
+            {
+                return framework.ToString();
+            }
+            else
+            {
+                return framework.Framework;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // no op
+            Debug.Assert(false, "Not Implemented");
+            return null;
+        }
+    }
+}
