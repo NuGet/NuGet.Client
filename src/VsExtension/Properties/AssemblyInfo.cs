ec2aeb9bd2c264d900471fc8e1f85a7f49a43e90
feiling <feiling@microsoft.com>
2015-05-26 11:15:10 -0700

    Remove binding redirection for newtonsoft.json.

--- src/VsExtension/Properties/AssemblyInfo.cs
+++ src/VsExtension/Properties/AssemblyInfo.cs
@@ -6,10 +6,3 @@ using Microsoft.VisualStudio.Shell;
 
 [assembly: AssemblyTitle("NuGet.Tools")]
 [assembly: AssemblyDescription("Visual Studio Extensibility Package (vsix)")]
-[assembly: ProvideBindingRedirection(
-    AssemblyName = "Newtonsoft.Json",
-    PublicKeyToken = "30ad4fe6b2a6aeed",
-    Culture = "neutral",
-    OldVersionLowerBound = "0.0.0.0",
-    OldVersionUpperBound = "6.0.0.0",
-    NewVersion = "6.0.0.0")]
