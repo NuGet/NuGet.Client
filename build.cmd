8b9b28a118ca1d5d6299a8a50562da932b97c597
Pranav K <prkrishn@hotmail.com>
2015-06-05 16:01:52 -0700

    Moving restore to Build\Build.proj

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 if "%VisualStudioVersion%"=="" call %VS2012CommandPromptBat%
 
-CALL restore.cmd
+msbuild Build\Build.proj /t:RestorePackages
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 msbuild NuGet.VisualStudioExtension.sln /p:VisualStudioVersion="14.0" /p:DeployExtension=false /p:EnableCodeAnalysis=true  /p:PlatformTarget=AnyCPU /v:M /m /fl /flp:v=D
