f3f02ada11deec32d5a4972be5ded036f0393572
deepakaravindr <daravind@microsoft.com>
2015-06-10 20:17:56 -0700

    Added a GetGlobalPackagesFolder to SettingsUtility
    Conflicts:
    	src/NuGet.Packaging/PackageExtraction/NuGetPackageUtils.cs
    	src/NuGet.Packaging/project.json

---
+++ src/NuGet.Logging/NullLogger.cs
@@ -0,0 +1,30 @@
+﻿namespace NuGet.Logging
+{
+    public class NullLogger : ILogger
+    {
+        private static ILogger _instance;
+
+        public static ILogger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new NullLogger();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void LogDebug(string data) { }
+
+        public void LogError(string data) { }
+
+        public void LogInformation(string data) { }
+
+        public void LogVerbose(string data) { }
+
+        public void LogWarning(string data) { }
+    }
+}
