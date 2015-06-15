18f005cc5d39bdeb158e9155ede3432628928420
deepakaravindr <daravind@microsoft.com>
2015-06-15 13:39:08 -0700

    Mutex + single thread. Mutex is used on a separate thread, so that, the mutex could be released on the same thread. And, use a couple of SemaphoreSlim such as start and end to wait on the mutex
    Conflicts:
    	src/NuGet.Common/ConcurrencyUtilities.cs

--- src/NuGet.Common/ConcurrencyUtilities.cs
+++ src/NuGet.Common/ConcurrencyUtilities.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,29 +15,70 @@ namespace NuGet.Common
             Func<Task<TVal>> action,
             CancellationToken token)
         {
-            return await action();
+            TVal result = default(TVal);
 
-            //while (true)
-            //{
-            //    token.ThrowIfCancellationRequested();
-            //    using (var fileLock = new Semaphore(initialCount: 1, maximumCount: 1, name: FilePathToLockName(filePath)))
-            //    {
-            //        if (fileLock.WaitOne(TimeSpan.FromSeconds(1)))
-            //        {
-            //            try
-            //            {
-            //                // Can perform the action
-            //                return await action();
-            //            }
-            //            finally
-            //            {
-            //                fileLock.Release();
-            //            }
-            //        }
+            var name = FilePathToLockName(filePath);
 
-            //        // Timed out. Still the semaphore is not released. Loop continues
-            //    }
-            //}
+            var lockStart = new SemaphoreSlim(0, 1);
+            var lockEnd = new SemaphoreSlim(1, 1);
+
+            var task = Task.Run(() => HandleMutex(name, lockStart, lockEnd, token));
+
+            try
+            {
+                await lockStart.WaitAsync(token);
+
+                token.ThrowIfCancellationRequested();
+
+                result = await action();
+            }
+            finally
+            {
+                lockEnd.Release();
+            }
+
+            return result;
+        }
+
+        private static void HandleMutex(string name, SemaphoreSlim lockStart, SemaphoreSlim lockEnd, CancellationToken token)
+        {
+            try
+            {
+                using (var filelock = new Mutex(initiallyOwned: false, name: name))
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            if (filelock.WaitOne(1000))
+                            {
+                                try
+                                {
+                                    lockStart.Release();
+                                }
+                                finally
+                                {
+                                    lockEnd.Wait();
+                                    filelock.ReleaseMutex();
+                                }
+
+                                // Job is done. break the loop
+                                break;
+                            }
+
+                            // Still the mutex is not released. Loop continues
+                        }
+                        catch (AbandonedMutexException)
+                        {
+                            // Mutex was abandoned. Possibly, because, the process holding the mutex was killed
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Fail(ex.ToString());
+            }
         }
 
         private static string FilePathToLockName(string filePath)
