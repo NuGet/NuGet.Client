91bc89ba458608263fc91eddf933126cbe066bbb
Justin Emgarten <justin@emgarten.com>
2015-06-19 12:10:29 -0700

    Adding AssemblyInfo

---
+++ src/NuGet.Common/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany(".NET Foundation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("\x00a9 .NET Foundation. All rights reserved.")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
