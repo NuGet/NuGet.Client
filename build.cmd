1eea727d459cef723199a7d0e06a136aa1661880
Pranav K <prkrishn@hotmail.com>
2015-06-18 18:39:05 -0700

    Prepping for 3.0 RTM packages

--- build.cmd
+++ build.cmd
@@ -3,8 +3,8 @@ cd %~dp0
 
 SETLOCAL
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+set DNX_FEED=https://www.myget.org/F/nuget-dnx-release/api/v2
 
-SET DNX_BUILD_VERSION=beta
 
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
@@ -21,9 +21,7 @@ IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
 
-IF "%SKIP_DNX_INSTALL%"=="1" goto run
-CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86
-CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -arch x86
+CALL packages\KoreBuild\build\dnvm install 1.0.0-beta6-12085 -a default
 
 :run
 CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
