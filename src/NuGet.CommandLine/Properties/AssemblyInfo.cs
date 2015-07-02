b32f4dba423648cd374873670d8806de855d7e39
feiling <feiling@microsoft.com>
2015-07-02 14:38:31 -0700

    Fix NuGet.CommandLine.nuspec, Add AssemblyInfo.cs.

---
+++ src/NuGet.CommandLine/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// Project-specific attributes
+
+[assembly: AssemblyTitle("NuGet Command Line")]
+[assembly: AssemblyDescription("NuGet Command Line")]
+
+// Common attributes
+
+[assembly: AssemblyCompany(".NET Foundation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("\x00a9 .NET Foundation. All rights reserved.")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
