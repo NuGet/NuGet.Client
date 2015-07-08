95aefdffbc4326cfff5372413ca07362f1677ba7
feiling <feiling@microsoft.com>
2015-07-08 14:54:52 -0700

    Adding back functional tests for nuget.exe. Added help command tests.

---
+++ test/NuGet.CommandLine.Test/NuGetHelpCommandTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NuGet.CommandLine.Test
+{
+    public class NuGetHelpCommandTest
+    {
+        [Theory]
+        [InlineData("config")]
+        [InlineData("delete")]
+        [InlineData("install")]
+        [InlineData("list")]
+        [InlineData("pack")]
+        [InlineData("push")]
+        [InlineData("restore")]
+        [InlineData("setApiKey")]
+        [InlineData("sources")]
+        [InlineData("spec")]
+        [InlineData("update")]
+        public void HelpCommand_HelpMessage(string command)
+        {
+            // Arrange
+            var targetDir = ConfigurationManager.AppSettings["TargetDir"];
+            var nugetexe = Path.Combine(targetDir, "nuget.exe");
+
+            // Act
+            var r = CommandRunner.Run(
+                nugetexe,
+                Directory.GetCurrentDirectory(),
+                "help " + command,
+                waitForExit: true);
+
+            // Assert
+            Assert.Equal(0, r.Item1);
+        }
+
+        // Tests that -ConfigFile is not included in the help message
+        [Fact]
+        public void HelpCommand_SpecCommand()
+        {
+            // Arrange
+            var targetDir = ConfigurationManager.AppSettings["TargetDir"];
+            var nugetexe = Path.Combine(targetDir, "nuget.exe");
+
+            // Act
+            var r = CommandRunner.Run(
+                nugetexe,
+                Directory.GetCurrentDirectory(),
+                "help spec",
+                waitForExit: true);
+
+            // Assert
+            Assert.Equal(0, r.Item1);
+            Assert.DoesNotContain("-ConfigFile", r.Item2, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
