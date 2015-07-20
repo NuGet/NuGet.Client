b275c06fc0f2552b0b5e10d266d124bb01c900d6
feiling <feiling@microsoft.com>
2015-07-20 15:19:27 -0700

    Make v3 feed the default source for nuget.exe.

--- src/NuGet.CommandLine/PackageSourceBuilder.cs
+++ src/NuGet.CommandLine/PackageSourceBuilder.cs
@@ -7,12 +7,9 @@ namespace NuGet
     {
         internal static Configuration.PackageSourceProvider CreateSourceProvider(Configuration.ISettings settings)
         {
-            var defaultPackageSource = new Configuration.PackageSource(NuGetConstants.V2FeedUrl);
-
-            var officialPackageSource = new Configuration.PackageSource(NuGetConstants.V2FeedUrl, LocalizedResourceManager.GetString("OfficialPackageSourceName"));
-            var v1PackageSource = new Configuration.PackageSource(NuGetConstants.V1FeedUrl, LocalizedResourceManager.GetString("OfficialPackageSourceName"));
-            var legacyV2PackageSource = new Configuration.PackageSource(NuGetConstants.V2LegacyFeedUrl, LocalizedResourceManager.GetString("OfficialPackageSourceName"));
-
+            var defaultPackageSource = new Configuration.PackageSource(
+                NuGetConstants.V3FeedUrl, NuGetConstants.FeedName);
+            
             var packageSourceProvider = new Configuration.PackageSourceProvider(
                 settings,
                 new[] { defaultPackageSource });
