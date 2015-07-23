6939b1eb3ee5060ca31611e4b372b9e84296be72
Pranav K <prkrishn@hotmail.com>
2015-07-23 09:10:19 -0700

    Removing unused \ commented out types

--- src/NuGet.CommandLine/ISolutionParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace NuGet
-{
-    internal interface ISolutionParser
-    {
-        IEnumerable<string> GetAllProjectFileNames(IFileSystem fileSystem, string solutionFile);
-    }
-}
