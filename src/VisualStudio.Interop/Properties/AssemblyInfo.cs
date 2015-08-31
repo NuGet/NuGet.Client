d638a1e855c027c7ab05b63c6a11809e86acb81e
Meni Zalzman <meniz@microsoft.com>
2015-08-31 10:24:17 -0700

    Updating version 3.2 RC and fixing the build to have AssemblyVersion

--- src/VisualStudio.Interop/Properties/AssemblyInfo.cs
+++ src/VisualStudio.Interop/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NuGet.VisualStudio.Interop")]
 [assembly: AssemblyDescription("NuGet/Visual Studio interop assembly")]
@@ -9,3 +11,11 @@ using System.Reflection;
 // The version of this assembly should never be changed.
 
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompany(".NET Foundation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("\x00a9 .NET Foundation. All rights reserved.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+
+[assembly: NeutralResourcesLanguage("en-US")]
