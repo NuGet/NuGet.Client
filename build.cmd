35619e027e1a48c049c4ca3a7291179bb1266f8f
Meni Zalzman <meniz@microsoft.com>
2015-09-02 14:24:38 -0700

    Locking dnu to beta6

--- build.cmd
+++ build.cmd
@@ -16,12 +16,12 @@ copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 :restore
 IF EXIST packages\KoreBuild goto run
-.nuget\NuGet.exe install KoreBuild -Source https://www.myget.org/F/aspnetrelease/api/v2 -ExcludeVersion -o packages -nocache -pre
+.nuget\NuGet.exe install KoreBuild -Source https://www.myget.org/F/aspnetmaster/api/v2 -ExcludeVersion -o packages -nocache -pre
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
 
 IF "%SKIP_DNX_INSTALL%"=="1" goto run
 SET DNX_FEED=https://www.nuget.org/api/v2
-CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86
+CALL packages\KoreBuild\build\dnvm install 1.0.0-beta6 -runtime CLR -arch x86
 CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -arch x86
 
 :run
