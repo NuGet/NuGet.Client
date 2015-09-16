ae28aa0b83162c6f1aed96c1cdb91d83355e209f
Justin Emgarten <justin@emgarten.com>
2015-09-15 23:03:27 -0700

    DebugConstants

--- VisualStudioExtension/src/VsConsole/PowerShellHost/DebugConstants.cs
+++ VisualStudioExtension/src/VsConsole/PowerShellHost/DebugConstants.cs
@@ -2,7 +2,7 @@
 using System.IO;
 namespace NuGetConsole.Host.PowerShell {
     public static class DebugConstants {
-        internal static string TestModulePath = Path.Combine(@"C:\src\NuGet3\VisualStudioExtension\src\VsConsole\PowerShellHost\..\..\..", @"test\EndToEnd\NuGet.Tests.psm1");
+        internal static string TestModulePath = Path.Combine(@"C:\src\nuget3\VisualStudioExtension\src\VsConsole\PowerShellHost\..\..\..", @"test\EndToEnd\NuGet.Tests.psm1");
     }
 }
 #endif
