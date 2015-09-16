df65834d695a7146af1f11348c87ad41e502eda8
Justin Emgarten <justin@emgarten.com>
2015-09-15 21:56:49 -0700

    Updating build scripts

---
+++ VisualStudioExtension/src/VsConsole/PowerShellHost/DebugConstants.cs
@@ -0,0 +1,8 @@
+#if DEBUG
+using System.IO;
+namespace NuGetConsole.Host.PowerShell {
+    public static class DebugConstants {
+        internal static string TestModulePath = Path.Combine(@"C:\src\NuGet3\VisualStudioExtension\src\VsConsole\PowerShellHost\..\..\..", @"test\EndToEnd\NuGet.Tests.psm1");
+    }
+}
+#endif
---
+++ nupkgs/readme.txt
@@ -0,0 +1,1 @@
+This folder contains nupkgs used within this repository.
