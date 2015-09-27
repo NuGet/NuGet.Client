4c1b10b9181ae63ca2cda3fd659e60327e3e06f1
Meni Zalzman <meniz@microsoft.com>
2015-09-27 00:35:32 -0700

    Removing the VISX old folder

--- VisualStudioExtension/build.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-if "%VisualStudioVersion%"=="" call %VS2012CommandPromptBat%
-
-msbuild Build\Build.proj /t:RestorePackages
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-msbuild NuGet.VisualStudioExtension.sln /p:VisualStudioVersion="14.0" /p:DeployExtension=false /p:EnableCodeAnalysis=true  /p:PlatformTarget=AnyCPU /v:M /m /fl /flp:v=D
