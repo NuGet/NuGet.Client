90167c9b9b4e8d7c442c7d67c19efa6a8e745745
Zhi Li <zhili128@outlook.com>
2015-10-13 11:58:29 -0700

    ported PR https://github.com/NuGet/NuGet.VisualStudioExtension/pull/76

---
+++ src/NuGet.Clients/PackageManagement.VisualStudio/IScriptPackageFile.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Versioning;
+
+namespace NuGet.PackageManagement.VisualStudio
+{
+    public interface IScriptPackageFile
+    {
+        /// <summary>
+        /// Gets the full path of the file inside the package.
+        /// </summary>
+        string Path
+        {
+            get;
+        }
+
+        FrameworkName TargetFramework
+        {
+            get;
+        }
+    }
+}
