81f552e801a7204e9b2831b8d94c1de1ae5a74b3
deepakaravindr <daravind@microsoft.com>
2015-10-12 18:24:10 -0700

    Updated init to support both v2 style and v3 style folder sources. Verified manually that it works. Need to add tests

--- src/NuGet.Clients/NuGet.CommandLine/Commands/InitCommand.cs
+++ src/NuGet.Clients/NuGet.CommandLine/Commands/InitCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -33,9 +34,9 @@ namespace NuGet.CommandLine
 
             // If the Destination Feed Folder does not exist, it will be created.
 
-            var packagePaths = Directory.EnumerateFiles(source, "*.nupkg");
+            var packagePaths = GetPackageFilePaths(source, "*" + ProjectManagement.Constants.PackageExtension);
 
-            if (packagePaths.Any())
+            if (packagePaths.Count > 0)
             {
                 foreach (var packagePath in packagePaths)
                 {
@@ -60,6 +61,95 @@ namespace NuGet.CommandLine
 
                 Console.LogInformation(message);
             }
+        }
+
+        /// <summary>
+        /// Helper method based on LocalPackageRepository and ExpandedPackageRepository
+        /// to avoid dependency on NuGet.Core. Links to the classes are
+        /// https://github.com/NuGet/NuGet2/blob/2.9/src/Core/Repositories/LocalPackageRepository.cs
+        /// AND
+        /// https://github.com/NuGet/NuGet2/blob/2.9/src/Core/Repositories/ExpandedPackageRepository.cs
+        /// </summary>
+        private static IReadOnlyList<string> GetPackageFilePaths(string source, string nupkgFilter)
+        {
+            var packagePaths = new List<string>();
+            var isV2StyleFolderSource = IsV2StyleFolderSource(source, nupkgFilter);
+
+            if (!isV2StyleFolderSource.HasValue)
+            {
+                // There are no nupkg files, v2-style or v3-style, under 'source'.
+                return packagePaths;
+            }
+
+            if (isV2StyleFolderSource.Value)
+            {
+                foreach (var idDirectory in Directory.EnumerateDirectories(source))
+                {
+                    // Since we need the .nupkg file for nuget.exe init, PackageSaveMode.Nuspec is not supported.
+                    // And, Default search option for EnumerateFiles is top directory only.
+                    var packagesAtIdDirectory = Directory.EnumerateFiles(idDirectory, nupkgFilter);
+
+                    packagePaths.AddRange(packagesAtIdDirectory);
+                }
+
+                var packagesAtRoot = Directory.EnumerateFiles(source, nupkgFilter);
+                packagePaths.AddRange(packagesAtRoot);
+            }
+            else
+            {
+                foreach (var idDirectory in Directory.EnumerateDirectories(source))
+                {
+                    var packageId = Path.GetFileName(idDirectory);
+
+                    foreach (var versionDirectory in Directory.EnumerateDirectories(idDirectory))
+                    {
+                        var packagesAtVersionDirectory = Directory.EnumerateFiles(versionDirectory, nupkgFilter);
+                        packagePaths.AddRange(packagesAtVersionDirectory);
+                    }
+                }
+            }
+
+            return packagePaths;
+        }
+
+        /// <summary>
+        /// Helper method based on the LazyLocalPackageRepository.cs to avoid dependency on NuGet.Core
+        /// https://github.com/NuGet/NuGet2/blob/2.9/src/Core/Repositories/LazyLocalPackageRepository.cs#L74
+        /// </summary>
+        /// <returns>Return true if source v2 style folder. Otherwise, false.
+        /// If no nupkgs were found under the source, returns null</returns>
+        private static bool? IsV2StyleFolderSource(string source, string nupkgFilter)
+        {
+            var packagesAtRoot = Directory.EnumerateFiles(source, nupkgFilter);
+
+            if (packagesAtRoot.Any())
+            {
+                return true;
+            }
+
+            foreach (var idDirectory in Directory.EnumerateDirectories(source))
+            {
+                // Since we need the .nupkg file for nuget.exe init, PackageSaveMode.Nuspec is not supported.
+                // And, Default search option for EnumerateFiles is top directory only.
+                var packagesAtIdDirectory = Directory.EnumerateFiles(idDirectory, nupkgFilter);
+
+                if (packagesAtIdDirectory.Any())
+                {
+                    return true;
+                }
+
+                foreach (var versionDirectory in Directory.EnumerateDirectories(idDirectory))
+                {
+                    var packagesAtVersionDirectory = Directory.EnumerateFiles(versionDirectory, nupkgFilter);
+
+                    if (packagesAtVersionDirectory.Any())
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
