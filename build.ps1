1740ba87da791c04fafc8ae8cd690ec30520d6b5
Alex Panov <alpaix@outlook.com>
2015-12-16 15:19:55 -0800

    Refactored build script for better error handling
    + Replaced throw's with Write-Error's to avoid script termination
    + Added -ErrorAction control over build steps execution
    + Split build script into granular build steps for better flow
    control
    + Split NuGet.Clients build into build and tests msbuild statements

--- build.ps1
+++ build.ps1
@@ -9,363 +9,117 @@ param (
     [switch]$SkipTests,
     [switch]$SkipRestore,
     [switch]$CleanCache,
+    [Parameter(ParameterSetName='RegularBuild')]
     [switch]$SkipILMerge,
     [switch]$DelaySign,
     [string]$MSPFXPath,
     [string]$NuGetPFXPath,
     [switch]$SkipXProj,
+    [Parameter(ParameterSetName='RegularBuild')]
     [switch]$SkipSubModules,
     [switch]$SkipCSProj,
     [Parameter(ParameterSetName='FastBuild')]
     [switch]$Fast
 )
 
+. "$PSScriptRoot\build\common.ps1"
+
 $RunTests = (-not $SkipTests) -and (-not $Fast)
 
-. .\build\common.ps1
+Write-Host ("`r`n" * 3)
+Trace-Log ('=' * 60)
 
-###Functions###
-
-## Clean the machine level cache from all package
-function CleanCache()
-{
-    Trace-Log Removing DNX packages
-
-    if (Test-Path $env:userprofile\.dnx\packages)
-    {
-        rm -r $env:userprofile\.dnx\packages -Force
-    }
-
-    Trace-Log Removing .NUGET packages
-
-    if (Test-Path $env:userprofile\.nuget\packages)
-    {
-        rm -r $env:userprofile\.nuget\packages -Force
-    }
-
-    Trace-Log Removing DNU cache
-
-    if (Test-Path $env:localappdata\dnu\cache)
-    {
-        rm -r $env:localappdata\dnu\cache -Force
-    }
-
-    Trace-Log Removing NuGet web cache
-
-    if (Test-Path $env:localappdata\NuGet\v3-cache)
-    {
-        rm -r $env:localappdata\NuGet\v3-cache -Force
-    }
-
-    Trace-Log Removing NuGet machine cache
-
-    if (Test-Path $env:localappdata\NuGet\Cache)
-    {
-        rm -r $env:localappdata\NuGet\Cache -Force
-    }
-}
-
-# Restore projects individually
-Function Restore-XProj {
-    param(
-        [parameter(ValueFromPipeline=$True)]
-        [string[]]$xprojDir
-    )
-    Process {
-        $projectJsonFile = Join-Path $xprojDir 'project.json'
-        $opts = @('restore', $projectJsonFile, '-s', 'https://www.myget.org/F/nuget-volatile/api/v3/index.json', '-s', 'https://api.nuget.org/v3/index.json')
-
-        Trace-Log "Restoring $projectJsonFile"
-        Trace-Log "dnu $opts"
-        & dnu $opts
-
-        if ($LASTEXITCODE -ne 0)
-        {
-            throw "Restore failed $projectJsonFile"
-        }
-    }
-}
-
-# Restore in parallel first to speed things up
-Function Restore-XProjFast {
-    param(
-        [string]$xprojDir
-    )
-    $opts = @('restore', $xprojDir, '--parallel', '--ignore-failed-sources', '-s', 'https://www.myget.org/F/nuget-volatile/api/v3/index.json', '-s', 'https://api.nuget.org/v3/index.json')
-
-    Trace-Log "Restoring $xprojDir"
-    Trace-Log "dnu $opts"
-    & dnu $opts
-
-    if ($LASTEXITCODE -ne 0)
-    {
-        throw "Restore failed $xprojDir"
-    }
-}
-
-## Building XProj projects
-function BuildXproj()
-{
-    ## Setting the DNX build version
-    if($ReleaseLabel -ne "Release")
-    {
-        $env:DNX_BUILD_VERSION="$ReleaseLabel-$BuildNumber"
-    }
-
-    # Setting the DNX AssemblyFileVersion
-    $env:DNX_ASSEMBLY_FILE_VERSION=$BuildNumber
-
-    $xprojects = Get-ChildItem src -rec -Filter '*.xproj' |`
-        %{ Split-Path $_.FullName -Parent } |`
-        ?{ -not $_.EndsWith('NuGet.CommandLine.XPlat') } # TODO: Remove this after fixing XPLAT!
-
-    if (-not $SkipRestore)
-    {
-        if ($Fast)
-        {
-            Restore-XProjFast src\NuGet.Core
-        }
-        else
-        {
-            Trace-Log 'Restoring XProj packages'
-            $xprojects | Restore-XProj
-        }
-    }
-
-    $opts = , 'pack'
-    $opts += $xprojects
-    $opts += @('--configuration', $Configuration, '--out', $artifacts)
-
-    Trace-Log
-    Trace-Log "dnu $opts"
-    &dnu $opts
-
-    if ($LASTEXITCODE -ne 0)
-    {
-        throw "Build failed $ProjectName"
-    }
-
-    if ($RunTests)
-    {
-        # Test assemblies should not be signed
-        if (Test-Path Env:\DNX_BUILD_KEY_FILE)
-        {
-            Remove-Item Env:\DNX_BUILD_KEY_FILE
-        }
-
-        if (Test-Path Env:\DNX_BUILD_DELAY_SIGN)
-        {
-            Remove-Item Env:\DNX_BUILD_DELAY_SIGN
-        }
-
-        $xtests = Get-ChildItem test\NuGet.Core.Tests -rec -Filter '*.xproj' |`
-            %{ Split-Path $_.FullName -Parent }
-
-        if ($Fast)
-        {
-            Restore-XProjFast test\NuGet.Core.Tests
-        }
-        else
-        {
-            Trace-Log 'Restoring XProj packages'
-            $xtests | Restore-XProj
-        }
-
-        foreach ($srcDir in $xtests)
-        {
-            Trace-Log "Running tests in $srcDir"
-
-            pushd $srcDir
-            & dnx test
-            popd
-
-            if ($LASTEXITCODE -ne 0)
-            {
-                throw "Tests failed $srcDir"
-            }
-        }
-    }
-
-    ## Copying nupkgs
-    Trace-Log "Moving the packages to $nupkgsDir"
-    Get-ChildItem $artifacts\*.nupkg -Recurse | % { Move-Item $_ $nupkgsDir }
-}
-
-function BuildCSproj()
-{
-    #Building the microsoft interop package for the test.utility
-    $interopLib = ".\lib\Microsoft.VisualStudio.ProjectSystem.Interop"
-    & dnu restore $interopLib -s https://www.myget.org/F/nuget-volatile/api/v2/ -s https://www.nuget.org/api/v2/
-    & dnu pack $interopLib
-    Get-ChildItem $interopLib\*.nupkg -Recurse | % { Move-Item $_ $nupkgsDir }
-
-    # Restore packages for NuGet.Tooling solution
-    & $nugetExe restore -msbuildVersion 14 .\NuGet.Clients.sln
-
-    # Build the solution
-    & $msbuildExe .\NuGet.Clients.sln "/p:Configuration=$Configuration;ReleaseLabel=$ReleaseLabel;BuildNumber=$BuildNumber;RunTests=$RunTests"
-
-    if ($LASTEXITCODE -ne 0)
-    {
-        throw "NuGet.Clients.sln Build failed "
-    }
-
-    Trace-Log "Copying the Vsix to $artifacts"
-    $visxLocation = Join-Path $artifacts "$Configuration\NuGet.Clients\VsExtension"
-    Copy-Item $visxLocation\NuGet.Tools.vsix $artifacts
-}
-
-function ILMergeNuGet()
-{
-    $nugetArtifictFolder = Join-Path $artifacts "$Configuration\NuGet.Clients\NuGet.CommandLine"
-
-    pushd $nugetArtifictFolder
-
-    Trace-Log "Creating the ilmerged nuget.exe"
-    & $ILMerge NuGet.exe NuGet.Client.dll NuGet.Commands.dll NuGet.Configuration.dll NuGet.ContentModel.dll NuGet.Core.dll NuGet.Credentials.dll NuGet.DependencyResolver.Core.dll NuGet.DependencyResolver.dll NuGet.Frameworks.dll NuGet.LibraryModel.dll NuGet.Logging.dll NuGet.PackageManagement.dll NuGet.Packaging.Core.dll NuGet.Packaging.Core.Types.dll NuGet.Packaging.dll NuGet.ProjectManagement.dll NuGet.ProjectModel.dll NuGet.Protocol.Core.Types.dll NuGet.Protocol.Core.v2.dll NuGet.Protocol.Core.v3.dll NuGet.Repositories.dll NuGet.Resolver.dll NuGet.RuntimeModel.dll NuGet.Versioning.dll Microsoft.Web.XmlTransform.dll Newtonsoft.Json.dll /log:mergelog.txt /out:$artifacts\NuGet.exe
-
-    if ($LASTEXITCODE -ne 0)
-    {
-        throw "ILMerge failed"
-    }
-
-    popd
-}
-
-###Functions###
+$BuildNumber = Get-BuildNumber $BuildNumber
+$startTime = [DateTime]::UtcNow
+Trace-Log "Build #$BuildNumber started at $startTime"
 
 # Move to the script directory
-$executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-pushd $executingScriptDirectory
+pushd $NuGetClientRoot
 
-$msbuildExe = "${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\msbuild.exe"
-$nugetExe = ".nuget\nuget.exe"
-$ILMerge = Join-Path $executingScriptDirectory "packages\ILMerge.2.14.1208\tools\ILMerge.exe"
-$dnvmLoc = Join-Path $env:USERPROFILE ".dnx\bin\dnvm.cmd"
-$nupkgsDir = Join-Path $executingScriptDirectory "nupkgs"
-$artifacts = Join-Path $executingScriptDirectory "artifacts"
-$startTime = [DateTime]::UtcNow
+$BuildErrors = @()
+Invoke-BuildStep 'Updating sub-modules' { Update-SubModules } `
+    -skip:($SkipSubModules -or $Fast) `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
-Trace-Log "Build started at $startTime"
-Trace-Log
+Invoke-BuildStep 'Cleaning artifacts' { Clear-Artifacts } `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
-if ($SkipSubModules -eq $False)
-{
-    if ((Test-Path -Path "submodules/FileSystem/src") -eq $False)
-    {
-        Trace-Log "Updating and initializing submodules"
-        & git submodule update --init
-    }
-    else
-    {
-        Trace-Log "Updating submodules"
-        & git submodule update
-    }
-}
+Invoke-BuildStep 'Cleaning nupkgs' { Clear-Nupkgs } -skip:$SkipXProj `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
-# Download NuGet.exe if missing
-if ((Test-Path $nugetExe) -eq $False)
-{
-    Trace-Log "Downloading nuget.exe"
-    wget https://dist.nuget.org/win-x86-commandline/latest-prerelease/nuget.exe -OutFile $nugetExe
-}
+Invoke-BuildStep 'Cleaning package cache' { Clear-PackageCache } -skip:(-not $CleanCache) `
+    -ev +BuildErrors -ea $ErrorActionPreference
+
+Invoke-BuildStep 'Installing NuGet.exe' { Install-NuGet } `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
 # Restoring tools required for build
-if ($SkipRestore -eq $False)
-{
-    Trace-Log "Restoring tools"
-    & $nugetExe restore .nuget\packages.config -SolutionDirectory .
-}
+Invoke-BuildStep 'Restoring solution packages' { Restore-SolutionPackages } -skip:$SkipRestore `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
-## Validating DNVM installed and install it if missing
-if ((Test-Path $dnvmLoc) -eq $False)
-{
-    Trace-Log "Downloading DNVM"
-    &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
-}
+Invoke-BuildStep 'Installing runtime' { Install-DNVM; Install-DNX } `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
-## Clean artifacts and nupkgs folder
-if (Test-Path $nupkgsDir)
-{
-    Trace-Log "Cleaning nupkgs folder"
-    Remove-Item $nupkgsDir\*.nupkg
-}
+Invoke-BuildStep 'Enabling delayed signing' {
+        param($MSPFXPath, $NuGetPFXPath) Enable-DelayedSigning $MSPFXPath $NuGetPFXPath
+    } `
+    -args $MSPFXPath, $NuGetPFXPath `
+    -skip:(-not $DelaySign) `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
-if( Test-Path $artifacts)
-{
-    Trace-Log "Cleaning the artifacts folder"
-    Remove-Item $artifacts\*.* -Recurse
-}
-
-## Make sure the needed DNX runtimes ex
-Trace-Log "Validating the correct DNX runtime set"
-$env:DNX_FEED="https://www.nuget.org/api/v2"
-& dnvm install 1.0.0-rc1-update1 -runtime CoreCLR -arch x86
-& dnvm install 1.0.0-rc1-update1 -runtime CLR -arch x86 -alias default
-
-if($CleanCache)
-{
-    CleanCache
-}
-
-# enable delay signed build
-if ($DelaySign)
-{
-    if (Test-Path $MSPFXPath)
-    {
-        Trace-Log "Setting NuGet.Core solution to delay sign using $MSPFXPath"
-        $env:DNX_BUILD_KEY_FILE=$MSPFXPath
-        $env:DNX_BUILD_DELAY_SIGN=$true
-    }
-
-    if (Test-Path $NuGetPFXPath)
-    {
-        Trace-Log "Setting NuGet.Clients solution to delay sign using $NuGetPFXPath"
-        $env:NUGET_PFX_PATH= $NuGetPFXPath
-
-        Trace-Log "Using the Microsoft Key for NuGet Command line $MSPFXPath"
-        $env:MS_PFX_PATH=$MSPFXPath
-    }
-}
-
-$SemanticVersionDate = "2015-11-30"
-
-if(!$BuildNumber)
-{
-    $R = ""
-    $BuildNumber = ([Math]::DivRem(([System.DateTime]::Now.Subtract([System.DateTime]::Parse($SemanticVersionDate)).TotalMinutes), 5, [ref]$R)).ToString('F0')
-}
-else
-{
-    $buildNum = [int]$BuildNumber
-    $BuildNumber = $buildNum.ToString("D4");
-}
-
-if(!$SkipXProj)
-{
-    ## Building all XProj projects
-    BuildXproj
-}
+Invoke-BuildStep 'Building NuGet.Core projects' {
+        param($Configuration, $ReleaseLabel, $BuildNumber, $SkipRestore, $Fast)
+        Build-CoreProjects $Configuration $ReleaseLabel $BuildNumber -SkipRestore:$SkipRestore -Fast:$Fast
+    } `
+    -args $Configuration, $ReleaseLabel, $BuildNumber, $SkipRestore, $Fast `
+    -skip:$SkipXProj `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
 ## Building the Tooling solution
-if (-not $SkipCSproj)
-{
-    BuildCSproj
-}
+Invoke-BuildStep 'Building NuGet.Clients projects' {
+        param($Configuration, $ReleaseLabel, $BuildNumber, $SkipRestore, $Fast)
+        Build-ClientsProjects $Configuration $ReleaseLabel $BuildNumber -SkipRestore:$SkipRestore -Fast:$Fast
+    } `
+    -args $Configuration, $ReleaseLabel, $BuildNumber, $SkipRestore, $Fast `
+    -skip:$SkipCSproj `
+    -ev +BuildErrors -ea $ErrorActionPreference
 
-if ((-not $SkipILMerge) -and (-not $SkipCSProj))
-{
-    ## Merging the NuGet.exe
-    ILMergeNuGet
-}
+Invoke-BuildStep 'Running NuGet.Core tests' {
+        param($SkipRestore, $Fast)
+        Test-CoreProjects -SkipRestore:$SkipRestore -Fast:$Fast
+    } `
+    -args $SkipRestore, $Fast `
+    -skip:($SkipXProj -or (-not $RunTests)) `
+    -ev +BuildErrors -ea $ErrorActionPreference
+
+Invoke-BuildStep 'Running NuGet.Clients tests' {
+        param($Configuration, $ReleaseLabel, $BuildNumber)
+        Test-ClientsProjects $Configuration $ReleaseLabel $BuildNumber
+    } `
+    -args $Configuration, $ReleaseLabel, $BuildNumber `
+    -skip:($SkipCSproj -or (-not $RunTests)) `
+    -ev +BuildErrors -ea $ErrorActionPreference
+
+Invoke-BuildStep 'Merging NuGet.exe' {
+        param($Configuration) Invoke-ILMerge $Configuration
+    } `
+    -args $Configuration `
+    -skip:($SkipILMerge -or $SkipCSProj -or $Fast) `
+    -ev +BuildErrors -ea $ErrorActionPreference
+
+popd
+
+Trace-Log ('-' * 60)
 
 ## Calculating Build time
 $endTime = [DateTime]::UtcNow
-$diff = [math]::Round(($endTime - $startTime).TotalMinutes, 4)
+Trace-Log "Build #$BuildNumber ended at $endTime"
+Trace-Log "Time elapsed $(Format-ElapsedTime ($endTime - $startTime))"
 
-Trace-Log
-Trace-Log "Build ended at $endTime"
-Trace-Log "Build took $diff minutes"
-Trace-Log
+if ($BuildErrors) {
+    Trace-Log "Build's completed with following errors:"
+    $BuildErrors | Out-Default
+}
 
-popd
+Trace-Log ('=' * 60)
+Write-Host ("`r`n" * 3)
--- build/common.ps1
+++ build/common.ps1
@@ -1,15 +1,412 @@
-Function Trace-Log($TraceMessage = ''){
+### Constants ###
+$NuGetClientRoot = Split-Path -Path $PSScriptRoot -Parent
+$MSBuildExe = Join-Path ${env:ProgramFiles(x86)} 'MSBuild\14.0\Bin\msbuild.exe'
+$NuGetExe = Join-Path $NuGetClientRoot '.nuget\nuget.exe'
+$ILMerge = Join-Path $NuGetClientRoot 'packages\ILMerge.2.14.1208\tools\ILMerge.exe'
+$DnvmCmd = Join-Path $env:USERPROFILE '.dnx\bin\dnvm.cmd'
+$Nupkgs = Join-Path $NuGetClientRoot nupkgs
+$Artifacts = Join-Path $NuGetClientRoot artifacts
+
+$OrigBgColor = $host.ui.rawui.BackgroundColor
+$OrigFgColor = $host.ui.rawui.ForegroundColor
+
+# MSBUILD has a nasty habit of leaving the foreground color red
+Function Reset-Colors {
+    $host.ui.rawui.BackgroundColor = $OrigBgColor
+    $host.ui.rawui.ForegroundColor = $OrigFgColor
+}
+
+### Functions ###
+Function Trace-Log($TraceMessage = '') {
     Write-Host "[$(Trace-Time)]`t$TraceMessage" -ForegroundColor Cyan
 }
 
-Function Trace-Time()
-{
-    $prev = $Global:LastTraceTime;
-    $currentTime = Get-Date;
-    $time = $currentTime.ToString("HH:mm:ss");
-    $diff = New-TimeSpan -Start $prev -End $currentTime
-    $Global:LastTraceTime = $currentTime;
-    "$time +$([math]::Round($diff.TotalSeconds, 0))"
+Function Error-Log($ErrorMessage) {
+    Write-Error "[$(Trace-Time)]`t$ErrorMessage"
+}
+
+Function Warning-Log($WarningMessage) {
+    Write-Warning "[$(Trace-Time)]`t$WarningMessage"
+}
+
+Function Trace-Time() {
+    $currentTime = Get-Date
+    $lastTime = $Global:LastTraceTime
+    $Global:LastTraceTime = $currentTime
+    "{0:HH:mm:ss} +{1:F0}" -f $currentTime, ($currentTime - $lastTime).TotalSeconds
 }
 
 $Global:LastTraceTime = Get-Date
+
+Function Format-ElapsedTime($ElapsedTime) {
+    '{0:F0}:{1:D2}' -f $ElapsedTime.TotalMinutes, $ElapsedTime.Seconds
+}
+
+Function Invoke-BuildStep {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True)]
+        [string]$BuildStep,
+        [Parameter(Mandatory=$True)]
+        [ScriptBlock]$Expression,
+        [Parameter(Mandatory=$False)]
+        [Alias('args')]
+        [Object[]]$Arguments,
+        [Alias('skip')]
+        [switch]$SkipExecution
+    )
+    if (-not $SkipExecution) {
+        Trace-Log "[BEGIN] $BuildStep"
+        $sw = [Diagnostics.Stopwatch]::StartNew()
+        try {
+            Invoke-Command $Expression -ArgumentList $Arguments -ErrorVariable err
+        }
+        finally {
+            $sw.Stop()
+            Reset-Colors
+            if (-not $err) {
+                Trace-Log "[DONE +$(Format-ElapsedTime $sw.Elapsed)] $BuildStep"
+            }
+            else {
+                Error-Log "[FAILED +$(Format-ElapsedTime $sw.Elapsed)] $BuildStep"
+            }
+        }
+    }
+    else {
+        Warning-Log "[SKIP] $BuildStep"
+    }
+}
+
+Function Update-Submodules {
+    [CmdletBinding()]
+    param()
+    if (-not (Test-Path -Path "$NuGetClientRoot/submodules/FileSystem/src")) {
+        Trace-Log 'Updating and initializing submodules'
+        & git submodule update --init 2>&1
+    }
+    else {
+        Trace-Log 'Updating submodules'
+        & git submodule update 2>&1
+    }
+}
+
+# Downloads NuGet.exe if missing
+Function Install-NuGet() {
+    if (-not (Test-Path $NuGetExe)) {
+        Trace-Log 'Downloading nuget.exe'
+        wget https://dist.nuget.org/win-x86-commandline/latest-prerelease/nuget.exe -OutFile $NuGetExe
+    }
+}
+
+# Validates DNVM installed and installs it if missing
+Function Install-DNVM() {
+    if (-not (Test-Path $DnvmCmd)) {
+        Trace-Log 'Downloading DNVM'
+        &{
+            $Branch='dev'
+            iex (`
+                (new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1')`
+            )
+        }
+    }
+}
+
+# Makes sure the needed DNX runtimes installed
+Function Install-DNX() {
+    Trace-Log 'Validating the correct DNX runtime set'
+    $env:DNX_FEED = 'https://www.nuget.org/api/v2'
+    & dnvm install 1.0.0-rc1-update1 -runtime CoreCLR -arch x86 2>&1
+    & dnvm install 1.0.0-rc1-update1 -runtime CLR -arch x86 -alias default 2>&1
+}
+
+# Enables delay signed build
+Function Enable-DelayedSigning($MSPFXPath, $NuGetPFXPath) {
+    if (Test-Path $MSPFXPath) {
+        Trace-Log "Setting NuGet.Core solution to delay sign using $MSPFXPath"
+        $env:DNX_BUILD_KEY_FILE=$MSPFXPath
+        $env:DNX_BUILD_DELAY_SIGN=$true
+    }
+
+    if (Test-Path $NuGetPFXPath) {
+        Trace-Log "Setting NuGet.Clients solution to delay sign using $NuGetPFXPath"
+        $env:NUGET_PFX_PATH= $NuGetPFXPath
+
+        Trace-Log "Using the Microsoft Key for NuGet Command line $MSPFXPath"
+        $env:MS_PFX_PATH=$MSPFXPath
+    }
+}
+
+Function Get-BuildNumber {
+    param([string]$BuildNumber)
+    $SemanticVersionDate = '2015-11-30'
+    if(-not $BuildNumber) {
+        '{0:F0}' -f (((Get-Date) - (Get-Date $SemanticVersionDate)).TotalMinutes / 5)
+    }
+    else {
+        '{0:D4}' -f ([int]$BuildNumber)
+    }
+}
+
+## Cleans the machine level cache from all packages
+Function Clear-PackageCache() {
+    Trace-Log 'Removing DNX packages'
+
+    if (Test-Path $env:userprofile\.dnx\packages) {
+        rm -r $env:userprofile\.dnx\packages -Force
+    }
+
+    Trace-Log 'Removing .NUGET packages'
+
+    if (Test-Path $env:userprofile\.nuget\packages) {
+        rm -r $env:userprofile\.nuget\packages -Force
+    }
+
+    Trace-Log 'Removing DNU cache'
+
+    if (Test-Path $env:localappdata\dnu\cache) {
+        rm -r $env:localappdata\dnu\cache -Force
+    }
+
+    Trace-Log 'Removing NuGet web cache'
+
+    if (Test-Path $env:localappdata\NuGet\v3-cache) {
+        rm -r $env:localappdata\NuGet\v3-cache -Force
+    }
+
+    Trace-Log 'Removing NuGet machine cache'
+
+    if (Test-Path $env:localappdata\NuGet\Cache) {
+        rm -r $env:localappdata\NuGet\Cache -Force
+    }
+}
+
+Function Clear-Artifacts() {
+    if( Test-Path $Artifacts) {
+        Trace-Log 'Cleaning the Artifacts folder'
+        Remove-Item $Artifacts\*.* -Recurse -Force
+    }
+}
+
+Function Clear-Nupkgs() {
+    if (Test-Path $Nupkgs) {
+        Trace-Log 'Cleaning nupkgs folder'
+        Remove-Item $Nupkgs\*.nupkg -Force
+    }
+}
+
+Function Restore-SolutionPackages() {
+    & $NuGetExe restore "${NuGetClientRoot}\.nuget\packages.config" -SolutionDirectory $NuGetClientRoot 2>&1
+    if (-not $?) {
+        Error-Log "Restore failed @""$NuGetClientRoot"". Code: ${LASTEXITCODE}"
+    }
+}
+
+# Restore projects individually
+Function Restore-XProject {
+    param(
+        [parameter(ValueFromPipeline=$True)]
+        [string[]]$XProjectLocation
+    )
+    Process {
+        $projectJsonFile = Join-Path $XProjectLocation 'project.json'
+        $opts = @('restore', $projectJsonFile, '-s', 'https://www.myget.org/F/nuget-volatile/api/v3/index.json', '-s', 'https://api.nuget.org/v3/index.json')
+
+        Trace-Log "Restoring packages @""$XProjectLocation"""
+        Trace-Log "dnu $opts"
+        & dnu $opts 2>&1
+
+        if (-not $?) {
+            Error-Log "Restore failed @""$XProjectLocation"". Code: $LASTEXITCODE"
+        }
+    }
+}
+
+# Restore in parallel first to speed things up
+Function Restore-XProjectsFast {
+    param(
+        [string]$XProjectsLocation
+    )
+    $opts = @('restore', $XProjectsLocation, '--parallel', '--ignore-failed-sources', '-s', 'https://www.myget.org/F/nuget-volatile/api/v3/index.json', '-s', 'https://api.nuget.org/v3/index.json')
+
+    Trace-Log "Restoring packages @""$XProjectsLocation"""
+    Trace-Log "dnu $opts"
+    & dnu $opts 2>&1
+
+    if (-not $?) {
+        Error-Log "Restore failed @""$XProjectsLocation"". Code: $LASTEXITCODE"
+    }
+}
+
+Function Find-XProjects($XProjectsLocation) {
+    Get-ChildItem $XProjectsLocation -Recurse -Filter '*.xproj' |`
+        %{ Split-Path $_.FullName -Parent } |`
+        ?{ -not $_.EndsWith('NuGet.CommandLine.XPlat') } # TODO: Remove this after fixing XPLAT!
+}
+
+Function Restore-XProjects {
+    param(
+        [string]$XProjectsLocation,
+        [switch]$Fast
+    )
+    if ($Fast) {
+        Restore-XProjectsFast $XProjectsLocation
+    }
+    else {
+        $xprojects = Find-XProjects $XProjectsLocation
+        $xprojects | Restore-XProject
+    }
+}
+
+Function Invoke-DnuPack {
+    param(
+        $XProjectsLocation,
+        $Configuration,
+        $ReleaseLabel,
+        $BuildNumber,
+        $Output
+    )
+    ## Setting the DNX build version
+    if($ReleaseLabel -ne 'Release') {
+        $env:DNX_BUILD_VERSION="${ReleaseLabel}-${BuildNumber}"
+    }
+
+    # Setting the DNX AssemblyFileVersion
+    $env:DNX_ASSEMBLY_FILE_VERSION=$BuildNumber
+
+    $xprojects = Find-XProjects $XProjectsLocation
+
+    $opts = , 'pack'
+    $opts += $xprojects
+    $opts += '--configuration', $Configuration
+    if ($Output) {
+        $opts += '--out', $Output
+    }
+
+    Trace-Log "dnu $opts"
+    &dnu $opts 2>&1
+
+    if (-not $?) {
+        Error-Log "Pack failed @""$XProjectsLocation"". Code: $LASTEXITCODE"
+    }
+}
+
+Function Build-CoreProjects {
+    param(
+        $Configuration,
+        $ReleaseLabel,
+        $BuildNumber,
+        $SkipRestore,
+        $Fast
+    )
+    $XProjectsLocation = Join-Path $NuGetClientRoot src\NuGet.Core
+
+    if (-not $SkipRestore) {
+        Restore-XProjects $XProjectsLocation -Fast:$Fast
+    }
+
+    Invoke-DnuPack $XProjectsLocation $Configuration $ReleaseLabel $BuildNumber $Artifacts
+
+    ## Moving nupkgs
+    Trace-Log "Moving the packages to $Nupkgs"
+    Get-ChildItem "${Artifacts}\*.nupkg" -Recurse | % { Move-Item $_ $Nupkgs }
+}
+
+Function Test-CoreProjects {
+    param(
+        $SkipRestore,
+        $Fast
+    )
+    # Test assemblies should not be signed
+    if (Test-Path Env:\DNX_BUILD_KEY_FILE) {
+        Remove-Item Env:\DNX_BUILD_KEY_FILE
+    }
+
+    if (Test-Path Env:\DNX_BUILD_DELAY_SIGN) {
+        Remove-Item Env:\DNX_BUILD_DELAY_SIGN
+    }
+
+    $XProjectsLocation = Join-Path $NuGetClientRoot test\NuGet.Core.Tests
+
+    if (-not $SkipRestore) {
+        Restore-XProjects $XProjectsLocation -Fast:$Fast
+    }
+
+    $xtests = Find-XProjects $XProjectsLocation
+    foreach ($xtestLocation in $xtests) {
+        Trace-Log "Running tests in ""$xtestLocation"""
+        pushd $xtestLocation
+        & dnx test 2>&1
+        popd
+    }
+}
+
+Function Build-ClientsProjects {
+    param(
+        $Configuration,
+        $ReleaseLabel,
+        $BuildNumber,
+        $SkipRestore,
+        $Fast
+    )
+    #Building the microsoft interop package for the test.utility
+    $interopLib = Join-Path $NuGetClientRoot lib\Microsoft.VisualStudio.ProjectSystem.Interop
+
+    if (-not $SkipRestore) {
+        & dnu restore $interopLib -s https://www.myget.org/F/nuget-volatile/api/v2/ -s https://www.nuget.org/api/v2/ 2>&1
+    }
+    Invoke-DnuPack $interopLib $Configuration $ReleaseLabel $BuildNumber
+    Get-ChildItem "$interopLib\*.nupkg" -Recurse | % { Move-Item $_ $Nupkgs -Force }
+
+    $solutionPath = Join-Path $NuGetClientRoot NuGet.Clients.sln
+    if (-not $SkipRestore) {
+        # Restore packages for NuGet.Tooling solution
+        & $NuGetExe restore -msbuildVersion 14 $solutionPath 2>&1
+    }
+
+    # Build the solution
+    $opts = @($solutionPath, "/p:Configuration=$Configuration;ReleaseLabel=$ReleaseLabel;BuildNumber=$BuildNumber")
+    if ($Fast) {
+        $opts += '/verbosity:minimal'
+    }
+
+    & $MSBuildExe $opts
+
+    if (-not $?) {
+        Error-Log "Build of NuGet.Clients.sln failed. Code: $LASTEXITCODE"
+    }
+
+    Trace-Log "Copying the Vsix to $Artifacts"
+    $visxLocation = Join-Path $Artifacts "$Configuration\NuGet.Clients\VsExtension"
+    Copy-Item "$visxLocation\NuGet.Tools.vsix" $Artifacts
+}
+
+Function Test-ClientsProjects {
+    param(
+        $Configuration,
+        $ReleaseLabel,
+        $BuildNumber
+    )
+    $testProjectsLocation = Join-Path $NuGetClientRoot test\NuGet.Clients.Tests
+    $testProjects = Get-ChildItem $testProjectsLocation -Recurse -Filter '*.csproj'`
+        | %{ $_.FullName }`
+        | ?{ -not $_.EndsWith('WebAppTest.csproj') }
+
+    foreach($testProj in $testProjects) {
+        & $MSBuildExe $testProj /t:RunTests "/p:Configuration=$Configuration;ReleaseLabel=$ReleaseLabel;BuildNumber=$BuildNumber;RunTests=true"
+
+        if (-not $?) {
+            Error-Log "Tests failed @""$testProj"". Code: $LASTEXITCODE"
+        }
+    }
+}
+
+# Merges the NuGet.exe
+Function Invoke-ILMerge($Configuration) {
+    $nugetArtifactsFolder = Join-Path $Artifacts "$Configuration\NuGet.Clients\NuGet.CommandLine"
+    pushd $nugetArtifactsFolder
+
+    Trace-Log 'Creating the ilmerged nuget.exe'
+    & $ILMerge NuGet.exe NuGet.Client.dll NuGet.Commands.dll NuGet.Configuration.dll NuGet.ContentModel.dll NuGet.Core.dll NuGet.Credentials.dll NuGet.DependencyResolver.Core.dll NuGet.DependencyResolver.dll NuGet.Frameworks.dll NuGet.LibraryModel.dll NuGet.Logging.dll NuGet.PackageManagement.dll NuGet.Packaging.Core.dll NuGet.Packaging.Core.Types.dll NuGet.Packaging.dll NuGet.ProjectManagement.dll NuGet.ProjectModel.dll NuGet.Protocol.Core.Types.dll NuGet.Protocol.Core.v2.dll NuGet.Protocol.Core.v3.dll NuGet.Repositories.dll NuGet.Resolver.dll NuGet.RuntimeModel.dll NuGet.Versioning.dll Microsoft.Web.XmlTransform.dll Newtonsoft.Json.dll /log:mergelog.txt /out:$Artifacts\NuGet.exe 2>&1
+
+    popd
+}
