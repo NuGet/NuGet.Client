f2da9be0d0d3fa3920d240cef8ea7aee65221616
feiling <feiling@microsoft.com>
2016-01-05 08:59:30 -0800

    Make project list in Solution Package Manager resizable.

---
+++ src/NuGet.Clients/PackageManagement.UI/Converters/ProjectAndSolutionViewMinHeightConverter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NuGet.PackageManagement.UI 
+{
+    class ProjectAndSolutionViewMinHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isSolution = (bool)value;
+            if (isSolution)
+            {
+                return 200.0;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // no op
+            Debug.Fail("Not Implemented");
+            return null;
+        }
+    }
+}
