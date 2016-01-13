3c7196ff1450ebdf9d7604b1d7f811423db4474f
deepakaravindr <daravind@microsoft.com>
2016-01-13 12:55:12 -0800

    Added a copy tools and setup machine script to copy the necessary CI tools and to set up registry keys specific to the CI machine. This is different from the setup machine script intended to be run on the dev boxes

---
+++ Scripts/CopyToolsAndSetupMachine.ps1
@@ -0,0 +1,47 @@
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string]$NuGetCIToolsFolder,
+    [Parameter(Mandatory=$true)]
+    [string]$FuncTestRoot)
+
+function CopyNuGetCITools
+{
+    param (
+    [Parameter(Mandatory=$true)]
+    [string]$NuGetCIToolsFolder,
+    [Parameter(Mandatory=$true)]
+    [string]$NuGetTestPath)
+
+    Write-Host 'Trying to copy necessary tools to ' $NuGetTestPath
+
+    Copy-Item $NuGetCIToolsFolder\*.exe $NuGetTestPath
+    Copy-Item $NuGetCIToolsFolder\*.exe.config $NuGetTestPath
+    Copy-Item $NuGetCIToolsFolder\*.dll $NuGetTestPath
+
+    Write-Host 'Copied the necessary tools to test path ' $NuGetTestPath
+}
+
+. "$PSScriptRoot\Utils.ps1"
+. "$PSScriptRoot\NuGetFunctionalTestUtils.ps1"
+
+$success = IsAdminPrompt
+
+if ($success -eq $false)
+{
+    $errorMessage = 'ERROR: Please re-run this script as an Administrator! ' +
+    'Actions such as installing VSIX, uninstalling VSIX and updating registry require admin privileges.'
+
+    Write-Error $errorMessage
+    exit 1
+}
+
+$NuGetTestPath = Join-Path $FuncTestRoot "EndToEnd"
+CopyNuGetCITools $NuGetCIToolsFolder $NuGetTestPath
+
+# Already checked if the prompt is an admin prompt
+$success = DisableCrashDialog
+if ($success -eq $false)
+{
+    Write-Error 'WARNING: Could not disable crash dialog'
+    exit 1
+}
