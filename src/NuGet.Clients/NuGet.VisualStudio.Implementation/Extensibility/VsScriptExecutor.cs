a848a45162e61af19cc37707a14068d1cf327338
deepakaravindr <daravind@microsoft.com>
2016-01-19 11:53:41 -0800

    Interface definition of IVsScriptExecutor and stub implementation of VsScriptExecutor

---
+++ src/NuGet.Clients/NuGet.VisualStudio.Implementation/Extensibility/VsScriptExecutor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace NuGet.VisualStudio
+{
+    [Export(typeof(IVsScriptExecutor))]
+    public class VsScriptExecutor : IVsScriptExecutor
+    {
+        public Task<bool> ExecuteInitScriptAsync(string packageId, string packageVersion)
+        {
+            return Task.FromResult<bool>(true);
+        }
+    }
+}
---
+++ src/NuGet.Clients/VisualStudio/Extensibility/IVsScriptExecutor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace NuGet.VisualStudio
+{
+    /// <summary>
+    /// Contains methods to execute powershell scripts from package(s) in a solution
+    /// </summary>
+    [ComImport]
+    [Guid("8c6750b0-214f-4052-93b2-284f12ebb896")]
+    public interface IVsScriptExecutor
+    {
+        /// <summary>
+        /// Executes the init script of the given package if available.
+        /// 1) If the init.ps1 script has already been executed by the powershell host, it will not be executed again.
+        /// True is returned.
+        /// 2) If the package is found on the packages folder, it will be used. Otherwise, the one on global packages
+        /// folder will be used. If that is not found either, it will return false and do nothing.
+        /// 3) Also, note if other scripts are executing while this call was made, it will wait for them to complete.
+        /// </summary>
+        /// <param name="packageId">Id of the package whose init.ps1 will be executed.</param>
+        /// <param name="packageVersion">Version of the package whose init.ps1 will be executed.</param>
+        /// <returns>Returns true if the script was executed or has been executed already.</returns>
+        Task<bool> ExecuteInitScriptAsync(string packageId, string packageVersion);
+    }
+}
