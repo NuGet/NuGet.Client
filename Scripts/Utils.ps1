58be2355ea667e2fb5b66d2fa47e64bf8ce52eda
deepakaravindr <daravind@microsoft.com>
2016-01-21 19:42:51 -0800

    Functional tests revamp
    Added a bunch of powershell scripts to do the following and more.
    Parameterized the scripts as much as possible
    such that nothing is hard-coded.
    
    1) Pick up bits from the given branch, build number and so on
    2) Install/Uninstall VSIX
    3) Launch VS and start the functional tests
    
    Implemented a bootstrap.ps1 that will used by CI to
    copy and extract EndToEnd.zip, which contains all the necessary scripts to
    run the functional tests
    
    Added a copy tools and setup machine script to copy the necessary CI tools
    and to set up registry keys specific to the CI machine. This is different
    from the setup machine script intended to be run on the dev boxes
    
    Install NuGet VSIX script does the following
    
    1) Kills any running instances of VS
    2) Uninstalls NuGet VSIX if installed
    3) Installs the NuGet VSIX as indicated by $NuGetDropPath after copying
    it to $FuncTestRoot
    
    Launches VS and waits until DTE has become available. Controlled by
    polling timeout and frequency
    
    Runs the functional tests after obtaining DTE which is guaranteed
    to be available. Also, provides real time monitoring. Finally, copies the
    test results to the CI

---
+++ Scripts/Utils.ps1
@@ -0,0 +1,39 @@
+﻿function CleanTempFolder()
+{
+    if (Test-Path $env:temp)
+    {
+        Write-Host 'Deleting temp folder'
+        rmdir $env:temp -Recurse -ErrorAction SilentlyContinue
+        Write-Host 'Done.'
+    }
+}
+
+function IsAdminPrompt()
+{
+    If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+        [Security.Principal.WindowsBuiltInRole] "Administrator"))
+    {
+        return $false
+    }
+
+    return $true
+}
+
+function DisableCrashDialog()
+{
+    $success = IsAdminPrompt
+    If ($success -eq $false)
+    {
+        $warningMessage = 'WARNING: Please re-run this script as an Administrator! Setting registry keys need admin privileges.'
+
+        Write-Host -ForegroundColor Yellow $warningMessage
+        return $false
+    }
+
+    # Set the registry key to prevent the 'Not Responding' window from showing up on a crash
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting" -Name ForceQueue -Value 1
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting\Consent" -Name DefaultConsent -Value 1
+
+    Write-host 'Windows error reporting Crash dialog has been disabled.'
+    return $true
+}
