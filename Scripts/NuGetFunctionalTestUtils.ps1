1609137832105fe119f94c666cc8b6fba4a9f378
deepakaravindr <daravind@microsoft.com>
2016-01-29 18:51:03 -0800

    Added logic to update Teamcity real time with functional test results.
    1. This enables the test tab on Teamcity UI and updates it real time
    just like xunit test runner does with unit tests.
    2. Refactored the scripts little bit and made it more robust.

--- Scripts/NuGetFunctionalTestUtils.ps1
+++ Scripts/NuGetFunctionalTestUtils.ps1
@@ -1,6 +1,59 @@
 ﻿. "$PSScriptRoot\Utils.ps1"
 . "$PSScriptRoot\VSUtils.ps1"
 
+function EscapeContentForTeamCity
+{
+    param (
+    [Parameter(Mandatory=$true)]
+    [string]$content)
+
+    if (!$content) {
+        return $content;
+    }
+
+    return $content.Replace("|", "||").Replace("'", "|'").Replace("`n", "|r|n").Replace("`r", "|r").Replace("]", "|]");
+}
+
+function WriteToTeamCity
+{
+    param (
+    [Parameter(Mandatory=$true)]
+    [string]$singleResult)
+
+    $parts = $singleResult -split " "
+
+    if ($parts.Length -lt 3)
+    {
+        Write-Host -ForegroundColor Red 'WARNING: PARSING ISSUES. CANNOT WRITE TO TEAM CITY!'
+        return $false
+    }
+    else
+    {
+        $status = $parts[0];
+        $testName = $parts[1];
+        $duration = $parts[2];
+
+        Write-Host "##teamcity[testStarted name='$testName']";
+
+        if ($status -eq "Failed")
+        {
+            if ($parts.Length -lt 4)
+            {
+               Write-Host -ForegroundColor Red 'WARNING: PARSING ISSUES. CANNOT WRITE TEST FAILURE TO TEAM CITY!'
+            }
+            else
+            {
+                $result = EscapeContentForTeamCity([string]::Join(" ", ($parts | select -skip 3)))
+                Write-Host "##teamcity[testFailed name='$testName' message='$result']"
+            }
+        }
+
+        Write-Host "##teamcity[testFinished name='$testName' duration='$duration']"
+    }
+
+    return $true
+}
+
 # This function requires a rewrite. This is a first cut
 function RealTimeLogResults
 {
@@ -12,47 +65,83 @@ function RealTimeLogResults
 
     $currentTestTime = 0
     $currentTestId = 0
-    $currentTestName = [string]$null    
+    $currentTestName = [string]$null
+    $currentBinFolder = [string]$null
+
+    # Get the current bin folder
+    while(!$currentBinFolder -and ($currentTestTime -le $EachTestTimoutInSecs))
+    {
+        start-sleep 1
+        $currentTestTime++
+        $currentBinFolder = (ls $NuGetTestPath\bin -ErrorAction SilentlyContinue | Sort-Object LastWriteTimeUtc -Descending | Select -First 1)
+    }
+
+    if (!$currentBinFolder)
+    {
+        Write-Error "Looks like no tests were run. There is no folder under $NuGetTestPath\\bin. Please investigate!"
+        return $null
+    }
+
+    $currentTestTime = 0
+
+    $log = Join-Path $currentBinFolder.FullName "log.txt"
+    $testResults = Join-Path $currentBinFolder.FullName "Realtimeresults.txt"
 
     While ($currentTestTime -le $EachTestTimoutInSecs)
     {
         Start-Sleep 1
         $currentTestTime++
-        $log = (Get-ChildItem $NuGetTestPath -Recurse -File log.txt -ErrorAction Ignore | sort LastWriteTime | select -last 1)
-        if ($log -and (Test-Path $log.FullName))
+        if ((Test-Path $log) -and (Test-Path $testResults))
         {
-            $content = Get-Content $log.FullName
+            $content = Get-Content $testResults
             if (($content.Count -gt 0) -and ($content.Count -gt $currentTestId))
             {
+                $content[($currentTestId)..($content.Count - 1)] | % {
+                    $result = WriteToTeamCity $_
+                    if ($result -eq $false)
+                    {
+                        # continues the while loop so that it can be tried again
+                        continue
+                    }
+                    Write-Host $_
+                }
+
                 $currentTestTime = 0
 
-                $content[($currentTestId)..($content.Count - 1)] | %{ Write-Host $_ }
-
-                $lastLine = $content[-1]
-                if (($lastLine -is [string]) -and $lastLine.Contains("Tests and/or Test cases, "))
+                $logContent = Get-Content $log
+                $logContentLastLine = $logContent[-1]
+                if (($logContentLastLine -is [string]) -and $logContentLastLine.Contains("Tests and/or Test cases, "))
                 {
                     # RUN HAS COMPLETED
 
-                    if ($lastLine.Contains(", 0 Failed"))
+                    if ($logContentLastLine.Contains(", 0 Failed"))
                     {
-                        Write-Host -ForegroundColor Green $lastLine
+                        Write-Host -ForegroundColor Green $logContentLastLine
                     }
                     else
                     {
-                        Write-Error $lastLine
+                        Write-Error $logContentLastLine
                     }
 
-                    $resultsFile = Join-Path $log.Directory.FullName results.html
+                    $resultsFile = Join-Path $currentBinFolder results.html
                     if (Test-Path $resultsFile)
                     {
                         Start-Process $resultsFile
                         return $resultsFile
+                    }
+                    else
+                    {
+                        return $testResults
                     }
                 }
                 else
                 {
                     $currentTestId = $content.Count
                 }
+            }
+            else
+            {
+                Write-Host "Current Test is $($currentTestId) and current test time is $($currentTestTime)"
             }
         }
     }
