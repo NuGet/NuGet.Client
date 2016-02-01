52cb7e1463c006ea1d24d031b956f26bdb0bd113
Todd Grunke <toddgrun@microsoft.com>
2016-02-01 10:51:33 -0800

    Initial port of PackageBuilder and it's dependencies. Doesn't currently build for a bunch of reasons, just wanted the initial port to match the sources from the 2.11 branch.

---
+++ src/NuGet.Core/NuGet.Packaging/PackageCreation/Authoring/ManifestContentFiles.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace NuGet
+{
+    [XmlType("files")]
+    public class ManifestContentFiles
+    {
+        [XmlAttribute("include")]
+        public string Include { get; set; }
+
+        [XmlAttribute("exclude")]
+        public string Exclude { get; set; }
+
+        [XmlAttribute("buildAction")]
+        public string BuildAction { get; set; }
+
+        [XmlAttribute("copyToOutput")]
+        public string CopyToOutput { get; set; }
+
+        [XmlAttribute("flatten")]
+        public string Flatten { get; set; }
+    }
+}
