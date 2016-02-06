6d112fb8459899b3dc098b1621e08c56c11fa88f
Rob Relyea <rob@relyeas.net>
2016-02-05 17:42:33 -0800

    Improve perf of InfiniteScrollList - image loading optimizations; deal with no network
    Images will only be stored in memory as 32x32 images, even if the web
    copy was larger.
    Images will be fetched from the internet cache, when available.
    Code will detect more than 50% failures of image downloads, and stop
    attempting to load images after that point.

--- src/NuGet.Clients/PackageManagement.UI/DetailControlModel.cs
+++ src/NuGet.Clients/PackageManagement.UI/DetailControlModel.cs
@@ -12,6 +12,7 @@ using NuGet.ProjectManagement;
 using NuGet.ProjectManagement.Projects;
 using NuGet.Protocol.Core.Types;
 using NuGet.Versioning;
+using System.Windows.Media.Imaging;
 
 namespace NuGet.PackageManagement.UI
 {
@@ -68,8 +69,8 @@ namespace NuGet.PackageManagement.UI
         {
             _searchResultPackage = searchResultPackage;
             _filter = filter;
-            OnPropertyChanged("Id");
-            OnPropertyChanged("IconUrl");
+            OnPropertyChanged(nameof(Id));
+            OnPropertyChanged(nameof(IconBitmapImage));
 
             var versions = await searchResultPackage.Versions.Value;
 
@@ -181,9 +182,12 @@ namespace NuGet.PackageManagement.UI
             get { return _searchResultPackage?.Id; }
         }
 
-        public Uri IconUrl
+        public BitmapImage IconBitmapImage
         {
-            get { return _searchResultPackage?.IconUrl; }
+            get
+            {
+                return _searchResultPackage?.IconBitmapImage;
+            }
         }
 
         private DetailedPackageMetadata _packageMetadata;
