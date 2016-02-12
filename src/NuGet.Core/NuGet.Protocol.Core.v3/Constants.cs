cf2b6dbd3c170265b74b15a717760653e2b71d91
johnataylor <johtaylo@microsoft.com>
2016-02-11 17:06:15 -0800

    refactored index json lookup to handle priority

--- src/NuGet.Core/NuGet.Protocol.Core.v3/Constants.cs
+++ src/NuGet.Core/NuGet.Protocol.Core.v3/Constants.cs
@@ -8,14 +8,12 @@ namespace NuGet.Protocol.Core.v3
         public static readonly string Version200 = "/2.0.0";
         public static readonly string Version300beta = "/3.0.0-beta";
         public static readonly string Version300 = "/3.0.0";
+        public static readonly string Version340 = "/3.4.0";
 
-        public static readonly string SearchQueryService = "SearchQueryService" + Version300beta;
+        public static readonly string[] SearchQueryService = { "SearchQueryService" + Version340, "SearchQueryService" + Version300beta };
+        public static readonly string[] RegistrationsBaseUrl = { "RegistrationsBaseUrl" + Version340, "RegistrationsBaseUrl" + Version300beta };
         public static readonly string SearchAutocompleteService = "SearchAutocompleteService" + Version300beta;
-        public static readonly string SearchGalleryQueryService = "SearchGalleryQueryService" + Version300beta;
-        public static readonly string MetricsService = "MetricsService" + Version300beta;
-        public static readonly string RegistrationsBaseUrl = "RegistrationsBaseUrl" + Version300beta;
         public static readonly string ReportAbuse = "ReportAbuseUriTemplate" + Version300;
-        public static readonly string Stats = "Stats" + Version300beta;
         public static readonly string LegacyGallery = "LegacyGallery" + Version200;
         public static readonly string PackagePublish = "PackagePublish" + Version200;
         public static readonly string PackageBaseAddress = "PackageBaseAddress" + Version300;
