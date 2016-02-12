696e1bb3cb517dd66d49256fc27b3d8884aa30bf
johnataylor <johtaylo@microsoft.com>
2016-02-11 17:06:15 -0800

    refactored index json lookup to handle priority

--- src/NuGet.Core/NuGet.Protocol.Core.v3/Resources/ServiceIndexResourceV3.cs
+++ src/NuGet.Core/NuGet.Protocol.Core.v3/Resources/ServiceIndexResourceV3.cs
@@ -14,21 +14,72 @@ namespace NuGet.Protocol.Core.v3
     /// </summary>
     public class ServiceIndexResourceV3 : INuGetResource
     {
-        private readonly JObject _index;
+        private readonly IDictionary<string, List<Uri>> _index;
         private readonly DateTime _requestTime;
+        private List<Uri> _empty;
 
         public ServiceIndexResourceV3(JObject index, DateTime requestTime)
         {
-            _index = index;
+            _index = MakeLookup(index);
             _requestTime = requestTime;
         }
 
-        /// <summary>
-        /// Raw json
-        /// </summary>
-        public virtual JObject Index
+        private static IDictionary<string, List<Uri>> MakeLookup(JObject index)
         {
-            get { return _index; }
+            var result = new Dictionary<string, List<Uri>>();
+
+            JToken resources;
+            if (index.TryGetValue("resources", out resources))
+            {
+                foreach (var resource in resources)
+                {
+                    JToken type = resource["@type"];
+                    JToken id = resource["@id"];
+
+                    if (type == null || id == null)
+                    {
+                        continue;
+                    }
+
+                    if (type.Type == JTokenType.Array)
+                    {
+                        foreach (var nType in type)
+                        {
+                            AddEndpoint(result, nType, id);
+                        }
+                    }
+                    else
+                    {
+                        AddEndpoint(result, type, id);
+                    }
+                }
+            }
+
+            return result; 
+        }
+
+        private static void AddEndpoint(IDictionary<string, List<Uri>> result, JToken typeToken, JToken idToken)
+        {
+            string type = (string)typeToken;
+            string id = (string)idToken;
+
+            if (type == null || id == null)
+            {
+                return;
+            }
+
+            List<Uri> ids;
+            if (!result.TryGetValue(type, out ids))
+            {
+                ids = new List<Uri>();
+                result.Add(type, ids);
+            }
+
+            Uri uri;
+            if (Uri.TryCreate(id, UriKind.Absolute, out uri))
+            {
+                ids.Add(new Uri(id));
+            }
         }
 
         /// <summary>
@@ -40,19 +91,52 @@ namespace NuGet.Protocol.Core.v3
         }
 
         /// <summary>
+        /// Empty set of endpoints - needed to efficiently meet indexer contract
+        /// </summary>
+        private List<Uri> Empty
+        {
+            get
+            {
+                if (_empty == null)
+                {
+                    _empty = new List<Uri>();
+                }
+                return _empty;
+            }
+        }
+
+        /// <summary>
         /// A list of endpoints for a service type
         /// </summary>
         public virtual IReadOnlyList<Uri> this[string type]
         {
             get
             {
-                return Index["resources"]
-                    .Where(j =>
-                        (j["@type"].Type == JTokenType.Array
-                            ? j["@type"].Any(v => (string)v == type)
-                            : ((string)j["@type"]) == type))
-                    .Select(o => o["@id"].ToObject<Uri>())
-                    .ToList();
+                List<Uri> endpoints;
+                if (_index.TryGetValue(type, out endpoints))
+                {
+                    return endpoints;
+                }
+                return Empty;
+            }
+        }
+
+        /// <summary>
+        /// A list of endpoints for a service type - in priority order
+        /// </summary>
+        public virtual IReadOnlyList<Uri> this[string[] types]
+        {
+            get
+            {
+                foreach (var type in types)
+                {
+                    var result = this[type];
+                    if (result.Count > 0)
+                    {
+                        return result;
+                    }
+                }
+                return Empty;
             }
         }
     }
--- test/NuGet.Core.Tests/NuGet.Protocol.Core.v3.Tests/ServiceIndexResourceV3ProviderTests.cs
+++ test/NuGet.Core.Tests/NuGet.Protocol.Core.v3.Tests/ServiceIndexResourceV3ProviderTests.cs
@@ -11,6 +11,8 @@ using NuGet.Configuration;
 using NuGet.Protocol.Core.Types;
 using Test.Utility;
 using Xunit;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace NuGet.Protocol.Core.v3.Tests
 {
@@ -154,8 +156,139 @@ xmlns=""http://www.w3.org/2007/app"" xmlns:atom=""http://www.w3.org/2005/Atom"">
 
             // Assert
             Assert.True(result.Item1);
-            var resource = Assert.IsType<ServiceIndexResourceV3>(result.Item2);
-            Assert.NotNull(resource.Index);
+
+
+        }
+
+        [Fact]
+        public async Task Query_For_Particular_Resource()
+        {
+            // Arrange
+            var source = "https://some-site.org/test.json";
+            var content = CreateTestIndex();
+
+            var httpProvider = StaticHttpSource.CreateHttpSource(new Dictionary<string, string> { { source, content } });
+            var provider = new ServiceIndexResourceV3Provider();
+            var sourceRepository = new SourceRepository(new PackageSource(source),
+                new INuGetResourceProvider[] { httpProvider, provider });
+
+            // Act
+            var result = await provider.TryCreate(sourceRepository, default(CancellationToken));
+
+            // Assert
+            Assert.True(result.Item1);
+
+            var resource = result.Item2 as ServiceIndexResourceV3;
+
+            Assert.NotNull(resource);
+
+            var endpoints = resource["Citrus"];
+
+            Assert.True(endpoints.Count == 1);
+
+            var endpointSet = new HashSet<string>(endpoints.Select(u => u.AbsoluteUri));
+            Assert.True(endpointSet.Contains("http://tempuri.org/orange"));
+        }
+
+        [Fact]
+        public async Task Query_For_Particular_Multi_Value_Resource()
+        {
+            // Arrange
+            var source = "https://some-site.org/test.json";
+            var content = CreateTestIndex();
+
+            var httpProvider = StaticHttpSource.CreateHttpSource(new Dictionary<string, string> { { source, content } });
+            var provider = new ServiceIndexResourceV3Provider();
+            var sourceRepository = new SourceRepository(new PackageSource(source),
+                new INuGetResourceProvider[] { httpProvider, provider });
+
+            // Act
+            var result = await provider.TryCreate(sourceRepository, default(CancellationToken));
+
+            // Assert
+            Assert.True(result.Item1);
+
+            var resource = result.Item2 as ServiceIndexResourceV3;
+
+            Assert.NotNull(resource);
+
+            var endpoints = resource["Fruit"];
+
+            Assert.True(endpoints.Count == 3);
+
+            var endpointSet = new HashSet<string>(endpoints.Select(u => u.AbsoluteUri));
+            Assert.True(endpointSet.Contains("http://tempuri.org/banana"));
+            Assert.True(endpointSet.Contains("http://tempuri.org/apple"));
+            Assert.True(endpointSet.Contains("http://tempuri.org/orange"));
+        }
+
+        [Fact]
+        public async Task Query_For_Resource_With_Precedence()
+        {
+            // Arrange
+            var source = "https://some-site.org/test.json";
+            var content = CreateTestIndex();
+
+            var httpProvider = StaticHttpSource.CreateHttpSource(new Dictionary<string, string> { { source, content } });
+            var provider = new ServiceIndexResourceV3Provider();
+            var sourceRepository = new SourceRepository(new PackageSource(source),
+                new INuGetResourceProvider[] { httpProvider, provider });
+
+            // Act
+            var result = await provider.TryCreate(sourceRepository, default(CancellationToken));
+
+            // Assert
+            Assert.True(result.Item1);
+
+            var resource = result.Item2 as ServiceIndexResourceV3;
+
+            Assert.NotNull(resource);
+
+            var endpoints = resource[new string[] { "Chocolate", "Vegetable" }];
+
+            Assert.True(endpoints.Count == 1);
+
+            var endpointSet = new HashSet<string>(endpoints.Select(u => u.AbsoluteUri));
+            Assert.True(endpointSet.Contains("http://tempuri.org/chocolate"));
+        }
+
+        private static string CreateTestIndex()
+        {
+            var obj = new JObject
+            {
+                { "version", "3.1.0-beta" },
+                { "resources", new JArray
+                    {
+                        new JObject
+                        {
+                            { "@type", "Fruit" },
+                            { "@id", "http://tempuri.org/banana" },
+                        },
+                        new JObject
+                        {
+                            { "@type", "Fruit" },
+                            { "@id", "http://tempuri.org/apple" },
+                        },
+                        new JObject
+                        {
+                            { "@type", new JArray { "Fruit", "Citrus" } },
+                            { "@id", "http://tempuri.org/orange" },
+                        },
+                        new JObject
+                        {
+                            { "@type", new JArray { "Vegetable" } },
+                            { "@id", "http://tempuri.org/cabbage" },
+                        },
+                        new JObject
+                        {
+                            { "@type", new JArray { "Chocolate" } },
+                            { "@id", "http://tempuri.org/chocolate" },
+                        },
+                    }
+                }
+            };
+
+            return obj.ToString();
         }
     }
 }
