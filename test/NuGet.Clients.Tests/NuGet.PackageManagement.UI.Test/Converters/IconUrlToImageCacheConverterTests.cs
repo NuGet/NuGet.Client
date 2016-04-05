3854695c400e6597c3f2b587d358219d7965792f
Alex Panov <alpaix@outlook.com>
2016-04-05 14:57:50 -0700

    Fixes https://github.com/NuGet/Home/issues/2423.
    Fixed VS crash caused by invalid package icon URL.
    As reported in the issue having uri with "malformed" scheme triggered
    NotSupportedException. In order to fix that this exception type was added to the
    "whitelist" of errors.
    
    (cherry picked from commit 47a592351d6350a32114a3bba3093dbb8ebbcca7)

---
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Converters/IconUrlToImageCacheConverterTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Xunit;
+
+namespace NuGet.PackageManagement.UI.Test
+{
+    public class IconUrlToImageCacheConverterTests
+    {
+        private static readonly ImageSource DefaultPackageIcon;
+
+        static IconUrlToImageCacheConverterTests()
+        {
+            DefaultPackageIcon = BitmapSource.Create(1, 1, 96, 96, PixelFormats.Bgr24, null, new byte[3] { 0, 0, 0 }, 3);
+            DefaultPackageIcon.Freeze();
+        }
+
+        [Fact]
+        public void Convert_WithMalformedUrlScheme_ReturnsDefault()
+        {
+            var iconUrl = new Uri("ttp://fake.com/image.png");
+
+            var converter = new IconUrlToImageCacheConverter();
+
+            var image = converter.Convert(
+                iconUrl, 
+                typeof(ImageSource),
+                DefaultPackageIcon, 
+                Thread.CurrentThread.CurrentCulture);
+
+            Assert.Same(DefaultPackageIcon, image);
+        }
+
+        [Fact]
+        public void Convert_WithLocalPath_ReturnsDefault()
+        {
+            var iconUrl = new Uri(@"C:\path\to\image.png");
+
+            var converter = new IconUrlToImageCacheConverter();
+
+            var image = converter.Convert(
+                iconUrl,
+                typeof(ImageSource),
+                DefaultPackageIcon,
+                Thread.CurrentThread.CurrentCulture);
+
+            Assert.Same(DefaultPackageIcon, image);
+        }
+
+        [Fact]
+        public void Convert_WithValidImageUrl_DownloadsImage()
+        {
+            var iconUrl = new Uri("http://go.microsoft.com/fwlink/?LinkID=288859");
+
+            var converter = new IconUrlToImageCacheConverter();
+
+            var image = converter.Convert(
+                iconUrl,
+                typeof(ImageSource),
+                DefaultPackageIcon,
+                Thread.CurrentThread.CurrentCulture) as BitmapImage;
+
+            Assert.NotNull(image);
+            Assert.NotSame(DefaultPackageIcon, image);
+            Assert.Equal(iconUrl, image.UriSource);
+        }
+    }
+}
