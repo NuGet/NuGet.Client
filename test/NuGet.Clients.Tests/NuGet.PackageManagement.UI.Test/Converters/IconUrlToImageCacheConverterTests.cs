33d913337661df6592e118bbf2861e2c89aa42b2
Alex Panov <alpaix@outlook.com>
2016-04-06 15:47:30 -0700

    Disabled test Convert_WithValidImageUrl_DownloadsImage
    Fails on CI. Tracking issue: https://github.com/NuGet/Home/issues/2474.

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Converters/IconUrlToImageCacheConverterTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Converters/IconUrlToImageCacheConverterTests.cs
@@ -24,9 +24,9 @@ namespace NuGet.PackageManagement.UI.Test
             var converter = new IconUrlToImageCacheConverter();
 
             var image = converter.Convert(
-                iconUrl, 
+                iconUrl,
                 typeof(ImageSource),
-                DefaultPackageIcon, 
+                DefaultPackageIcon,
                 Thread.CurrentThread.CurrentCulture);
 
             Assert.Same(DefaultPackageIcon, image);
@@ -66,7 +66,7 @@ namespace NuGet.PackageManagement.UI.Test
             Assert.Equal(iconUrl, image.UriSource);
         }
 
-        [Fact]
+        [Fact(Skip="Fails on CI. Tracking issue: https://github.com/NuGet/Home/issues/2474")]
         public void Convert_WithValidImageUrl_DownloadsImage()
         {
             var iconUrl = new Uri("http://fake.com/image.png");
