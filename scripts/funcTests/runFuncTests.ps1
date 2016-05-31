3d957032ef55a16b9f8777941fa40b73ad74ba64
Alex Panov <alpaix@outlook.com>
2016-05-31 15:33:49 -0700

    Fixed functional tests script

--- scripts/funcTests/runFuncTests.ps1
+++ scripts/funcTests/runFuncTests.ps1
@@ -1,3 +1,23 @@
+<#
+.SYNOPSIS
+Build and run functional tests
+
+.PARAMETER DepBuildBranch
+Build branch (unused)
+
+.PARAMETER DepCommitID
+Commit ID (unused)
+
+.PARAMETER DepBuildNumber
+Build number (unused)
+
+.PARAMETER CleanCache
+Switch to clean local package cache
+
+.EXAMPLE
+.\scripts\funcTests\runFuncTests.ps1 -Verbose
+#>
+[CmdletBinding()]
 param (
     [string]$DepBuildBranch="",
     [string]$DepCommitID="",
@@ -16,12 +36,15 @@ trap {
 
 . "$PSScriptRoot\..\..\build\common.ps1"
 
+Write-Host ("`r`n" * 3)
+Trace-Log ('=' * 60)
+
+$startTime = [DateTime]::UtcNow
+
 $FuncScriptsRoot = Split-Path -Path $PSScriptRoot -Parent
 $NuGetClientRoot = Split-Path -Path $FuncScriptsRoot -Parent
-$FuncTestRoot = Join-Path $NuGetClientRoot "test\\NuGet.Core.FuncTests"
-$SrcRoot = Join-Path $NuGetClientRoot "src\\NuGet.Core"
-
-pushd $NuGetClientRoot
+$FuncTestRoot = Join-Path $NuGetClientRoot "test\NuGet.Core.FuncTests"
+$SrcRoot = Join-Path $NuGetClientRoot "src\NuGet.Core"
 
 Write-Host "Dependent Build Details are as follows:"
 Write-Host "Branch: $DepBuildBranch"
@@ -52,8 +75,11 @@ Invoke-BuildStep 'Restoring projects' { Restore-XProjects } `
     -ev +BuildErrors
 
 # Run tests
-$xtests = Find-XProjects $FuncTestRoot
-$xtests | Test-XProject -ev +BuildErrors
+Invoke-BuildStep 'Running tests' {
+    param($FuncTestRoot)
+    $xtests = Find-XProjects $FuncTestRoot
+    $xtests | Test-XProject
+} -args $FuncTestRoot -ev +BuildErrors
 
 Trace-Log ('-' * 60)
 
@@ -69,11 +95,7 @@ if ($BuildErrors) {
     Error-Log "Build's completed with $($BuildErrors.Count) error(s):`r`n$($ErrorLines -join "`r`n")" -Fatal
 }
 
-Trace-Log ('=' * 60)
-
-if ($BuildErrors) {
-    Throw $BuildErrors.Count
-}
+Write-Host ("`r`n" * 3)
 
 # Return success
 exit 0
