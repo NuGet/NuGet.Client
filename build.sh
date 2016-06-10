b26a26ed7e812cd607cf7ba0a62ab7e2c3ca2b1f
Rohit Agrawal <ragrawal@microsoft.com>
2016-06-10 14:56:56 -0700

    add serviceable to nuget packages by default

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ curl -o cli/dotnet-install.sh https://raw.githubusercontent.com/dotnet/cli/rel/1
 
 # Run install.sh
 chmod +x cli/dotnet-install.sh
-cli/dotnet-install.sh -i cli -c preview -v 1.0.0-preview2-002911
+cli/dotnet-install.sh -i cli -c preview -v 1.0.0-preview2-003030
 
 # Display current version
 DOTNET="$(pwd)/cli/dotnet"
