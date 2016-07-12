b615ef01913d4917bddae6b25e3770124c1a7a89
Alex Panov <alpaix@outlook.com>
2016-07-11 18:10:53 -0700

    :tada: Updated version to 3.5.1-beta1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 param (
     [ValidateSet("debug", "release")]
     [string]$Configuration = 'debug',
-    [ValidateSet("Release","rtm", "rc", "beta", "beta2", "final", "xprivate", "zlocal")]
+    [ValidateSet("release","rtm", "rc", "beta", "beta1", "beta2", "final", "xprivate", "zlocal")]
     [string]$ReleaseLabel = 'zlocal',
     [int]$BuildNumber,
     [switch]$SkipRestore,
