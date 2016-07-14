080e692cc1774add754e98dc71389b5c76e50172
Justin Emgarten <justin@emgarten.com>
2016-07-14 16:23:51 -0700

    Allow netstandard to install contentFiles under the any tfm
    By default ContentModel translates 'any' to 'dotnet', this works for the lib folder but for scenarios such as contentFiles any does refer to all frameworks. This change updates contentModel to allow per pattern token replacement tables.
    
    Fixes https://github.com/NuGet/Home/issues/3118

---
+++ src/NuGet.Core/NuGet.ContentModel/PatternTable.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NuGet.ContentModel
+{
+    /// <summary>
+    /// Replacement token table organized by property.
+    /// </summary>
+    public class PatternTable
+    {
+        private readonly Dictionary<string, Dictionary<string, object>> _table
+            = new Dictionary<string, Dictionary<string, object>>(StringComparer.Ordinal);
+
+        public PatternTable()
+            : this(Enumerable.Empty<PatternTableEntry>())
+        {
+        }
+
+        public PatternTable(IEnumerable<PatternTableEntry> entries)
+        {
+            if (entries == null)
+            {
+                throw new ArgumentNullException(nameof(entries));
+            }
+
+            foreach (var entry in entries)
+            {
+                Dictionary<string, object> byProp;
+                if (!_table.TryGetValue(entry.PropertyName, out byProp))
+                {
+                    byProp = new Dictionary<string, object>(StringComparer.Ordinal);
+                    _table.Add(entry.PropertyName, byProp);
+                }
+
+                byProp.Add(entry.Name, entry.Value);
+            }
+        }
+
+        /// <summary>
+        /// Lookup a token and get the replacement if it exists.
+        /// </summary>
+        /// <param name="propertyName">Property moniker</param>
+        /// <param name="name">Token name</param>
+        /// <param name="value">Replacement value</param>
+        public bool TryLookup(string propertyName, string name, out object value)
+        {
+            if (propertyName == null)
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            Dictionary<string, object> byProp;
+            if (_table.TryGetValue(propertyName, out byProp))
+            {
+                return byProp.TryGetValue(name, out value);
+            }
+
+            value = null;
+            return false;
+        }
+    }
+}
