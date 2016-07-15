9885169a84f1524fb5ba3fc5ee62df7bff61cc6b
Ankit Mishra <anmishr@microsoft.com>
2016-07-15 15:31:48 -0700

    Removed stray test file

--- InstallPackageTest.ps1
+++
@@ -1,7 +0,0 @@
-
-function Test-InstallPackageWithInvalidLocalSource {
-	# Arrange
-
-	# Act & Assert
-	Assert-Throws { Install-Package Rules -source c:\temp\data } "Unable to find package 'Rules' at source c:\temp\data. Source not found."
-}
