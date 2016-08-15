0f8347f8e1113554ef80ae1a890189ecd34df544
Ankit Mishra <anmishr@microsoft.com>
2016-08-15 15:06:05 -0700

    Fixing resource name error message

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XPlatLocalsTests.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XPlatLocalsTests.cs
@@ -25,20 +25,31 @@ namespace NuGet.XPlat.FuncTest
             Assert.Equal(0, exitCode);
         }
 
-        [Fact]
-        public static void Locals_Success_InvalidArguments_HelpMessage()
+        [Theory]
+        [InlineData("locals")]
+        [InlineData("locals --list")]
+        [InlineData("locals -l")]
+        [InlineData("locals --clear")]
+        [InlineData("locals -c")]
+        public static void Locals_Success_InvalidArguments_HelpMessage(String args)
         {
             var log = new TestCommandOutputLogger();
-            var args = new List<string>()
-            {
-                "locals",
-                "--list"
-            };
-            var exitCode = Program.MainInternal(args.ToArray(), log);
-            Assert.Equal("usage: NuGet locals < all | http - cache | global - packages | temp > [-clear | -list] \nFor more information, visit http://docs.nuget.org/docs/reference/command-line-reference", log.ShowErrors());
+            var exitCode = Program.MainInternal(args.Split(null), log);
+            Assert.Equal("usage: NuGet locals <all | http-cache | global-packages | temp> [-clear | -list]\r\nFor more information, visit http://docs.nuget.org/docs/reference/command-line-reference", log.ShowErrors());
             Assert.Equal(1, exitCode);
         }
-    }
 
-   
+        [Theory]
+        [InlineData("locals --list unknownResource")]
+        [InlineData("locals -l unknownResource")]
+        [InlineData("locals --clear unknownResource")]
+        [InlineData("locals -c unknownResource")]
+        public static void Locals_Success_InvalidResourceName_HelpMessage(String args)
+        {
+            var log = new TestCommandOutputLogger();
+            var exitCode = Program.MainInternal(args.Split(null), log);
+            Assert.Equal("An invalid local resource name was provided. Please provide one of the following values: http-cache, temp, global-packages, all.", log.ShowErrors());
+            Assert.Equal(1, exitCode);
+        }
+    }   
 }
