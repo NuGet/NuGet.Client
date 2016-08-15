be96c8f371dc776338c642a31beca070dbfd6b85
Ankit Mishra <anmishr@microsoft.com>
2016-08-15 15:45:41 -0700

    Adding tests for xplat locals

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XPlatLocalsTests.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XPlatLocalsTests.cs
@@ -3,24 +3,19 @@
 
 using NuGet.CommandLine.XPlat;
 using System;
-using System.Collections.Generic;
 using Xunit;
 
 namespace NuGet.XPlat.FuncTest
 {
     public class XPlatLocalsTests
     {
-        [Fact]
-        public static void Locals_List_Succeeds()
+        [Theory]
+        [InlineData("locals --list all")]
+        [InlineData("locals -l all")]
+        public static void Locals_List_Succeeds(String args)
         {
             var log = new TestCommandOutputLogger();
-            var args = new List<string>()
-            {
-                "locals",
-                "--list",
-                "all"
-            };
-            var exitCode = Program.MainInternal(args.ToArray(), log);
+            var exitCode = Program.MainInternal(args.Split(null), log);
             Assert.Equal(string.Empty, log.ShowErrors());
             Assert.Equal(0, exitCode);
         }
@@ -35,7 +30,7 @@ namespace NuGet.XPlat.FuncTest
         {
             var log = new TestCommandOutputLogger();
             var exitCode = Program.MainInternal(args.Split(null), log);
-            Assert.Equal("usage: NuGet locals <all | http-cache | global-packages | temp> [-clear | -list]\r\nFor more information, visit http://docs.nuget.org/docs/reference/command-line-reference", log.ShowErrors());
+            Assert.Equal("usage: NuGet locals <all | http-cache | global-packages | temp> [-clear | -list]"+ Environment.NewLine + "For more information, visit http://docs.nuget.org/docs/reference/command-line-reference", log.ShowErrors());
             Assert.Equal(1, exitCode);
         }
 
