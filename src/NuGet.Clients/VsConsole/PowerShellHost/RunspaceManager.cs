edcf220f3bbc5f0ddc8794feeafb9deb175f1745
Zhi Li <zhili128@outlook.com>
2016-08-17 13:20:24 -0700

    review change

--- src/NuGet.Clients/VsConsole/PowerShellHost/RunspaceManager.cs
+++ src/NuGet.Clients/VsConsole/PowerShellHost/RunspaceManager.cs
@@ -4,12 +4,10 @@
 using System;
 using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Reflection;
-using System.Windows.Media;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.PowerShell;
@@ -62,15 +60,6 @@ namespace NuGetConsole.Host.PowerShell.Implementation
                     ScopedItemOptions.AllScope | ScopedItemOptions.Constant)
                 );
 
-            // check language mode for current session
-            var languageMode = initialSessionState.LanguageMode;
-
-            if (languageMode != PSLanguageMode.FullLanguage)
-            {
-                console.Write(String.Format(CultureInfo.CurrentCulture, Resources.LanguageModeWarning, languageMode.ToString()) + Environment.NewLine,
-                    Colors.Black, Colors.Yellow);
-            }
-
             // this is used by the functional tests
             var sourceRepositoryProvider = ServiceLocator.GetInstance<ISourceRepositoryProvider>();
             var solutionManager = ServiceLocator.GetInstance<ISolutionManager>();
@@ -97,7 +86,7 @@ namespace NuGetConsole.Host.PowerShell.Implementation
             //
             Runspace.DefaultRunspace = runspace;
 
-            return Tuple.Create(new RunspaceDispatcher(runspace), host);
+            return Tuple.Create(new RunspaceDispatcher(runspace, console), host);
         }
 
         private static void SetupExecutionPolicy(RunspaceDispatcher runspace)
