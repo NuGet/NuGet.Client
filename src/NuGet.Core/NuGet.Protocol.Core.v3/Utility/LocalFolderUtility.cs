09559ff0019df9fe2aed36a5cb09e80b77476ee8
Zhi Li <zhili128@outlook.com>
2016-08-23 14:53:49 -0700

    Fix GetFeedType perf issue, https://github.com/NuGet/Home/issues/3355

--- src/NuGet.Core/NuGet.Protocol.Core.v3/Utility/LocalFolderUtility.cs
+++ src/NuGet.Core/NuGet.Protocol.Core.v3/Utility/LocalFolderUtility.cs
@@ -668,6 +668,62 @@ namespace NuGet.Protocol
             yield break;
         }
 
+        public static FeedType GetLocalFeedType(string root, ILogger log)
+        {
+            if (root == null)
+            {
+                throw new ArgumentNullException(nameof(root));
+            }
+
+            if (log == null)
+            {
+                throw new ArgumentNullException(nameof(log));
+            }
+
+            // Check for package files one level deep.
+            DirectoryInfo rootDirectoryInfo = GetAndVerifyRootDirectory(root);
+
+            try
+            {
+                // Search root directory for *.Nupkg, return V2 if there is any Nupkg file.
+                if (rootDirectoryInfo.EnumerateFiles(NupkgFilter, SearchOption.TopDirectoryOnly).Any())
+                {
+                    return FeedType.FileSystemV2;
+                }
+
+                foreach (var idDir in rootDirectoryInfo.EnumerateDirectories())
+                {
+                    // Search first sub directory for *.Nupkg, return V2 if there is any Nupkg file.
+                    if (idDir.EnumerateFiles(NupkgFilter, SearchOption.TopDirectoryOnly).Any())
+                    {
+                        return FeedType.FileSystemV2;
+                    }
+
+                    foreach (var versionDir in idDir.EnumerateDirectories())
+                    {
+                        // If we have files in the format {packageId}/{version}/{packageId}.{version}.nupkg, return V3. 
+                        var package = GetPackageV3(root, idDir.Name, versionDir.Name, log);
+
+                        if (package != null)
+                        {
+                            return FeedType.FileSystemV3;
+                        }
+                    }
+                }
+            }
+
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+            catch (DirectoryNotFoundException)
+            {
+
+            }
+
+            return FeedType.FileSystemUnknown;
+        }
+
         /// <summary>
         /// Verify that a path could be a valid directory. Throw a FatalProtocolException otherwise.
         /// </summary>
@@ -882,22 +938,11 @@ namespace NuGet.Protocol
 
             foreach (var versionDir in GetDirectoriesSafe(idRoot, log))
             {
-                NuGetVersion version;
-                if (NuGetVersion.TryParse(versionDir.Name, out version))
-                {
-                    var identity = new PackageIdentity(id, version);
+                var package = GetPackageV3(root, id, versionDir.Name, log);
 
-                    // Read the package, this may be null if files are missing
-                    var package = GetPackageV3(root, identity, log);
-
-                    if (package != null)
-                    {
-                        yield return package;
-                    }
-                }
-                else
+                if (package != null)
                 {
-                    log.LogWarning(string.Format(CultureInfo.CurrentCulture, Strings.UnableToParseFolderV3Version, versionDir));
+                    yield return package;
                 }
             }
 
@@ -1012,6 +1057,24 @@ namespace NuGet.Protocol
         private static FileInfo[] GetNupkgsFromDirectory(DirectoryInfo root, ILogger log)
         {
             return GetFilesSafe(root, NupkgFilter, log);
+        }
+
+        private static LocalPackageInfo GetPackageV3(string root, string id, string version, ILogger log)
+        {
+            NuGetVersion nugetVersion;
+            if (NuGetVersion.TryParse(version, out nugetVersion))
+            {
+                var identity = new PackageIdentity(id, nugetVersion);
+
+                // Read the package, this may be null if files are missing
+                return GetPackageV3(root, identity, log);
+            }
+            else
+            {
+                log.LogWarning(string.Format(CultureInfo.CurrentCulture, Strings.UnableToParseFolderV3Version, version));
+            }
+
+            return null;
         }
     }
 }
