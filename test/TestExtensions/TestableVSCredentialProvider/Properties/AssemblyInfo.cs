de4023633ef75c2a6c8a7bcfc75ae368ff34a7a3
Peter Spillman <pspill@microsoft.com>
2016-09-14 14:16:14 -0400

    Add TestableVSCredentialProvider

---
+++ test/TestExtensions/TestableVSCredentialProvider/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Test.TestExtensions.TestableVSCredentialProvider")]
+[assembly: AssemblyDescription("Test implementation of a Visual Studio credential provider")]
+[assembly: ComVisible(false)]
