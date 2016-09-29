2d433fed2b3f20636b60e8ab3a18ac954df4ee81
Joel Verhagen <jver@microsoft.com>
2016-09-29 14:13:41 -0700

    Pass push timeout down and add functional tests
    Fix https://github.com/NuGet/Home/issues/2785

--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetPushCommandTest.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetPushCommandTest.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using NuGet.Configuration;
 using NuGet.Test.Utility;
 using Xunit;
@@ -17,6 +21,99 @@ namespace NuGet.CommandLine.Test
 
         private readonly string _originalCredentialProvidersEnvar =
             Environment.GetEnvironmentVariable(ExtensionLocator.CredentialProvidersEnvar);
+
+        /// <summary>
+        /// 100 seconds is significant because that is the default timeout on <see cref="HttpClient"/>.
+        /// Related to https://github.com/NuGet/Home/issues/2785.
+        /// </summary>
+        [Fact]
+        public void PushCommand_AllowsTimeoutToBeSpecifiedHigherThan100Seconds()
+        {
+            // Arrange
+            using (var packageDirectory = TestFileSystemUtility.CreateRandomTestFolder())
+            {
+                var nuget = Util.GetNuGetExePath();
+                var sourcePath = Util.CreateTestPackage("PackageA", "1.1.0", packageDirectory);
+                var outputPath = Path.Combine(packageDirectory, "pushed.nupkg");
+
+                using (var server = new MockServer())
+                {
+                    server.Put.Add("/push", r =>
+                    {
+                        Thread.Sleep(TimeSpan.FromSeconds(101));
+
+                        byte[] buffer = MockServer.GetPushedPackage(r);
+                        using (var outputStream = new FileStream(outputPath, FileMode.Create))
+                        {
+                            outputStream.Write(buffer, 0, buffer.Length);
+                        }
+
+                        return HttpStatusCode.Created;
+                    });
+
+                    server.Start();
+
+                    // Act
+                    var result = CommandRunner.Run(
+                        nuget,
+                        packageDirectory,
+                        $"push {sourcePath} -Source {server.Uri}push -Timeout 110",
+                        waitForExit: true,
+                        timeOutInMilliseconds: 120 * 1000); // 120 seconds
+
+                    // Assert
+                    server.Stop();
+                    Assert.True(0 == result.Item1, $"{result.Item2} {result.Item3}");
+                    Assert.Contains("Your package was pushed.", result.Item2);
+                    Assert.True(File.Exists(outputPath), "The package should have been pushed");
+                    Assert.Equal(File.ReadAllBytes(sourcePath), File.ReadAllBytes(outputPath));
+                }
+            }
+        }
+
+        [Fact]
+        public void PushCommand_AllowsTimeoutToBeSpecifiedLowerThan100Seconds()
+        {
+            // Arrange
+            using (var packageDirectory = TestFileSystemUtility.CreateRandomTestFolder())
+            {
+                var nuget = Util.GetNuGetExePath();
+                var sourcePath = Util.CreateTestPackage("PackageA", "1.1.0", packageDirectory);
+                var outputPath = Path.Combine(packageDirectory, "pushed.nupkg");
+
+                using (var server = new MockServer())
+                {
+                    server.Put.Add("/push", r =>
+                    {
+                        Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                        byte[] buffer = MockServer.GetPushedPackage(r);
+                        using (var outputStream = new FileStream(outputPath, FileMode.Create))
+                        {
+                            outputStream.Write(buffer, 0, buffer.Length);
+                        }
+
+                        return HttpStatusCode.Created;
+                    });
+
+                    server.Start();
+
+                    // Act
+                    var result = CommandRunner.Run(
+                        nuget,
+                        packageDirectory,
+                        $"push {sourcePath} -Source {server.Uri}push -Timeout 1",
+                        waitForExit: true,
+                        timeOutInMilliseconds: 20 * 1000); // 20 seconds
+
+                    // Assert
+                    server.Stop();
+                    Assert.True(1 == result.Item1, $"{result.Item2} {result.Item3}");
+                    Assert.DoesNotContain("Your package was pushed.", result.Item2);
+                    Assert.False(File.Exists(outputPath), "The package should not have been pushed");
+                }
+            }
+        }
 
         // Tests pushing to a source that is a v2 file system directory.
         [Fact]
