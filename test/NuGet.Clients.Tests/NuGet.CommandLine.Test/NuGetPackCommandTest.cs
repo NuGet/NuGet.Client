768cef613f1e26328ac081b870fe19b37b0c2300
Zhi Li <zhili128@outlook.com>
2016-09-30 14:23:42 -0700

    Skip tests on Mono
    https://github.com/NuGet/Home/issues/3529
    https://github.com/NuGet/Home/issues/3525

--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetPackCommandTest.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetPackCommandTest.cs
@@ -788,6 +788,9 @@ namespace Proj2
         [Fact]
         public void PackCommand_PclProjectWithProjectJsonAndTargetsNetStandard()
         {
+            // Skip this test on Mono
+            if (RuntimeEnvironmentHelper.IsMono) return;
+
             // This bug tests issue: https://github.com/NuGet/Home/issues/3108
             var nugetexe = Util.GetNuGetExePath();
 
@@ -3552,6 +3555,9 @@ stuff \n <<".Replace("\r\n", "\n");
         [Fact]
         public void PackCommand_PackJsonCorrectLibPathInNupkg()
         {
+            // Skip this test on Mono
+            if (RuntimeEnvironmentHelper.IsMono) return;
+
             var nugetexe = Util.GetNuGetExePath();
 
             using (var workingDirectory = TestFileSystemUtility.CreateRandomTestFolder())
@@ -3676,6 +3682,9 @@ stuff \n <<".Replace("\r\n", "\n");
         [InlineData(".exe")]
         public void PackCommand_PackJsonCorrectLibPathInNupkgWithOutputName(string extension)
         {
+            // Skip this test on Mono
+            if (RuntimeEnvironmentHelper.IsMono) return;
+
             var nugetexe = Util.GetNuGetExePath();
 
             using (var testFolder = TestFileSystemUtility.CreateRandomTestFolder())
@@ -3859,6 +3868,9 @@ stuff \n <<".Replace("\r\n", "\n");
         [Fact]
         public void PackCommand_BuildProjectJsonWithFullBasePath()
         {
+            // Skip this test on Mono
+            if (RuntimeEnvironmentHelper.IsMono) return;
+
             var nugetexe = Util.GetNuGetExePath();
 
             using (var workingDirectory = TestFileSystemUtility.CreateRandomTestFolder())
@@ -3923,6 +3935,9 @@ stuff \n <<".Replace("\r\n", "\n");
         [Fact]
         public void PackCommand_BuildProjectJsonWithRelativeBasePath()
         {
+            // Skip this test on Mono
+            if (RuntimeEnvironmentHelper.IsMono) return;
+
             var nugetexe = Util.GetNuGetExePath();
 
             using (var workingDirectory = TestFileSystemUtility.CreateRandomTestFolder())
