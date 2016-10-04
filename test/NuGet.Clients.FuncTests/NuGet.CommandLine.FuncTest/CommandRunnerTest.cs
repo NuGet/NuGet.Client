edb7bc7b13c0944c3e8c44b5fd7127606bcd6a6a
Joel Verhagen <jver@microsoft.com>
2016-10-04 12:56:57 -0700

    Add tests that attempt to fill the stdout buffer

--- test/NuGet.Clients.FuncTests/NuGet.CommandLine.FuncTest/CommandRunnerTest.cs
+++ test/NuGet.Clients.FuncTests/NuGet.CommandLine.FuncTest/CommandRunnerTest.cs
@@ -4,6 +4,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using NuGet.Common;
 using NuGet.Test.Utility;
@@ -38,6 +39,72 @@ namespace NuGet.CommandLine.FuncTest
 
             // Assert
             Assert.All(successes, Assert.True);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(64)]
+        public void Run_DoesNotHangWhenReadingLargeStdout(int outputSizeInKilobytes)
+        {
+            // Arrange
+            using (var testDirectory = TestFileSystemUtility.CreateRandomTestFolder())
+            {
+                // Write the line of the desired size.
+                var filePath = Path.Combine(testDirectory, "file.txt");
+                var expectedLineContent = new string('a', 14);
+                var expectedLine = expectedLineContent + "\r\n"; // a line that is 16 bytes long
+                var expectedLineCount = (1024 * outputSizeInKilobytes) / 16;
+                using (var fileStream = File.OpenWrite(filePath))
+                using (var fileWriter = new StreamWriter(fileStream, Encoding.ASCII))
+                {
+                    for (var i = 0; i < expectedLineCount; i++)
+                    {
+                        fileWriter.Write(expectedLine);
+                    }
+                }
+                
+                // Run a program that just reads a file to stdout.
+                string fileName;
+                string args;
+                if (RuntimeEnvironmentHelper.IsWindows)
+                {
+                    fileName = @"C:\Windows\System32\cmd.exe";
+                    args = $"/c type {filePath}";
+                }
+                else
+                {
+                    fileName = "/bin/cat";
+                    args = filePath;
+                }
+
+                // Act
+                var result = CommandRunner.Run(
+                    fileName,
+                    Directory.GetCurrentDirectory(),
+                    args,
+                    waitForExit: true);
+
+                // Assert
+                Assert.Equal(0, result.Item1);
+
+                var actualLineCount = 0;
+                using (var stringReader = new StringReader(result.Item2))
+                {
+                    string actualLineContent;
+                    while ((actualLineContent = stringReader.ReadLine()) != null)
+                    {
+                        actualLineCount++;
+                        Assert.Equal(expectedLineContent, actualLineContent);
+                    }
+                }
+
+                Assert.Equal(expectedLineCount, actualLineCount);
+            }
         }
 
         private static void VerifyWithCommandRunner()
