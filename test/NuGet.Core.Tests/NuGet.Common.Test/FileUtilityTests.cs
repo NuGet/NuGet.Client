8a7ac3d1e8878c88f6fdfb8aca096a47a359b6d9
Damon Tivel <dtivel@microsoft.com>
2016-10-04 12:16:14 -0700

    Fix several glob/path bugs
    Fix NuGet/Home#3526.
    Fix NuGet/Home#3405.
    Fix a bug with PathResolver.NormalizeBasePath(...) where ../ was
    unsupported on Unix-flavored operating systems.
    Add >150 unit tests.
    Add copyright headers to files.

--- test/NuGet.Core.Tests/NuGet.Common.Test/FileUtilityTests.cs
+++ test/NuGet.Core.Tests/NuGet.Common.Test/FileUtilityTests.cs
@@ -1,4 +1,7 @@
-﻿using NuGet.Test.Utility;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using NuGet.Test.Utility;
 using System.IO;
 using Xunit;
 
--- test/NuGet.Core.Tests/NuGet.Packaging.Test/FallbackPackagePathResolverTests.cs
+++ test/NuGet.Core.Tests/NuGet.Packaging.Test/FallbackPackagePathResolverTests.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using NuGet.Packaging.Core;
 using NuGet.Test.Utility;
