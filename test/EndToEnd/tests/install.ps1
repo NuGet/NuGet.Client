edc924d689496ab270c3bf156fb2bddcea7f6f6d
Ankit Mishra <anmishr@microsoft.com>
2016-10-14 11:48:17 -0700

    Adding test case for nested and non nested install.ps1 within the tools dir

--- test/EndToEnd/tests/install.ps1
+++ test/EndToEnd/tests/install.ps1
@@ -2258,7 +2258,7 @@ function Test-ToolsPathForInitAndInstallScriptPointToToolsFolder
     Assert-Package $p 'packageA'
 }
 
-function Test-InstallPackageWithNestedToolsDirectory
+function Test-InstallPackageWithNestedPS1InsideTools
 {
     param($context)
 
@@ -2266,12 +2266,28 @@ function Test-InstallPackageWithNestedToolsDirectory
     $p = New-ClassLibrary
 	$solutionDir = Get-SolutionDir
 
-    # Act 
-    $p | Install-Package TestPackage.SupportingMultipleFrameworks -Version 1.2.0 -Source $context.RepositoryPath
+    # Act
+	$p | Install-Package TestPackage.SupportingMultipleFrameworksNestedPS1 -Version 1.2.0 -Source $context.RepositoryRoot
 
     # Assert
-    Assert-Package $p 'TestPackage.SupportingMultipleFrameworks'
-	Assert-PathExists (Join-Path $solutionDir packages\TestPackage.SupportingMultipleFrameworks.1.2.0\version.txt)
+    Assert-Package $p 'TestPackage.SupportingMultipleFrameworksNestedPS1'
+	Assert-PathExists (Join-Path $solutionDir packages\TestPackage.SupportingMultipleFrameworksNestedPS1.1.2.0\version.txt)
+}
+
+function Test-InstallPackageWithNonNestedPS1InsideTools
+{
+    param($context)
+
+    # Arrange
+    $p = New-ClassLibrary
+	$solutionDir = Get-SolutionDir
+
+    # Act
+	$p | Install-Package TestPackage.SupportingMultipleFrameworksNonNestedPS1 -Version 1.2.0 -Source $context.RepositoryRoot
+
+    # Assert
+    Assert-Package $p 'TestPackage.SupportingMultipleFrameworksNonNestedPS1'
+	Assert-PathExists (Join-Path $solutionDir packages\TestPackage.SupportingMultipleFrameworksNonNestedPS1.1.2.0\version.txt)
 }
 
 function Test-InstallFailCleansUpSatellitePackageFiles 
