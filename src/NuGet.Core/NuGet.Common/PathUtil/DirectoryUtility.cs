1428f555a24d289a404a0acbcd59a5358be052ab
Tom Deseyn <tom.deseyn@gmail.com>
2016-12-09 21:38:13 +0100

    CreateSharedDirectory: fix race condition between creation and setting permissions

--- src/NuGet.Core/NuGet.Common/PathUtil/DirectoryUtility.cs
+++ src/NuGet.Core/NuGet.Common/PathUtil/DirectoryUtility.cs
@@ -9,8 +9,6 @@ namespace NuGet.Common
     /// </summary>
     public static class DirectoryUtility
     {
-        private static object s_sharedFolderCreationLock = new object();
-
         /// <summary>
         /// Creates all directories and subdirectories in the specified path unless they already exist.
         /// New directories can be read and written by all users.
@@ -23,28 +21,67 @@ namespace NuGet.Common
             }
             else
             {
-                lock (s_sharedFolderCreationLock)
+                path = Path.GetFullPath(path);
+                var root = Path.GetPathRoot(path);
+                var sepPos = root.Length - 1;
+                do
                 {
-                    path = Path.GetFullPath(path);
-                    var root = Path.GetPathRoot(path);
-                    var sepPos = root.Length - 1;
-                    do
+                    sepPos = path.IndexOf(Path.DirectorySeparatorChar, sepPos + 1);
+                    var currentPath = sepPos == -1 ? path : path.Substring(0, sepPos);
+                    if (!Directory.Exists(currentPath))
                     {
-                        sepPos = path.IndexOf(Path.DirectorySeparatorChar, sepPos + 1);
-                        var currentPath = sepPos == -1 ? path : path.Substring(0, sepPos);
-                        if (!Directory.Exists(currentPath))
-                        {
-                            Directory.CreateDirectory(currentPath);
-                            chmod(currentPath, UGO_RWX);
-                        }
-                    } while (sepPos != -1);
+                        CreateSingleSharedDirectory(currentPath);
+                    }
+                } while (sepPos != -1);
+            }
+        }
+
+        private static void CreateSingleSharedDirectory(string path)
+        {
+            // Creating a directory and setting the permissions are two operations. To avoid race
+            // conditions, we create a different directory, set the permissions and rename it. We
+            // create it under the parent directory to make sure it is on the same volume.
+            var parentDir = Path.GetDirectoryName(path);
+            var tempDir = Path.Combine(parentDir, Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempDir);
+            if (chmod(tempDir, UGO_RWX) == -1)
+            {
+                // it's very unlikely we can't set the permissions of a directory we just created
+                TryDeleteDirectory(tempDir);
+                var errno = Marshal.GetLastWin32Error();
+                throw new InvalidOperationException($"Unable to set permission while creating {path}, errno={errno}.");
+            }
+            try
+            {
+                Directory.Move(tempDir, path);
+            }
+            catch
+            {
+                TryDeleteDirectory(tempDir);
+                if (Directory.Exists(path))
+                {
+                    return;
+                }
+                else
+                {
+                    throw;
                 }
             }
         }
 
-        private const int UGO_RWX = 0x1ff;
+        private static void TryDeleteDirectory(string path)
+        {
+            try
+            {
+                Directory.Delete(path);
+            }
+            catch
+            {}
+        }
 
-        [DllImport("libc")]
+        private const int UGO_RWX = 0x1ff; // 0777
+
+        [DllImport("libc", SetLastError = true)]
         private static extern int chmod(string pathname, int mode);
     }
 }
