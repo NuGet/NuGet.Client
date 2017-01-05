3b399d935e8bde3f4787cd3f4b73dcc6217778e6
Joel Verhagen <jver@microsoft.com>
2017-01-05 08:17:12 -0800

    Add IVsNuGetPathContext interfacts

---
+++ src/NuGet.Clients/NuGet.VisualStudio/Extensibility/IVsNuGetPathContext.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace NuGet.VisualStudio
+{
+    /// <summary>
+    /// NuGet path information specific to the current context (e.g. project context).
+    /// </summary>
+    [ComImport]
+    [Guid("24A1A187-75EE-4296-A8B3-59F0E0707119")]
+    public interface IVsNuGetPathContext
+    {
+        /// <summary>
+        /// User package folder directory.
+        /// </summary>
+        string UserPackageFolder { get; }
+
+        /// <summary>
+        /// Fallback package folder locations.
+        /// </summary>
+        IReadOnlyList<string> FallbackPackageFolders { get; }
+    }
+}
---
+++ src/NuGet.Clients/NuGet.VisualStudio/Extensibility/IVsNuGetPathContextFactory.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Runtime.InteropServices;
+
+namespace NuGet.VisualStudio
+{
+    /// <summary>
+    /// A factory to initialize <see cref="IVsNuGetPathContext"/> instances.
+    /// </summary>
+    [ComImport]
+    [Guid("5BAC7095-F674-4778-8788-E15FFF77F96B")]
+    public interface IVsNuGetPathContextFactory
+    {
+        /// <summary>
+        /// Create an <see cref="IVsNuGetPathContext"/> based on the given DTE project.
+        /// </summary>
+        /// <param name="project">The DTE project.</param>
+        /// <returns>The path context.</returns>
+        IVsNuGetPathContext Create(EnvDTE.Project project);
+    }
+}
