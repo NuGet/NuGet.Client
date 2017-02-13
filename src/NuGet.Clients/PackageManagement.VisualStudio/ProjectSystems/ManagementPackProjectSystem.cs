7105e7c7715934be2124f90a2837f4f6fd7e794a
Mike Faulkinbury <mike@integrasource.com>
2017-02-13 16:20:14 -0600

    added support for mp files replaced tests for VSAE with try/catch/log

--- src/NuGet.Clients/PackageManagement.VisualStudio/ProjectSystems/ManagementPackProjectSystem.cs
+++ src/NuGet.Clients/PackageManagement.VisualStudio/ProjectSystems/ManagementPackProjectSystem.cs
@@ -7,8 +7,9 @@ using NuGet.ProjectManagement;
 using System.Reflection;
 using System.IO;
 
-//TODO: refactor to safely build a no-op proxy when VSAE is not installed (DON'T BREAK THE BUILD)
+//NOTE: Safely build a no-op proxy when VSAE is not installed (DON'T BREAK THE BUILD)
 #if VisualStudioAuthoringExtensionsInstalled
+using Microsoft.EnterpriseManagement.Configuration;
 using Microsoft.EnterpriseManagement.Configuration.IO;
 using Microsoft.EnterpriseManagement.Packaging;
 using System.Runtime.InteropServices;
@@ -22,51 +23,95 @@ namespace NuGet.PackageManagement.VisualStudio
     {
         private dynamic _projectMgr;
         private dynamic _mpReferenceContainerNode;
-        private MethodInfo _isAlreadyAddedMethodInfo;
-        private bool _isVsaeInstalled = false;
 
 #if VisualStudioAuthoringExtensionsInstalled
+        private MethodInfo _isAlreadyAddedMethodInfo;
         delegate bool IsAlreadyAddedInternalDelegate(out ReferenceNode existingNodeInternal);
 #endif
 
         public ManagementPackProjectSystem(EnvDTEProject envDTEProject, INuGetProjectContext nuGetProjectContext)
             : base(envDTEProject, nuGetProjectContext)
         {
-            _projectMgr = EnvDTEProject.Object;
-
-            dynamic oaReferenceFolderItem = this.EnvDTEProject.ProjectItems.Item(1);
-
-            // get the mp reference container node using reflection
-            Type refFolderType = oaReferenceFolderItem.GetType();
-            BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
-            PropertyInfo refFolderPropinfo = refFolderType.GetProperty("Node", bindingFlags);
-
-            if (refFolderPropinfo != null)
+            try
             {
-                _mpReferenceContainerNode = refFolderPropinfo.GetValue(oaReferenceFolderItem);
-            }
+                _projectMgr = EnvDTEProject.Object;
+
+                dynamic oaReferenceFolderItem = this.EnvDTEProject.ProjectItems.Item(1);
+
+                // get the mp reference container node using reflection
+                Type refFolderType = oaReferenceFolderItem.GetType();
+                BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+                PropertyInfo refFolderPropinfo = refFolderType.GetProperty("Node", bindingFlags);
+
+                if (refFolderPropinfo != null)
+                {
+                    _mpReferenceContainerNode = refFolderPropinfo.GetValue(oaReferenceFolderItem);
+                }
 
 #if VisualStudioAuthoringExtensionsInstalled
-        _isVsaeInstalled = true;
 
-            _isAlreadyAddedMethodInfo = typeof(ManagementPackReferenceNode).GetMethod("IsAlreadyAdded",
-                bindingFlags,
-                null,
-                new[] { typeof(ReferenceNode).MakeByRefType() },
-                null);
+                _isAlreadyAddedMethodInfo = typeof(ManagementPackReferenceNode).GetMethod("IsAlreadyAdded",
+                    bindingFlags,
+                    null,
+                    new[] { typeof(ReferenceNode).MakeByRefType() },
+                    null);
 #endif
+            }
+            catch (Exception ex)
+            {
+                NuGetProjectContext.Log(MessageLevel.Error, $"Unexpected exception in ctor: {ex.Message}.");
+            }
         }
 
         public override void AddReference(string referencePath)
         {
-            if (!_isVsaeInstalled)
+            try
             {
-                NuGetProjectContext.Log(MessageLevel.Info, "Visual Studio Authoring Extensions are not installed. Skipping AddReference.");
-                return;
-            }
+                var extension = Path.GetExtension(referencePath);
 
-            //TODO: handle case when referencePath is an .mp file (rather than an .mpb)
-            AddManagementPackReferencesFromBundle(referencePath);
+                if (extension == ".mpb") // management pack bundle
+                {
+                    AddManagementPackReferencesFromBundle(referencePath);
+                }
+                else if (extension == ".mp") // sealed management pack
+                {
+                    AddManagementPackReferenceFromSealedMp(referencePath);
+                }
+                else
+                {
+                    NuGetProjectContext.Log(MessageLevel.Warning, $"Unexpected reference extension ({extension}). Skipping AddReference.");
+                }
+            }
+            catch (Exception ex)
+            {
+                NuGetProjectContext.Log(MessageLevel.Error, $"Unexpected exception in AddReference: {ex.Message}.");
+            }
+        }
+
+        private void AddManagementPackReferenceFromSealedMp(string sealedMpPath)
+        {
+#if VisualStudioAuthoringExtensionsInstalled
+
+            try
+            {
+                using (var mpFileStore = new ManagementPackFileStore())
+                {
+                    mpFileStore.AddDirectory(Path.GetDirectoryName(sealedMpPath));
+                    using (var managementPack = new ManagementPack(sealedMpPath, mpFileStore))
+                    {
+                        AddManagementPackReference(managementPack, sealedMpPath);
+                    }
+                }
+            }
+            catch (COMException ex)
+            {
+                NuGetProjectContext.Log(MessageLevel.Error, $"Unexpected COM exception while adding reference: {ex.ErrorCode}-{ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                NuGetProjectContext.Log(MessageLevel.Error, $"Unexpected exception while adding reference: {ex.Message}");
+            }
+#endif
         }
 
         private void AddManagementPackReferencesFromBundle(string bundlePath)
@@ -81,44 +126,10 @@ namespace NuGet.PackageManagement.VisualStudio
                 {
                     ManagementPackBundle bundle = bundleReader.Read(bundlePath, mpFileStore);
 
-                    foreach (var managementPack in bundle.ManagementPacks)
-                    {
-                        string identity = $"{managementPack.Name} (Version={managementPack.Version},PublicKeyToken={managementPack.KeyToken})";
-
-                        try
+                    foreach (var managementPack in bundle.ManagementPacks) using (managementPack)
                         {
-                            if (!managementPack.Sealed)
-                            {
-                                LogProcessingResult(identity, ProcessStatus.NotSealed);
-                                continue;
-                            }
-
-                            using (var packReferenceNode = new ManagementPackReferenceNode(_projectMgr, bundlePath, managementPack.Name))
-                            {
-                                ReferenceNode existingEquivalentNode;
-
-                                var isAlreadyAddedInternal = (IsAlreadyAddedInternalDelegate)Delegate.CreateDelegate(
-                                                              typeof(IsAlreadyAddedInternalDelegate),
-                                                              packReferenceNode,
-                                                              _isAlreadyAddedMethodInfo);
-
-                                if (isAlreadyAddedInternal(out existingEquivalentNode))
-                                {
-                                    LogProcessingResult(identity, ProcessStatus.AlreadyExists);
-                                    existingEquivalentNode.Dispose();
-                                    continue;
-                                }
-
-                                packReferenceNode.AddReference();
-                                LogProcessingResult(identity, ProcessStatus.Success);
-                            }
-
+                            AddManagementPackReference(managementPack, bundlePath);
                         }
-                        catch (Exception ex)
-                        {
-                            LogProcessingResult(identity, ProcessStatus.Failed, ProcessingOp.AddingReference, ex.Message);
-                        }
-                    }
                 }
 
             }
@@ -132,6 +143,47 @@ namespace NuGet.PackageManagement.VisualStudio
             }
 #endif
         }
+
+#if VisualStudioAuthoringExtensionsInstalled
+        private void AddManagementPackReference(ManagementPack managementPack, string hintPath)
+        {
+            string identity = $"{managementPack.Name} (Version={managementPack.Version},PublicKeyToken={managementPack.KeyToken})";
+
+            try
+            {
+                if (!managementPack.Sealed)
+                {
+                    LogProcessingResult(identity, ProcessStatus.NotSealed);
+                    return;
+                }
+
+                using (var packReferenceNode = new ManagementPackReferenceNode(_projectMgr, hintPath, managementPack.Name))
+                {
+                    ReferenceNode existingEquivalentNode;
+
+                    var isAlreadyAddedInternal = (IsAlreadyAddedInternalDelegate)Delegate.CreateDelegate(
+                                                  typeof(IsAlreadyAddedInternalDelegate),
+                                                  packReferenceNode,
+                                                  _isAlreadyAddedMethodInfo);
+
+                    if (isAlreadyAddedInternal(out existingEquivalentNode))
+                    {
+                        LogProcessingResult(identity, ProcessStatus.AlreadyExists);
+                        existingEquivalentNode.Dispose();
+                        return;
+                    }
+
+                    packReferenceNode.AddReference();
+                    LogProcessingResult(identity, ProcessStatus.Success);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LogProcessingResult(identity, ProcessStatus.Failed, ProcessingOp.AddingReference, ex.Message);
+            }
+        }
+#endif
 
         private void LogProcessingResult(string identity, ProcessStatus status, ProcessingOp operation = ProcessingOp.AddingReference, string detail = null)
         {
@@ -171,14 +223,23 @@ namespace NuGet.PackageManagement.VisualStudio
 
         public override bool ReferenceExists(string name)
         {
-            if (!_isVsaeInstalled)
+            bool result = false;
+
+            try
             {
-                NuGetProjectContext.Log(MessageLevel.Info, "Visual Studio Authoring Extensions are not installed. Skipping ReferenceExists.");
-                return false;
+                dynamic reference;
+                if(TryGetExistingReference(name, out reference))
+                {
+                    result = true;
+                    (reference as IDisposable)?.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                NuGetProjectContext.Log(MessageLevel.Error, $"Unexpected exception in ReferenceExists: {ex.Message}.");
             }
 
-            dynamic reference;
-            return TryGetExistingReference(name, out reference);
+            return result;
         }
 
         private bool TryGetExistingReference(string name, out dynamic managementPackReference)
@@ -190,11 +251,15 @@ namespace NuGet.PackageManagement.VisualStudio
 
             foreach (dynamic reference in _mpReferenceContainerNode.EnumReferences())
             {
-                if (String.Equals(reference.Name, referenceName, StringComparison.OrdinalIgnoreCase))
+                if (string.Equals(reference.Name, referenceName, StringComparison.OrdinalIgnoreCase))
                 {
                     found = true;
                     managementPackReference = reference;
                     break;
+                }
+                else
+                {
+                    (reference as IDisposable)?.Dispose();
                 }
             }
 
@@ -203,32 +268,33 @@ namespace NuGet.PackageManagement.VisualStudio
 
         public override void RemoveReference(string name)
         {
-            if (!_isVsaeInstalled)
+            try
             {
-                NuGetProjectContext.Log(MessageLevel.Info, "Visual Studio Authoring Extensions are not installed. Skipping RemoveReference.");
-                return;
+                RemoveManagementPackReference(name);
             }
-
-            RemoveManagementPackReference(name);
+            catch (Exception ex)
+            {
+                NuGetProjectContext.Log(MessageLevel.Error, $"Unexpected exception in RemoveReference: {ex.Message}.");
+            }
         }
 
         private void RemoveManagementPackReference(string name)
         {
             dynamic reference;
-            if (TryGetExistingReference(name, out reference))
-            {
-                var identity = reference.Name;
-                try
+            if (TryGetExistingReference(name, out reference)) using (reference)
                 {
-                    bool shouldRemoveFromStorage = false; // the nuget folder uninstall will take care of file cleanup
-                    reference.Remove(shouldRemoveFromStorage);
-                    LogProcessingResult(identity, ProcessStatus.Success);
+                    var identity = reference.Name;
+                    try
+                    {
+                        bool shouldRemoveFromStorage = false; // the nuget folder uninstall will take care of file cleanup
+                        reference.Remove(shouldRemoveFromStorage);
+                        LogProcessingResult(identity, ProcessStatus.Success);
+                    }
+                    catch (Exception ex)
+                    {
+                        LogProcessingResult(identity, ProcessStatus.Failed, ProcessingOp.RemovingReference, ex.Message);
+                    }
                 }
-                catch (Exception ex)
-                {
-                    LogProcessingResult(identity, ProcessStatus.Failed, ProcessingOp.RemovingReference, ex.Message);
-                }
-            }
         }
 
         protected override bool IsBindingRedirectSupported
