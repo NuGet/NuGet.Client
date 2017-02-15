2c9ea7de21da6e571bfcb1f8dde501eb21bbef3a
Ankit Mishra <anmishr@microsoft.com>
2017-02-14 16:57:32 -0800

    Fixing nuget.exe download to v3.5.0

--- test/EndToEnd/NuGet.Tests.psm1
+++ test/EndToEnd/NuGet.Tests.psm1
@@ -22,9 +22,9 @@ if ((Test-Path $nugetExePath) -eq $False)
 {
     Write-Host -BackgroundColor Yellow -ForegroundColor Black 'nuget.exe cannot be found at' $nugetExePath
     Write-Host "Downloading nuget.exe"
-    wget https://dist.nuget.org/win-x86-commandline/latest-prerelease/nuget.exe -OutFile $nugetExePath
+    wget https://dist.nuget.org/win-x86-commandline/v3.5.0/nuget.exe -OutFile $nugetExePath
 }
-
+
 # Enable NuGet Test Mode
 $env:NuGetTestModeEnabled = "True"
 
