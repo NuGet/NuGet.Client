ad837e4d658fe6cddb87fac782bdf03805453a9a
Ashish Jain (NUGET) <asja@microsoft.com>
2017-03-07 12:10:07 -0800

    Revert "Load RestoreManagerPackage asynchronously with UI context auto-load"
    This reverts commit 50c2540714c6679007ca8cd81e3c9f8344014903.

--- src/NuGet.Clients/NuGet.SolutionRestoreManager/RestoreManagerPackage.cs
+++ src/NuGet.Clients/NuGet.SolutionRestoreManager/RestoreManagerPackage.cs
@@ -24,10 +24,10 @@ namespace NuGet.SolutionRestoreManager
     /// Visual Studio extension package designed to bootstrap solution restore components.
     /// Loads on solution open to attach to build events.
     /// </summary>
-    // Flag AllowsBackgroundLoading is set to True and Flag PackageAutoLoadFlags is set to BackgroundLoad
-    // which will allow this package to be loaded asynchronously
+    // Flag AllowsBackgroundLoading is set to False because switching to Main thread wiht JTF is creating
+    // performance overhead in InitializeAsync() API.
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
     [Guid(PackageGuidString)]
     public sealed class RestoreManagerPackage : AsyncPackage
     {
