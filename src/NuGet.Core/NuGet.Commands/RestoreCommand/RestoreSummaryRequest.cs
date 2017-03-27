a9de84f03a45c04223d2287c17d647a79c5d1897
scottbommarito <scottbom@microsoft.com>
2017-03-27 11:49:06 -0700

    add header to more files

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/RestoreSummaryRequest.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/RestoreSummaryRequest.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
 using NuGet.Common;
 using NuGet.Configuration;
 using NuGet.Protocol.Core.Types;
--- src/NuGet.Core/NuGet.Protocol.Core.Types/FeedTypePackageSource.cs
+++ src/NuGet.Core/NuGet.Protocol.Core.Types/FeedTypePackageSource.cs
@@ -1,4 +1,7 @@
-﻿using NuGet.Configuration;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using NuGet.Configuration;
 
 namespace NuGet.Protocol
 {
