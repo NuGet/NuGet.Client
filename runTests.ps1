9bc6277c6d71f1e61dc1339dbc28d9946e25e221
Ankit Mishra <anmishr@microsoft.com>
2017-03-31 15:36:11 -0700

    setting label to preview1

--- runTests.ps1
+++ runTests.ps1
@@ -49,7 +49,7 @@ param (
     [ValidateSet("debug", "release")]
     [Alias('c')]
     [string]$Configuration,
-    [ValidateSet("release","rtm", "rc", "rc1", "rc2", "rc3", "rc4", "beta", "beta1", "beta2", "final", "xprivate", "zlocal")]
+    [ValidateSet("release","rtm", "rc", "rc1", "rc2", "rc3", "rc4", "beta", "beta1", "beta2", "final", "preview1", "xprivate", "zlocal")]
     [Alias('l')]
     [string]$ReleaseLabel = 'zlocal',
     [Alias('n')]
