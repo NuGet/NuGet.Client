4a969c9f44c064d60eb66de75d13020d098ae0d8
Alex Panov <alpaix@outlook.com>
2017-04-06 00:44:30 -0700

    Fixed attributes of NuGet.VisualStudio.Interop
    Resolves NuGet/Home#4952.
    
    After migration assembly attributes in the `NuGet.VisualStudio.Interop` project have been broken. That led to producing an invalid assembly which TemplateWizard wouldn't discover and load correctly.
    
    Original `AssemblyInfo.cs` file is restored to resolve the issue.

---
+++ src/NuGet.Clients/NuGet.VisualStudio.Interop/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.VisualStudio.Interop")]
+[assembly: AssemblyDescription("NuGet/Visual Studio interop assembly")]
+
+// The version of this assembly should never be changed.
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("NuGet")]
+[assembly: AssemblyCopyright("Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+
+[assembly: NeutralResourcesLanguage("en-US")]
