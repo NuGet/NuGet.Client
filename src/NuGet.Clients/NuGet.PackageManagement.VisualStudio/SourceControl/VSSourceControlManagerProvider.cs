7d3ee58532f84f24efe313bba98619d1f0aef989
Alex Panov <alpaix@outlook.com>
2017-04-17 18:23:50 -0700

    Fixing TFS provider
    Resolves NuGet/Client.Engineering#29.
    
    Earlier commit @c4e0a93 rendered the mechanism of dynamic TFS assembly
    discovery irrelevant for TFS provider bootstrapping as there's only one TFS variation
    left in client code base v4.0+.
    
    This change eliminates two layers of indirection along with the dynamic
    assembly loading. Instead, `VsSourceControlManagerProvider` will
    instantiate a `DefaultTFSSourceControlManager` object in explicit fashion.
    
    All TFS functionality has been merged into `NuGet.PackageManagement.VisualStudio` project.

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/SourceControl/VSSourceControlManagerProvider.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/SourceControl/VSSourceControlManagerProvider.cs
@@ -3,9 +3,8 @@
 
 using System;
 using System.ComponentModel.Composition;
-using System.Linq;
 using EnvDTE80;
-using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft;
 using Microsoft.VisualStudio.Shell;
 using NuGet.ProjectManagement;
 using NuGet.VisualStudio;
@@ -13,25 +12,24 @@ using NuGet.VisualStudio;
 namespace NuGet.PackageManagement.VisualStudio
 {
     [Export(typeof(ISourceControlManagerProvider))]
-    internal sealed class VSSourceControlManagerProvider : ISourceControlManagerProvider
+    internal sealed class VsSourceControlManagerProvider : ISourceControlManagerProvider
     {
-        private readonly Lazy<EnvDTE.DTE> _dte;
-        private readonly Lazy<IComponentModel> _componentModel;
-
         private const string TfsProviderName = "{4CA58AB2-18FA-4F8D-95D4-32DDF27D184C}";
 
-        [ImportingConstructor]
-        public VSSourceControlManagerProvider(
-            [Import(typeof(SVsServiceProvider))]
-            IServiceProvider serviceProvider)
-        {
-            if (serviceProvider == null)
-            {
-                throw new ArgumentNullException(nameof(serviceProvider));
-            }
+        private readonly Configuration.ISettings _settings;
+        private readonly Lazy<EnvDTE.DTE> _dte;
 
-            _componentModel = new Lazy<IComponentModel>(
-                () => serviceProvider.GetComponentModel());
+        [ImportingConstructor]
+        public VsSourceControlManagerProvider(
+            [Import(typeof(SVsServiceProvider))]
+            IServiceProvider serviceProvider,
+            Configuration.ISettings vsSettings)
+        {
+            Assumes.Present(serviceProvider);
+            Assumes.Present(vsSettings);
+
+            _settings = vsSettings;
+
             _dte = new Lazy<EnvDTE.DTE>(
                 () => serviceProvider.GetDTE());
         }
@@ -39,47 +37,41 @@ namespace NuGet.PackageManagement.VisualStudio
         public SourceControlManager GetSourceControlManager()
         {
             return NuGetUIThreadHelper.JoinableTaskFactory.Run(async delegate
+            {
+                await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                if (_dte.Value.SourceControl != null)
                 {
-                    await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-
-                    if (_dte.Value.SourceControl != null)
+                    var sourceControl = (SourceControl2)_dte.Value.SourceControl;
+                    if (sourceControl != null)
                     {
-                        var sourceControl = (SourceControl2)_dte.Value.SourceControl;
-                        if (sourceControl != null)
+                        SourceControlBindings sourceControlBindings = null;
+                        try
                         {
-                            SourceControlBindings sourceControlBinding = null;
-                            try
-                            {
-                                // Get the binding for this solution
-                                sourceControlBinding = sourceControl.GetBindings(_dte.Value.Solution.FullName);
-                            }
-                            catch (NotImplementedException)
-                            {
-                                // Some source control providers don't bother to implement this.
-                                // TFS might be the only one using it
-                            }
-
-                            if (sourceControlBinding == null
-                                || String.IsNullOrEmpty(sourceControlBinding.ProviderName)
-                                ||
-                                !sourceControlBinding.ProviderName.Equals(TfsProviderName, StringComparison.OrdinalIgnoreCase))
-                            {
-                                // Return null, if the Source control provider is not TFS
-                                return null;
-                            }
-
-                            var tfsProviders = _componentModel.Value.GetExtensions<ITFSSourceControlManagerProvider>();
-                            if (tfsProviders != null
-                                && tfsProviders.Any())
-                            {
-                                return tfsProviders.Select(provider => provider.GetTFSSourceControlManager(sourceControlBinding))
-                                    .FirstOrDefault(tp => tp != null);
-                            }
+                            // Get the binding for this solution
+                            sourceControlBindings = sourceControl.GetBindings(_dte.Value.Solution.FullName);
                         }
-                    }
+                        catch (NotImplementedException)
+                        {
+                            // Some source control providers don't bother to implement this.
+                            // TFS might be the only one using it
+                        }
 
-                    return null;
-                });
+                        if (sourceControlBindings == null
+                            || string.IsNullOrEmpty(sourceControlBindings.ProviderName)
+                            ||
+                            !sourceControlBindings.ProviderName.Equals(TfsProviderName, StringComparison.OrdinalIgnoreCase))
+                        {
+                            // Return null, if the Source control provider is not TFS
+                            return null;
+                        }
+
+                        return new DefaultTFSSourceControlManager(_settings, sourceControlBindings);
+                    }
+                }
+
+                return null;
+            });
         }
     }
 }
