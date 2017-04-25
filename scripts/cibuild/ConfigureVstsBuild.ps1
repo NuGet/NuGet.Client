8a3cdb80fb21e0d2825ac50d811a54cc41250e1f
ragrawal <ragrawal@microsoft.com>
2017-04-25 12:50:45 -0700

    set vsix build number to the insertable version

--- scripts/cibuild/ConfigureVstsBuild.ps1
+++ scripts/cibuild/ConfigureVstsBuild.ps1
@@ -31,6 +31,48 @@ param
     [string]$BuildRTM
 )
 
+Function Get-Version {
+    param(
+        [string]$ProductVersion,
+        [string]$build
+    )
+        Write-Host "Evaluating the new VSIX Version : ProductVersion $ProductVersion, build $build"
+        # Generate the new minor version: 4.0.0 => 40000, 4.11.5 => 41105. 
+        # This assumes we only get to NuGet major/minor 99 at worst, otherwise the logic breaks. 
+        #The final version for NuGet 4.0.0, build number 3128 would be 15.0.40000.3128
+        $finalVersion = "15.0.$((-join ($ProductVersion -split '\.' | %{ '{0:D2}' -f ($_ -as [int]) } )).TrimStart("0")).$build"    
+    
+        Write-Host "The new VSIX Version is: $finalVersion"
+        return $finalVersion    
+}
+
+Function Update-VsixVersion {
+    param(
+        [string]$ReleaseProductVersion,
+        [string]$manifestName,
+        [int]$buildNumber
+    )
+    $vsixManifest = Join-Path $env:BUILD_REPOSITORY_LOCALPATH\src\NuGet.Clients\NuGet.VisualStudio.Client $manifestName
+
+    Write-Host "Updating the VSIX version in manifest $vsixManifest"
+
+    [xml]$xml = get-content $vsixManifest
+    $root = $xml.PackageManifest
+
+    # Reading the current version from the manifest
+    $oldVersion = $root.Metadata.Identity.Version
+    # Evaluate the new version
+    $newVersion = Get-Version $ReleaseProductVersion $buildNumber
+    Write-Host "Updating the VSIX version [$oldVersion] => [$newVersion]"
+    # setting the revision to the new version
+    $root.Metadata.Identity.Version = "$newVersion"
+
+    $xml.Save($vsixManifest)
+
+    Write-Host "Updated the VSIX version [$oldVersion] => [$($root.Metadata.Identity.Version)]"
+}
+
+$msbuildExe = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\bin\msbuild.exe'
 if ($BuildRTM -eq 'true')
 {
     # Set the $(NupkgOutputDir) build variable in VSTS build
@@ -41,7 +83,7 @@ if ($BuildRTM -eq 'true')
         $numberOfTries++
         Start-Sleep -s 15
     }
-    until ((Test-Path $BuildInfoJsonFile) -or ($numberOfTries -gt 10))
+    until ((Test-Path $BuildInfoJsonFile) -or ($numberOfTries -gt 50))
     $json = (Get-Content $BuildInfoJsonFile -Raw) | ConvertFrom-Json
     $currentBuild = [System.Decimal]::Parse($json.BuildNumber)
     # Set the $(Revision) build variable in VSTS build
@@ -63,5 +105,12 @@ else
 
     New-Item $BuildInfoJsonFile -Force
     $jsonRepresentation | ConvertTo-Json | Set-Content $BuildInfoJsonFile
-
+    $productVersion = & $msbuildExe $env:BUILD_REPOSITORY_LOCALPATH\build\config.props /v:m /nologo /t:GetSemanticVersion
+    if (-not $?)
+    {
+        Write-Error "Failed to get product version."
+        exit 1
+    }
+    Update-VsixVersion -manifestName source.extension.vs15.vsixmanifest -ReleaseProductVersion $productVersion -buildNumber $newBuildCounter
+    Update-VsixVersion -manifestName source.extension.vs15.insertable.vsixmanifest -ReleaseProductVersion $productVersion -buildNumber $newBuildCounter
 }
