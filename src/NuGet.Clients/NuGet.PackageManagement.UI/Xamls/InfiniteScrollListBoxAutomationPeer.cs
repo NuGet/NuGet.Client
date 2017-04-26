0e2d78cd07bdad0d69ccf796a355f91e2b9efbe9
scoban <sbanni@users.noreply.github.com>
2017-04-26 14:26:38 -0700

    Accessibility bug fixes (#1337)
    * Give default package management an accessible name
    
    * Don't report LoadingStatusIndicator as an item in the packages list with narrator
    
    * Set automationproperties.name for packagesource combobox
    
    * Change accessibility name for the text box when the status changes
    
    * Fix tab index order of the NuGet Package manager options page

---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBoxAutomationPeer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace NuGet.PackageManagement.UI
+{
+    internal class InfiniteScrollListBoxAutomationPeer : ListBoxAutomationPeer
+    {
+        public InfiniteScrollListBoxAutomationPeer(ListBox owner) : base(owner) { }
+
+        protected override List<AutomationPeer> GetChildrenCore()
+        {
+            // Don't return the LoadingStatusIndicator as an AutomationPeer, otherwise narrator will report it as an item in the list of packages, even when not visible
+            return base.GetChildrenCore()?.Where(lbiap => !(((ListBoxItemAutomationPeer)lbiap).Item is LoadingStatusIndicator)).ToList() ?? new List<AutomationPeer>();
+        }
+    }
+}
