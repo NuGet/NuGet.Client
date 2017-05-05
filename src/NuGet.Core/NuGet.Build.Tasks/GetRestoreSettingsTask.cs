2959316fb21dade94e82e1b473a04060c1210139
Nikolche Kolev <nikolev@microsoft.com>
2017-05-04 23:34:27 -0700

    cont...

--- src/NuGet.Core/NuGet.Build.Tasks/GetRestoreSettingsTask.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/GetRestoreSettingsTask.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -117,7 +117,7 @@ namespace NuGet.Build.Tasks
             var configFilePaths = new List<string>();
             foreach (var config in settings.Priority)
             {
-                configFilePaths.Add(config.Root + config.FileName);
+                configFilePaths.Add(Path.GetFullPath(Path.Combine(config.Root, config.FileName)));
             }
             ConfigFilePaths = configFilePaths.ToArray();
 
