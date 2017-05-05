b5007a451d7e7aa530f13727059270c6d9d3c945
Ankit Mishra <mishra14@users.noreply.github.com>
2017-05-04 18:47:17 -0700

    Adding unit tests around CollectoreLogger and RestoreLogMessage (#1364)

--- test/NuGet.Core.Tests/NuGet.Common.Test/CollectorLoggerTests.cs
+++ test/NuGet.Core.Tests/NuGet.Common.Test/CollectorLoggerTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -11,6 +11,53 @@ namespace NuGet.Common.Test
 {
     public class CollectorLoggerTests
     {
+
+        [Fact]
+        public void CollectorLogger_PassesLogMessagesToInnerLogger()
+        {
+
+            // Arrange
+            var innerLogger = new Mock<ILogger>();
+            var collector = new CollectorLogger(innerLogger.Object, LogLevel.Debug);
+
+            // Act
+            collector.Log(new RestoreLogMessage(LogLevel.Debug, "Debug"));
+            collector.Log(new RestoreLogMessage(LogLevel.Verbose, "Verbose"));
+            collector.Log(new RestoreLogMessage(LogLevel.Information, "Information"));
+            collector.Log(new RestoreLogMessage(LogLevel.Warning, "Warning"));
+            collector.Log(new RestoreLogMessage(LogLevel.Error, "Error"));
+
+            // Assert
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Debug, "Debug", Times.Once());
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Verbose, "Verbose", Times.Once());
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Information, "Information", Times.Once());
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Warning, "Warning", Times.Once());
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Error, "Error", Times.Once());
+        }
+
+        [Fact]
+        public void CollectorLogger_PassesLogCallsToInnerLogger()
+        {
+
+            // Arrange
+            var innerLogger = new Mock<ILogger>();
+            var collector = new CollectorLogger(innerLogger.Object, LogLevel.Debug);
+
+            // Act
+            collector.Log(LogLevel.Debug, "Debug");
+            collector.Log(LogLevel.Verbose, "Verbose");
+            collector.Log(LogLevel.Information, "Information");
+            collector.Log(LogLevel.Warning, "Warning");
+            collector.Log(LogLevel.Error, "Error");
+
+            // Assert
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Debug, "Debug", Times.Once());
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Verbose, "Verbose", Times.Once());
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Information, "Information", Times.Once());
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Warning, "Warning", Times.Once());
+            VerifyInnerLoggerCalls(innerLogger, LogLevel.Error, "Error", Times.Once());
+        }
+
         [Fact]
         public void CollectorLogger_PassesToInnerLogger()
         {
@@ -99,6 +146,54 @@ namespace NuGet.Common.Test
             Assert.Equal(new[] { "NU1000: ErrorA" }, errorsA.Select(e => e.FormatMessage()));
             Assert.Equal(new[] { "NU1000: ErrorA", "NU1000: ErrorB", "NU1000: ErrorC" }, errorsAbc.Select(e => e.FormatMessage()));
             Assert.Equal(new[] { "NU1000: ErrorA", "NU1000: ErrorB", "NU1000: ErrorC" }, errordEnd.Select(e => e.FormatMessage()));
+        }
+
+        [Fact]
+        public void CollectorLogger_CollectsWarnings()
+        {
+            // Arrange
+            var innerLogger = new Mock<ILogger>();
+            var collector = new CollectorLogger(innerLogger.Object);
+
+            // Act
+            var warningsStart = collector.Errors.ToArray();
+            collector.LogWarning("WarningA");
+            var warningA = collector.Errors.ToArray();
+            collector.LogWarning("WarningB");
+            collector.LogWarning("WarningC");
+            var warningAbc = collector.Errors.ToArray();
+            var warningsEnd = collector.Errors.ToArray();
+
+            // Assert
+            Assert.Empty(warningsStart);
+            Assert.Equal(new[] { "NU1500: WarningA" }, warningA.Select(e => e.FormatMessage()));
+            Assert.Equal(new[] { "NU1500: WarningA", "NU1500: WarningB", "NU1500: WarningC" }, warningAbc.Select(e => e.FormatMessage()));
+            Assert.Equal(new[] { "NU1500: WarningA", "NU1500: WarningB", "NU1500: WarningC" }, warningsEnd.Select(e => e.FormatMessage()));
+        }
+
+        [Fact]
+        public void CollectorLogger_CollectsOnlyErrorsAndWarnings()
+        {
+            // Arrange
+            var innerLogger = new Mock<ILogger>();
+            var collector = new CollectorLogger(innerLogger.Object);
+
+            // Act
+            var warningsStart = collector.Errors.ToArray();
+            collector.LogWarning("WarningA");
+            collector.LogDebug("Debug");
+            var warningA = collector.Errors.ToArray();
+            collector.LogInformation("Information");
+            collector.LogWarning("WarningB");
+            collector.LogWarning("WarningC");
+            var warningAbc = collector.Errors.ToArray();
+            var warningsEnd = collector.Errors.ToArray();
+
+            // Assert
+            Assert.Empty(warningsStart);
+            Assert.Equal(new[] { "NU1500: WarningA" }, warningA.Select(e => e.FormatMessage()));
+            Assert.Equal(new[] { "NU1500: WarningA", "NU1500: WarningB", "NU1500: WarningC" }, warningAbc.Select(e => e.FormatMessage()));
+            Assert.Equal(new[] { "NU1500: WarningA", "NU1500: WarningB", "NU1500: WarningC" }, warningsEnd.Select(e => e.FormatMessage()));
         }
 
         [Fact]
