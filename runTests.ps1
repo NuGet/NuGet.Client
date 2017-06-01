a982c4735e33b8107752faa9ade3c7a8c040e910
Justin Emgarten <justin@emgarten.com>
2017-06-01 00:04:37 -0700

    Adding preview2 and preview3 release labels for build.ps1

--- runTests.ps1
+++ runTests.ps1
@@ -43,7 +43,7 @@ param (
     [ValidateSet("debug", "release")]
     [Alias('c')]
     [string]$Configuration,
-    [ValidateSet("release","rtm", "rc", "rc1", "rc2", "rc3", "rc4", "beta", "beta1", "beta2", "final", "preview1", "xprivate", "zlocal")]
+    [ValidateSet("release","rtm", "rc", "rc1", "rc2", "rc3", "rc4", "beta", "beta1", "beta2", "final", "preview1", "preview2", "preview3", "xprivate", "zlocal")]
     [Alias('l')]
     [string]$ReleaseLabel = 'zlocal',
     [Alias('n')]
