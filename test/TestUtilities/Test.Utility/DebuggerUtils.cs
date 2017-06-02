4406e650f0ef8cdc5dd8522ec53de9c6ed8badce
Nikolche Kolev <nikolev@microsoft.com>
2017-06-01 20:21:01 -0700

    remove redundant stuff

--- test/TestUtilities/Test.Utility/DebuggerUtils.cs
+++ test/TestUtilities/Test.Utility/DebuggerUtils.cs
@@ -23,10 +23,5 @@ namespace Test.Utility
             Debugger.Launch();
 #endif
         }
-
-        public static void Launch()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
