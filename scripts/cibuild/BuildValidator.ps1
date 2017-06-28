35bba7aa77d58734dcfa5154b99ecfbd3d55bdd2
Ankit Mishra <anmishr@microsoft.com>
2017-06-27 17:53:09 -0700

    testing new localization validator

--- scripts/cibuild/BuildValidator.ps1
+++ scripts/cibuild/BuildValidator.ps1
@@ -23,14 +23,23 @@ param
 
 
 if ($BuildRTM -eq 'false')
-{    
+{   
     $NuGetClientRoot = $env:BUILD_REPOSITORY_LOCALPATH
-    $LocValidator = [System.IO.Path]::Combine($NuGetClientRoot, 'packages', 'NuGetValidator.Localization.1.2.0', 'tools', 'NuGetValidator.Localization.exe')
-    $VsixLocation = [System.IO.Path]::Combine($BuildOutputTargetPath, 'artifacts', 'VS15', 'Insertable', 'NuGet.Tools.vsix' ) 
-    $VsixExtractLocation = Join-Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY "extractedVsix"
-    $LogOutputDir = Join-Path $BuildOutputTargetPath "LocalizationValidation"
+    $NuGetValidator = [System.IO.Path]::Combine($NuGetClientRoot, 'packages', 'NuGetValidator.1.3.1', 'tools', 'NuGetValidator.exe')
+    $ArtifactsLocation = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts')
+    $VsixLocation = [System.IO.Path]::Combine($BuildOutputTargetPath, 'artifacts', 'VS15', 'Insertable', 'NuGet.Tools.vsix' )
+    $VsixExtractLocation = [System.IO.Path]::Combine($env:SYSTEM_DEFAULTWORKINGDIRECTORY, 'extractedVsix' ) 
+    $ArtifactsLogOutputDir = [System.IO.Path]::Combine($BuildOutputTargetPath, 'LocalizationValidation', 'artifacts' )
+    $VsixLogOutputDir = [System.IO.Path]::Combine($BuildOutputTargetPath, 'LocalizationValidation', 'vsix' )
     $LocalizationRepository = [System.IO.Path]::Combine($NuGetClientRoot, 'submodules', 'NuGet.Build.Localization', 'localize', 'comments', '15')
-    & $LocValidator $VsixLocation $VsixExtractLocation $LogOutputDir $LocalizationRepository
+
+    Write-Host "Running: $NuGetValidator localization --artifacts-path $ArtifactsLocation --output-path $ArtifactsLogOutputDir --comments-path $LocalizationRepository"
+    & $NuGetValidator localization --artifacts-path $ArtifactsLocation --output-path $ArtifactsLogOutputDir --comments-path $LocalizationRepository
+
+    # We no longer validate the vsix for localization. Leaving this here, in case this is needed in the future.
+    # Write-Host "\n\nRunning: $NuGetValidator localization --vsix --vsix-path $VsixLocation --vsix-extract-path $VsixExtractLocation --output-path $VsixLogOutputDir --comments-path $LocalizationRepository"
+    # & $NuGetValidator localization --vsix --vsix-path $VsixLocation --vsix-extract-path $VsixExtractLocation --output-path $VsixLogOutputDir --comments-path $LocalizationRepository
+    
     # We want to exit the process with success even if there are errors.
     exit 0
 }
