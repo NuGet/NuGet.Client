a3e5475c098108ae445d5c46f976f64ce7e14882
ragrawal <ragrawal@microsoft.com>
2017-07-11 11:43:45 -0700

    add logic to create uwp classlib > rs2

--- test/TestExtensions/API.Test/UwpProjectTestUtils.cs
+++ test/TestExtensions/API.Test/UwpProjectTestUtils.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.IO;
 using System.Reflection;
 
@@ -8,6 +9,7 @@ namespace API.Test
 {
     public class UwpProjectTestsUtil
     {
+        private static readonly Version RS2 = new Version(10, 0, 15063, 0);
         // TODO: Handle creating the right project template for TPV > RS2
         // Returns the path to the solution file
         public static string CreateUwpClassLibrary(string projectName,
@@ -24,14 +26,29 @@ namespace API.Test
             {
                 targetPlatformMinVersion = "10.0.10586.0";
             }
+
+            var tpv = new Version(targetPlatformVersion);
             Directory.CreateDirectory(Path.Combine(outputPath, projectName, projectName));
             var solutionDir = Path.Combine(outputPath, projectName);
-            FormatAndCopyTemplateFiles("AssemblyInfo.cs", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, "AssemblyInfo.cs", "Properties");
-            FormatAndCopyTemplateFiles("Class1.cs", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, "Class1.cs");
-            FormatAndCopyTemplateFiles("project.json", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, "project.json");
-            FormatAndCopyTemplateFiles("projectjsonbaseduwpproject.csproj", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, projectName + ".csproj");
-            FormatAndCopyTemplateFiles("project.rd.xml", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, projectName + ".rd.xml", "Properties");
+
             FormatAndCopyTemplateFiles("project.sln", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, projectName + ".sln");
+            FormatAndCopyTemplateFiles("project.rd.xml", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, projectName + ".rd.xml", "Properties");
+            FormatAndCopyTemplateFiles("Class1.cs", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, "Class1.cs");
+            FormatAndCopyTemplateFiles("AssemblyInfo.cs", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, "AssemblyInfo.cs", "Properties");
+            if (tpv >= RS2)
+            {
+                FormatAndCopyTemplateFiles("packagerefbaseduwpproject.csproj", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, projectName + ".csproj");
+            }
+            else
+            {                
+                FormatAndCopyTemplateFiles("project.json", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, "project.json");
+                FormatAndCopyTemplateFiles("projectjsonbaseduwpproject.csproj", projectName, solutionDir, targetPlatformVersion, targetPlatformMinVersion, projectName + ".csproj");
+                
+                
+            }
+
+            
+            
             return Path.Combine(solutionDir, projectName + ".sln");
         }
 
