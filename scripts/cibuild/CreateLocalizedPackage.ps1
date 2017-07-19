9bfd01317397d065d2f997a42702b6cf30e72110
Ankit Mishra <anmishr@microsoft.com>
2017-07-18 17:13:17 -0700

    Testing ps1 with buildRTM true and false

--- scripts/cibuild/CreateLocalizedPackage.ps1
+++ scripts/cibuild/CreateLocalizedPackage.ps1
@@ -18,22 +18,28 @@ param
     [string]$Version
 )
 
-
+# Declare the output path based on the build type
 if ($BuildRTM -eq 'false')
 {
-
-    $NuGetClientRoot = $env:BUILD_REPOSITORY_LOCALPATH
-    $LocProjPath = [System.IO.Path]::Combine($NuGetClientRoot, 'build', 'loc.proj')
-    $NuGetExe = [System.IO.Path]::Combine($NuGetClientRoot, '.nuget', 'nuget.exe')
-    $LocalizationNuspec = [System.IO.Path]::Combine($NuGetClientRoot, 'build', 'Nuspec', 'NuGet.Localization.nuspec')
-    $LocalizedFiles = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts', 'LocalizedFiles')
     $OutputPath = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts', 'nupkgs')
-
-    # 1. Move the localized files into a common location.
-    Write-Host "Running: $MSBuildExe $LocProjPath /t:MoveLocalizedFilesToLocalizedArtifacts"
-    & $MSBuildExe $LocProjPath /t:MoveLocalizedFilesToLocalizedArtifacts
-
-    # 2. Pack the localization package nuspec
-    Write-Host "Running: $NuGetExe pack $LocalizationNuspec -properties Version=3.0.0`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $OutputPath"
-    & $NuGetExe pack $LocalizationNuspec -properties Version=$Version`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $OutputPath
 }
+else
+{
+    $OutputPath = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts', 'ReleaseNupkgs')
+}
+
+# Declare common variables
+$NuGetClientRoot = $env:BUILD_REPOSITORY_LOCALPATH
+$LocProjPath = [System.IO.Path]::Combine($NuGetClientRoot, 'build', 'loc.proj')
+$NuGetExe = [System.IO.Path]::Combine($NuGetClientRoot, '.nuget', 'nuget.exe')
+$LocalizationNuspec = [System.IO.Path]::Combine($NuGetClientRoot, 'build', 'Nuspec', 'NuGet.Localization.nuspec')
+$LocalizedFiles = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts', 'LocalizedFiles')
+
+
+# 1. Move the localized files into a common location.
+Write-Host "Running: $MSBuildExe $LocProjPath /t:MoveLocalizedFilesToLocalizedArtifacts"
+& $MSBuildExe $LocProjPath /t:MoveLocalizedFilesToLocalizedArtifacts
+
+# 2. Pack the localization package nuspec
+Write-Host "Running: $NuGetExe pack $LocalizationNuspec -properties Version=3.0.0`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $OutputPath"
+& $NuGetExe pack $LocalizationNuspec -properties Version=$Version`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $OutputPath
