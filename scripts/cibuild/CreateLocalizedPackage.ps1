a9899a1bb9ccba873887624e6995c08b3c18df25
Ankit Mishra <anmishr@microsoft.com>
2017-07-20 17:46:15 -0700

    cleaning up and adding the right build number

--- scripts/cibuild/CreateLocalizedPackage.ps1
+++ scripts/cibuild/CreateLocalizedPackage.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-Creates the common localization package for NuGet.
+Creates the common localization package for NuGet packages except NuGet.CommandLine.nupkg.
 
 .DESCRIPTION
 This script is used to create the common localization package for NuGet.
@@ -8,6 +8,11 @@ This script is used to create the common localization package for NuGet.
 .PARAMETER BuildRTM
 True/false depending on whether nupkgs are being with or without the release labels.
 
+.PARAMETER BuildConfiguration
+Build Configuration used for finding the localized files.
+
+.PARAMETER BuildNumber
+Build Number used for creating the right package version for pre release package.
 #>
 
 param
@@ -17,33 +22,52 @@ param
     [Parameter(Mandatory=$True)]
     [string]$BuildConfiguration,
     [Parameter(Mandatory=$True)]
-    [string]$Version
+    [string]$BuildNumber
 )
 
-# Declare the output path based on the build type
+# Localization is not done for $BuildRTM = 'true'
+# Here we pack for relelase and prerelease
 if ($BuildRTM -eq 'false')
 {
-    $OutputPath = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts', 'nupkgs')
+    #Same as config.props
+    $PackageReleaseVersion = "4.3.0"
+    $ReleaseLabel = "rtm"
 
-    # Declare common variables
     $NuGetClientRoot = $env:BUILD_REPOSITORY_LOCALPATH
     $LocProjPath = [System.IO.Path]::Combine($NuGetClientRoot, 'build', 'loc.proj')
     $NuGetExe = [System.IO.Path]::Combine($NuGetClientRoot, '.nuget', 'nuget.exe')
     $LocalizationNuspec = [System.IO.Path]::Combine($NuGetClientRoot, 'build', 'Nuspec', 'NuGet.Localization.nuspec')
     $LocalizedFiles = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts', 'LocalizedFiles')
+    $NupkgsOutputPath = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts', 'nupkgs')
+    $ReleaseNupkgsOutputPath = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts', 'releasenupkgs')
+
+    # Safe for VSTS CI machines
     $MSBuildExe = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\bin\msbuild.exe'
 
+    # Define package versions for pre release and release
+    $PackageVersion = "$PackageReleaseVersion-$ReleaseLabel-$BuildNumber"
+    if ($ReleaseLabel -Ne 'rtm') 
+    {
+        $ReleasePackageVersion = "$PackageReleaseVersion-$ReleaseLabel"
+    } 
+    else 
+    {
+        $ReleasePackageVersion = "$PackageReleaseVersion"
+    }
 
     # 1. Move the localized files into a common location.
     Write-Host "Running: $MSBuildExe $LocProjPath /t:MoveLocalizedFilesToLocalizedArtifacts /p:Configuration=$BuildConfiguration"
     & $MSBuildExe $LocProjPath /t:MoveLocalizedFilesToLocalizedArtifacts /p:Configuration=$BuildConfiguration
 
+    # 2. If any localized paths exist then Pack the localization package nuspec
     if ( Test-Path $LocalizedFiles ) 
     {
-        # 2. If any localized paths exist then Pack the localization package nuspec
-        Write-Host "Running: $NuGetExe pack $LocalizationNuspec -properties Version=$Version`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $OutputPath"
-        & $NuGetExe pack $LocalizationNuspec -properties Version=$Version`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $OutputPath
+        # Build pre release version
+        Write-Host "Running: $NuGetExe pack $LocalizationNuspec -properties Version=$PackageVersion`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $NupkgsOutputPath"
+        & $NuGetExe pack $LocalizationNuspec -properties Version=$PackageVersion`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $NupkgsOutputPath
 
-        Copy-Item $OutputPath\* "\\nuget\nuget\Share\Users\anmishr"
+        # Build release version
+        Write-Host "Running: $NuGetExe pack $LocalizationNuspec -properties Version=$ReleasePackageVersion`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $ReleaseNupkgsOutputPath"
+        & $NuGetExe pack $LocalizationNuspec -properties Version=$ReleasePackageVersion`;LocalizationFilesDirectory=$LocalizedFiles -OutputDirectory $ReleaseNupkgsOutputPath
     }
 }
