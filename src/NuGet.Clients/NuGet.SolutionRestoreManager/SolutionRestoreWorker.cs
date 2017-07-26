9cdef6ad86ca58d5a23cfd09a3a42a4ceaf006e0
Zhi Li <zhili128@outlook.com>
2017-07-26 10:06:02 -0700

    fix build error

--- src/NuGet.Clients/NuGet.SolutionRestoreManager/SolutionRestoreWorker.cs
+++ src/NuGet.Clients/NuGet.SolutionRestoreManager/SolutionRestoreWorker.cs
@@ -310,7 +310,7 @@ namespace NuGet.SolutionRestoreManager
         {
             // get all build integrated based nuget projects and delete the cache file.
             await Task.WhenAll(
-                SolutionManager.GetNuGetProjects().OfType<BuildIntegratedNuGetProject>().Select(async e =>
+                (await SolutionManager.GetNuGetProjectsAsync()).OfType<BuildIntegratedNuGetProject>().Select(async e =>
                     Common.FileUtility.Delete(await e.GetCacheFilePathAsync())));
 
             Interlocked.Exchange(ref _restoreJobContext, new SolutionRestoreJobContext());
