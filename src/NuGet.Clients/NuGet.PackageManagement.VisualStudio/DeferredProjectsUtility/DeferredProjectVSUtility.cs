5ea968a675abde7053ad40776ef5dc373fa5165f
Zhi Li <zhili128@outlook.com>
2017-07-27 21:50:41 -0700

    fix build for dev14

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/DeferredProjectsUtility/DeferredProjectVSUtility.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/DeferredProjectsUtility/DeferredProjectVSUtility.cs
@@ -41,7 +41,7 @@ namespace NuGet.PackageManagement.VisualStudio
         {
 #if VS14
             // for Dev14 always return false since DPL not exists there.
-            return await Task.FromResult(false);
+            return Task.FromResult(false);
 #else
             return NuGetUIThreadHelper.JoinableTaskFactory.Run(async () =>
             {
