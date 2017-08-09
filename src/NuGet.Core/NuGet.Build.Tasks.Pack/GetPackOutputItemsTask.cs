66f2bffff2b16eea50470a32a572b28fadf0f0fc
ragrawal <ragrawal@microsoft.com>
2017-08-08 18:22:52 -0700

    fix bugs

--- src/NuGet.Core/NuGet.Build.Tasks.Pack/GetPackOutputItemsTask.cs
+++ src/NuGet.Core/NuGet.Build.Tasks.Pack/GetPackOutputItemsTask.cs
@@ -19,11 +19,6 @@ namespace NuGet.Build.Tasks.Pack
     public class GetPackOutputItemsTask : Task
     {
         [Required]
-        public bool IncludeSymbols { get; set; }
-
-        public bool IncludeSource { get; set; }
-
-        [Required]
         public string PackageId { get; set; }
 
         [Required]
@@ -54,19 +49,16 @@ namespace NuGet.Build.Tasks.Pack
 
             var nupkgFileName = PackCommandRunner.GetOutputFileName(PackageId, version, isNupkg: true, symbols: false);
             var nuspecFileName = PackCommandRunner.GetOutputFileName(PackageId, version, isNupkg: false, symbols: false);
+            var nupkgSymbolsFileName = PackCommandRunner.GetOutputFileName(PackageId, version, isNupkg: true, symbols: true);
+            var nuspecSymbolsFileName = PackCommandRunner.GetOutputFileName(PackageId, version, isNupkg: false, symbols: true);
 
             var outputs = new List<ITaskItem>();
             outputs.Add(new TaskItem(Path.Combine(PackageOutputPath, nupkgFileName)));
             outputs.Add(new TaskItem(Path.Combine(NuspecOutputPath, nuspecFileName)));
+            
 
-            if (IncludeSource || IncludeSymbols)
-            {
-                var nupkgSymbolsFileName = PackCommandRunner.GetOutputFileName(PackageId, version, isNupkg: true, symbols: true);
-                var nuspecSymbolsFileName = PackCommandRunner.GetOutputFileName(PackageId, version, isNupkg: false, symbols: true);
-                
-                outputs.Add(new TaskItem(Path.Combine(PackageOutputPath, nupkgSymbolsFileName)));
-                outputs.Add(new TaskItem(Path.Combine(NuspecOutputPath, nuspecSymbolsFileName)));
-            }
+            outputs.Add(new TaskItem(Path.Combine(PackageOutputPath, nupkgSymbolsFileName)));
+            outputs.Add(new TaskItem(Path.Combine(NuspecOutputPath, nuspecSymbolsFileName)));
 
             OutputPackItems = outputs.ToArray();
             return true;
