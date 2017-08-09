d638b8717d1b5d7b629753edd95207cf0bfc2aa5
Zhi Li <zhili128@outlook.com>
2017-08-09 15:36:29 -0700

    Fixed OSX check issue

--- src/NuGet.Core/NuGet.Common/RuntimeEnvironmentHelper.cs
+++ src/NuGet.Core/NuGet.Common/RuntimeEnvironmentHelper.cs
@@ -1,13 +1,17 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Runtime.InteropServices;
 
 namespace NuGet.Common
 {
     public static class RuntimeEnvironmentHelper
     {
         private static Lazy<bool> _isMono = new Lazy<bool>(() => Type.GetType("Mono.Runtime") != null);
+
+        [DllImport("libc")]
+        static extern int uname(IntPtr buf);
 
         public static bool IsDev14 { get; set; }
 
@@ -48,8 +52,35 @@ namespace NuGet.Common
 
                 return false;
 #else
-                var platform = (int)Environment.OSVersion.Platform;
-                return platform == 6;
+                var buf = IntPtr.Zero;
+
+                try
+                {
+                    buf = Marshal.AllocHGlobal(8192);
+
+                    // This is a hacktastic way of getting sysname from uname ()
+                    if (uname(buf) == 0)
+                    {
+                        var os = Marshal.PtrToStringAnsi(buf);
+
+                        if (os == "Darwin")
+                        {
+                            return true;
+                        }
+                    }
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    if (buf != IntPtr.Zero)
+                    {
+                        Marshal.FreeHGlobal(buf);
+                    }
+                }
+
+                return false;
 #endif
             }
         }
