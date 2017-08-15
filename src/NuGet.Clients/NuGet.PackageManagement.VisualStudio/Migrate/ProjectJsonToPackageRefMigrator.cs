e7cea91f8a3508e720fa6ebdc286a1f2338191a8
Nikolche Kolev <nikolev@microsoft.com>
2017-08-15 16:01:27 -0700

    revert migrator

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Migrate/ProjectJsonToPackageRefMigrator.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Migrate/ProjectJsonToPackageRefMigrator.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -117,7 +117,7 @@ namespace NuGet.PackageManagement.VisualStudio
                 buildProject.RemoveItem(projectJsonItem);
             }
         }
-
+        
 
         private static async Task CreateBackupAsync(
             BuildIntegratedNuGetProject project,
