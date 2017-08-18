90a42ba5b590317cdb6dd438cb157c044c94fe64
Nikolche Kolev <nikolev@microsoft.com>
2017-08-18 14:30:19 -0700

    Change template to net46, add non deferred tests as well

--- test/EndToEnd/tests/BuildIntegratedTest.ps1
+++ test/EndToEnd/tests/BuildIntegratedTest.ps1
@@ -589,3 +589,26 @@ function Test-BuildIntegratedLegacyRebuildDoesNotDeleteCacheFile {
     #Assert
     Assert-ProjectCacheFileExists $project
 }
+
+function Test-BuildIntegratedProjectGetPackageTransitive {
+    [SkipTestForVS14()]
+    param($Context, $TestCase)
+
+    $projectR = New-Project $TestCase.ProjectTemplate
+    $projectT = New-Project BuildIntegratedClassLibrary
+
+    $projectT | Add-ProjectReference -ProjectTo $projectR
+    $projectR | Install-Package NuGet.Versioning -Version 1.0.7
+    Clean-Solution
+
+    $projectT = $projectT | Select-Object UniqueName, ProjectName, FullName
+
+    # Act (Restore)
+    Build-Solution
+
+    Assert-ProjectJsonLockFilePackage $projectT NuGet.Versioning 1.0.7
+}
+
+function TestCases-BuildIntegratedProjectGetPackageTransitive{
+    BuildProjectTemplateTestCases 'PackageReferenceClassLibrary', 'BuildIntegratedClassLibrary'
+}
