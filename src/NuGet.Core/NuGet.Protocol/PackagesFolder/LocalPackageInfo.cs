784049175a1ebdf347b7150cc1dbad3a8b1b7149
Justin Emgarten <justin@emgarten.com>
2017-08-31 19:00:17 -0700

    Cache library file lists and reduce pattern matching allocations
    * Cache library files across projects during restore to avoid multiple disk hits.
    * Parse pattern expressions once instead of during each pattern check.

--- src/NuGet.Core/NuGet.Protocol/PackagesFolder/LocalPackageInfo.cs
+++ src/NuGet.Core/NuGet.Protocol/PackagesFolder/LocalPackageInfo.cs
@@ -1,7 +1,9 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NuGet.Packaging;
 using NuGet.Versioning;
 
@@ -10,6 +12,7 @@ namespace NuGet.Repositories
     public class LocalPackageInfo
     {
         private readonly Lazy<NuspecReader> _nuspec;
+        private readonly Lazy<IReadOnlyList<string>> _files;
 
         public LocalPackageInfo(
             string packageId,
@@ -25,6 +28,7 @@ namespace NuGet.Repositories
             ManifestPath = manifestPath;
             ZipPath = zipPath;
             _nuspec = nuspec;
+            _files = new Lazy<IReadOnlyList<string>>(() => GetFiles());
         }
 
         public string Id { get; }
@@ -43,9 +47,52 @@ namespace NuGet.Repositories
         /// </summary>
         public NuspecReader Nuspec => _nuspec.Value;
 
+        /// <summary>
+        /// Package files with OPC files filtered out.
+        /// Cached to avoid reading the same files multiple times.
+        /// </summary>
+        public IReadOnlyList<string> Files => _files.Value;
+
         public override string ToString()
         {
             return Id + " " + Version + " (" + (ManifestPath ?? ZipPath) + ")";
+        }
+
+        /// <summary>
+        /// Read files from a package folder.
+        /// </summary>
+        private IReadOnlyList<string> GetFiles()
+        {
+            using (var packageReader = new PackageFolderReader(ExpandedPath))
+            {
+                // Get package files, excluding directory entries and OPC files
+                // This is sorted before it is written out
+                return packageReader.GetFiles()
+                    .Where(file => IsAllowedLibraryFile(file))
+                    .ToList();
+            }
+        }
+
+        /// <summary>
+        /// True if the file should be added to the lock file library
+        /// Fale if it is an OPC file or empty directory
+        /// </summary>
+        private static bool IsAllowedLibraryFile(string path)
+        {
+            switch (path)
+            {
+                case "_rels/.rels":
+                case "[Content_Types].xml":
+                    return false;
+            }
+
+            if (path.EndsWith("/", StringComparison.Ordinal)
+                || path.EndsWith(".psmdcp", StringComparison.Ordinal))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
