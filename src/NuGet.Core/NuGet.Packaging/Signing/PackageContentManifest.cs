491b921255f675886792843fbcaac4ffa66a7e59
Justin Emgarten <justin@emgarten.com>
2017-10-14 01:08:14 -0700

    Adding PackageContentManifest reader and writer

--- src/NuGet.Core/NuGet.Packaging/Signing/PackageContentManifest.cs
+++ src/NuGet.Core/NuGet.Packaging/Signing/PackageContentManifest.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using NuGet.Shared;
 using NuGet.Versioning;
 
 namespace NuGet.Packaging.Signing
@@ -15,12 +16,12 @@ namespace NuGet.Packaging.Signing
     /// </summary>
     public sealed class PackageContentManifest
     {
-        private readonly Stream _stream;
-
         /// <summary>
         /// Current default manifest version.
         /// </summary>
         public static readonly SemanticVersion DefaultVersion = new SemanticVersion(1, 0, 0);
+        private static readonly Encoding ManifestEncoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+        private const int MaxSize = 1024 * 1024;
 
         /// <summary>
         /// Manifest format version.
@@ -32,11 +33,25 @@ namespace NuGet.Packaging.Signing
         /// </summary>
         public HashAlgorithm HashAlgorithm { get; }
 
-        private PackageContentManifest(SemanticVersion version, HashAlgorithm hashAlgorithm, Stream stream)
+        /// <summary>
+        /// All content entries from the package excluding signing files.
+        /// </summary>
+        public IReadOnlyList<PackageContentManifestFileEntry> PackageEntries { get; }
+
+        /// <summary>
+        /// Create a PackageContentManifest.
+        /// </summary>
+        /// <param name="version">Manifest format version.</param>
+        /// <param name="hashAlgorithm">Hash algorithm used to hash package entries.</param>
+        /// <param name="packageEntries">All entries in the package except signing files.</param>
+        public PackageContentManifest(
+            SemanticVersion version,
+            HashAlgorithm hashAlgorithm,
+            IEnumerable<PackageContentManifestFileEntry> packageEntries)
         {
             HashAlgorithm = hashAlgorithm;
             Version = version ?? throw new ArgumentNullException(nameof(version));
-            _stream = stream ?? throw new ArgumentNullException(nameof(stream));
+            PackageEntries = packageEntries?.AsList().AsReadOnly() ?? throw new ArgumentNullException(nameof(packageEntries));
         }
 
         /// <summary>
@@ -44,7 +59,21 @@ namespace NuGet.Packaging.Signing
         /// </summary>
         public void Save(Stream stream)
         {
-            throw new NotImplementedException();
+            using (var writer = new StreamWriter(stream, ManifestEncoding, bufferSize: 8192, leaveOpen: true))
+            {
+                // Write headers
+                WriteItem(writer, ManifestConstants.Version, Version.ToNormalizedString());
+                WriteItem(writer, ManifestConstants.HashAlgorithm, HashAlgorithm.ToString().ToUpperInvariant());
+
+                // Order entries
+                var entries = PackageEntries.OrderBy(e => e.Path, StringComparer.Ordinal);
+
+                // Write entries to the manifest
+                foreach (var entry in entries)
+                {
+                    WritePackageEntry(writer, entry);
+                }
+            }
         }
 
         /// <summary>
@@ -52,32 +81,187 @@ namespace NuGet.Packaging.Signing
         /// </summary>
         public static PackageContentManifest Load(Stream stream)
         {
-            throw new NotImplementedException();
-        }
+            SemanticVersion version = null;
+            var hashAlgorithm = HashAlgorithm.Unknown;
+            var entries = new List<PackageContentManifestFileEntry>();
 
-        /// <summary>
-        /// Create a PackageContentManifest.
-        /// </summary>
-        public static PackageContentManifest Create(HashAlgorithm hashAlgorithm, IEnumerable<PackageContentManifestFileEntry> fileEntries, SemanticVersion version)
-        {
-            var stream = new MemoryStream();
-            using (var writer = new StreamWriter(stream, Encoding.UTF8, bufferSize: 8192, leaveOpen: true))
+            if (stream.Length > MaxSize)
             {
-                WriteItem(writer, "Version", version.ToNormalizedString());
-                WriteItem(writer, "Hash-Algorithm", version.ToNormalizedString());
+                throw new SignatureException("Manifest file is too large.");
             }
 
-            stream.Position = 0;
+            using (var reader = new StreamReader(stream, ManifestEncoding, detectEncodingFromByteOrderMarks: false))
+            {
+                var state = ReaderState.Version;
+                string path = null;
 
-            return new PackageContentManifest(version, hashAlgorithm, stream);
+                var line = reader.ReadLine();
+                while (line != null)
+                {
+                    switch (state)
+                    {
+                        case ReaderState.Version:
+                            version = ReadVersion(version, line);
+                            state = ReaderState.HashAlgorithm;
+                            break;
+                        case ReaderState.HashAlgorithm:
+                            hashAlgorithm = ReadHashAlgorithm(hashAlgorithm, line);
+                            state = ReaderState.Empty;
+                            break;
+                        case ReaderState.Empty:
+                            ReadNewLine(line);
+                            state = ReaderState.Path;
+                            break;
+                        case ReaderState.Path:
+                            path = GetValueOrThrow(line, ManifestConstants.Path);
+                            state = ReaderState.HashValue;
+                            break;
+                        case ReaderState.HashValue:
+                            var hashValue = GetValueOrThrow(line, ManifestConstants.HashValue);
+                            entries.Add(new PackageContentManifestFileEntry(path, hashValue));
+                            path = null;
+                            state = ReaderState.Empty;
+                            break;
+                    }
+
+                    line = reader.ReadLine();
+                }
+
+                // Verify that the manifest ended at a stopping point.
+                if (state != ReaderState.Empty)
+                {
+                    ThrowInvalidFormat();
+                }
+            }
+
+            return new PackageContentManifest(version, hashAlgorithm, entries);
         }
 
         /// <summary>
-        /// Write key:value to the manifest stream.
+        /// Get the hash algorithm and ensure that it is valid.
+        /// </summary>
+        private static HashAlgorithm ReadHashAlgorithm(HashAlgorithm hashAlgorithm, string line)
+        {
+            var hashAlgorithmString = GetValueOrThrow(line, ManifestConstants.HashAlgorithm);
+
+            if (Enum.TryParse<HashAlgorithm>(hashAlgorithmString, ignoreCase: false, result: out var parsedHashAlgorithm)
+                && parsedHashAlgorithm != HashAlgorithm.Unknown)
+            {
+                hashAlgorithm = parsedHashAlgorithm;
+            }
+            else
+            {
+                ThrowInvalidFormat();
+            }
+
+            return hashAlgorithm;
+        }
+
+        /// <summary>
+        /// Get the manifest version and ensure that it is 1.0.0.
+        /// </summary>
+        private static SemanticVersion ReadVersion(SemanticVersion version, string line)
+        {
+            var versionString = GetValueOrThrow(line, ManifestConstants.Version);
+
+            // 1.0.0 is only allowed version
+            if (StringComparer.Ordinal.Equals(versionString, DefaultVersion.ToNormalizedString()))
+            {
+                version = DefaultVersion;
+            }
+            else
+            {
+                ThrowInvalidFormat();
+            }
+
+            return version;
+        }
+
+        /// <summary>
+        /// Read a new line, throw if something else is present.
+        /// </summary>
+        private static void ReadNewLine(string line)
+        {
+            if (line == null || line.Length > 0)
+            {
+                ThrowInvalidFormat();
+            }
+        }
+
+        /// <summary>
+        /// Read a key value pair from the manifest.
+        /// </summary>
+        /// <param name="line">Manifest line.</param>
+        /// <param name="key">Expected key name.</param>
+        /// <returns>Value of the entry.</returns>
+        private static string GetValueOrThrow(string line, string key)
+        {
+            string value = null;
+
+            if (line != null)
+            {
+                var pos = line.IndexOf(':');
+
+                // Verify that : exists
+                if (pos > 0)
+                {
+                    // Verify the key is the expected name.
+                    var actualKey = line.Substring(0, pos);
+                    if (StringComparer.Ordinal.Equals(key, actualKey))
+                    {
+                        // Read the rest of the string as the value.
+                        value = line.Substring(pos + 1);
+                    }
+                }
+            }
+
+            // fail if anything is out of place
+            if (value == null)
+            {
+                ThrowInvalidFormat();
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Fail due to an invalid manifest format.
+        /// </summary>
+        private static void ThrowInvalidFormat()
+        {
+            throw new SignatureException("Invalid signing manifest format");
+        }
+
+        /// <summary>
+        /// Write a package entry with a package path and hash.
+        /// </summary>
+        private static void WritePackageEntry(TextWriter writer, PackageContentManifestFileEntry entry)
+        {
+            // Write a blank line to start a new section.
+            WriteEOL(writer);
+
+            // Path:file
+            WriteItem(writer, ManifestConstants.Path, entry.Path);
+
+            // Hash-Value:hash
+            WriteItem(writer, ManifestConstants.HashValue, entry.Hash);
+        }
+
+        /// <summary>
+        /// Write key:value with EOL to the manifest stream.
         /// </summary>
         private static void WriteItem(TextWriter writer, string key, string value)
         {
-            writer.WriteLine(FormatItem(key, value));
+            writer.Write(FormatItem(key, value));
+            WriteEOL(writer);
+        }
+
+        /// <summary>
+        /// Write an end of line to the manifest writer.
+        /// </summary>
+        private static void WriteEOL(TextWriter writer)
+        {
+            writer.Write(ManifestConstants.LF);
         }
 
         /// <summary>
@@ -103,8 +287,21 @@ namespace NuGet.Packaging.Signing
             public const string Version = nameof(Version);
             public const string HashAlgorithm = "Hash-Algorithm";
             public const string HashValue = "Hash-Value";
-            public const string Path = "Path";
-            public const string EOL = "\n";
+            public const string Path = nameof(Path);
+            public const string LF = "\n";
+        }
+
+        /// <summary>
+        /// Manifest reader state. This represents the expected
+        /// entry for the line.
+        /// </summary>
+        private enum ReaderState
+        {
+            Version,
+            HashAlgorithm,
+            Empty,
+            Path,
+            HashValue
         }
     }
 }
--- test/NuGet.Core.Tests/NuGet.Packaging.Test/SigningTests/PackageContentManifestTests.cs
+++ test/NuGet.Core.Tests/NuGet.Packaging.Test/SigningTests/PackageContentManifestTests.cs
@@ -3,7 +3,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using FluentAssertions;
+using NuGet.Common;
 using NuGet.Packaging.Signing;
 using NuGet.Versioning;
 using Xunit;
@@ -18,8 +21,144 @@ namespace NuGet.Packaging.Test.SigningTests
             var version = new SemanticVersion(1, 0, 0);
             var files = new List<PackageContentManifestFileEntry>();
 
-            var manifest = PackageContentManifest.Create(version, HashAlgorithm.SHA256, files);
+            var manifest = new PackageContentManifest(version, HashAlgorithm.SHA256, files);
 
+            manifest.Version.ToNormalizedString().Should().Be("1.0.0");
+            manifest.HashAlgorithm.Should().Be(HashAlgorithm.SHA256);
+            manifest.PackageEntries.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void PackageContentManifest_CreateManifestWithNoFilesRoundTripVerifyResult()
+        {
+            var version = new SemanticVersion(1, 0, 0);
+            var files = new List<PackageContentManifestFileEntry>();
+
+            var manifest = new PackageContentManifest(version, HashAlgorithm.SHA256, files);
+
+            using (var stream = new MemoryStream())
+            {
+                // Write
+                manifest.Save(stream);
+                stream.Position = 0;
+
+                // Read
+                var manifest2 = PackageContentManifest.Load(stream);
+
+                // Verify
+                manifest2.Version.ToNormalizedString().Should().Be("1.0.0");
+                manifest2.HashAlgorithm.Should().Be(HashAlgorithm.SHA256);
+                manifest2.PackageEntries.Should().BeEmpty();
+            }
+        }
+
+        [Fact]
+        public void PackageContentManifest_CreateManifestVerifyNoByteOrderMarkingOnFile()
+        {
+            var version = new SemanticVersion(1, 0, 0);
+            var files = new List<PackageContentManifestFileEntry>();
+
+            var manifest = new PackageContentManifest(version, HashAlgorithm.SHA256, files);
+
+            using (var stream = new MemoryStream())
+            {
+                manifest.Save(stream);
+                stream.Position = 0;
+
+                stream.ReadByte().Should().Be(86, "The file should start with V. If 239 is found then it is a BOM.");
+            }
+        }
+
+        [Fact]
+        public void PackageContentManifest_CreateManifestWithFilesVerifySameResultsAfterRoundTrip()
+        {
+            var version = new SemanticVersion(1, 0, 0);
+            var files = new List<PackageContentManifestFileEntry>
+            {
+                new PackageContentManifestFileEntry("lib/net45/a.dll", "abc"),
+                new PackageContentManifestFileEntry("lib/net46/a.dll", "xyz")
+            };
+
+            var manifest = new PackageContentManifest(version, HashAlgorithm.SHA256, files);
+
+            using (var stream = new MemoryStream())
+            {
+                // Write
+                manifest.Save(stream);
+                stream.Position = 0;
+
+                // Read
+                var manifest2 = PackageContentManifest.Load(stream);
+
+                // Verify
+                manifest2.Version.ToNormalizedString().Should().Be("1.0.0");
+                manifest2.HashAlgorithm.Should().Be(HashAlgorithm.SHA256);
+                manifest2.PackageEntries.ShouldBeEquivalentTo(files);
+            }
+        }
+
+        [Fact]
+        public void PackageContentManifest_CreateManifestWithFilesVerifyHashIsConsistent()
+        {
+            var version = new SemanticVersion(1, 0, 0);
+            var files = new List<PackageContentManifestFileEntry>
+            {
+                new PackageContentManifestFileEntry("lib/net45/a.dll", "abc"),
+                new PackageContentManifestFileEntry("lib/net46/a.dll", "xyz")
+            };
+
+            var manifest = new PackageContentManifest(version, HashAlgorithm.SHA256, files);
+
+            using (var stream = new MemoryStream())
+            {
+                // Write
+                manifest.Save(stream);
+                stream.Position = 0;
+
+                var hashProvider = new CryptoHashProvider("SHA512");
+                var hash = Convert.ToBase64String(hashProvider.CalculateHash(stream));
+                hash.Should().Be("0IkgJ0iyymWMdvg3LQPjKlNeh0tWLFpAqfpciaNtdRe6UnWCDtCPFQMFcSfqbKsdA30iJAf3eIKgCrI17Tid9w==");
+            }
+        }
+
+        [Fact]
+        public void PackageContentManifest_CreateManifestWithFilesVerifyFileContent()
+        {
+            var version = new SemanticVersion(1, 0, 0);
+            var files = new List<PackageContentManifestFileEntry>
+            {
+                new PackageContentManifestFileEntry("lib/net45/a.dll", "abc"),
+                new PackageContentManifestFileEntry("lib/net46/a.dll", "xyz")
+            };
+
+            var manifest = new PackageContentManifest(version, HashAlgorithm.SHA256, files);
+
+            using (var stream = new MemoryStream())
+            {
+                // Write
+                manifest.Save(stream);
+                stream.Position = 0;
+
+                var content = stream.ReadToEnd();
+
+                var expected = @"Version:1.0.0
+Hash-Algorithm:SHA256
+
+Path:lib/net45/a.dll
+Hash-Value:abc
+
+Path:lib/net46/a.dll
+Hash-Value:xyz
+";
+
+                var expectedLines = expected.Split('\n');
+                var contentLines = content.Split('\n');
+
+                for(var i=0; i < expectedLines.Length && i < contentLines.Length; i++)
+                {
+                    contentLines[i].Trim().Should().Be(expectedLines[i].Trim());
+                }
+            }
         }
     }
 }
