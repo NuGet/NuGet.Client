c054c8fa1f3c9ec8bd09368d6e1f54ae02df9faf
Ankit Mishra <anmishr@microsoft.com>
2017-11-02 12:04:36 -0700

    Fixing a build break after merging dev

--- src/NuGet.Core/NuGet.Packaging/PackageFolderReader.cs
+++ src/NuGet.Core/NuGet.Packaging/PackageFolderReader.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using NuGet.Common;
 using NuGet.Frameworks;
 using NuGet.Packaging.Core;
