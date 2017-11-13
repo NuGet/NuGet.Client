760c4f0a029030dae30f6b963b8e204d9a0f02a4
Pato Beltran <pbelsal@gmail.com>
2017-11-13 15:56:51 -0800

    Don't fail if output window is not present

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/VisualStudioHostExtension.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/VisualStudioHostExtension.cs
@@ -14,9 +14,18 @@ namespace NuGet.Tests.Apex
 
         public static bool HasNoErrorsInOutputWindows(this VisualStudioHost host)
         {
-            var outputPane = host.ObjectModel.Shell.ToolWindows.OutputWindow.GetOutputPane(_nugetOutputWindowGuid);
+            try
+            {
+                var outputPane = host.ObjectModel.Shell.ToolWindows.OutputWindow.GetOutputPane(_nugetOutputWindowGuid);
 
-            return !outputPane.Text.ToLowerInvariant().Contains("failed");
+                return !outputPane.Text.ToLowerInvariant().Contains("failed");
+            }
+            catch(ArgumentException)
+            {
+                // If no output has been printed into the nuget output window then the output pane will not be initialized
+                // If no output has been printed we know there is no error in the output window.
+                return true;
+            }
         }
 
         public static void SelectProjectInSolutionExplorer(this VisualStudioHost host, string project)
