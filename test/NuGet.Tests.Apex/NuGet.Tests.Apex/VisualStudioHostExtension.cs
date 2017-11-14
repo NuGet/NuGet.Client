227709d202a41af5327d20767ea7de54bf3a6671
Pato Beltran <pbelsal@gmail.com>
2017-11-13 17:31:21 -0800

    fix spacing

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/VisualStudioHostExtension.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/VisualStudioHostExtension.cs
@@ -5,7 +5,7 @@ namespace NuGet.Tests.Apex
 {
     public static class VisualStudioHostExtension
     {
-        static private readonly Guid _nugetOutputWindowGuid = new Guid("CEC55EC8-CC51-40E7-9243-57B87A6F6BEB"); 
+        static private readonly Guid _nugetOutputWindowGuid = new Guid("CEC55EC8-CC51-40E7-9243-57B87A6F6BEB");
 
         public static bool HasNoErrorsInErrorList(this VisualStudioHost host)
         {
@@ -20,7 +20,7 @@ namespace NuGet.Tests.Apex
 
                 return !outputPane.Text.ToLowerInvariant().Contains("failed");
             }
-            catch(ArgumentException)
+            catch (ArgumentException)
             {
                 // If no output has been printed into the nuget output window then the output pane will not be initialized
                 // If no output has been printed we know there is no error in the output window.
@@ -42,7 +42,7 @@ namespace NuGet.Tests.Apex
                 var outputPane = host.ObjectModel.Shell.ToolWindows.OutputWindow.GetOutputPane(_nugetOutputWindowGuid);
                 outputPane.Clear();
             }
-            catch(ArgumentException)
+            catch (ArgumentException)
             {
                 //if outputPane doesn't exist, ignore it
             }
