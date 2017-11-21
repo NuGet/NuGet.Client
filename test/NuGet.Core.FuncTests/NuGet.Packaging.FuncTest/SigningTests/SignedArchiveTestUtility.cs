21951d4ba667bdf1b2d506c161ff13aaa1b2617e
Ankit Mishra <anmishr@microsoft.com>
2017-11-21 13:17:22 -0800

    Adding SIgningTestFixture to make cert generation common

--- test/NuGet.Core.FuncTests/NuGet.Packaging.FuncTest/SigningTests/SignedArchiveTestUtility.cs
+++ test/NuGet.Core.FuncTests/NuGet.Packaging.FuncTest/SigningTests/SignedArchiveTestUtility.cs
@@ -46,9 +46,58 @@ namespace NuGet.Packaging.FuncTest
         }
 
         /// <summary>
+        /// Generates a signed copy of a package and returns the path to that package
+        /// This method timestamps a package and should only be used with tests marked with [CIOnlyFact]
+        /// </summary>
+        /// <param name="testCert">Certificate to be used while signing the package</param>
+        /// <param name="nupkg">Package to be signed</param>
+        /// <returns>Path to the signed copy of the package</returns>
+        public static async Task<string> CreateSignedAndTimeStampedPackageAsync(TrustedTestCert<TestCertificate> testCert, SimpleTestPackageContext nupkg)
+        {
+            var testLogger = new TestLogger();
+            var zipReadStream = nupkg.CreateAsStream();
+            var zipWriteStream = nupkg.CreateAsStream();
+
+            var signPackage = new SignedPackageArchive(zipReadStream, zipWriteStream);
+
+            var signedPackagePath = Path.GetTempFileName();
+
+            // Sign the package
+            await SignAndTimeStampPackageAsync(testLogger, testCert.Source.Cert, signPackage);
+
+            zipWriteStream.Seek(offset: 0, loc: SeekOrigin.Begin);
+
+            using (Stream fileStream = File.OpenWrite(signedPackagePath))
+            {
+                zipWriteStream.CopyTo(fileStream);
+            }
+
+            return signedPackagePath;
+        }
+
+        /// <summary>
         /// Sign a package for test purposes.
+        /// This does not timestamp a signature and can be used outside corp network.
         /// </summary>
         private static async Task SignPackageAsync(TestLogger testLogger, X509Certificate2 cert, SignedPackageArchive signPackage)
+        {
+            var testSignatureProvider = new X509SignatureProvider(timestampProvider: null);
+            var signer = new Signer(signPackage, testSignatureProvider);
+
+            var request = new SignPackageRequest()
+            {
+                Certificate = cert,
+                SignatureHashAlgorithm = Common.HashAlgorithmName.SHA256
+            };
+
+            await signer.SignAsync(request, testLogger, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Sign and timestamp a package for test purposes.
+        /// This method timestamps a package and should only be used with tests marked with [CIOnlyFact]
+        /// </summary>
+        private static async Task SignAndTimeStampPackageAsync(TestLogger testLogger, X509Certificate2 cert, SignedPackageArchive signPackage)
         {
             var testSignatureProvider = new X509SignatureProvider(new Rfc3161TimestampProvider(new Uri(_internalTimestampServer)));
             var signer = new Signer(signPackage, testSignatureProvider);
---
+++ test/NuGet.Core.FuncTests/NuGet.Packaging.FuncTest/SigningTests/SigningTestFixture.cs
@@ -0,0 +1,69 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using NuGet.Packaging.Signing;
+using Test.Utility.Signing;
+
+namespace NuGet.Packaging.FuncTest
+{
+    /// <summary>
+    /// Used to bootstrap functional tests for signing.
+    /// </summary>
+    public class SigningTestFixture : IDisposable
+    {
+        private TrustedTestCert<TestCertificate> _trustedTestCert;
+        private IList<ISignatureVerificationProvider> _trustProviders;
+        private SigningSpecifications _signingSpecifications;
+
+        public TrustedTestCert<TestCertificate> TrustedTestCertificate
+        {
+            get
+            {
+                if (_trustedTestCert == null)
+                {
+                    _trustedTestCert = TestCertificate.Generate().WithTrust();
+                }
+
+                return _trustedTestCert;
+            }
+        }
+
+        public IList<ISignatureVerificationProvider> TrustProviders
+        {
+            get
+            {
+                if (_trustProviders == null)
+                {
+                    _trustProviders = new List<ISignatureVerificationProvider>()
+                    {
+                        new X509SignatureVerificationProvider(),
+                        new NuGetIntegrityVerificationProvider(),
+                        new TimestampVerificationProvider()
+                    };
+                }
+
+                return _trustProviders;
+            }
+        }
+
+        public SigningSpecifications SigningSpecifications
+        {
+            get
+            {
+                if (_signingSpecifications == null)
+                {
+                    _signingSpecifications = SigningSpecifications.V1;
+                }
+
+                return _signingSpecifications;
+            }
+        }
+
+        public void Dispose()
+        {
+            _trustedTestCert?.Dispose();
+        }
+    }
+}
