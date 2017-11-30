9da669bdf5c68d84d853a84d95755f01396b7bfa
Nikolche Kolev <nikolev@microsoft.com>
2017-11-29 17:48:51 -0800

    Add basic tests

--- test/NuGet.Core.Tests/NuGet.Commands.Test/RestoreRunnerTests.cs
+++ test/NuGet.Core.Tests/NuGet.Commands.Test/RestoreRunnerTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -96,6 +96,103 @@ namespace NuGet.Commands.Test
                     // Assert
                     Assert.True(summary.Success, "Failed: " + string.Join(Environment.NewLine, logger.Messages));
                     Assert.Equal(1, summary.FeedsUsed.Count);
+                    Assert.True(File.Exists(lockPath), lockPath);
+                    Assert.False(File.Exists(Path.Combine(project1.FullName, "project1.nuget.targets")));
+                    Assert.False(File.Exists(Path.Combine(project1.FullName, "project1.nuget.props")));
+                }
+            }
+        }
+
+        [Fact]
+        public async Task RestoreRunner_BasicRestore_BuildAssetsChanged()
+        {
+            // Arrange
+            var sources = new List<PackageSource>();
+
+            var project1Json = @"
+            {
+              ""version"": ""1.0.0"",
+              ""description"": """",
+              ""authors"": [ ""author"" ],
+              ""tags"": [ """" ],
+              ""projectUrl"": """",
+              ""licenseUrl"": """",
+              ""frameworks"": {
+                ""net45"": {
+                }
+              }
+            }";
+
+            using (var workingDir = TestDirectory.Create())
+            {
+                var projectName = "project1";
+                var packagesDir = new DirectoryInfo(Path.Combine(workingDir, "globalPackages"));
+                var packageSource = new DirectoryInfo(Path.Combine(workingDir, "packageSource"));
+                var project1 = new DirectoryInfo(Path.Combine(workingDir, "projects", projectName));
+
+                packagesDir.Create();
+                packageSource.Create();
+                project1.Create();
+                sources.Add(new PackageSource(packageSource.FullName));
+
+                var specPath1 = Path.Combine(project1.FullName, "project.json");
+                var dgPath = Path.Combine(project1.FullName, "project.dg");
+
+                File.WriteAllText(specPath1, project1Json);
+
+                var spec1 = JsonPackageSpecReader.GetPackageSpec(project1Json, projectName, specPath1);
+
+                spec1 = spec1.EnsureRestoreMetadata();
+                spec1.RestoreMetadata.Sources = new List<PackageSource> { new PackageSource(packageSource.FullName) };
+                spec1.RestoreMetadata.PackagesPath = packagesDir.FullName;
+                var dgSpec = new DependencyGraphSpec();
+                dgSpec.AddProject(spec1);
+                dgSpec.AddRestore(projectName);
+
+                var logger = new TestLogger();
+                var lockPath = Path.Combine(project1.FullName, "project.assets.json");
+
+                var sourceRepos = sources.Select(source => Repository.Factory.GetCoreV3(source.Source)).ToList();
+
+                var providerCache = new RestoreCommandProvidersCache();
+                using (var cacheContext = new SourceCacheContext())
+                {
+                    var restoreContext = new RestoreArgs()
+                    {
+                        CacheContext = cacheContext,
+                        DisableParallel = true,
+                        GlobalPackagesFolder = packagesDir.FullName,
+                        Sources = new List<string>() { packageSource.FullName },
+                        Log = logger,
+                        CachingSourceProvider = new CachingSourceProvider(new TestPackageSourceProvider(sources)),
+                        PreLoadedRequestProviders = new List<IPreLoadedRestoreRequestProvider>()
+                        {
+                            new DependencyGraphSpecRequestProvider(providerCache, dgSpec)
+                        },
+                        AllowNoOp = true
+                    };
+
+                    // Act
+                    var summaries = await RestoreRunner.RunAsync(restoreContext);
+                    var summary = summaries.Single();
+
+                    // Assert
+                    Assert.True(summary.Success, "Failed: " + string.Join(Environment.NewLine, logger.Messages));
+                    Assert.True(summary.BuildFilesChanged, "Build files did not change");
+                    Assert.Equal(1, summary.FeedsUsed.Count);
+                    Assert.True(File.Exists(lockPath), lockPath);
+                    Assert.False(File.Exists(Path.Combine(project1.FullName, "project1.nuget.targets")));
+                    Assert.False(File.Exists(Path.Combine(project1.FullName, "project1.nuget.props")));
+
+                    // Act again
+                    var noopsummaries = await RestoreRunner.RunAsync(restoreContext);
+                    var noopsummary = noopsummaries.Single();
+
+                    // Assert
+                    Assert.True(noopsummary.Success, "Failed: " + string.Join(Environment.NewLine, logger.Messages));
+                    Assert.True(noopsummary.NoOpRestore);
+                    Assert.False(noopsummary.BuildFilesChanged, "Build files did change");
+                    Assert.Equal(1, noopsummary.FeedsUsed.Count);
                     Assert.True(File.Exists(lockPath), lockPath);
                     Assert.False(File.Exists(Path.Combine(project1.FullName, "project1.nuget.targets")));
                     Assert.False(File.Exists(Path.Combine(project1.FullName, "project1.nuget.props")));
