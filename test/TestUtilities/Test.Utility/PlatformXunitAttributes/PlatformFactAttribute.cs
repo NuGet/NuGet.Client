57c238ae5c99511f61d017b920dec0ae8a62726c
Ankit Mishra <anmishr@microsoft.com>
2017-12-18 23:40:40 -0800

    Skipping NetworkCallCount_RestoreLargePackagesConfigWithMultipleSourcesMainlyV2 test on mono (#1877)
    Skipping  `NetworkCallCountTest.NetworkCallCount_RestoreLargePackagesConfigWithMultipleSourcesMainlyV2`  on mono because it is flaky on mono.

--- test/TestUtilities/Test.Utility/PlatformXunitAttributes/PlatformFactAttribute.cs
+++ test/TestUtilities/Test.Utility/PlatformXunitAttributes/PlatformFactAttribute.cs
@@ -1,9 +1,9 @@
-﻿using System;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NuGet.Common;
 using Xunit;
 
 namespace NuGet.Test.Utility
