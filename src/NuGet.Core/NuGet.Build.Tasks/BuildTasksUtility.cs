472fd61cbb5ed518e7a7fb14ba6312c84b856a15
Nikolche Kolev <nikolev@microsoft.com>
2017-12-22 13:15:06 -0800

    cleanup

--- src/NuGet.Core/NuGet.Build.Tasks/BuildTasksUtility.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/BuildTasksUtility.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
@@ -94,7 +94,6 @@ namespace NuGet.Build.Tasks
         {
             ProjectStyle.DotnetCliTool,
             ProjectStyle.PackageReference,
-            ProjectStyle.DotnetToolReference, // TODO NK - dobule check if there really needs to be
             ProjectStyle.Standalone,
             ProjectStyle.ProjectJson
         };
