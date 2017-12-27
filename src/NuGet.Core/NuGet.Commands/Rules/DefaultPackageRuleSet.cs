41e8bde59e9a9e62e71b049ab52a379574d6f3c2
Kirill Katsnelson <kkm@pobox.com>
2017-12-27 02:41:41 -0800

    Retire package Summary Missing analysis rule (#1906)
    Because the Summary field has been retired itself, per
    https://github.com/NuGet/Home/issues/4587#issuecomment-288913324
    https://github.com/dotnet/project-system/issues/2937#issuecomment-351907557

--- src/NuGet.Core/NuGet.Commands/Rules/DefaultPackageRuleSet.cs
+++ src/NuGet.Core/NuGet.Commands/Rules/DefaultPackageRuleSet.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -15,7 +15,6 @@ namespace NuGet.Commands.Rules
                 new MisplacedAssemblyRule(),
                 new MisplacedScriptFileRule(),
                 new MisplacedTransformFileRule(),
-                new MissingSummaryRule(),
                 new InitScriptNotUnderToolsRule(),
                 new WinRTNameIsObsoleteRule(),
                 new DefaultManifestValuesRule(),
