6fbdb95baf5906ff0ab2ec28ddfd53c320ee8c8d
Damon Tivel <dtivel@microsoft.com>
2018-01-17 06:39:36 -0800

    Fix functional test.

--- test/NuGet.Core.FuncTests/NuGet.Packaging.FuncTest/SigningTests/SigningUtilityTests.cs
+++ test/NuGet.Core.FuncTests/NuGet.Packaging.FuncTest/SigningTests/SigningUtilityTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#if IS_DESKTOP
+
 using System;
 using System.Security.Cryptography.X509Certificates;
 using NuGet.Common;
@@ -36,3 +38,4 @@ namespace NuGet.Packaging.FuncTest
         }
     }
 }
+#endif
