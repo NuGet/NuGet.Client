0f053baff26d7ad325b3171744953366a38971c0
Pato Beltran <pbelsal@gmail.com>
2018-01-24 15:57:57 -0800

    add missing null check

--- src/NuGet.Core/NuGet.Packaging/Signing/SignatureVerificationProviderFactory.cs
+++ src/NuGet.Core/NuGet.Packaging/Signing/SignatureVerificationProviderFactory.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 
 namespace NuGet.Packaging.Signing
@@ -9,6 +10,11 @@ namespace NuGet.Packaging.Signing
     {
         public static IEnumerable<ISignatureVerificationProvider> GetSignatureVerificationProviders(SignatureVerificationProviderArgs args)
         {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
             return new List<ISignatureVerificationProvider>()
             {
                 new IntegrityVerificationProvider(),
