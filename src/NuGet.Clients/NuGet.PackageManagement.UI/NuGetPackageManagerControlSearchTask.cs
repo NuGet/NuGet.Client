a6ea95015e20cfa706e07f7a3c8af394f1601a8e
Rohit Agrawal <ragrawal@microsoft.com>
2018-01-24 18:21:28 -0800

    narrate number of results obtained on searching in PM UI (#1974)
    * narrate number of results obtained on searching in PM UI
    
    * marking status complete at end of execution
    
    * refactoring
    
    * order values

---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/NuGetPackageManagerControlSearchTask.cs
@@ -0,0 +1,73 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.Shell.Interop;
+using NuGet.VisualStudio;
+
+namespace NuGet.PackageManagement.UI
+{
+    internal sealed class NuGetPackageManagerControlSearchTask : IVsSearchTask
+    {
+        private PackageManagerControl _packageManagerControl;
+        private IVsSearchCallback _searchCallback;
+        private IVsSearchQuery _searchQuery;
+
+        public NuGetPackageManagerControlSearchTask(PackageManagerControl packageManagerControl, uint id, IVsSearchQuery pSearchQuery, IVsSearchCallback pSearchCallback)
+        {
+            _packageManagerControl = packageManagerControl;
+            _searchCallback = pSearchCallback;
+            _searchQuery = pSearchQuery;
+            Id = id;
+            ErrorCode = 0;
+            SetStatus(VsSearchTaskStatus.Created);
+
+        }
+        public void Start()
+        {
+            SetStatus(VsSearchTaskStatus.Started);
+            NuGetUIThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                _packageManagerControl.SearchPackagesAndRefreshUpdateCount(searchText: _searchQuery.SearchString, useCacheForUpdates: true, pSearchCallback: _searchCallback, searchTask: this);
+                SetStatus(VsSearchTaskStatus.Completed);
+            });
+        }
+
+        public uint Id { get; private set; }
+
+        public IVsSearchQuery SearchQuery
+        {
+            get
+            {
+                return _searchQuery;
+            }
+            set
+            {
+                _searchQuery = value;
+            }
+        }
+
+        public uint Status { get; private set; }
+
+        public int ErrorCode { get; private set; }
+
+        public void Stop()
+        {
+            SetStatus(VsSearchTaskStatus.Stopped);
+        }
+
+        private void SetStatus(VsSearchTaskStatus taskStatus)
+        {
+            Status = (uint)taskStatus;
+        }
+
+        private enum VsSearchTaskStatus : uint
+        {
+            Created = 0,
+            Started = 1,
+            Completed = 2,
+            Stopped = 3,
+            Error = 4
+        }
+    }
+}
