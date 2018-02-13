3cfa0d51e338c32aa16ac05098cd7b74d595717c
campersau <buchholz.bastian@googlemail.com>
2018-02-13 14:45:14 +0100

    support IsListed in V3 GetMetadataAsync

--- src/NuGet.Core/NuGet.Protocol/Converters/ParserConstants.cs
+++ src/NuGet.Core/NuGet.Protocol/Converters/ParserConstants.cs
@@ -1,4 +1,4 @@
-﻿ // Copyright (c) .NET Foundation. All rights reserved.
+ // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -50,5 +50,6 @@ namespace NuGet.Protocol
         public const string PackageContent = "packageContent";
         public const string Versions = "versions";
         public const string PrefixReserved = "verified";
+        public const string Listed = "listed";
     }
 }
