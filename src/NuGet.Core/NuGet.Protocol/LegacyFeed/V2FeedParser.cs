7db5c113a2abb4d75ae6721cf5cd1c8c7bc02a98
Nikolche Kolev <nikolev@microsoft.com>
2018-02-26 22:22:47 -0800

    cleanup

--- src/NuGet.Core/NuGet.Protocol/LegacyFeed/V2FeedParser.cs
+++ src/NuGet.Core/NuGet.Protocol/LegacyFeed/V2FeedParser.cs
@@ -487,7 +487,6 @@ namespace NuGet.Protocol
                         cacheKey = $"list_{relativeUri}_page{page}";
                         docRequest = LoadXmlAsync(nextUri, cacheKey, ignoreNotFounds, sourceCacheContext, log, token);
                     }
-
                 }
             }
 
