21e2090c60b19ab82af47fdcef8b85e2f3d4c52c
Nikolche Kolev <nikolev@microsoft.com>
2018-03-25 18:22:22 -0700

    Revert "fix discovery of xplat cli"
    This reverts commit 7bf8621a85ad040e2251537be619f419df5d949a.

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/DotnetCliUtil.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/DotnetCliUtil.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -35,7 +35,7 @@ namespace NuGet.XPlat.FuncTest
 #else
                 "Release",
 #endif
-                "netcoreapp2.0",
+                "netcoreapp1.0",
                 XPlatDll);
         }
 
