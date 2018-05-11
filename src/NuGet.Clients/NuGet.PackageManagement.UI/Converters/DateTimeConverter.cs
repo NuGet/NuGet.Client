8d8f0961ab98cb7d6a925cc58192bafc52544740
Ankit Mishra <anmishr@microsoft.com>
2018-05-11 13:10:13 -0700

    Fixing the string formatting on Package metadata published date for Japanese (#2229)
    ## Bug
    Fixes: [57506](https://devdiv.visualstudio.com/DevDiv/NuGet/_workitems/edit/457506)
    Regression: No  
    
    ## Fix
    Details: Fixes the string formatting over the published date in the package metadata page. This PR adds a custom formatting for ja-JP as requested by loicalization vendors.
    
    ## Testing/Validation
    Tests Added: No  
    Reason for not adding tests:  UI text changes.
    Validation done:  Manual validation in ENG, TRK and JP.

---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Converters/DateTimeConverter.cs
@@ -0,0 +1,43 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NuGet.PackageManagement.UI
+{
+    /// <summary>
+    /// DateTime Converter for published date in package metadata.
+    /// </summary>
+    public class DateTimeConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts DateTime object into custom formatting.
+        /// Goal of the converter is to have custom formatting for japanese since "D" formatting does not include day of the week.
+        /// </summary>
+        /// <returns>string value of the date time.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            var dateTime = DateTime.Parse(value.ToString());
+            if (string.Equals(culture.Name, "ja-JP"))
+            {
+                return $"{dateTime.ToString("D", culture)} {dateTime.ToString("dddd", culture)} ({ dateTime.ToString("d", culture)})";
+            }
+            else
+            {
+                return $"{dateTime.ToString("D", culture)} ({ dateTime.ToString("d", culture)})";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
