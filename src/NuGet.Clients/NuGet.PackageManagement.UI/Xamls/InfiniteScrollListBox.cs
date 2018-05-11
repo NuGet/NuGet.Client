535b12086fa533d36bbac721bd25a10ed3c276cc
Ashish Jain <asja@microsoft.com>
2018-05-11 16:03:34 -0700

    Safeguard InfiniteScollList operations across multiple threads (#2233)

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBox.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBox.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows.Automation.Peers;
+using System.Threading;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 
 namespace NuGet.PackageManagement.UI
 { 
     internal class InfiniteScrollListBox : ListBox
     {
+        public readonly SemaphoreSlim ItemsLock = new SemaphoreSlim(1, 1);
+
         protected override AutomationPeer OnCreateAutomationPeer()
         {
             return new InfiniteScrollListBoxAutomationPeer(this);
