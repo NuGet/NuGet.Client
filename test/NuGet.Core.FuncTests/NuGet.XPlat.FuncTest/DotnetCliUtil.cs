f8b978e74e8bff28a7e33b407dae824320e65117
Nikolche Kolev <nikolev@microsoft.com>
2018-05-22 12:48:15 -0700

    move xplat libraries to ns2.0

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/DotnetCliUtil.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/DotnetCliUtil.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -35,7 +35,7 @@ namespace NuGet.XPlat.FuncTest
 #else
                 "Release",
 #endif
-                "netcoreapp1.0",
+                "netcoreapp2.0",
                 XPlatDll);
         }
 
