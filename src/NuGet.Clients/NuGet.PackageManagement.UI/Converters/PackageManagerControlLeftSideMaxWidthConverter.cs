59ee3b4bb3d2ed981527972495018ce6421ea5fe
Pato Beltran <pbelsal@gmail.com>
2018-06-04 14:41:45 -0700

    Improve responsiveness of PMUI and make UI usable in small sizes

---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Converters/PackageManagerControlLeftSideMaxWidthConverter.cs
@@ -0,0 +1,27 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Windows.Data;
+
+namespace NuGet.PackageManagement.UI
+{
+    public class PackageManagerControlLeftSideMaxWidthConverter : IValueConverter
+    {
+        public object Convert(object value,
+        Type targetType,
+        object parameter,
+        System.Globalization.CultureInfo culture)
+        {
+            return System.Convert.ToDouble(value) - System.Convert.ToDouble(parameter);
+        }
+
+        public object ConvertBack(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return System.Convert.ToDouble(value) + System.Convert.ToDouble(parameter);
+        }
+    }
+}
