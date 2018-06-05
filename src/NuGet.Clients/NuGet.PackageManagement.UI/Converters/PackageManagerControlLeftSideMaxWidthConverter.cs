8b8a8d781921d17488f2654298fc17f699a5874d
Pato Beltran <pbelsal@gmail.com>
2018-06-05 12:29:25 -0700

    Binded constant numbers to code to make it more readable

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Converters/PackageManagerControlLeftSideMaxWidthConverter.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Converters/PackageManagerControlLeftSideMaxWidthConverter.cs
@@ -2,26 +2,26 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace NuGet.PackageManagement.UI
 {
-    public class PackageManagerControlLeftSideMaxWidthConverter : IValueConverter
+    public class PackageManagerControlLeftSideMaxWidthConverter : IMultiValueConverter
     {
-        public object Convert(object value,
-        Type targetType,
-        object parameter,
-        System.Globalization.CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return System.Convert.ToDouble(value) - System.Convert.ToDouble(parameter);
+            if (values[0] == DependencyProperty.UnsetValue || values[1] == DependencyProperty.UnsetValue)
+            {
+                return 0;
+            }
+            return System.Convert.ToDouble(values[0]) - System.Convert.ToDouble(values[1]);
         }
 
-        public object ConvertBack(object value,
-            Type targetType,
-            object parameter,
-            System.Globalization.CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            return System.Convert.ToDouble(value) + System.Convert.ToDouble(parameter);
+            throw new NotImplementedException();
         }
     }
 }
