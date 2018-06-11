412ab6e364b1500833f7be8fa63335db5bf54361
Ankit Mishra <anmishr@microsoft.com>
2018-06-11 14:58:31 -0700

    Improving validaity message

--- src/NuGet.Core/NuGet.Packaging/Strings.Designer.cs
+++ src/NuGet.Core/NuGet.Packaging/Strings.Designer.cs
@@ -782,7 +782,7 @@ namespace NuGet.Packaging {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to PackageSignatureVerificationLog:{0}PackageIdentitiy: {1}{0}Source: {2}{0}Valid: {3}.
+        ///   Looks up a localized string similar to PackageSignatureVerificationLog:{0}PackageIdentity: {1}{0}Source: {2}{0}PackageSignatureValidity: {3}.
         /// </summary>
         internal static string PackageSignatureVerificationLog {
             get {
