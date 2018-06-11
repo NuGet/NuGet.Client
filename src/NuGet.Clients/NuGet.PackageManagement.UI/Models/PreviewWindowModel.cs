0143644ae6e206efc51760e46ef411d721c3d705
Pato Beltran <pbelsal@gmail.com>
2018-06-11 15:09:40 -0700

    Let jaws and narrator announce more information about the preview changes window

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/PreviewWindowModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/PreviewWindowModel.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
@@ -10,6 +10,10 @@ namespace NuGet.PackageManagement.UI
         public IEnumerable<PreviewResult> PreviewResults { get; }
 
         public string Title { get; private set; }
+
+        public int ButtonMinWidth => 86;
+        public int DoNotShowAgainMinWidth => 180;
+        public int WindowMinwidth => 2 * ButtonMinWidth + DoNotShowAgainMinWidth;
 
         public PreviewWindowModel(IEnumerable<PreviewResult> results)
         {
