b5bf13e6722ded094c4a63fb24037595cd721f86
Abood AbuHashem <t-ababuh@microsoft.com>
2018-06-26 16:56:36 -0700

    Merged tests

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NuGetConsoleTestCase.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NuGetConsoleTestCase.cs
@@ -136,8 +136,10 @@ namespace NuGet.Tests.Apex
         }
 
         [NuGetWpfTheory]
-        [MemberData(nameof(GetPackageReferenceTemplates))]
-        public async Task UpdatePackageFromPMCForPRAsync(ProjectTemplate projectTemplate)
+        [MemberData(nameof(GetPackageReferenceTemplates), null, "2.0.0")]
+        [MemberData(nameof(GetPackageReferenceTemplates), "-ToHighestPatch", "1.0.1")]
+        [MemberData(nameof(GetPackageReferenceTemplates), "-ToHighestMinor", "1.1.0")]
+        public async Task UpdatePackageFromPMCForPRAsync(ProjectTemplate projectTemplate, string flag, string expectedVersion)
         {
             // Arrange
             EnsureVisualStudioHost();
@@ -160,80 +162,19 @@ namespace NuGet.Tests.Apex
                 testContext.Project.Build();
                 testContext.NuGetApexTestService.WaitForAutoRestore();
 
-                nugetConsole.UpdatePackageFromPMC(packageName, packageVersion2);
+                if(flag == null) {
+                    nugetConsole.UpdatePackageFromPMC(packageName, packageVersion2);
+                }
+                else
+                {
+                    nugetConsole.UpdatePackageFromPMCWithConstraints(packageName, flag);
+                }
                 testContext.Project.Build();
 
-                CommonUtility.AssertPackageReferenceExists(VisualStudio, testContext.Project, packageName, packageVersion2, XunitLogger);
-                CommonUtility.AssertPackageInAssetsFile(VisualStudio, testContext.Project, packageName, packageVersion2, XunitLogger);
+                CommonUtility.AssertPackageReferenceExists(VisualStudio, testContext.Project, packageName, expectedVersion, XunitLogger);
+                CommonUtility.AssertPackageInAssetsFile(VisualStudio, testContext.Project, packageName, expectedVersion, XunitLogger);
             }
         }
-
-        [NuGetWpfTheory]
-        [MemberData(nameof(GetPackageReferenceTemplates))]
-        public async Task UpdatePackagePatchOnlyFromPMCForPRAsync(ProjectTemplate projectTemplate)
-        {
-            // Arrange
-            EnsureVisualStudioHost();
-
-            using (var testContext = new ApexTestContext(VisualStudio, projectTemplate, XunitLogger))
-            {
-                var packageName = "TestPackage";
-                var packageVersion1 = "1.0.0";
-                var packageVersion2 = "2.0.0";
-                var packageVersion3 = "1.0.1";
-                var packageVersion4 = "1.1.0";
-                await CommonUtility.CreatePackageInSourceAsync(testContext.PackageSource, packageName, packageVersion1);
-                await CommonUtility.CreatePackageInSourceAsync(testContext.PackageSource, packageName, packageVersion2);
-                await CommonUtility.CreatePackageInSourceAsync(testContext.PackageSource, packageName, packageVersion3);
-                await CommonUtility.CreatePackageInSourceAsync(testContext.PackageSource, packageName, packageVersion4);
-
-                var nugetConsole = GetConsole(testContext.Project);
-
-                nugetConsole.InstallPackageFromPMC(packageName, packageVersion1);
-                testContext.Project.Build();
-                testContext.NuGetApexTestService.WaitForAutoRestore();
-
-                nugetConsole.UpdatePackageFromPMCWithConstraints(packageName, flags: "-ToHighestPatch");
-                testContext.Project.Build();
-
-                CommonUtility.AssertPackageReferenceExists(VisualStudio, testContext.Project, packageName, packageVersion3, XunitLogger);
-                CommonUtility.AssertPackageInAssetsFile(VisualStudio, testContext.Project, packageName, packageVersion3, XunitLogger);
-            }
-        }
-
-        [NuGetWpfTheory]
-        [MemberData(nameof(GetPackageReferenceTemplates))]
-        public async Task UpdatePackageMinorOnlyFromPMCForPRAsync(ProjectTemplate projectTemplate)
-        {
-            // Arrange
-            EnsureVisualStudioHost();
-
-            using (var testContext = new ApexTestContext(VisualStudio, projectTemplate, XunitLogger))
-            {
-                var packageName = "TestPackage";
-                var packageVersion1 = "1.0.0";
-                var packageVersion2 = "2.0.0";
-                var packageVersion3 = "1.0.1";
-                var packageVersion4 = "1.1.0";
-                await CommonUtility.CreatePackageInSourceAsync(testContext.PackageSource, packageName, packageVersion1);
-                await CommonUtility.CreatePackageInSourceAsync(testContext.PackageSource, packageName, packageVersion2);
-                await CommonUtility.CreatePackageInSourceAsync(testContext.PackageSource, packageName, packageVersion3);
-                await CommonUtility.CreatePackageInSourceAsync(testContext.PackageSource, packageName, packageVersion4);
-
-                var nugetConsole = GetConsole(testContext.Project);
-
-                nugetConsole.InstallPackageFromPMC(packageName, packageVersion1);
-                testContext.Project.Build();
-                testContext.NuGetApexTestService.WaitForAutoRestore();
-
-                nugetConsole.UpdatePackageFromPMCWithConstraints(packageName, flags: "-ToHighestMinor");
-                testContext.Project.Build();
-
-                CommonUtility.AssertPackageReferenceExists(VisualStudio, testContext.Project, packageName, packageVersion4, XunitLogger);
-                CommonUtility.AssertPackageInAssetsFile(VisualStudio, testContext.Project, packageName, packageVersion4, XunitLogger);
-            }
-        }
-
 
         [NuGetWpfTheory]
         [MemberData(nameof(GetPackagesConfigTemplates))]
@@ -556,12 +497,12 @@ namespace NuGet.Tests.Apex
             }
         }
 
-        public static IEnumerable<object[]> GetPackageReferenceTemplates()
+        public static IEnumerable<object[]> GetPackageReferenceTemplates(string flag, string expectedVersion)
         {
             for (var i = 0; i < CommonUtility.GetIterations(); i++)
             {
                 //yield return new object[] { ProjectTemplate.NetCoreConsoleApp };
-                yield return new object[] { ProjectTemplate.NetStandardClassLib };
+                yield return new object[] { ProjectTemplate.NetStandardClassLib , flag , expectedVersion};
             }
         }
 
