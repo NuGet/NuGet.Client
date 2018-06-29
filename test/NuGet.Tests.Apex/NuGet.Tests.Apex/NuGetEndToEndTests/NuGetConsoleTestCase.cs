ad317ade2f10e73695533eb1a041ac687b56113e
Abood AbuHashem <t-ababuh@microsoft.com>
2018-06-28 17:36:11 -0700

    Fixing some tests that broke

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NuGetConsoleTestCase.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NuGetConsoleTestCase.cs
@@ -506,6 +506,15 @@ namespace NuGet.Tests.Apex
             }
         }
 
+        public static IEnumerable<object[]> GetPackageReferenceTemplates()
+        {
+            for (var i = 0; i < CommonUtility.GetIterations(); i++)
+            {
+                //yield return new object[] { ProjectTemplate.NetCoreConsoleApp };
+                yield return new object[] { ProjectTemplate.NetStandardClassLib };
+            }
+        }
+
         public static IEnumerable<object[]> GetPackagesConfigTemplates()
         {
             for (var i = 0; i < CommonUtility.GetIterations(); i++)
