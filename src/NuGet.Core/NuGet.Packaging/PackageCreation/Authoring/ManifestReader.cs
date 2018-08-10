989d695203a9b075ab9ef81b910bd11505007113
Rohit Agrawal (NUGET) <ragrawal@microsoft.com>
2018-08-10 11:12:09 -0700

    change exception type to invaliddataexception

--- src/NuGet.Core/NuGet.Packaging/PackageCreation/Authoring/ManifestReader.cs
+++ src/NuGet.Core/NuGet.Packaging/PackageCreation/Authoring/ManifestReader.cs
@@ -148,7 +148,7 @@ namespace NuGet.Packaging
             {
                 // Wrap the exception to pinpoint the exact property that is problematic,
                 // and include a hint about replacement tokens.
-                throw new Exception(string.Format(NuGetResources.Manifest_PropertyValueReadFailure, value, element.Name.LocalName), ex);
+                throw new InvalidDataException(string.Format(NuGetResources.Manifest_PropertyValueReadFailure, value, element.Name.LocalName), ex);
             }
         }
 
