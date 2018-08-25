da77b885989ed65956fb94c77a1ad2271c704e9e
Nikolche Kolev <nikolev@microsoft.com>
2018-08-24 17:40:55 -0700

    cleanup

--- scripts/perftests/RunPerformanceTestRunner.ps1
+++ scripts/perftests/RunPerformanceTestRunner.ps1
@@ -1,5 +1,5 @@
 Param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$resultsDirectoryPath,
     [string]$nugetClientFilePath,
     [string]$testDirectoryPath,
@@ -9,19 +9,19 @@ Param(
 
     . "$PSScriptRoot\PerformanceTestUtilities.ps1"
 
-    If(![string]::IsNullOrEmpty($testDirectoryPath) -And $(GetAbsolutePath $resultsDirectoryPath).StartsWith($(GetAbsolutePath $testDirectoryPath)))
+    if (![string]::IsNullOrEmpty($testDirectoryPath) -And $(GetAbsolutePath $resultsDirectoryPath).StartsWith($(GetAbsolutePath $testDirectoryPath))) 
     {
         Log "$resultsDirectoryPath cannot be a subdirectory of $testDirectoryPath" "red"
         exit(1)
     }
-    
-    if([string]::IsNullOrEmpty($nugetClientFilePath) -Or !$(Test-Path $nugetClientFilePath))
+        
+    if ([string]::IsNullOrEmpty($nugetClientFilePath) -Or !$(Test-Path $nugetClientFilePath)) 
     {
         Log "The NuGet client at '$nugetClientFilePath' cannot be resolved. Attempting to use the fallback option." "yellow"
 
         $relativePath = $([System.IO.Path]::Combine("..", "..", "artifacts", "VS15", "NuGet.exe"))
         $nugetClientFilePath = GetAbsolutePath $([System.IO.Path]::Combine($PSScriptRoot, $relativePath))
-        if(!$(Test-Path $nugetClientFilePath))
+        if (!$(Test-Path $nugetClientFilePath)) 
         {
             Log "The project has not been built and there is not NuGet.exe available at $nugetClientFilePath. Either build the exe or pass a path to a NuGet client."
             exit(1)
@@ -29,19 +29,23 @@ Param(
         Log "Using the fallback client from $nugetClientFilePath"
     }
 
-    if([string]::IsNullOrEmpty($testDirectoryPath))
+    if ([string]::IsNullOrEmpty($testDirectoryPath)) 
     {
-        $testDirectoryPath = $([System.IO.Path]::Combine($env:TEMP,"np"))
+        $testDirectoryPath = $([System.IO.Path]::Combine($env:TEMP, "np"))
     }
 
-    $testDirectoryPath = GetAbsolutePath $testDirectoryPath
-
-    Log "Discovering the test cases."
-    $testFiles = $(Get-ChildItem $PSScriptRoot\testCases "Test-*.ps1" ) | ForEach-Object { $_.FullName }
-
-    $testFiles | ForEach-Object { . $_ $nugetClientFilePath $([System.IO.Path]::Combine($testDirectoryPath,"source")) $resultsDirectoryPath $([System.IO.Path]::Combine($testDirectoryPath,"logs")) }
-
-    if(-not $SkipCleanup)
+    try 
     {
-        Remove-Item -r -force $testDirectoryPath -ErrorAction Ignore > $null
+        $testDirectoryPath = GetAbsolutePath $testDirectoryPath
+
+        Log "Discovering the test cases."
+        $testFiles = $(Get-ChildItem $PSScriptRoot\testCases "Test-*.ps1" ) | ForEach-Object { $_.FullName }
+
+        $testFiles | ForEach-Object { . $_ $nugetClientFilePath $([System.IO.Path]::Combine($testDirectoryPath, "source")) $resultsDirectoryPath $([System.IO.Path]::Combine($testDirectoryPath, "logs")) }
+
+    }
+    finally {
+        if (!$SkipCleanup) {
+            Remove-Item -r -force $testDirectoryPath -ErrorAction Ignore > $null
+        }
     }
