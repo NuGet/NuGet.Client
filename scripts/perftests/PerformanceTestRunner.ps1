1ccec5f7fe37cf99d321f7f1927e2e179368dacd
Nikolche Kolev <nikolev@microsoft.com>
2018-08-27 13:08:32 -0700

    add the ability to specify multiple clients

--- scripts/perftests/PerformanceTestRunner.ps1
+++ scripts/perftests/PerformanceTestRunner.ps1
@@ -1,7 +1,7 @@
 Param(
     [Parameter(Mandatory = $true)]
     [string]$resultsDirectoryPath,
-    [string]$nugetClient,
+    [string[]]$nugetClients,
     [string]$testDirectoryPath,
     [string]$logsDirectoryPath,
     [switch]$SkipCleanup
@@ -14,21 +14,7 @@ Param(
         Log "$resultsDirectoryPath cannot be a subdirectory of $testDirectoryPath" "red"
         exit(1)
     }
-        
-    if ([string]::IsNullOrEmpty($nugetClient) -Or !$(Test-Path $nugetClient)) 
-    {
-        Log "The NuGet client at '$nugetClient' cannot be resolved. Attempting to use the fallback option." "yellow"
-
-        $relativePath = $([System.IO.Path]::Combine("..", "..", "artifacts", "VS15", "NuGet.exe"))
-        $nugetClient = GetAbsolutePath $([System.IO.Path]::Combine($PSScriptRoot, $relativePath))
-        if (!$(Test-Path $nugetClient)) 
-        {
-            Log "The project has not been built and there is not NuGet.exe available at $nugetClient. Either build the exe or pass a path to a NuGet client."
-            exit(1)
-        }
-        Log "Using the fallback client from $nugetClient"
-    }
-
+    
     if ([string]::IsNullOrEmpty($testDirectoryPath)) 
     {
         $testDirectoryPath = $([System.IO.Path]::Combine($env:TEMP, "np"))
@@ -36,17 +22,39 @@ Param(
 
     try 
     {
-        $testDirectoryPath = GetAbsolutePath $testDirectoryPath
-        Log "Discovering the test cases."
-        $testFiles = $(Get-ChildItem $PSScriptRoot\testCases "Test-*.ps1" ) | ForEach-Object { $_.FullName }
-        $testFiles | ForEach-Object { 
-            try 
-            {
-            . $_ -nugetClient $nugetClient -sourceRootDirectory $([System.IO.Path]::Combine($testDirectoryPath, "source")) -resultsDirectoryPath $resultsDirectoryPath -logsPath $([System.IO.Path]::Combine($testDirectoryPath, "logs")) 
+        foreach($nugetClient in $nugetClients){
+            try {
+                if ([string]::IsNullOrEmpty($nugetClient) -Or !$(Test-Path $nugetClient)) 
+                {
+                    Log "The NuGet client at '$nugetClient' cannot be resolved. Attempting to use the fallback option." "yellow"
+            
+                    $relativePath = $([System.IO.Path]::Combine("..", "..", "artifacts", "VS15", "NuGet.exe"))
+                    $nugetClient = GetAbsolutePath $([System.IO.Path]::Combine($PSScriptRoot, $relativePath))
+                    if (!$(Test-Path $nugetClient)) 
+                    {
+                        Log "The project has not been built and there is not NuGet.exe available at $nugetClient. Either build the exe or pass a path to a NuGet client."
+                        exit(1)
+                    }
+                    Log "Using the fallback client from $nugetClient"
+                }
+
+                $testDirectoryPath = GetAbsolutePath $testDirectoryPath
+                Log "Discovering the test cases."
+                $testFiles = $(Get-ChildItem $PSScriptRoot\testCases "Test-*.ps1" ) | ForEach-Object { $_.FullName }
+                $testFiles | ForEach-Object { 
+                    try 
+                    {
+                    . $_ -nugetClient $nugetClient -sourceRootDirectory $([System.IO.Path]::Combine($testDirectoryPath, "source")) -resultsDirectoryPath $resultsDirectoryPath -logsPath $([System.IO.Path]::Combine($testDirectoryPath, "logs")) 
+                    } 
+                    catch 
+                    {
+                        Log "Problem running the test case $_" "red"
+                    }
+                }
             } 
             catch 
             {
-                Log "Problem running the test case $_" "red"
+                Log "Problem running the tests with $nugetClient" "red"
             }
         }
     }
