88a3c59ea3c6dfaf0982b7a7394c603d8e9c0f58
Nikolche Kolev <nikolev@microsoft.com>
2018-08-29 23:32:46 -0700

    Migrate NuGet.Packaging.Core

--- src/NuGet.Core/NuGet.Packaging.Core/IPackageCoreReader.cs
+++ src/NuGet.Core/NuGet.Packaging.Core/IPackageCoreReader.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -17,63 +17,48 @@ namespace NuGet.Packaging.Core
     public interface IPackageCoreReader : IDisposable
     {
         /// <summary>
-        /// Gets the package identity.
+        /// Identity of the package
         /// </summary>
-        /// <returns>A package identity.</returns>
+        /// <returns></returns>
         PackageIdentity GetIdentity();
 
         /// <summary>
-        /// Gets the minimum client version needed to consume the package.
+        /// Minimum client version needed to consume the package.
         /// </summary>
-        /// <returns>A NuGet version.</returns>
         NuGetVersion GetMinClientVersion();
 
         /// <summary>
         /// Gets zero or more package types from the .nuspec.
         /// </summary>
-        /// <returns>A readonly list of package types.</returns>
         IReadOnlyList<PackageType> GetPackageTypes();
 
         /// <summary>
-        /// Gets a file stream from the package.
+        /// Returns a file stream from the package.
         /// </summary>
-        /// <returns>A stream for a file in the package.</returns>
         Stream GetStream(string path);
 
         /// <summary>
-        /// Gets all files in the package.
+        /// All files in the nupkg
         /// </summary>
-        /// <returns>An enumerable of files in the package.</returns>
         IEnumerable<string> GetFiles();
 
         /// <summary>
-        /// Gets files in a folder in the package.
+        /// Files in a folder
         /// </summary>
         /// <param name="folder">Folder path</param>
-        /// <returns>An enumerable of files under specified folder.</returns>
+        /// <returns>A collection of files under specified folder</returns>
         IEnumerable<string> GetFiles(string folder);
 
         /// <summary>
-        /// Gets a nuspec stream.
+        /// Nuspec stream
         /// </summary>
-        /// <returns>A stream for the nuspec.</returns>
         Stream GetNuspec();
 
         /// <summary>
-        /// Gets a nuspec file path.
+        /// Nuspec path
         /// </summary>
-        /// <returns>The nuspec file path.</returns>
         string GetNuspecFile();
 
-        /// <summary>
-        /// Copies files from a package to a new location.
-        /// </summary>
-        /// <param name="destination">The destination folder path.</param>
-        /// <param name="packageFiles">The package files to copy.</param>
-        /// <param name="extractFile">A package file extraction delegate.</param>
-        /// <param name="logger">A logger.</param>
-        /// <param name="token">A cancellation token.</param>
-        /// <returns>An enumerable of paths of files copied to the destination.</returns>
         IEnumerable<string> CopyFiles(
             string destination,
             IEnumerable<string> packageFiles,
