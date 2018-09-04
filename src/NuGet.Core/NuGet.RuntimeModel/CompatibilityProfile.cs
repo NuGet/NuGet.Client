4142998490454b6b53e16bf15597f003e8f3b5d5
Nikolche Kolev <nikolev@microsoft.com>
2018-09-04 01:15:37 -0700

    Migrate NuGet.RuntimeModel

---
+++ src/NuGet.Core/NuGet.RuntimeModel/CompatibilityProfile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NuGet.Frameworks;
+using NuGet.Shared;
+
+namespace NuGet.RuntimeModel
+{
+    public class CompatibilityProfile : IEquatable<CompatibilityProfile>
+    {
+        public string Name { get; }
+        public IList<FrameworkRuntimePair> RestoreContexts { get; }
+
+        public CompatibilityProfile(string name)
+            : this(name, Enumerable.Empty<FrameworkRuntimePair>())
+        { }
+
+        public CompatibilityProfile(string name, IEnumerable<FrameworkRuntimePair> restoreContexts)
+        {
+            Name = name;
+            RestoreContexts = restoreContexts.ToList();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {string.Join(",", RestoreContexts)}";
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = new HashCodeCombiner();
+
+            hashCode.AddObject(Name);
+            hashCode.AddSequence(RestoreContexts);
+
+            return hashCode.CombinedHash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as CompatibilityProfile);
+        }
+
+        public bool Equals(CompatibilityProfile other)
+        {
+            return other != null &&
+                string.Equals(Name, other.Name, StringComparison.Ordinal) &&
+                RestoreContexts.OrderedEquals(other.RestoreContexts, r => r);
+        }
+    }
+}
---
+++ src/NuGet.Core/NuGet.RuntimeModel/JsonObjectWriter.cs
@@ -0,0 +1,168 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NuGet.RuntimeModel
+{
+    /// <summary>
+    /// Generates JSON from an object graph.
+    ///
+    /// This is non-private only to facilitate unit testing.
+    /// </summary>
+    public sealed class JsonObjectWriter : IObjectWriter
+    {
+        private readonly Stack<JContainer> _containers;
+        private JContainer _currentContainer;
+        private bool _isReadOnly;
+        private readonly JObject _root;
+
+        public JsonObjectWriter()
+        {
+            _containers = new Stack<JContainer>();
+            _root = new JObject();
+
+            _currentContainer = _root;
+        }
+
+        public void WriteObjectStart(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            ThrowIfReadOnly();
+
+            _containers.Push(_currentContainer);
+
+            var newContainer = new JObject();
+
+            _currentContainer[name] = newContainer;
+            _currentContainer = newContainer;
+        }
+
+        public void WriteObjectEnd()
+        {
+            ThrowIfReadOnly();
+
+            if (_currentContainer == _root)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _currentContainer = GetPreviousContainer();
+        }
+
+        public void WriteNameValue(string name, int value)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            ThrowIfReadOnly();
+
+            _currentContainer[name] = value;
+        }
+
+        public void WriteNameValue(string name, bool value)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            ThrowIfReadOnly();
+
+            _currentContainer[name] = value;
+        }
+
+        public void WriteNameValue(string name, string value)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            ThrowIfReadOnly();
+
+            _currentContainer[name] = value;
+        }
+
+        public void WriteNameArray(string name, IEnumerable<string> values)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            ThrowIfReadOnly();
+
+            _currentContainer[name] = new JArray(values);
+        }
+
+        /// <summary>
+        /// Gets the JSON for the object.
+        ///
+        /// Once <see cref="GetJson"/> is called, no further writing is allowed.
+        /// </summary>
+        public string GetJson()
+        {
+            _isReadOnly = true;
+
+            return _root.ToString();
+        }
+
+        /// <summary>
+        /// Gets the JObject (in-memory JSON model) for the object.
+        /// 
+        /// Once <see cref="GetJObject"/> is called, no further writing is allowed.
+        /// </summary>
+        /// <returns></returns>
+        public JObject GetJObject()
+        {
+            _isReadOnly = true;
+
+            return _root;
+        }
+
+        /// <summary>
+        /// Writes the result to a <c>JsonTextWriter</c>.
+        ///
+        /// Once WriteTo is called, no further writing is allowed.
+        /// </summary>
+        public void WriteTo(JsonTextWriter writer)
+        {
+            if (writer == null)
+            {
+                throw new ArgumentNullException(nameof(writer));
+            }
+
+            _isReadOnly = true;
+
+            _root.WriteTo(writer);
+        }
+
+        private JContainer GetPreviousContainer()
+        {
+            if (_containers.Count == 0)
+            {
+                return null;
+            }
+
+            return _containers.Pop();
+        }
+
+        private void ThrowIfReadOnly()
+        {
+            if (_isReadOnly)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
