514e08735000c4a89289e947476f645fb6ce6b45
Nikolche Kolev <nikolev@microsoft.com>
2018-09-04 00:55:37 -0700

    Migrate NuGet.Protocol.Core.v2 and fix nuget.protocol.core.types project reference

---
+++ src/NuGet.Core/NuGet.Protocol.Core.v2/Resources/PackageRepositoryResourceV2.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGet.Protocol.Core.v2
+{
+    /// <summary>
+    /// Holds an IPackageRepository
+    /// </summary>
+    public class PackageRepositoryResourceV2 : V2Resource
+    {
+        public PackageRepositoryResourceV2(IPackageRepository repository)
+            : base(repository)
+        {
+        }
+    }
+}
---
+++ src/NuGet.Core/NuGet.Protocol.Core.v2/Resources/V2Resource.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using NuGet.Protocol.Core.Types;
+
+namespace NuGet.Protocol.Core.v2
+{
+    /// <summary>
+    /// Represents a resource provided by a V2 server. [ Like search resource, metadata resource]
+    /// *TODOS: Add a trace source , Resource description ?
+    /// </summary>
+    public class V2Resource : INuGetResource
+    {
+        private IPackageRepository _v2Client;
+
+        public V2Resource(V2Resource resource)
+        {
+            _v2Client = resource.V2Client;
+        }
+
+        public V2Resource(IPackageRepository repo)
+        {
+            _v2Client = repo;
+        }
+
+        public IPackageRepository V2Client
+        {
+            get { return _v2Client; }
+        }
+    }
+}
