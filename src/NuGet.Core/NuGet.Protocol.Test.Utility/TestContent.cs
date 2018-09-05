41a39a3991fd39dcfad298597735bb1a1da20d04
Rohit Agrawal (NUGET) <ragrawal@microsoft.com>
2018-09-05 15:36:48 -0700

    add nuget.protocol.test.utility

---
+++ src/NuGet.Core/NuGet.Protocol.Test.Utility/TestContent.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Utility
+{
+    public class TestContent : HttpContent
+    {
+        private MemoryStream _stream;
+
+        public TestContent(string s)
+        {
+            _stream = new MemoryStream(Encoding.UTF8.GetBytes(s));
+            _stream.Seek(0, SeekOrigin.Begin);
+        }
+
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            return _stream.CopyToAsync(stream);
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = _stream.Length;
+            return true;
+        }
+    }
+}
---
+++ src/NuGet.Core/NuGet.Protocol.Test.Utility/TestHttpHandlerProvider.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Protocol.Core.Types;
+
+namespace Test.Utility
+{
+    public class TestHttpHandlerProvider : ResourceProvider
+    {
+        private Func<HttpClientHandler> _messageHandlerFactory;
+
+        public TestHttpHandlerProvider(Func<HttpClientHandler> messageHandlerFactory)
+            : base(typeof(HttpHandlerResource), "testhandler", NuGetResourceProviderPositions.First)
+        {
+            _messageHandlerFactory = messageHandlerFactory;
+        }
+
+        public override Task<Tuple<bool, INuGetResource>> TryCreate(SourceRepository source, CancellationToken token)
+        {
+            var result = new Tuple<bool, INuGetResource>(true, new TestHttpHandler(_messageHandlerFactory()));
+            return Task.FromResult(result);
+        }
+    }
+}
