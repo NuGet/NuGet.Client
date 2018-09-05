4ede22dc7b373b8c37ad02ca8f714e69518416ac
Rohit Agrawal (NUGET) <ragrawal@microsoft.com>
2018-09-05 15:40:48 -0700

    add nuget.protocol.visualstudio

---
+++ src/NuGet.Core/NuGet.Protocol.VisualStudio/Extensions/FactoryExtensionsVS.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using NuGet.Protocol.Core.Types;
+using NuGet.Protocol;
+
+namespace NuGet.Protocol.VisualStudio
+{
+    public static class FactoryExtensionsVS
+    {
+        public static SourceRepository GetVisualStudio(this Repository.RepositoryFactory factory, string source)
+        {
+            return Repository.CreateSource(Repository.Provider.GetVisualStudio(), source);
+        }
+
+        public static SourceRepository GetVisualStudio(this Repository.RepositoryFactory factory, Configuration.PackageSource source)
+        {
+            return Repository.CreateSource(Repository.Provider.GetVisualStudio(), source);
+        }
+
+        /// <summary>
+        /// Core V2 + Core V3 + VS
+        /// </summary>
+        public static IEnumerable<Lazy<INuGetResourceProvider>> GetVisualStudio(this Repository.ProviderFactory factory)
+        {
+            foreach (var provider in Repository.Provider.GetCoreV3())
+            {
+                yield return provider;
+            }
+
+            yield break;
+        }
+    }
+}
---
+++ src/NuGet.Core/NuGet.Protocol.VisualStudio/VSSourceRepositoryProvider.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using NuGet.Configuration;
+using NuGet.Protocol.Core.Types;
+
+namespace NuGet.Protocol.VisualStudio
+{
+    /// <summary>
+    /// SourceRepositoryProvider is the high level source for repository objects representing package sources.
+    /// </summary>
+    [Export(typeof(ISourceRepositoryProvider))]
+    public sealed class ExtensibleSourceRepositoryProvider : ISourceRepositoryProvider
+    {
+
+        // TODO: add support for reloading sources when changes occur
+        private readonly Configuration.IPackageSourceProvider _packageSourceProvider;
+        private IEnumerable<Lazy<INuGetResourceProvider>> _resourceProviders;
+        private List<SourceRepository> _repositories;
+
+        /// <summary>
+        /// Public parameter-less constructor for SourceRepositoryProvider
+        /// </summary>
+        public ExtensibleSourceRepositoryProvider()
+        {
+        }
+
+        /// <summary>
+        /// Public importing constructor for SourceRepositoryProvider
+        /// </summary>
+        [ImportingConstructor]
+        public ExtensibleSourceRepositoryProvider([ImportMany] IEnumerable<Lazy<INuGetResourceProvider>> resourceProviders, [Import] Configuration.ISettings settings)
+            : this(new Configuration.PackageSourceProvider(settings, migratePackageSources: null), resourceProviders)
+        {
+        }
+
+        /// <summary>
+        /// Non-MEF constructor
+        /// </summary>
+        public ExtensibleSourceRepositoryProvider(Configuration.IPackageSourceProvider packageSourceProvider, IEnumerable<Lazy<INuGetResourceProvider>> resourceProviders)
+        {
+            _packageSourceProvider = packageSourceProvider;
+            _resourceProviders = Repository.Provider.GetVisualStudio().Concat(resourceProviders);
+            _repositories = new List<SourceRepository>();
+
+            // Refresh the package sources
+            Init();
+
+            // Hook up event to refresh package sources when the package sources changed
+            packageSourceProvider.PackageSourcesChanged += (sender, e) => { Init(); };
+        }
+
+        /// <summary>
+        /// Retrieve repositories
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<SourceRepository> GetRepositories()
+        {
+            return _repositories;
+        }
+
+        /// <summary>
+        /// Create a repository for one time use.
+        /// </summary>
+        public SourceRepository CreateRepository(PackageSource source)
+        {
+            return CreateRepository(source, FeedType.Undefined);
+        }
+
+        /// <summary>
+        /// Create a repository for one time use.
+        /// </summary>
+        public SourceRepository CreateRepository(PackageSource source, FeedType type)
+        {
+            return new SourceRepository(source, _resourceProviders, type);
+        }
+
+        public Configuration.IPackageSourceProvider PackageSourceProvider
+        {
+            get { return _packageSourceProvider; }
+        }
+
+        private void Init()
+        {
+            _repositories.Clear();
+            foreach (var source in _packageSourceProvider.LoadPackageSources())
+            {
+                if (source.IsEnabled)
+                {
+                    var sourceRepo = new SourceRepository(source, _resourceProviders);
+                    _repositories.Add(sourceRepo);
+                }
+            }
+        }
+    }
+}
