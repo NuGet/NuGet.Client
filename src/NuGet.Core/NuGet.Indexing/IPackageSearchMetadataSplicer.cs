881b3905cd8621346bfa919227ba4a3fa8fb35b2
Rohit Agrawal (NUGET) <ragrawal@microsoft.com>
2018-09-05 15:04:22 -0700

    migrate nuget.indexing

--- src/NuGet.Core/NuGet.Indexing/IPackageSearchMetadataSplicer.cs
+++ src/NuGet.Core/NuGet.Indexing/IPackageSearchMetadataSplicer.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-using NuGet.Protocol.Core.Types;
+﻿using NuGet.Protocol.Core.Types;
 
 namespace NuGet.Indexing
 {
