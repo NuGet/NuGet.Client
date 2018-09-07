eee931e4f4510585ad8f73b0cf79be712aeabdb6
Rohit Agrawal (NUGET) <ragrawal@microsoft.com>
2018-09-07 14:39:20 -0700

    migrate nuget.visualstudio and get it to build

--- src/NuGet.Clients/NuGet.VisualStudio/Extensibility/IVsPathContext.cs
+++ src/NuGet.Clients/NuGet.VisualStudio/Extensibility/IVsPathContext.cs
@@ -8,8 +8,6 @@ namespace NuGet.VisualStudio
 {
     /// <summary>
     /// NuGet path information specific to the current context (e.g. project context).
-    /// Represents captured snapshot associated with current project/solution settings.
-    /// Should be discarded immediately after all queries are done.
     /// </summary>
     [ComImport]
     [Guid("24A1A187-75EE-4296-A8B3-59F0E0707119")]
@@ -26,33 +24,5 @@ namespace NuGet.VisualStudio
         /// <see cref="string"/>.
         /// </summary>
         IEnumerable FallbackPackageFolders { get; }
-
-        /// <summary>
-        /// Fetch a package directory containing the provided asset path.
-        /// </summary>
-        /// <param name="packageAssetPath">Absolute path to package asset file.</param>
-        /// <param name="packageDirectoryPath">Full path to a package directory. 
-        /// <code>null</code> if returned falue is <code>false</code>.</param>
-        /// <returns>
-        /// <code>true</code> when a package containing the given file was found, <code>false</code> - otherwise.
-        /// </returns>
-        /// <example>
-        /// Suppose the project is a packages.config project and the following asset paths are provided:
-        /// 
-        /// - C:\src\MyProject\packages\NuGet.Versioning.3.5.0-rc1-final\lib\net45\NuGet.Versioning.dll
-        /// - C:\path\to\non\package\assembly\Newtonsoft.Json.dll
-        /// - C:\src\MyOtherProject\packages\NuGet.Core.2.12.0\lib\net40\NuGet.Core.dll
-        /// - C:\src\MyProject\packages\Autofac.3.5.2\lib\net40\Autofac.dll
-        /// - C:\src\MyProject\packages\Autofac.3.5.2\lib\net40\Autofac.Fake.dll
-        /// 
-        /// The result will be:
-        /// 
-        /// - C:\src\MyProject\packages\NuGet.Versioning.3.5.0-rc1-final
-        /// - null
-        /// - null
-        /// - C:\src\MyProject\packages\Autofac.3.5.2
-        /// - C:\src\MyProject\packages\Autofac.3.5.2
-        /// </example>
-        bool TryResolvePackageAsset(string packageAssetPath, out string packageDirectoryPath);
     }
 }
