f50b49ac6827d04d6e8a57ed8f075ffc698d73de
Nikolche Kolev <nikolev@microsoft.com>
2018-09-11 17:41:27 -0700

    remove infix to postfix conversion algorithm

--- test/NuGet.Core.Tests/NuGet.Packaging.Test/LicensesTests/LicenseExpressionEvaluatorTests.cs
+++ test/NuGet.Core.Tests/NuGet.Packaging.Test/LicensesTests/LicenseExpressionEvaluatorTests.cs
@@ -10,44 +10,6 @@ namespace NuGet.Packaging.Test
 {
     public class LicenseExpressionEvaluatorTests
     {
-        [Theory]
-        [InlineData("MIT OR LPL-1.0", "MIT LPL-1.0 OR")]
-        [InlineData("MIT AND LPL-1.0", "MIT LPL-1.0 AND")]
-        [InlineData("MIT WITH LPL-1.0", "MIT LPL-1.0 WITH")]
-        [InlineData("MIT OR (GPL-1.0 WITH E8)", "MIT GPL-1.0 E8 WITH OR")]
-        [InlineData("MIT OR GPL-1.0 WITH E8 AND APACHE-2.0", "MIT GPL-1.0 E8 WITH APACHE-2.0 AND OR")]
-        [InlineData("MIT OR GPL-1.0 WITH E8", "MIT GPL-1.0 E8 WITH OR")]
-        [InlineData("(LGPL-2.1 AND BSD-2-CLAUSE)", "LGPL-2.1 BSD-2-CLAUSE AND")]
-        [InlineData("MIT OR (LPL-1.0 OR GPL-1.0 WITH E8)", "MIT LPL-1.0 GPL-1.0 E8 WITH OR OR")]
-        [InlineData("MIT OR (GPL-1.0 WITH E8 OR LPL-1.0)", "MIT GPL-1.0 E8 WITH LPL-1.0 OR OR")]
-        [InlineData("MIT WITH E8 AND GP-1.0 OR LPL-1.0", "MIT E8 WITH GP-1.0 AND LPL-1.0 OR")]
-        [InlineData("MIT OR GPL-1.0 AND LPL-1.0 WITH E8", "MIT GPL-1.0 LPL-1.0 E8 WITH AND OR")]
-        //[InlineData("MIT OR ((GPL-1.0 WITH E8) AND APACHE-2.0)", "MIT GPL-1.0 E8 WITH APACHE-2.0 AND OR")]
-        //[InlineData("((MIT) AND (LPL-1.0))", "MIT LPL-1.0 AND")]
-        //[InlineData("((( (LGPL-2.1) AND BSD-2-CLAUSE)))", "LGPL-2.1 BSD-2-CLAUSE AND")]
-        //[InlineData("MIT OR (LPL-1.0 OR (GPL-1.0 WITH E8))", "MIT LPL-1.0 GPL-1.0 E8 WITH OR OR")]//[InlineData("MIT OR (LPL-1.0 AND (GPL-1.0 WITH E8) AND LGPL-2.1)", "MIT LPL-1.0 GPL-1.0 E8 WITH AND LGPL-2.1 AND OR")]
-        //[InlineData("MIT OR ((GPL-1.0 WITH E8) AND LPL-1.0)", "MIT GPL-1.0 E8 WITH LPL-1.0 AND OR")]
-
-        public void LicenseExpressionEvaluator_ConvertsComplexExpressions(string infix, string postfix)
-        {
-            var postfixExpression = LicenseExpressionEvaluator.ConvertInfixToPostfixLicenseExpression(new LicenseExpressionTokenizer(infix).Tokenize().ToArray());
-            Assert.Equal(postfix, string.Join(" ", postfixExpression.Select(e => e.Value)));
-        }
-
-        [Theory]
-        [InlineData("(LGPL-2.1 AND BSD-2-CLAUSE")]
-        [InlineData("MIT AND LPL-1.0)")]
-        [InlineData("MIT (AND) LPL-1.0")]
-        [InlineData("((MIT) (AND) (LPL-1.0))")]
-        [InlineData("MIT (AND LPL-1.0)")]
-        [InlineData("MIT  () OR LPL-1.0")]
-        [InlineData("MIT OR GPL-1.0 (WITH E8 OR LPL-1.0 )")]
-        public void LicenseExpressionEvaluator_ThrowsWhenBracketsDoNotMatch(string infix)
-        {
-            Assert.Throws<ArgumentException>(() => LicenseExpressionEvaluator.ConvertInfixToPostfixLicenseExpression(new LicenseExpressionTokenizer(infix).Tokenize().ToArray()));
-        }
-
-
         // TODO NK - Add tests with plus
         [Theory]
         [InlineData("MIT OR LPL-1.0", "MIT OR LPL-1.0", "OR")]
