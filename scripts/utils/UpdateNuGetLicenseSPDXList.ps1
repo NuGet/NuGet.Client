e9ed1d69640fa6797d501c2671f7b6f4e70d1f71
Nikolche Kolev <nikolev@microsoft.com>
2018-09-14 01:03:08 -0700

    work

--- scripts/utils/UpdateNuGetLicenseSPDXList.ps1
+++ scripts/utils/UpdateNuGetLicenseSPDXList.ps1
@@ -10,14 +10,16 @@ Downloads the SPDX License list, builds and runs the GenerateLicenseList tool an
 $licenseListDirectory = $([System.IO.Path]::Combine($env:TEMP, "NuGet", "licenseList"))
 
 try {
-    Write-Host "Downloading the license list."
+    $gitRepo = "https://github.com/spdx/license-list-data.git"
+    Write-Host "Downloading the license list from $gitRepo"
     git clone -b master --single-branch https://github.com/spdx/license-list-data.git $licenseListDirectory
     $generateLicenseList = $([System.IO.Path]::GetFullPath($([System.IO.Path]::Combine($PSScriptRoot, "..", "..", "test\TestExtensions\GenerateLicenseList\GenerateLicenseList.csproj"))))
 
     $licenses = $([System.IO.Path]::Combine($licenseListDirectory, "json", "licenses.json"))
     $exceptions = $([System.IO.Path]::Combine($licenseListDirectory, "json", "exceptions.json"))
     $targetFile = $([System.IO.Path]::GetFullPath($([System.IO.Path]::Combine($PSScriptRoot, "..", "..", "src\NuGet.Core\NuGet.Packaging\Licenses\NuGetLicenseData.cs"))))
-    Write-Host "Generating the license list!"
+    
+    Write-Host "Generating the license list."
 
     dotnet run --project $generateLicenseList $licenses $exceptions $targetFile
 }
