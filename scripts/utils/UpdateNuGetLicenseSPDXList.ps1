2179bc8518624209d6d9eb1b1eecc55738c47bb0
Nikolche Kolev <nikolev@microsoft.com>
2018-09-14 00:58:42 -0700

    work

--- scripts/utils/UpdateNuGetLicenseSPDXList.ps1
+++ scripts/utils/UpdateNuGetLicenseSPDXList.ps1
@@ -6,3 +6,23 @@ Updates the NuGet License file with the latest SPDX License list.
 Downloads the SPDX License list, builds and runs the GenerateLicenseList tool and later cleans up the check out SPDX license list data.
 
 #>
+
+$licenseListDirectory = $([System.IO.Path]::Combine($env:TEMP, "NuGet", "licenseList"))
+
+try {
+    Write-Host "Downloading the license list."
+    git clone -b master --single-branch https://github.com/spdx/license-list-data.git $licenseListDirectory
+    $generateLicenseList = $([System.IO.Path]::GetFullPath($([System.IO.Path]::Combine($PSScriptRoot, "..", "..", "test\TestExtensions\GenerateLicenseList\GenerateLicenseList.csproj"))))
+
+    $licenses = $([System.IO.Path]::Combine($licenseListDirectory, "json", "licenses.json"))
+    $exceptions = $([System.IO.Path]::Combine($licenseListDirectory, "json", "exceptions.json"))
+    $targetFile = $([System.IO.Path]::GetFullPath($([System.IO.Path]::Combine($PSScriptRoot, "..", "..", "src\NuGet.Core\NuGet.Packaging\Licenses\NuGetLicenseData.cs"))))
+    Write-Host "Generating the license list!"
+
+    dotnet run --project $generateLicenseList $licenses $exceptions $targetFile
+}
+finally {
+    Write-Host "Cleaning up."
+    Write-Host "Removing $licenseListDirectory."
+    Remove-Item -Force -Recurse $licenseListDirectory
+}
