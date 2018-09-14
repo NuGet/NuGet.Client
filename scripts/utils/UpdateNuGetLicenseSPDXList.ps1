ef76c7d6d3e51dc5d2fc63cfcd4b61ea67a4bc34
Nikolche Kolev <nikolev@microsoft.com>
2018-09-14 00:31:50 -0700

    work

---
+++ scripts/utils/UpdateNuGetLicenseSPDXList.ps1
@@ -0,0 +1,8 @@
+<#
+.SYNOPSIS
+Updates the NuGet License file with the latest SPDX License list.
+
+.DESCRIPTION
+Downloads the SPDX License list, builds and runs the GenerateLicenseList tool and later cleans up the check out SPDX license list data.
+
+#>
