2d7b68dea6ba222a5e04ac18f6b24e75b25c5b6b
Rohit Agrawal (NUGET) <ragrawal@microsoft.com>
2018-09-20 12:18:30 -0700

    fix projectfactory tests

--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/ProjectFactoryTest.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/ProjectFactoryTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -87,7 +87,7 @@ namespace NuGet.CommandLine
                     testAssembly.GetCustomAttributes<AssemblyMetadataAttribute>()
                         .Where(attr => attr.Key == "owner")
                         .Select(attr => attr.Value)
-                        .FirstOrDefault(),
+                        .FirstOrDefault() ?? "",
                     xdoc.XPathSelectElement("/package/metadata/authors").Value);
             }
         }
