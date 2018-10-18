1ad76f3d214e24ebdc8adc59c4a7acdac158a2df
Patricio Beltran <pbelsal@gmail.com>
2018-10-18 15:54:25 -0700

    Add tests for equals, ElementName and Clone (#2481)

--- src/NuGet.Core/NuGet.Configuration/Settings/Items/CredentialsItem.cs
+++ src/NuGet.Core/NuGet.Configuration/Settings/Items/CredentialsItem.cs
@@ -148,6 +148,11 @@ namespace NuGet.Configuration
                 newSetting.SetOrigin(Origin);
             }
 
+            foreach(var attr in Attributes)
+            {
+                newSetting.AddAttribute(attr.Key, attr.Value);
+            }
+
             return newSetting;
         }
 
