6ad5fed556817c70bef8e7722e523b222027df5b
Nikolche Kolev <nikolev@microsoft.com>
2018-11-16 11:46:43 -0800

    fix compilation

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/LicenseAcceptanceWindow.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/LicenseAcceptanceWindow.xaml.cs
@@ -1,13 +1,9 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Globalization;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
-using Microsoft.Internal.VisualStudio.PlatformUI.Automation;
-using NuGet.PackageManagement.UI.Utility;
-using NuGet.VisualStudio;
 
 namespace NuGet.PackageManagement.UI
 {
