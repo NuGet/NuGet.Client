c9271b01481875b609ba9f9e4c8f46a287d6835c
Pato Beltran <pbelsal@gmail.com>
2018-12-05 11:18:12 -0800

    fix tests

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/DotnetCliUtil.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/DotnetCliUtil.cs
@@ -35,7 +35,7 @@ namespace NuGet.XPlat.FuncTest
 #else
                 "Release",
 #endif
-                "netcoreapp2.0",
+                "netcoreapp2.1",
                 XPlatDll);
         }
 
