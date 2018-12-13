2529e92956ed200ab6381d364f4a93fccb1f50d0
Fernando Aguiilar <feaguila@microsoft.com>
2018-12-12 18:09:42 -0800

    spec, verify tests

---
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetSpecCommandTest.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NuGet.CommandLine.Test
+{
+    public class NuGetSpecCommandTests
+    {
+        [Theory]
+        [InlineData("spec -AssemblyPath x b a")]
+        [InlineData("nuget spec a b -Force")]
+        [InlineData("spec a b -?")]
+        public void SpecCommand_Failure_InvalidArguments(string cmd)
+        {
+            Util.TestCommandInvalidArguments(cmd);
+        }
+    }
+}
