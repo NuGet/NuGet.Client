4562edfa6d32271f71ec61822140ce164128a6c7
Pato Beltran <pbelsal@gmail.com>
2019-01-03 12:39:42 -0800

    update code to address submodule removal

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/ContentFiles/VirtualFileProvider.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/ContentFiles/VirtualFileProvider.cs
@@ -1,10 +1,11 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNet.FileProviders;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
 
 namespace NuGet.Commands
@@ -49,7 +50,7 @@ namespace NuGet.Commands
 
                 // Check if the entire subpath was matched
                 if (i == subPathParts.Length)
-                {
+                { 
                     // All items are files. The last string in the array will be the file name.
                     if (i == file.Length - 1)
                     {
@@ -77,7 +78,23 @@ namespace NuGet.Commands
 
         public IChangeToken Watch(string filter)
         {
-            return NoopChangeToken.Singleton;
+            return NullChangeToken.Singleton;
+        }
+
+        private class EnumerableDirectoryContents : IDirectoryContents
+        {
+            private readonly IEnumerable<IFileInfo> _entries;
+
+            public EnumerableDirectoryContents(IEnumerable<IFileInfo> entries)
+            {
+                _entries = entries ?? throw new ArgumentNullException(nameof(entries));
+            }
+
+            public bool Exists => true;
+
+            public IEnumerator<IFileInfo> GetEnumerator() => _entries.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => _entries.GetEnumerator();
         }
     }
 }
