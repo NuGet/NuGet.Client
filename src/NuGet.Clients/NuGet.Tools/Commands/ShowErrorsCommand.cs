7454ec1068bdec81c277c90628a8e5e91dca23ac
Nikolche Kolev <nikolev@microsoft.com>
2019-01-05 03:43:32 -0800

    work

--- src/NuGet.Clients/NuGet.Tools/Commands/ShowErrorsCommand.cs
+++ src/NuGet.Clients/NuGet.Tools/Commands/ShowErrorsCommand.cs
@@ -29,7 +29,9 @@ namespace NuGetVSExtension
             // get all services we need for display and activation of the NuGet output pane
             _vsOutputWindow = new Lazy<IVsOutputWindow>(() =>
             {
+#pragma warning disable VSTHRD011 // Use AsyncLazy<T>
                 return NuGetUIThreadHelper.JoinableTaskFactory.Run(async () =>
+#pragma warning restore VSTHRD011 // Use AsyncLazy<T>
                 {
                     await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
                     return (IVsOutputWindow)serviceProvider.GetService(typeof(SVsOutputWindow));
@@ -38,7 +40,9 @@ namespace NuGetVSExtension
 
             _vsUiShell = new Lazy<IVsUIShell>(() =>
             {
+#pragma warning disable VSTHRD011 // Use AsyncLazy<T>
                 return NuGetUIThreadHelper.JoinableTaskFactory.Run(async () =>
+#pragma warning restore VSTHRD011 // Use AsyncLazy<T>
                 {
                     await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
                     return (IVsUIShell)serviceProvider.GetService(typeof(SVsUIShell));
