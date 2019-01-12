89a8563dc83f7875c6406539d4f71db5ce61b7de
Andy Zivkovic <Andy.Zivkovic@microsoft.com>
2019-01-11 16:58:07 -0800

    Fix version number
    Previous increment should have only changed NetCoreAssemblyBuildNumber,
    not the patch version number.

--- src/NuGet.Clients/NuGet.Tools/NuGetPackage.cs
+++ src/NuGet.Clients/NuGet.Tools/NuGetPackage.cs
@@ -68,7 +68,7 @@ namespace NuGetVSExtension
     public sealed class NuGetPackage : AsyncPackage, IVsPackageExtensionProvider, IVsPersistSolutionOpts
     {
         // It is displayed in the Help - About box of Visual Studio
-        public const string ProductVersion = "5.0.1";
+        public const string ProductVersion = "5.0.0";
         private const string F1KeywordValuePmUI = "VS.NuGet.PackageManager.UI";
         private static readonly object _credentialsPromptLock = new object();
         private readonly HashSet<Uri> _credentialRequested = new HashSet<Uri>();
