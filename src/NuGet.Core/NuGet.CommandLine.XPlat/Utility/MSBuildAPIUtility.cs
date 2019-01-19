faa21b75fdcd9cbc8ffaaa1677a35e38511461d1
Heng Liu <liu.heng@microsoft.com>
2019-01-18 16:37:47 -0800

    fix bug in dotnet list package & add test

--- src/NuGet.Core/NuGet.CommandLine.XPlat/Utility/MSBuildAPIUtility.cs
+++ src/NuGet.Core/NuGet.CommandLine.XPlat/Utility/MSBuildAPIUtility.cs
@@ -375,6 +375,11 @@ namespace NuGet.CommandLine.XPlat
                 requestedTargets = filteredTargets;
             }
 
+            // In 2.2.100 of CLI. DotNet list package would show a section for each TFM and for each TFM/RID.
+            // Filtering the Targets to ignore TargetFramework + RID combination, only keep TargetFramework in requestedTargets.
+            // So that only one section will be shown for each TFM.
+            requestedTargets = requestedTargets.Where(target => target.RuntimeIdentifier == null).ToList();
+
             foreach (var target in requestedTargets)
             {
                 // Find the tfminformation corresponding to the target to
