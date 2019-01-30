3e2628490d92ecdafa7a03cdeb297ccb9f63b53c
Andy Zivkovic <zivkan@users.noreply.github.com>
2019-01-30 15:11:40 -0800

    Avoid needing to update build number ever again (#2702)

--- build/common.ps1
+++ build/common.ps1
@@ -415,15 +415,10 @@ Function Set-DelaySigning {
 }
 
 Function Get-BuildNumber() {
-    $SemanticVersionDate = '2017-02-27'
-    try {
-        [uint16](((Get-Date) - (Get-Date $SemanticVersionDate)).TotalMinutes / 5)
-    }
-    catch {
-        # Build number is a 16-bit integer. The limitation is imposed by VERSIONINFO.
-        # https://msdn.microsoft.com/en-gb/library/aa381058.aspx
-        Error-Log "Build number is out of range! Consider advancing SemanticVersionDate." -Fatal
-    }
+    $NuGetEpoch = '2010-08-29T23:58:25-07:00' # NuGet client first commit!
+    # Build number is a 16-bit integer. The limitation is imposed by VERSIONINFO.
+    # https://msdn.microsoft.com/en-gb/library/aa381058.aspx
+    [uint16]((((Get-Date) - (Get-Date $NuGetEpoch)).TotalMinutes / 5) % [uint16]::MaxValue)
 }
 
 Function Format-BuildNumber([int]$BuildNumber) {
