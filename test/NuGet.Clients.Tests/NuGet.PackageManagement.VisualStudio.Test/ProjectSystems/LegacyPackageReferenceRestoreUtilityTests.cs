9a7c0c94113dab7d6bba4f5ccb69e83a2c43f94a
Nikolche Kolev <nikolev@microsoft.com>
2019-02-19 17:38:07 -0800

    re-enable test

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.VisualStudio.Test/ProjectSystems/LegacyPackageReferenceRestoreUtilityTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.VisualStudio.Test/ProjectSystems/LegacyPackageReferenceRestoreUtilityTests.cs
@@ -1666,8 +1666,7 @@ namespace NuGet.PackageManagement.VisualStudio.Test
             }
         }
 
-        // Disabled this test for now. TODO: https://github.com/NuGet/Home/issues/7745
-        // [Fact]
+        [Fact]
         public async void DependencyGraphRestoreUtility_LegacyPackageRef_Restore_BuildTransitive()
         {
             using (var packageSource = TestDirectory.Create())
