1fb329f5eb4d87a5efc63d40262c7d4e5c1680c0
Fernando Aguilar <feaguila@microsoft.com>
2019-03-22 15:15:02 -0700

    Renamed list command to search command. Added deprecation messages to list command

--- src/NuGet.Clients/NuGet.CommandLine/NuGetResources.Designer.cs
+++ src/NuGet.Clients/NuGet.CommandLine/NuGetResources.Designer.cs
@@ -19,7 +19,7 @@ namespace NuGet.CommandLine {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class NuGetResources {
@@ -5538,6 +5538,15 @@ namespace NuGet.CommandLine {
         public static string ListCommand_ListNotSupported {
             get {
                 return ResourceManager.GetString("ListCommand_ListNotSupported", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to list command is deprecated. Use search command instead..
+        /// </summary>
+        public static string ListCommandDeprecatedMessage {
+            get {
+                return ResourceManager.GetString("ListCommandDeprecatedMessage", resourceCulture);
             }
         }
         
