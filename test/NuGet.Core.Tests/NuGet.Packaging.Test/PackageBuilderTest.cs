7b9704b5f5cfb87504e6af49a0987214c07b2158
Nikolche Kolev <nikolev@microsoft.com>
2019-03-26 11:44:43 -0700

    Improve the error case handling in the ManifestMetadata

--- test/NuGet.Core.Tests/NuGet.Packaging.Test/PackageBuilderTest.cs
+++ test/NuGet.Core.Tests/NuGet.Packaging.Test/PackageBuilderTest.cs
@@ -1979,7 +1979,7 @@ Description is required.");
         }
 
         [Fact]
-        public void PackageBuilderThrowsWhenLicenseUrlIsWhiteSpace()
+        public void PackageBuilderThrowsWhenLicenseUrlIsWhiteSpaceAndLicenseExpressionIsNotNull()
         {
             // Arrange
             string spec = @"<?xml version=""1.0"" encoding=""utf-8""?>
@@ -1990,13 +1990,14 @@ Description is required.");
     <authors>Velio Ivanov</authors>
     <description>This is the Description (With, Comma-Separated, Words, in Parentheses).</description>
     <language>en-US</language>
+    <license type=""expression"">MIT</license>
     <licenseUrl>    </licenseUrl>
     <requireLicenseAcceptance>false</requireLicenseAcceptance>
   </metadata>
 </package>";
 
             // Act
-            ExceptionAssert.Throws<Exception>(() => new PackageBuilder(spec.AsStream(), null), "LicenseUrl cannot be empty.");
+            ExceptionAssert.Throws<Exception>(() => new PackageBuilder(spec.AsStream(), null), $"LicenseUrl cannot be empty.{Environment.NewLine}The licenseUrl and license elements cannot be used together.");
         }
 
         [Fact]
