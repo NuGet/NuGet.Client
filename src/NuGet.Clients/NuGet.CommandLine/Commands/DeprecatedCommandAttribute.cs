e8fc73ac400126a7aea8bc4fcef2e9e77d730889
Fernando Aguilar <feaguila@microsoft.com>
2019-04-09 18:14:55 -0700

    Added copyright notice and AtttributeUsage attribute

--- src/NuGet.Clients/NuGet.CommandLine/Commands/DeprecatedCommandAttribute.cs
+++ src/NuGet.Clients/NuGet.CommandLine/Commands/DeprecatedCommandAttribute.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace NuGet.CommandLine
 {
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public sealed class DeprecatedCommandAttribute : Attribute
     {
         public Type AlternativeCommand { get; set; }
