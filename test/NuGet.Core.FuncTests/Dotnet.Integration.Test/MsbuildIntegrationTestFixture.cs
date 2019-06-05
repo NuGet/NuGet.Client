def7181576c21520a1800ab813f884932b38dd9b
Heng Liu <liu.heng@microsoft.com>
2019-06-04 21:45:52 -0700

    Revert "remove handle.exe, use handle64.exe only"
    This reverts commit 285afb7b59855e07306765d839843a2bc7a80b6b.

--- test/NuGet.Core.FuncTests/Dotnet.Integration.Test/MsbuildIntegrationTestFixture.cs
+++ test/NuGet.Core.FuncTests/Dotnet.Integration.Test/MsbuildIntegrationTestFixture.cs
@@ -492,8 +492,11 @@ namespace Dotnet.Integration.Test
             var resultbefore = "";
             try
             {
+                var result = RunHandle(handleArgs);
                 var result64 = RunHandle64(handleArgs);
                 resultbefore = "@@@@before delete : \n" + "The path is : " + path + "\n" +
+                                 "  @@@@The reuslts of running handle.exe is : \n" +
+                                                 result.AllOutput + "\n" +
                                  "  @@@@The reuslts of running handle64.exe is : \n" +
                                                  result64.AllOutput + "\n\n";
 
@@ -505,8 +508,12 @@ namespace Dotnet.Integration.Test
             }
             catch (UnauthorizedAccessException)
             {
+                var result = RunHandle(handleArgs);
                 var result64 = RunHandle64(handleArgs);
                 throw new UnauthorizedAccessException(resultbefore + "throw unauthorizedAccessException customized by Heng : \n" +
+                                                 "The path is : " + path + "\n" +
+                                                 "  %%%%The reuslts of running handle.exe is : \n" +
+                                                 result.AllOutput + "\n" +
                                                  "  %%%%The reuslts of running handle64.exe is : \n" +
                                                  result64.AllOutput + "\n");
             }
