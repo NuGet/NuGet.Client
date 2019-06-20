1a1156f2f493f2885f02711a6789b729c787b838
t-josi <t-josi@microsoft.com>
2019-06-20 15:48:57 -0700

    added changes

--- src/NuGet.Core/NuGet.Packaging/Rules/NoRefOrLibFolderInPackageRule.cs
+++ src/NuGet.Core/NuGet.Packaging/Rules/NoRefOrLibFolderInPackageRule.cs
@@ -11,6 +11,7 @@ namespace NuGet.Packaging.Rules
 {
     class NoRefOrLibFolderInPackageRule : IPackageRule
     {
+        private static readonly string BuildDir = PackagingConstants.Folders.Build + Path.DirectorySeparatorChar;
         public string MessageFormat { get; }
 
         public NoRefOrLibFolderInPackageRule(string messageFormat)
@@ -20,28 +21,27 @@ namespace NuGet.Packaging.Rules
 
         public IEnumerable<PackagingLogMessage> Validate(PackageArchiveReader package)
         {
-            var files = package.GetFiles();
-            if (!files
-                    .Select(t => PathUtility.GetPathWithDirectorySeparator(t))
-                    .Any(t => t.StartsWith(PackagingConstants.Folders.Lib + Path.DirectorySeparatorChar, StringComparison.OrdinalIgnoreCase)
+            var files = package.GetFiles().ToList().Select(t => PathUtility.GetPathWithDirectorySeparator(t));
+            if (!files.
+                Any(t => t.StartsWith(PackagingConstants.Folders.Lib + Path.DirectorySeparatorChar, StringComparison.OrdinalIgnoreCase)
                     || t.StartsWith(PackagingConstants.Folders.Ref + Path.DirectorySeparatorChar, StringComparison.OrdinalIgnoreCase)))
             {
                 //if you can't find the ref and lib folder, then find the build folder
-                if (files
-                    .Select(t => PathUtility.GetPathWithDirectorySeparator(t))
-                    .Any(t => t.StartsWith(PackagingConstants.Folders.Build + Path.DirectorySeparatorChar, StringComparison.OrdinalIgnoreCase)))
+                if (files.
+                    Select(t => PathUtility.GetPathWithDirectorySeparator(t)).
+                    Any(t => t.StartsWith(BuildDir, StringComparison.OrdinalIgnoreCase)))
                 {
-                    //if you can't find any folders other than native or any, raise an NU5127
-                    if (files
-                        .Select(t => PathUtility.GetPathWithDirectorySeparator(t))
-                        .Where(t => t.StartsWith(PackagingConstants.Folders.Build + Path.DirectorySeparatorChar))
-                        .Any(t => !t.StartsWith(PackagingConstants.Folders.Build + Path.DirectorySeparatorChar + PackagingConstants.Folders.Native + Path.DirectorySeparatorChar,
-                        StringComparison.OrdinalIgnoreCase)
-                        && !t.StartsWith(PackagingConstants.Folders.Build + Path.DirectorySeparatorChar + PackagingConstants.Folders.Any + Path.DirectorySeparatorChar,
-                        StringComparison.OrdinalIgnoreCase)))
+                    //if you can find any folders other than native or any, raise an NU5127
+                    if (files.
+                        Where(t => t.StartsWith(BuildDir)).
+                        Any(t => !t.StartsWith(BuildDir + PackagingConstants.Folders.Native + Path.DirectorySeparatorChar,
+                                StringComparison.OrdinalIgnoreCase)
+                            && !t.StartsWith(BuildDir + PackagingConstants.Folders.Any + Path.DirectorySeparatorChar,
+                                StringComparison.OrdinalIgnoreCase)))
                     {
                         var issue = new List<PackagingLogMessage>();
-                        issue.Add(PackagingLogMessage.CreateWarning(string.Format(MessageFormat,package.NuspecReader.GetId()), NuGetLogCode.NU5127));
+                        issue.Add(PackagingLogMessage.CreateWarning(string.Format(MessageFormat,package.NuspecReader.GetId()),
+                            NuGetLogCode.NU5127));
                         return issue;
                     }
                 }
