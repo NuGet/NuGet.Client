0cafbb804225f6bff61cf261ce0b0052aeb7d60a
Nikolche Kolev <nikolev@microsoft.com>
2019-08-09 11:59:46 -0700

    Revert "Update test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetPackCommandTest.cs"
    This reverts commit bacb1cf0eac56927b88474491eeabc7dc4fc6d8c.

--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetPackCommandTest.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetPackCommandTest.cs
@@ -5661,7 +5661,7 @@ $@"<package xmlns='http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd'>
                     }
 
                     using (var reader = new FileStream(packagePath, FileMode.Open))
-                    using (var ms = new MemoryStream(reader.Length))
+                    using (var ms = new MemoryStream())
                     {
                         reader.CopyTo(ms);
                         packageBytes[i] = ms.ToArray();
