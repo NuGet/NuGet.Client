f49ebd7e3d6ec42e09f0660e131d5ca358c9fd26
Nikolche Kolev <nikolev@microsoft.com>
2019-08-09 12:03:58 -0700

    Revert "update the bootstrapped CLI version that we use for testing, reenable the test"
    This reverts commit 42f1cd1787a633d701d6102ed3d69bea6f724d05.

--- test/NuGet.Core.FuncTests/Dotnet.Integration.Test/DotnetListPackageTests.cs
+++ test/NuGet.Core.FuncTests/Dotnet.Integration.Test/DotnetListPackageTests.cs
@@ -198,7 +198,7 @@ namespace Dotnet.Integration.Test
             }
         }
 
-        [PlatformFact(Platform.Windows)]
+        [Fact(Skip = "Requires a version of dotnet.exe that supports forwarding the --deprecated command argument.")]
         public void DotnetListPackage_DeprecatedAndOutdated_Fail()
         {
             using (var pathContext = new SimpleTestPathContext())
