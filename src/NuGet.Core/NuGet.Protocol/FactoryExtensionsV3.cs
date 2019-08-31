1d160d4f33d5b3922962213b05fa908bd8b0b31b
Damon Tivel <dtivel@microsoft.com>
2019-08-30 17:00:49 -0700

    API:  enable custom INuGetResourceProvider's (#3035)
    Resolve https://github.com/NuGet/Home/issues/8524.

--- src/NuGet.Core/NuGet.Protocol/FactoryExtensionsV3.cs
+++ src/NuGet.Core/NuGet.Protocol/FactoryExtensionsV3.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using NuGet.Configuration;
 using NuGet.Protocol.Core.Types;
-using NuGet.Protocol.LocalRepositories;
 
 namespace NuGet.Protocol
 {
@@ -28,56 +27,12 @@ namespace NuGet.Protocol
 
         public static IEnumerable<Lazy<INuGetResourceProvider>> GetCoreV3(this Repository.ProviderFactory factory)
         {
-            yield return new Lazy<INuGetResourceProvider>(() => new FeedTypeResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new DependencyInfoResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new DownloadResourcePluginProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new DownloadResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new MetadataResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new RawSearchResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new RegistrationResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new SymbolPackageUpdateResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new ReportAbuseResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PackageDetailsUriResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new ServiceIndexResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new ODataServiceDocumentResourceV2Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new HttpHandlerResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new HttpSourceResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PluginFindPackageByIdResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new HttpFileSystemBasedFindPackageByIdResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new RemoteV3FindPackageByIdResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new RemoteV2FindPackageByIdResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalV3FindPackageByIdResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalV2FindPackageByIdResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PackageUpdateResourceV2Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PackageUpdateResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new DependencyInfoResourceV2FeedProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new DownloadResourceV2FeedProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new MetadataResourceV2FeedProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new V3FeedListResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new V2FeedListResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalPackageListResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PackageSearchResourceV2FeedProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PackageSearchResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PackageMetadataResourceV2FeedProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PackageMetadataResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new AutoCompleteResourceV2FeedProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new AutoCompleteResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new PluginResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new RepositorySignatureResourceProvider());
+            if (factory == null)
+            {
+                throw new ArgumentNullException(nameof(factory));
+            }
 
-            // Local repository providers
-            yield return new Lazy<INuGetResourceProvider>(() => new FindLocalPackagesResourceUnzippedProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new FindLocalPackagesResourceV2Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new FindLocalPackagesResourceV3Provider());
-            yield return new Lazy<INuGetResourceProvider>(() => new FindLocalPackagesResourcePackagesConfigProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalAutoCompleteResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalDependencyInfoResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalDownloadResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalMetadataResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalPackageMetadataResourceProvider());
-            yield return new Lazy<INuGetResourceProvider>(() => new LocalPackageSearchResourceProvider());
-
-            yield break;
+            return factory.GetCoreV3();
         }
     }
 }
--- src/NuGet.Core/NuGet.Protocol/Repository.cs
+++ src/NuGet.Core/NuGet.Protocol/Repository.cs
@@ -1,23 +1,35 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using NuGet.Configuration;
+using NuGet.Protocol.LocalRepositories;
 
 namespace NuGet.Protocol.Core.Types
 {
     public static class Repository
     {
-        public static RepositoryFactory Factory
-        {
-            get { return new RepositoryFactory(); }
-        }
+        private static ProviderFactory _providerFactory = new ProviderFactory();
+
+        public static RepositoryFactory Factory { get; } = new RepositoryFactory();
 
         public static ProviderFactory Provider
         {
-            get { return new ProviderFactory(); }
+            get
+            {
+                return _providerFactory;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                _providerFactory = value;
+            }
         }
 
         public class RepositoryFactory
@@ -27,7 +39,57 @@ namespace NuGet.Protocol.Core.Types
 
         public class ProviderFactory
         {
-            // Methods are added by extension
+            public virtual IEnumerable<Lazy<INuGetResourceProvider>> GetCoreV3()
+            {
+                yield return new Lazy<INuGetResourceProvider>(() => new FeedTypeResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new DependencyInfoResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new DownloadResourcePluginProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new DownloadResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new MetadataResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new RawSearchResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new RegistrationResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new SymbolPackageUpdateResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new ReportAbuseResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PackageDetailsUriResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new ServiceIndexResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new ODataServiceDocumentResourceV2Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new HttpHandlerResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new HttpSourceResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PluginFindPackageByIdResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new HttpFileSystemBasedFindPackageByIdResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new RemoteV3FindPackageByIdResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new RemoteV2FindPackageByIdResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalV3FindPackageByIdResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalV2FindPackageByIdResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PackageUpdateResourceV2Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PackageUpdateResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new DependencyInfoResourceV2FeedProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new DownloadResourceV2FeedProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new MetadataResourceV2FeedProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new V3FeedListResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new V2FeedListResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalPackageListResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PackageSearchResourceV2FeedProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PackageSearchResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PackageMetadataResourceV2FeedProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PackageMetadataResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new AutoCompleteResourceV2FeedProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new AutoCompleteResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new PluginResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new RepositorySignatureResourceProvider());
+
+                // Local repository providers
+                yield return new Lazy<INuGetResourceProvider>(() => new FindLocalPackagesResourceUnzippedProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new FindLocalPackagesResourceV2Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new FindLocalPackagesResourceV3Provider());
+                yield return new Lazy<INuGetResourceProvider>(() => new FindLocalPackagesResourcePackagesConfigProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalAutoCompleteResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalDependencyInfoResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalDownloadResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalMetadataResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalPackageMetadataResourceProvider());
+                yield return new Lazy<INuGetResourceProvider>(() => new LocalPackageSearchResourceProvider());
+            }
         }
 
         /// <summary>
