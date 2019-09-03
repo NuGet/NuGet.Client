34e0edff977ecab47e53c66479e35bfda3107661
Scott Bommarito <scottbom@microsoft.com>
2019-09-03 12:57:32 -0700

    remove autogenerated junk reference

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/DetailControlModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/DetailControlModel.cs
@@ -7,7 +7,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Controls;
 using NuGet.PackageManagement.VisualStudio;
 using NuGet.Packaging.Core;
 using NuGet.ProjectManagement;
