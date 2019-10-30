ad05721e3fede5534142654d46dfa0df983d4955
Jeff Kluge <jeffkl@microsoft.com>
2019-10-30 13:14:22 -0700

    Update nuget.assert.ps1

--- test/EndToEnd/nuget.assert.ps1
+++ test/EndToEnd/nuget.assert.ps1
@@ -349,7 +349,7 @@ function Get-CacheFilePathFromProjectPath {
         $ProjectPath
         )
         
-    $projectCacheFilePath = Join-Path (Split-Path -parent $ProjectPath) (Join-Path "obj" "$(Split-Path -Leaf $ProjectPath).nuget.cache")
+    $projectCacheFilePath = Join-Path (Split-Path -parent $ProjectPath) (Join-Path "obj" "project.assets.nuget.cache")
 
     Write-Host "Evaluated cache file path:" $projectCacheFilePath
 
