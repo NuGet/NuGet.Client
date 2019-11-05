4dfb562715abd03638731fd1017c95969b9d759a
Rob Relyea <rrelyea@microsoft.com>
2019-11-04 16:57:04 -0800

    tweaks for review

--- src/NuGet.Clients/NuGet.CommandLine/Commands/SourcesCommand.cs
+++ src/NuGet.Clients/NuGet.CommandLine/Commands/SourcesCommand.cs
@@ -2,11 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Globalization;
 using System.Linq;
 using NuGet.Commands;
-using NuGet.Configuration;
-using NuGet.Credentials;
 
 namespace NuGet.CommandLine
 {
