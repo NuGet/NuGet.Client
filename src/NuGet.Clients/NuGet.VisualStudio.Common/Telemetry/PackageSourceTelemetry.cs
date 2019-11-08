56fdeb4192229a545efff0f924756f8269645202
Andy Zivkovic <Andy.Zivkovic@microsoft.com>
2019-11-08 15:29:22 -0800

    More fixes after more rebases

--- src/NuGet.Clients/NuGet.VisualStudio.Common/Telemetry/PackageSourceTelemetry.cs
+++ src/NuGet.Clients/NuGet.VisualStudio.Common/Telemetry/PackageSourceTelemetry.cs
@@ -13,13 +13,13 @@ namespace NuGet.VisualStudio.Telemetry
 {
     public sealed class PackageSourceTelemetry : IDisposable
     {
-        private ConcurrentDictionary<Uri, Data> _data;
+        private ConcurrentDictionary<string, Data> _data;
         private List<SourceRepository> _sources;
         private Guid _parentId;
 
         public PackageSourceTelemetry(List<SourceRepository> sources, Guid parentId)
         {
-            _data = new ConcurrentDictionary<Uri, Data>();
+            _data = new ConcurrentDictionary<string, Data>();
             ProtocolDiagnostics.Event += ProtocolDiagnostics_Event;
             _sources = sources;
             _parentId = parentId;
@@ -111,7 +111,7 @@ namespace NuGet.VisualStudio.Telemetry
             foreach (var kvp in _data)
             {
                 Data data = kvp.Value;
-                Uri sourceUri = kvp.Key;
+                string source = kvp.Key;
                 if (data.Metadata.EventTiming.Requests != 0 || data.Nupkg.EventTiming.Requests != 0)
                 {
                     var @event = new TelemetryEvent("PackageSourceDiagnostics",
@@ -121,18 +121,18 @@ namespace NuGet.VisualStudio.Telemetry
                         });
 
                     // source info
-                    @event.AddPiiData("source.url", sourceUri);
-                    var msFeed = GetMsFeed(sourceUri);
+                    @event.AddPiiData("source.url", source);
+                    var msFeed = GetMsFeed(source);
                     if (msFeed != null)
                     {
                         @event["source.msfeed"] = msFeed;
                     }
 
-                    SourceRepository source = _sources.FirstOrDefault(s => s.PackageSource.SourceUri == sourceUri);
+                    SourceRepository sourceFeed = _sources.FirstOrDefault(s => s.PackageSource.SourceUri.OriginalString == source);
                     if (source != null)
                     {
-                        @event["source.type"] = source.PackageSource.IsHttp ? "http" : source.PackageSource.IsLocal ? "local" : "unknown";
-                        @event["source.protocol"] = source.PackageSource.ProtocolVersion;
+                        @event["source.type"] = sourceFeed.PackageSource.IsHttp ? "http" : sourceFeed.PackageSource.IsLocal ? "local" : "unknown";
+                        @event["source.protocol"] = sourceFeed.PackageSource.ProtocolVersion;
                     }
 
                     // metadata
@@ -214,30 +214,33 @@ namespace NuGet.VisualStudio.Telemetry
             return @event;
         }
 
-        private string GetMsFeed(Uri sourceUri)
+        private string GetMsFeed(string source)
         {
-            var scheme = sourceUri.Scheme;
-            if (scheme.Equals("http", StringComparison.OrdinalIgnoreCase) || scheme.Equals("https", StringComparison.OrdinalIgnoreCase))
+            if (Uri.TryCreate(source, UriKind.Absolute, out var sourceUri))
             {
-                var host = sourceUri.Host;
-                if (host.Equals("nuget.org", StringComparison.OrdinalIgnoreCase) || host.EndsWith(".nuget.org", StringComparison.OrdinalIgnoreCase))
+                var scheme = sourceUri.Scheme;
+                if (scheme.Equals("http", StringComparison.OrdinalIgnoreCase) || scheme.Equals("https", StringComparison.OrdinalIgnoreCase))
                 {
-                    return "nuget.org";
+                    var host = sourceUri.Host;
+                    if (host.Equals("nuget.org", StringComparison.OrdinalIgnoreCase) || host.EndsWith(".nuget.org", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return "nuget.org";
+                    }
+                    else if (host.Equals("pkgs.dev.azure.com", StringComparison.OrdinalIgnoreCase) || host.EndsWith(".pkgs.visualstudio.com", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return "Azure Artifacts";
+                    }
+                    else if (host.Equals("nuget.pkg.github.com", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return "GitHub Package Registry";
+                    }
                 }
-                else if (host.Equals("pkgs.dev.azure.com", StringComparison.OrdinalIgnoreCase) || host.EndsWith(".pkgs.visualstudio.com", StringComparison.OrdinalIgnoreCase))
+                else
                 {
-                    return "Azure Artifacts";
-                }
-                else if (host.Equals("nuget.pkg.github.com", StringComparison.OrdinalIgnoreCase))
-                {
-                    return "GitHub Package Registry";
-                }
-            }
-            else
-            {
-                if (sourceUri.LocalPath.EndsWith(@"\Microsoft SDKs\NuGetPackages\", StringComparison.OrdinalIgnoreCase))
-                {
-                    return "VS Offline";
+                    if (sourceUri.LocalPath.EndsWith(@"\Microsoft SDKs\NuGetPackages\", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return "VS Offline";
+                    }
                 }
             }
 
