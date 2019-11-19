7de72dc373c5b55adb6bfa7b0324c6bcd075f0cc
Jeff Kluge <jeffkl@microsoft.com>
2019-11-19 12:52:17 -0800

    Refactor restore logic out of RestoreTask
    This will allow me to reuse the logic in a later commit that uses a different mechanism for building the DependencyGraphSpec

--- src/NuGet.Core/NuGet.Build.Tasks/BuildTasksUtility.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/BuildTasksUtility.cs
@@ -1,10 +1,22 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
+#if IS_CORECLR
+using System.Runtime.InteropServices;
+#endif
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Build.Framework;
+using NuGet.Commands;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Credentials;
 using NuGet.ProjectModel;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
 
 namespace NuGet.Build.Tasks
 {
@@ -97,5 +109,103 @@ namespace NuGet.Build.Tasks
             ProjectStyle.Standalone,
             ProjectStyle.ProjectJson
         };
+
+        public static async Task<bool> RestoreAsync(
+            DependencyGraphSpec dependencyGraphSpec,
+            bool interactive,
+            bool recursive,
+            bool noCache,
+            bool ignoreFailedSources,
+            bool disableParallel,
+            bool force,
+            bool forceEvaluate,
+            bool hideWarningsAndErrors,
+            Common.ILogger log,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                DefaultCredentialServiceUtility.SetupDefaultCredentialService(log, !interactive);
+
+                // Set connection limit
+                NetworkProtocolUtility.SetConnectionLimit();
+
+                // Set user agent string used for network calls
+#if IS_CORECLR
+                UserAgent.SetUserAgentString(new UserAgentStringBuilder("NuGet .NET Core MSBuild Task")
+                    .WithOSDescription(RuntimeInformation.OSDescription));
+#else
+                // OS description is set by default on Desktop
+                UserAgent.SetUserAgentString(new UserAgentStringBuilder("NuGet Desktop MSBuild Task"));
+#endif
+
+                // This method has no effect on .NET Core.
+                NetworkProtocolUtility.ConfigureSupportedSslProtocols();
+
+                var providerCache = new RestoreCommandProvidersCache();
+
+                using (var cacheContext = new SourceCacheContext())
+                {
+                    cacheContext.NoCache = noCache;
+                    cacheContext.IgnoreFailedSources = ignoreFailedSources;
+
+                    // Pre-loaded request provider containing the graph file
+                    var providers = new List<IPreLoadedRestoreRequestProvider>();
+
+                    // Add all child projects
+                    if (recursive)
+                    {
+                        AddAllProjectsForRestore(dependencyGraphSpec);
+                    }
+
+                    if (dependencyGraphSpec.Restore.Count < 1)
+                    {
+                        // Restore will fail if given no inputs, but here we should skip it and provide a friendly message.
+                        log.LogMinimal(Strings.NoProjectsToRestore);
+                        return true;
+                    }
+
+                    providers.Add(new DependencyGraphSpecRequestProvider(providerCache, dependencyGraphSpec));
+
+                    var restoreContext = new RestoreArgs()
+                    {
+                        CacheContext = cacheContext,
+                        LockFileVersion = LockFileFormat.Version,
+                        // 'dotnet restore' fails on slow machines (https://github.com/NuGet/Home/issues/6742)
+                        // The workaround is to pass the '--disable-parallel' option.
+                        // We apply the workaround by default when the system has 1 cpu.
+                        // This will fix restore failures on VMs with 1 CPU and containers with less or equal to 1 CPU assigned.
+                        DisableParallel = Environment.ProcessorCount == 1 ? true : disableParallel,
+                        Log = log,
+                        MachineWideSettings = new XPlatMachineWideSetting(),
+                        PreLoadedRequestProviders = providers,
+                        AllowNoOp = !force,
+                        HideWarningsAndErrors = hideWarningsAndErrors,
+                        RestoreForceEvaluate = forceEvaluate
+                    };
+
+                    if (restoreContext.DisableParallel)
+                    {
+                        HttpSourceResourceProvider.Throttle = SemaphoreSlimThrottle.CreateBinarySemaphore();
+                    }
+
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    var restoreSummaries = await RestoreRunner.RunAsync(restoreContext, cancellationToken);
+
+                    // Summary
+                    RestoreSummary.Log(log, restoreSummaries);
+
+                    return restoreSummaries.All(x => x.Success);
+                }
+            }
+            finally
+            {
+                // The CredentialService lifetime is for the duration of the process. We should not leave a potentially unavailable logger. 
+                // We need to update the delegating logger with a null instance
+                // because the tear downs of the plugins and similar rely on idleness and process exit.
+                DefaultCredentialServiceUtility.UpdateCredentialServiceDelegatingLogger(NullLogger.Instance);
+            }
+        }
     }
 }
