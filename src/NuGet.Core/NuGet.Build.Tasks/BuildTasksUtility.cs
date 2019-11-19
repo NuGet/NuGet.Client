ebfd45a81dd97cb36c95cc51c973859466f7bfcc
Jeff Kluge <jeffkl@microsoft.com>
2019-11-19 14:17:43 -0800

    Refactor logic in _GetRestoreProjectStyle target to a task (part 2)
    This is a reworking of my original change but this attempt should not break the Mono scenario.
    
    This change only touches the task assembly and not any common assembly.  In the Mono scenario, an older version of NuGet.Commands.dll is loaded and when the newer logic would call into it we'd get MissingMethodExceptions.
    
    This assembly is not loaded by anything but MSBuild so we know its always going to be the correct version.

--- src/NuGet.Core/NuGet.Build.Tasks/BuildTasksUtility.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/BuildTasksUtility.cs
@@ -1,9 +1,12 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Microsoft.Build.Framework;
+using NuGet.Configuration;
 using NuGet.ProjectModel;
 
 namespace NuGet.Build.Tasks
@@ -97,5 +100,80 @@ namespace NuGet.Build.Tasks
             ProjectStyle.Standalone,
             ProjectStyle.ProjectJson
         };
+
+        /// <summary>
+        /// Determines the restore style of a project.
+        /// </summary>
+        /// <param name="restoreProjectStyle">An optional user supplied restore style.</param>
+        /// <param name="hasPackageReferenceItems">A <see cref="bool"/> indicating whether or not the project has any PackageReference items.</param>
+        /// <param name="projectJsonPath">An optional path to the project's project.json file.</param>
+        /// <param name="projectDirectory">The full path to the project directory.</param>
+        /// <param name="projectName">The name of the project file.</param>
+        /// <param name="log">An <see cref="NuGet.Common.ILogger"/> object used to log messages.</param>
+        /// <returns>A <see cref="Tuple{ProjectStyle, Boolean}"/> containing the project style and a value indicating if the project is using a style that is compatible with PackageReference.
+        /// If the value of <paramref name="restoreProjectStyle"/> is not empty and could not be parsed, <code>null</code> is returned.</returns>
+        public static (ProjectStyle ProjectStyle, bool IsPackageReferenceCompatibleProjectStyle) GetProjectRestoreStyle(string restoreProjectStyle, bool hasPackageReferenceItems, string projectJsonPath, string projectDirectory, string projectName, Common.ILogger log)
+        {
+            ProjectStyle projectStyle;
+
+            // Allow a user to override by setting RestoreProjectStyle in the project.
+            if (!string.IsNullOrWhiteSpace(restoreProjectStyle))
+            {
+                if (!Enum.TryParse(restoreProjectStyle, ignoreCase: true, out projectStyle))
+                {
+                    // Any value that is not recognized is treated as Unknown
+                    projectStyle = ProjectStyle.Unknown;
+                }
+            }
+            else if (hasPackageReferenceItems)
+            {
+                // If any PackageReferences exist treat it as PackageReference. This has priority over project.json.
+                projectStyle = ProjectStyle.PackageReference;
+            }
+            else if (!string.IsNullOrWhiteSpace(projectJsonPath))
+            {
+                // If this is not a PackageReference project check if project.json or projectName.project.json exists.
+                projectStyle = ProjectStyle.ProjectJson;
+            }
+            else if (ProjectHasPackagesConfigFile(projectDirectory, projectName))
+            {
+                // If this is not a PackageReference or ProjectJson project check if packages.config or packages.ProjectName.config exists
+                projectStyle = ProjectStyle.PackagesConfig;
+            }
+            else
+            {
+                // This project is either a packages.config project or one that does not use NuGet at all.
+                projectStyle = ProjectStyle.Unknown;
+            }
+
+            bool isPackageReferenceCompatibleProjectStyle = projectStyle == ProjectStyle.PackageReference || projectStyle == ProjectStyle.DotnetToolReference;
+
+            return (projectStyle, isPackageReferenceCompatibleProjectStyle);
+        }
+
+        /// <summary>
+        /// Determines if the project has a packages.config file.
+        /// </summary>
+        /// <param name="projectDirectory">The full path of the project directory.</param>
+        /// <param name="projectName">The name of the project file.</param>
+        /// <returns><code>true</code> if a packages.config exists next to the project, otherwise <code>false</code>.</returns>
+        private static bool ProjectHasPackagesConfigFile(string projectDirectory, string projectName)
+        {
+            string packagesConfigPath = Path.Combine(projectDirectory, NuGetConstants.PackageReferenceFile);
+
+            if (File.Exists(packagesConfigPath))
+            {
+                return true;
+            }
+
+            packagesConfigPath = Path.Combine(projectDirectory, $"packages.{projectName}.config");
+
+            if (File.Exists(packagesConfigPath))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
