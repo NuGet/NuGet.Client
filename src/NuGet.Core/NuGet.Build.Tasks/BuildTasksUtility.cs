0c59e87628fbcbd158162ebb61638ce20e0dc75c
Jeff Kluge <jeffkl@microsoft.com>
2019-11-20 12:40:37 -0800

    Refactor restore logic out of RestoreTask (#3134)

--- src/NuGet.Core/NuGet.Build.Tasks/BuildTasksUtility.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/BuildTasksUtility.cs
@@ -1,10 +1,23 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
+#if IS_CORECLR
+using System.Runtime.InteropServices;
+#endif
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Commands;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Credentials;
 using NuGet.ProjectModel;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
 
 namespace NuGet.Build.Tasks
 {
@@ -34,8 +47,7 @@ namespace NuGet.Build.Tasks
         public static void AddAllProjectsForRestore(DependencyGraphSpec spec)
         {
             // Add everything from projects except for packages.config and unknown project types
-            foreach (var project in spec.Projects
-                .Where(project => RestorableTypes.Contains(project.RestoreMetadata.ProjectStyle)))
+            foreach (var project in spec.Projects.Where(DoesProjectSupportRestore))
             {
                 spec.AddRestore(project.RestoreMetadata.ProjectUniqueName);
             }
@@ -57,6 +69,16 @@ namespace NuGet.Build.Tasks
             {
                 properties.Add(toKey, propertyValue);
             }
+        }
+
+        /// <summary>
+        /// Determines if the specified <see cref="PackageSpec" /> supports restore.
+        /// </summary>
+        /// <param name="packageSpec">A <see cref="PackageSpec" /> for a project.</param>
+        /// <returns><code>true</code> if the project supports restore, otherwise <code>false</code>.</returns>
+        internal static bool DoesProjectSupportRestore(PackageSpec packageSpec)
+        {
+            return RestorableTypes.Contains(packageSpec.RestoreMetadata.ProjectStyle);
         }
 
         public static string GetPropertyIfExists(ITaskItem item, string key)
@@ -97,5 +119,113 @@ namespace NuGet.Build.Tasks
             ProjectStyle.Standalone,
             ProjectStyle.ProjectJson
         };
+
+        internal static async Task<bool> RestoreAsync(
+            DependencyGraphSpec dependencyGraphSpec,
+            bool interactive,
+            bool recursive,
+            bool noCache,
+            bool ignoreFailedSources,
+            bool disableParallel,
+            bool force,
+            bool forceEvaluate,
+            bool hideWarningsAndErrors,
+            Common.ILogger log,
+            CancellationToken cancellationToken)
+        {
+            if(dependencyGraphSpec == null)
+            {
+                throw new ArgumentNullException(nameof(dependencyGraphSpec));
+            }
+
+            if (log == null)
+            {
+                throw new ArgumentNullException(nameof(log));
+            }
+
+            try
+            {
+                DefaultCredentialServiceUtility.SetupDefaultCredentialService(log, !interactive);
+
+                // Set connection limit
+                NetworkProtocolUtility.SetConnectionLimit();
+
+                // Set user agent string used for network calls
+#if IS_CORECLR
+                UserAgent.SetUserAgentString(new UserAgentStringBuilder("NuGet .NET Core MSBuild Task")
+                    .WithOSDescription(RuntimeInformation.OSDescription));
+#else
+                // OS description is set by default on Desktop
+                UserAgent.SetUserAgentString(new UserAgentStringBuilder("NuGet Desktop MSBuild Task"));
+#endif
+
+                // This method has no effect on .NET Core.
+                NetworkProtocolUtility.ConfigureSupportedSslProtocols();
+
+                var providerCache = new RestoreCommandProvidersCache();
+
+                using (var cacheContext = new SourceCacheContext())
+                {
+                    cacheContext.NoCache = noCache;
+                    cacheContext.IgnoreFailedSources = ignoreFailedSources;
+
+                    // Pre-loaded request provider containing the graph file
+                    var providers = new List<IPreLoadedRestoreRequestProvider>();
+
+                    if (dependencyGraphSpec.Restore.Count < 1)
+                    {
+                        // Restore will fail if given no inputs, but here we should skip it and provide a friendly message.
+                        log.LogMinimal(Strings.NoProjectsToRestore);
+                        return true;
+                    }
+
+                    // Add all child projects
+                    if (recursive)
+                    {
+                        AddAllProjectsForRestore(dependencyGraphSpec);
+                    }
+
+                    providers.Add(new DependencyGraphSpecRequestProvider(providerCache, dependencyGraphSpec));
+
+                    var restoreContext = new RestoreArgs()
+                    {
+                        CacheContext = cacheContext,
+                        LockFileVersion = LockFileFormat.Version,
+                        // 'dotnet restore' fails on slow machines (https://github.com/NuGet/Home/issues/6742)
+                        // The workaround is to pass the '--disable-parallel' option.
+                        // We apply the workaround by default when the system has 1 cpu.
+                        // This will fix restore failures on VMs with 1 CPU and containers with less or equal to 1 CPU assigned.
+                        DisableParallel = Environment.ProcessorCount == 1 ? true : disableParallel,
+                        Log = log,
+                        MachineWideSettings = new XPlatMachineWideSetting(),
+                        PreLoadedRequestProviders = providers,
+                        AllowNoOp = !force,
+                        HideWarningsAndErrors = hideWarningsAndErrors,
+                        RestoreForceEvaluate = forceEvaluate
+                    };
+
+                    if (restoreContext.DisableParallel)
+                    {
+                        HttpSourceResourceProvider.Throttle = SemaphoreSlimThrottle.CreateBinarySemaphore();
+                    }
+
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    var restoreSummaries = await RestoreRunner.RunAsync(restoreContext, cancellationToken);
+
+                    // Summary
+                    RestoreSummary.Log(log, restoreSummaries);
+
+                    return restoreSummaries.All(x => x.Success);
+                }
+            }
+            finally
+            {
+                // The CredentialService lifetime is for the duration of the process. We should not leave a potentially unavailable logger. 
+                // We need to update the delegating logger with a null instance
+                // because the tear downs of the plugins and similar rely on idleness and process exit.
+                DefaultCredentialServiceUtility.UpdateCredentialServiceDelegatingLogger(NullLogger.Instance);
+            }
+        }
     }
 }
