302e21b9dba5df47d55b9988fdf9cf7693d73178
Fernando Aguilar <feaguila@microsoft.com>
2020-01-09 13:02:55 -0800

    LocalPackageSearchMetadata tests

--- test/NuGet.Core.Tests/NuGet.Protocol.Tests/LocalPackageSearchMetadataTests.cs
+++ test/NuGet.Core.Tests/NuGet.Protocol.Tests/LocalPackageSearchMetadataTests.cs
@@ -3,15 +3,57 @@
 
 using System;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
+using NuGet.Test.Utility;
 using NuGet.Versioning;
 using Xunit;
 
 namespace NuGet.Protocol.Tests
 {
-    public class LocalPackageSearchMetadataTests
+    public class LocalPackageSearchMetadataTests : IDisposable
     {
+        private readonly LocalPackageSearchMetadata _testInstance;
+        private readonly TestDirectory _packageDir;
+
+        public LocalPackageSearchMetadataTests()
+        {
+            var pkgId = new PackageIdentity("nuget.lpsm.test", new NuGetVersion(0, 0, 1));
+
+            var nuspecBuilder = NuspecBuilder.Create()
+                .WithPackageId(pkgId.Id)
+                .WithPackageVersion(pkgId.Version.ToNormalizedString())
+                .WithIcon("icon.jpg");
+
+            var pkgCtx = new SimpleTestPackageContext(pkgId.Id, pkgId.Version.ToNormalizedString());
+            pkgCtx.Nuspec = XDocument.Parse(nuspecBuilder.Build().ToString());
+
+            _packageDir = TestDirectory.Create();
+            
+            SimpleTestPackageUtility.CreatePackagesAsync(_packageDir.Path, pkgCtx).Wait();
+
+            var pkgPath = Path.Combine(_packageDir.Path, $"{pkgId.Id}.{pkgId.Version.ToNormalizedString()}.nupkg");
+            var info = new LocalPackageInfo(
+                identity: pkgId,
+                path: pkgPath,
+                lastWriteTimeUtc: DateTime.UtcNow,
+                nuspec: new Lazy<Packaging.NuspecReader>(() =>
+                {
+                    var reader = new PackageArchiveReader(pkgPath);
+                    return reader.NuspecReader;
+                }),
+                getPackageReader: () => new PackageArchiveReader(pkgPath));
+
+            _testInstance = new LocalPackageSearchMetadata(info);            
+        }
+
+        public void Dispose()
+        {
+            _packageDir.Dispose();
+        }
+
         [Fact]
         public async Task DeprecationMetadataIsNull()
         {
@@ -25,6 +67,21 @@ namespace NuGet.Protocol.Tests
             var localPackageSearchMetadata = new LocalPackageSearchMetadata(localPackageInfo);
 
             Assert.Null(await localPackageSearchMetadata.GetDeprecationMetadataAsync());
+        }
+
+        [Fact]
+        public void LocalPackageInfo_NotNull()
+        {
+            Assert.NotNull(_testInstance.LocalPackageInfo);
+        }
+
+        [Fact]
+        public void EmbeddedIcon_IconUrl_ReturnsFile()
+        {
+            Assert.NotNull(_testInstance.IconUrl);
+            Assert.True(_testInstance.IconUrl.IsFile);
+            Assert.True(_testInstance.IconUrl.IsAbsoluteUri);
+            Assert.NotNull(_testInstance.IconUrl.Fragment);
         }
     }
 }
