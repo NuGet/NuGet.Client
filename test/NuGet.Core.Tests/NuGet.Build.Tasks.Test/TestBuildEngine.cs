870841c87ad76468e1712a20e51d558fc0026201
Jeff Kluge <jeffkl@microsoft.com>
2020-01-23 12:57:39 -0800

    Add unit tests for RestoreTaskEx (#3190)

--- test/NuGet.Core.Tests/NuGet.Build.Tasks.Test/TestBuildEngine.cs
+++ test/NuGet.Core.Tests/NuGet.Build.Tasks.Test/TestBuildEngine.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using Microsoft.Build.Framework;
 using NuGet.Common;
 using NuGet.Test.Utility;
@@ -12,25 +13,44 @@ namespace NuGet.Build.Tasks.Test
     /// <summary>
     /// MSBuild logger -> TestLogger
     /// </summary>
-    public class TestBuildEngine : IBuildEngine
+    public class TestBuildEngine : IBuildEngine6
     {
         /// <summary>
         /// Test logger
         /// </summary>
         public TestLogger TestLogger = new TestLogger();
 
+        private readonly IReadOnlyDictionary<string, string> _globalProperties;
+
+        public TestBuildEngine()
+        {
+        }
+
+        public TestBuildEngine(IReadOnlyDictionary<string, string> globalProperties)
+        {
+            _globalProperties = globalProperties;
+        }
+        public int ColumnNumberOfTaskNode => 0;
+
         public bool ContinueOnError => false;
+
+        public bool IsRunningMultipleNodes { get; }
 
         public int LineNumberOfTaskNode => 0;
 
-        public int ColumnNumberOfTaskNode => 0;
-
         public string ProjectFileOfTaskNode => string.Empty;
 
-        public bool BuildProjectFile(string projectFileName, string[] targetNames, IDictionary globalProperties, IDictionary targetOutputs)
-        {
-            return true;
-        }
+        public bool BuildProjectFile(string projectFileName, string[] targetNames, IDictionary globalProperties, IDictionary targetOutputs) => true;
+
+        public bool BuildProjectFile(string projectFileName, string[] targetNames, IDictionary globalProperties, IDictionary targetOutputs, string toolsVersion) => throw new NotImplementedException();
+
+        public bool BuildProjectFilesInParallel(string[] projectFileNames, string[] targetNames, IDictionary[] globalProperties, IDictionary[] targetOutputsPerProject, string[] toolsVersion, bool useResultsCache, bool unloadProjectsOnCompletion) => throw new NotImplementedException();
+
+        public BuildEngineResult BuildProjectFilesInParallel(string[] projectFileNames, string[] targetNames, IDictionary[] globalProperties, IList<string>[] removeGlobalProperties, string[] toolsVersion, bool returnTargetOutputs) => throw new NotImplementedException();
+
+        public IReadOnlyDictionary<string, string> GetGlobalProperties() => _globalProperties;
+
+        public object GetRegisteredTaskObject(object key, RegisteredTaskObjectLifetime lifetime) => throw new NotImplementedException();
 
         public void LogCustomEvent(CustomBuildEventArgs e)
         {
@@ -41,7 +61,6 @@ namespace NuGet.Build.Tasks.Test
         {
             var message = new RestoreLogMessage(LogLevel.Error, e.Message)
             {
-
                 FilePath = e.File,
                 ProjectPath = e.ProjectFile
             };
@@ -77,6 +96,8 @@ namespace NuGet.Build.Tasks.Test
             TestLogger.Log(message);
         }
 
+        public void LogTelemetry(string eventName, IDictionary<string, string> properties) => throw new NotImplementedException();
+
         public void LogWarningEvent(BuildWarningEventArgs e)
         {
             var message = new RestoreLogMessage(LogLevel.Warning, e.Message)
@@ -92,5 +113,13 @@ namespace NuGet.Build.Tasks.Test
 
             TestLogger.Log(message);
         }
+
+        public void Reacquire() => throw new NotImplementedException();
+
+        public void RegisterTaskObject(object key, object obj, RegisteredTaskObjectLifetime lifetime, bool allowEarlyCollection) => throw new NotImplementedException();
+
+        public object UnregisterTaskObject(object key, RegisteredTaskObjectLifetime lifetime) => throw new NotImplementedException();
+
+        public void Yield() => throw new NotImplementedException();
     }
 }
