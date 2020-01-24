2e063a274777e4abc8621aa78f6bee7054e37cde
Jeff Kluge <jeffkl@microsoft.com>
2020-01-24 12:27:06 -0800

    Revert: Improve MSBuild SDK resolver by parsing global.json with System.Runtime.Serialization.Json #3157 (#3203)

--- src/NuGet.Core/Microsoft.Build.NuGetSdkResolver/GlobalJsonReader.cs
+++ src/NuGet.Core/Microsoft.Build.NuGetSdkResolver/GlobalJsonReader.cs
@@ -1,11 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.Build.Shared;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Xml;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Microsoft.Build.Framework;
 
 namespace Microsoft.Build.NuGetSdkResolver
@@ -25,26 +28,27 @@ namespace Microsoft.Build.NuGetSdkResolver
         /// <returns>A <see cref="Dictionary{String,String}"/> of MSBuild SDK versions from a global.json if found, otherwise <code>null</code>.</returns>
         public static Dictionary<string, string> GetMSBuildSdkVersions(SdkResolverContext context)
         {
-            var projectFile = new FileInfo(context.ProjectFilePath);
+            var projectDirectory = Directory.GetParent(context.ProjectFilePath);
 
-            if (!TryGetPathOfFileAbove(GlobalJsonFileName, projectFile.Directory, out var globalJsonPath))
+            if (projectDirectory == null
+                || !projectDirectory.Exists
+                || !TryGetPathOfFileAbove(GlobalJsonFileName, projectDirectory.FullName, out var globalJsonPath))
             {
                 return null;
             }
 
-            // Read the contents of global.json
-            var globalJsonContents = File.ReadAllText(globalJsonPath);
+            var contents = File.ReadAllText(globalJsonPath);
 
             // Look ahead in the contents to see if there is an msbuild-sdks section.  Deserializing the file requires us to load
-            // additional assemblies which can be a waste since global.json is usually ~100 bytes of text.
-            if (globalJsonContents.IndexOf(MSBuildSdksPropertyName, StringComparison.Ordinal) == -1)
+            // Newtonsoft.Json which is 500 KB while a global.json is usually ~100 bytes of text.
+            if (contents.IndexOf(MSBuildSdksPropertyName, StringComparison.Ordinal) == -1)
             {
                 return null;
             }
 
             try
             {
-                return ParseMSBuildSdksFromGlobalJson(globalJsonPath);
+                return Deserialize(contents);
             }
             catch (Exception e)
             {
@@ -56,86 +60,85 @@ namespace Microsoft.Build.NuGetSdkResolver
         }
 
         /// <summary>
-        /// Searches for a file based on the specified starting directory.
+        /// Deserializes a global.json and returns the MSBuild SDK versions
         /// </summary>
-        /// <param name="fileName">The name of the file to search for.</param>
-        /// <param name="startingDirectory">An optional <see cref="DirectoryInfo" /> representing the directory to start the search in.</param>
-        /// <param name="result">Receives the <see cref="FileInfo" /> of the file if found, otherwise <code>null</code>.</param>
-        /// <returns><code>true</code> if a file was found in the directory or any parent directory, otherwise <code>false</code>.</returns>
-        public static bool TryGetPathOfFileAbove(string fileName, DirectoryInfo startingDirectory, out string result)
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static Dictionary<string, string> Deserialize(string value)
         {
-            result = null;
+            return JsonConvert.DeserializeObject<GlobalJsonFile>(value).MSBuildSdks;
+        }
 
-            if (startingDirectory == null || !startingDirectory.Exists)
-            {
-                return false;
-            }
-
-            var lookInDirectory = startingDirectory;
-
-            do
-            {
-                var possibleFile = new FileInfo(Path.Combine(lookInDirectory.FullName, fileName));
-
-                if (possibleFile.Exists)
-                {
-                    result = possibleFile.FullName;
-
-                    return true;
-                }
-
-                lookInDirectory = lookInDirectory.Parent;
-            }
-            while (lookInDirectory != null);
-
-            return false;
+        private sealed class GlobalJsonFile
+        {
+            [JsonProperty(MSBuildSdksPropertyName)]
+            public Dictionary<string, string> MSBuildSdks { get; set; }
         }
 
         /// <summary>
-        /// Parses a global.json and returns the MSBuild SDK versions.
+        /// Searches for a file based on the specified starting directory.
         /// </summary>
-        /// <remarks>
-        /// NoInlining is enabled ensure that System.Runtime.Serialization.Json.dll isn't loaded unless the method is called.
-        /// </remarks>
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        private static Dictionary<string, string> ParseMSBuildSdksFromGlobalJson(string path)
+        /// <param name="file">The file to search for.</param>
+        /// <param name="startingDirectory">An optional directory to start the search in.  The default location is the directory
+        /// of the file containing the property function.</param>
+        /// <returns>The full path of the file if it is found, otherwise an empty string.</returns>
+        private static string GetPathOfFileAbove(string file, string startingDirectory)
         {
-            Dictionary<string, string> sdks = null;
+            // Search for a directory that contains that file
+            var directoryName = GetDirectoryNameOfFileAbove(startingDirectory, file);
 
-            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
-            using (var reader = System.Runtime.Serialization.Json.JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max))
+            return string.IsNullOrEmpty(directoryName) ? string.Empty : NormalizePath(Path.Combine(directoryName, file));
+        }
+
+        private static bool TryGetPathOfFileAbove(string file, string startingDirectory, out string fullPath)
+        {
+            fullPath = GetPathOfFileAbove(file, startingDirectory);
+
+            return fullPath != string.Empty;
+        }
+
+        /// <summary>
+        /// Locate a file in either the directory specified or a location in the
+        /// directory structure above that directory.
+        /// </summary>
+        private static string GetDirectoryNameOfFileAbove(string startingDirectory, string fileName)
+        {
+            // Canonicalize our starting location
+            var lookInDirectory = Path.GetFullPath(startingDirectory);
+
+            do
             {
-                while (reader.Read())
+                // Construct the path that we will use to test against
+                var possibleFileDirectory = Path.Combine(lookInDirectory, fileName);
+
+                // If we successfully locate the file in the directory that we're
+                // looking in, simply return that location. Otherwise we'll
+                // keep moving up the tree.
+                if (File.Exists(possibleFileDirectory))
                 {
-                    if (reader.LocalName.Equals(MSBuildSdksPropertyName) && reader.Depth == 1)
-                    {
-                        while (reader.Read())
-                        {
-                            if (reader.NodeType == XmlNodeType.Element)
-                            {
-                                string name = reader.LocalName.Trim();
-
-                                if (!string.IsNullOrWhiteSpace(name) && reader.Read() && reader.NodeType == XmlNodeType.Text)
-                                {
-                                    if (sdks == null)
-                                    {
-                                        sdks = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-                                    }
-
-                                    var value = reader.Value.Trim();
-
-                                    if (!string.IsNullOrWhiteSpace(value))
-                                    {
-                                        sdks[name] = value;
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    // We've found the file, return the directory we found it in
+                    return lookInDirectory;
+                }
+                else
+                {
+                    // GetDirectoryName will return null when we reach the root
+                    // terminating our search
+                    lookInDirectory = Path.GetDirectoryName(lookInDirectory);
                 }
             }
+            while (lookInDirectory != null);
 
-            return sdks;
+            // When we didn't find the location, then return an empty string
+            return string.Empty;
+        }
+
+        private static string NormalizePath(string path)
+        {
+            return FixFilePath(Path.GetFullPath(path));
+
+        }
+        private static string FixFilePath(string path)
+        {
+            return string.IsNullOrEmpty(path) || Path.DirectorySeparatorChar == '\\' ? path : path.Replace('\\', '/');
         }
     }
 }
