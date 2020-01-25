9d7a043203eb97dcafe4ea8202a4afefdd49ad0b
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2020-01-25 11:52:18 -0800

    added unit test for msbuild package config restore switch

--- src/NuGet.Core/NuGet.Common/XmlUtility.cs
+++ src/NuGet.Core/NuGet.Common/XmlUtility.cs
@@ -6,21 +6,18 @@ namespace NuGet.Common
 {
     public static class XmlUtility
     {
-        //
-        // Summary:
-        //     Creates a new System.Xml.Linq.XDocument from an System.Xml.XmlReader.
-        //
-        // Parameters:
-        //   filePath:
-        //     The URI for the file containing the XML data.
-        //
-        // Returns:
-        //     An System.Xml.Linq.XDocument that contains the contents of the specified System.Xml.XmlReader.
+        /// <summary>
+        /// Creates a new System.Xml.Linq.XDocument from an System.Xml.XmlReader.
+        /// </summary>
+        /// <param name="filePath">Path for the file containing the XML data.</param>
+        /// <returns>An <see cref="System.Xml.Linq.XDocument"/> contains the contents of the specified Xml file.</returns>
         public static XDocument Load(string filePath)
         {
             if (string.IsNullOrEmpty(filePath))
                 throw new ArgumentNullException(nameof(filePath));
 
+            //This overloaded method of XmlReader.Create creates an instance of
+            //XmlReaderSettings with default values that are safe
             using (var reader = XmlReader.Create(filePath))
             {
                 return XDocument.Load(reader);
