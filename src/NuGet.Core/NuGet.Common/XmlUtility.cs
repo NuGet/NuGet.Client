828ef539540e726151217f9330fd5f12a75d4809
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2020-01-28 18:00:14 -0800

    added null check for parameter

--- src/NuGet.Core/NuGet.Common/XmlUtility.cs
+++ src/NuGet.Core/NuGet.Common/XmlUtility.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -15,6 +16,11 @@ namespace NuGet.Common
         /// <returns>An <see cref="System.Xml.Linq.XDocument"/> that contains the contents of the specified file.</returns>
         public static XDocument Load(string filePath)
         {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException(Strings.ArgumentNullOrEmpty, nameof(filePath));
+            }
+
             //This overloaded method of XmlReader.Create creates an instance of
             //XmlReaderSettings with default values that are safe
             using (var reader = XmlReader.Create(filePath))
