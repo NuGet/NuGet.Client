4c171de8f66650b7782eceaa8b0feedf6c8a9f6c
Rob Relyea <rrelyea@microsoft.com>
2020-02-19 15:03:11 -0800

    Fix accessibilty of PackageItemControl - move from ControlTemplate to LogicalTree (#3236)
    * Fix accessibilty of PackageItemControl - move from ControlTemplate to LogicalTree

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/PackageStatus.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/PackageStatus.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 namespace NuGet.PackageManagement.UI
@@ -9,7 +9,7 @@ namespace NuGet.PackageManagement.UI
 
         // the latest applicable version is installed.
         Installed,
-        
+
         UpdateAvailable,
 
         // The package is installed but may not be managed.
