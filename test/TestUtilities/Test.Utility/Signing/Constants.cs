ec19f7f3a4bad9e06f9c657499cfda709fef24bb
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2020-02-18 22:04:14 -0800

    created a constants file to maintain strings and modified string refs

---
+++ test/TestUtilities/Test.Utility/Signing/Constants.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Utility.Signing
+{
+    public static class Constants
+    {
+        public const string Windows_NetFulFrameworkFolder = "Windows_NetFulFramework";
+        public const string Windows_NetCoreFolder = "Windows_NetCore";
+        public const string Mac_NetCoreFolder = "Mac_NetCore";
+        public const string Linux_NetCoreFolder = "Linux_NetCore";
+        public const string PreGenPackagesFolder = "PreGenPackages";
+    }
+}
