fdc9aaf5b9d112060d06613f65114e4c5fa45d9c
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2020-02-25 13:45:28 -0800

    reverting changes. GetFirstFileName API moved to TestFileSystemUtility

--- src/NuGet.Core/NuGet.Common/PathUtil/FileUtility.cs
+++ src/NuGet.Core/NuGet.Common/PathUtil/FileUtility.cs
@@ -2,9 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,18 +24,6 @@ namespace NuGet.Common
             var fileName = $"{Guid.NewGuid()}.tmp".ToLowerInvariant();
 
             return Path.GetFullPath(Path.Combine(directory, fileName));
-        }
-        /// <summary>
-        /// Returns the name of first file that match the specified search pattern
-        /// </summary>
-        /// <param name="path"></param>
-        /// <param name="searchPattern"><see cref="System.IO.Directory.GetFiles(string, string)"/></param>
-        /// <returns></returns>
-        public static string GetFirstFileName(string path, string searchPattern)
-        {
-            var files = Directory.GetFiles(path, searchPattern);
-
-            return files.FirstOrDefault();
         }
 
         /// <summary>
