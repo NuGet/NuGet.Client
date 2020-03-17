6b14f21ee4925fa8e8fb0f92007517f04a5960e1
Fernando Aguilar <fer.aguilar.reyes@gmail.com>
2020-03-17 10:49:57 -0600

    Addressed feedback

--- tools-local/doc.tasks/GenerateMarkdownDoc.cs
+++ tools-local/doc.tasks/GenerateMarkdownDoc.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.XPath;
 using Microsoft.Build.Framework;
@@ -12,9 +13,13 @@ namespace NuGetTasks
     public class GenerateMarkdownDoc : Task
     {
         /// <summary>
-        /// Project files to process
+        /// Project files item group
         /// </summary>
-        public ITaskItem[] ProjectFiles { get; set; }
+        public ITaskItem[] ProductProjects { get; set; }
+        /// <summary>
+        /// Test project files item group
+        /// </summary>
+        public ITaskItem[] TestProjects { get; set; }
         /// <summary>
         /// Filepath to the generated documentation
         /// </summary>
@@ -30,11 +35,23 @@ namespace NuGetTasks
         /// <summary>
         /// Title for the document
         /// </summary>
-        public string Title { get; set; } = "NuGet Project Overview";
+        public string Title { get; set; } = "NuGet Client Projects Overview";
+        /// <summary>
+        /// Sub-title for projects list
+        /// </summary>
+        public string ProjectsSubtitle { get; set; } = "Projects";
+        /// <summary>
+        /// Sub-title for test projects list
+        /// </summary>
+        public string TestProjectsSubtitle { get; set; } = "Test Projects";
+        /// <summary>
+        /// FullPath MSBuild item metadata
+        /// </summary>
+        private static readonly string MetaFullPath = "FullPath";
 
         public override bool Execute()
         {
-            var outputFilePath = OutputFile.GetMetadata("FullPath");
+            var outputFilePath = OutputFile.GetMetadata(MetaFullPath);
 
             var dirname = Path.GetDirectoryName(outputFilePath);
             Directory.CreateDirectory(dirname);
@@ -47,19 +64,30 @@ namespace NuGetTasks
                 file.WriteLine($"date-generated: {DateTime.Now:s}");
                 file.WriteLine("---\n");
 
-                file.WriteLine($"# {Title}\n\n");
+                file.WriteLine($"\n\n# {Title}\n\n");
 
-                for (int i = 0; i < ProjectFiles.Length; i++)
-                {
-                    var projectPath = ProjectFiles[i].GetMetadata("FullPath");
-                    var desc = GetDescriptions(projectPath);
+                file.WriteLine($"\n\n## {ProjectsSubtitle}\n\n");
+                SortAndWriteDescriptions(ProductProjects, file);
 
-                    file.WriteLine($"- [`{RelativizePath(projectPath)}`]({GenerateGitHubLink(projectPath)}): {desc}");
-                }
+                file.WriteLine($"\n\n## {TestProjectsSubtitle}\n\n");
+                SortAndWriteDescriptions(TestProjects, file);
             }
             Log.LogMessage(MessageImportance.Low, "Documentation complete");
 
             return true;
+        }
+
+        private void SortAndWriteDescriptions(ITaskItem[] projects, StreamWriter file)
+        {
+            Array.Sort(projects, (a, b) => a.GetMetadata(MetaFullPath).CompareTo(b.GetMetadata(MetaFullPath)));
+
+            for (int i = 0; i < projects.Length; i++)
+            {
+                var projectPath = projects[i].GetMetadata(MetaFullPath);
+                var desc = GetDescriptions(projectPath);
+
+                file.WriteLine($"- [`{RelativizePath(projectPath)}`]({GenerateGitHubLink(projectPath)}): {desc}");
+            }
         }
 
         private string GetDescriptions(string projectFilePath)
