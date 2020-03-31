abd278337102dfce8c2751923febfcec50502dda
Jeff Kluge <jeffkl@microsoft.com>
2020-03-31 13:17:26 -0700

    Add escape hatch

--- src/NuGet.Core/NuGet.Build.Tasks.Console/MSBuildStaticGraphRestore.cs
+++ src/NuGet.Core/NuGet.Build.Tasks.Console/MSBuildStaticGraphRestore.cs
@@ -117,7 +117,7 @@ namespace NuGet.Build.Tasks.Console
                     forceEvaluate: IsOptionTrue(nameof(RestoreTaskEx.ForceEvaluate), options),
                     hideWarningsAndErrors: IsOptionTrue(nameof(RestoreTaskEx.HideWarningsAndErrors), options),
                     restorePC: IsOptionTrue(nameof(RestoreTaskEx.RestorePackagesConfig), options),
-                    cleanupAssetsForUnsupportedProjects: true,
+                    cleanupAssetsForUnsupportedProjects: IsOptionTrue(nameof(RestoreTaskEx.CleanupAssetsForUnsupportedProjects), options),
                     log: MSBuildLogger,
                     cancellationToken: CancellationToken.None);
             }
