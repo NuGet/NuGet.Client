f7623b144ceaa6034b2c93a263c9f1da94ec59ef
Cristina Manu <cmanu@microsoft.com>
2020-03-31 11:56:57 -0700

    Test update and revert the version as a version change will invalidate loc files for the package config scenarios when restore with old tools.

--- test/NuGet.Core.Tests/NuGet.ProjectModel.Test/ProjectLockFile/LockFileUtilitiesTests.cs
+++ test/NuGet.Core.Tests/NuGet.ProjectModel.Test/ProjectLockFile/LockFileUtilitiesTests.cs
@@ -18,7 +18,7 @@ namespace NuGet.ProjectModel.Test.ProjectLockFile
         {
             var x = new PackagesLockFileBuilder().Build();
             var y = new PackagesLockFileBuilder()
-                .WithVersion(2)
+                .WithVersion(PackagesLockFileFormat.Version + 1)
                 .Build();
 
             var actual = PackagesLockFileUtilities.IsLockFileStillValid(x, y);
