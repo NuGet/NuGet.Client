64a412deaeed8d5b16059a7c0f7c05a382edbeaa
Nikolche Kolev <nikolev@microsoft.com>
2020-04-06 11:02:30 -0700

    work

--- src/NuGet.Clients/NuGet.SolutionRestoreManager/BrokeredServicesUtility.cs
+++ src/NuGet.Clients/NuGet.SolutionRestoreManager/BrokeredServicesUtility.cs
@@ -11,17 +11,18 @@ namespace NuGet.SolutionRestoreManager
 {
     internal static class BrokeredServicesUtility
     {
+        // It is very important that these names and versions are kept the same as the ones in NuGet.VisualStudio.Internal.Contracts.NuGetServices.
+        internal static string NuGetSolutionServiceName = "NuGetSolutionService";
+        internal static string NuGetSolutionServiceVersion = "1.0.0";
+
+        internal static ServiceRpcDescriptor NuGetSolutionService = new ServiceJsonRpcDescriptor(
+            new ServiceMoniker(NuGetSolutionServiceName, new Version(NuGetSolutionServiceVersion)),
+            ServiceJsonRpcDescriptor.Formatters.UTF8,
+            ServiceJsonRpcDescriptor.MessageDelimiters.HttpLikeHeaders);
+
         internal static BrokeredServiceFactory GetNuGetSolutionServicesFactory()
         {
             return (mk, options, sb, ct) => new ValueTask<object>(new NuGetSolutionService());
         }
-
-        internal static string NuGetSolutionServiceName = "NuGetSolutionService";
-        internal static string NuGetSolutionServiceVersion = "1.0.0";
-
-        public static ServiceRpcDescriptor NuGetSolutionService = new ServiceJsonRpcDescriptor(
-            new ServiceMoniker(NuGetSolutionServiceName, new Version(NuGetSolutionServiceVersion)),
-            ServiceJsonRpcDescriptor.Formatters.UTF8,
-            ServiceJsonRpcDescriptor.MessageDelimiters.HttpLikeHeaders);
     }
 }
