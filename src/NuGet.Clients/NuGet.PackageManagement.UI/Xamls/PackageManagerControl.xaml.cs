dfefc32a47f65185335a9da0f4ce143ed6b17d43
Fernando Aguilar <fer.aguilar.reyes@gmail.com>
2020-04-07 13:57:25 -0500

    ***NO_CI*** testing converter strategy

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageManagerControl.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageManagerControl.xaml.cs
@@ -998,7 +998,7 @@ namespace NuGet.PackageManagement.UI
             }
         }
 
-        private bool IsTabChanged = false;
+        internal bool IsTabChanged { get; set; } = false;
 
         private void Filter_SelectionChanged(object sender, FilterChangedEventArgs e)
         {
