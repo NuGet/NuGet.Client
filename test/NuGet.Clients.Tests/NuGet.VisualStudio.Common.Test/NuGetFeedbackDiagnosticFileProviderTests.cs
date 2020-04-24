813ae872d74bd0140aff757b87b5aa513996079e
Rob Relyea <rrelyea@microsoft.com>
2020-04-24 12:34:59 -0700

    move nuget.tools.test tests into appropriate test projects

--- test/NuGet.Clients.Tests/NuGet.VisualStudio.Common.Test/NuGetFeedbackDiagnosticFileProviderTests.cs
+++ test/NuGet.Clients.Tests/NuGet.VisualStudio.Common.Test/NuGetFeedbackDiagnosticFileProviderTests.cs
@@ -2,10 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.IO;
-using NuGet.VisualStudio.Common;
 using Xunit;
 
-namespace NuGet.Tools.Test
+namespace NuGet.VisualStudio.Common.Test
 {
     public class NuGetFeedbackDiagnosticFileProviderTests
     {
