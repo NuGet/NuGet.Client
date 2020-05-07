56192b1c7946aaa666bc56dba2f857724b808a92
Rob Relyea <rrelyea@microsoft.com>
2020-05-07 11:23:02 -0700

    work in progress - show PackageManager UI from OE.Client.dll codepaths

--- src/NuGet.Clients/NuGet.VisualStudio.OnlineEnvironment.Client/GuidList.cs
+++ src/NuGet.Clients/NuGet.VisualStudio.OnlineEnvironment.Client/GuidList.cs
@@ -9,5 +9,14 @@ namespace NuGet.VisualStudio.OnlineEnvironment.Client
     {
         // NuGet Output window pane
         public static Guid GuidNuGetOutputWindowPaneGuid = Guid.Parse("CEC55EC8-CC51-40E7-9243-57B87A6F6BEB");
+        public static readonly Guid NuGetEditorType = Guid.Parse(NuGetEditorTypeString);
+
+        // Unique identifier of the editor factory that created an instance of the document view and document data objects.
+        // Used when creating document windows of Package Manager
+        private const string NuGetEditorTypeString = "95501c48-a850-47c1-a785-2aaa96637f81";
+
+        // Online Environments in 16.4
+        public const string CloudEnvironmentConnectedUIContextGuidString = "{CE73BF3D-D614-438A-9B93-24E9E9D7453A}";
+        public static readonly Guid CloudEnvironmentConnectedUIContextGuid = new Guid(CloudEnvironmentConnectedUIContextGuidString);
     }
 }
--- src/NuGet.Clients/NuGet.VisualStudio.OnlineEnvironment.Client/SolutionExplorer/PackageManagerUICommandHandler.cs
+++ src/NuGet.Clients/NuGet.VisualStudio.OnlineEnvironment.Client/SolutionExplorer/PackageManagerUICommandHandler.cs
@@ -2,23 +2,118 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel.Composition;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using EnvDTE;
+using Microsoft;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Threading;
 using Microsoft.VisualStudio.Workspace.VSIntegration.UI;
+using NuGet.Configuration;
+using NuGet.PackageManagement;
+using NuGet.PackageManagement.UI;
+using NuGet.PackageManagement.VisualStudio;
+using NuGet.ProjectManagement;
 using IAsyncServiceProvider = Microsoft.VisualStudio.Shell.IAsyncServiceProvider;
+using Resx = NuGet.PackageManagement.UI.Resources;
 using Task = System.Threading.Tasks.Task;
+using vsShellInterop = Microsoft.VisualStudio.Shell.Interop;
 
 namespace NuGet.VisualStudio.OnlineEnvironment.Client
 {
     internal class PackageManagerUICommandHandler
     {
-        private const bool IsPackageManagerUISupportAvailable = false;
+        private const bool IsPackageManagerUISupportAvailable = true;
         private readonly JoinableTaskFactory _joinableTaskFactory;
         private readonly IAsyncServiceProvider _asyncServiceProvider;
+
+        private AsyncLazy<vsShellInterop.IVsMonitorSelection> _vsMonitorSelection;
+        private vsShellInterop.IVsMonitorSelection VsMonitorSelection => ThreadHelper.JoinableTaskFactory.Run(_vsMonitorSelection.GetValueAsync);
+
+        private DTE _dte;
+        private DTEEvents _dteEvents;
+        private const string F1KeywordValuePmUI = "VS.NuGet.PackageManager.UI";
+        private uint _solutionExistsAndFullyLoadedContextCookie;
+        private uint _solutionNotBuildingAndNotDebuggingContextCookie;
+        private uint _solutionExistsCookie;
+
+        private bool _initialized;
 
         public PackageManagerUICommandHandler(JoinableTaskFactory joinableTaskFactory, IAsyncServiceProvider asyncServiceProvider)
         {
             _joinableTaskFactory = joinableTaskFactory ?? throw new ArgumentNullException(nameof(joinableTaskFactory));
             _asyncServiceProvider = asyncServiceProvider ?? throw new ArgumentNullException(nameof(asyncServiceProvider));
+			Initialize();
+		}
+
+        [Import]
+        private Lazy<IConsoleStatus> ConsoleStatus { get; set; }
+
+        [Import]
+        private Lazy<IDeleteOnRestartManager> DeleteOnRestartManager { get; set; }
+
+        [Import]
+        private Lazy<INuGetUILogger> OutputConsoleLogger { get; set; }
+
+        [Import]
+        private Lazy<INuGetProjectContext> ProjectContext { get; set; }
+
+        [Import]
+        private Lazy<ISettings> Settings { get; set; }
+
+        [Import]
+        private Lazy<IVsSolutionManager> SolutionManager { get; set; }
+
+        [Import]
+        private Lazy<SolutionUserOptions> SolutionUserOptions { get; set; }
+
+        /// <summary>
+        /// This initializes the IVSSourceControlTracker, even though SourceControlTracker is unused.
+        /// </summary>
+        [Import]
+        private Lazy<IVsSourceControlTracker> SourceControlTracker { get; set; }
+
+        [Import]
+        private Lazy<INuGetUIFactory> UIFactory { get; set; }
+
+
+        //TODO: need GetService or to convert to use GetServiceAsync
+        private IDisposable ProjectRetargetingHandler { get; set; }
+        private IDisposable ProjectUpgradeHandler { get; set; }
+
+		private void Initialize()
+		{
+            _vsMonitorSelection = new AsyncLazy<vsShellInterop.IVsMonitorSelection>(
+                async () =>
+                {
+                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                    // get the UI context cookie for the debugging mode
+                    var vsMonitorSelection = await _asyncServiceProvider.GetServiceAsync(typeof(vsShellInterop.IVsMonitorSelection)) as vsShellInterop.IVsMonitorSelection;
+                    Assumes.Present(vsMonitorSelection);
+                    // get the solution not building and not debugging cookie
+                    var guidCmdUI = VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_guid;
+                    vsMonitorSelection.GetCmdUIContextCookie(
+                        ref guidCmdUI, out _solutionExistsAndFullyLoadedContextCookie);
+
+                    guidCmdUI = VSConstants.UICONTEXT.SolutionExistsAndNotBuildingAndNotDebugging_guid;
+                    vsMonitorSelection.GetCmdUIContextCookie(
+                        ref guidCmdUI, out _solutionNotBuildingAndNotDebuggingContextCookie);
+
+                    guidCmdUI = VSConstants.UICONTEXT.SolutionExists_guid;
+                    vsMonitorSelection.GetCmdUIContextCookie(
+                        ref guidCmdUI, out _solutionExistsCookie);
+
+                    return vsMonitorSelection;
+                },
+                ThreadHelper.JoinableTaskFactory);
         }
 
         public bool IsPackageManagerUISupported(WorkspaceVisualNodeBase workspaceVisualNodeBase)
@@ -35,9 +130,348 @@ namespace NuGet.VisualStudio.OnlineEnvironment.Client
             _joinableTaskFactory.RunAsync(() => OpenPackageManagerUIAsync(workspaceVisualNodeBase));
         }
 
-        private Task OpenPackageManagerUIAsync(WorkspaceVisualNodeBase workspaceVisualNodeBase)
+        private async Task OpenPackageManagerUIAsync(WorkspaceVisualNodeBase workspaceVisualNodeBase)
         {
-            return Task.CompletedTask;
+            await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+        	await ShowPackageManagerUI(workspaceVisualNodeBase.NodeMoniker);
         }
+
+        /// <summary>
+        /// Initialize all MEF imports for this package and also add required event handlers.
+        /// </summary>
+        private async Task InitializeMEFAsync()
+        {
+            _initialized = true;
+
+            var componentModel = await _asyncServiceProvider.GetServiceAsync(typeof(SComponentModel)) as IComponentModel;
+            Assumes.Present(componentModel);
+            componentModel.DefaultCompositionService.SatisfyImportsOnce(this);
+
+            await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            //    SolutionManager.Value.AfterNuGetProjectRenamed += SolutionManager_NuGetProjectRenamed;
+
+            Brushes.LoadVsBrushes();
+
+            _dte = (DTE)await _asyncServiceProvider.GetServiceAsync(typeof(vsShellInterop.SDTE));
+            Assumes.Present(_dte);
+
+            _dteEvents = _dte.Events.DTEEvents;
+            //_dteEvents.OnBeginShutdown += OnBeginShutDown;
+
+            if (SolutionManager.Value.NuGetProjectContext == null)
+            {
+                SolutionManager.Value.NuGetProjectContext = ProjectContext.Value;
+            }
+
+            // when NuGet loads, if the current solution has some package
+            // folders marked for deletion (because a previous uninstalltion didn't succeed),
+            // delete them now.
+            if (await SolutionManager.Value.IsSolutionOpenAsync())
+            {
+                await DeleteOnRestartManager.Value.DeleteMarkedPackageDirectoriesAsync(ProjectContext.Value);
+            }
+
+            //ProjectRetargetingHandler = new ProjectRetargetingHandler(_dte, SolutionManager.Value, this, componentModel);
+            //ProjectUpgradeHandler = new ProjectUpgradeHandler(this, SolutionManager.Value);
+
+            SolutionUserOptions.Value.LoadSettings();
+        }
+
+ private async Task ShowPackageManagerUI(string projectPath)
+        {
+            await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            if (ShouldMEFBeInitialized())
+            {
+                await InitializeMEFAsync();
+            }
+
+            // vsShellInterop.IVsWindowFrame windowFrame = null;
+            // if (windowFrame == null)
+            // {
+            //     windowFrame = await CreateNewWindowFrameAsync(projectPath);
+            // }
+
+            var window = await CreateNewWindowFrameAsync(projectPath);
+            if (window != null)
+            {
+                Search(window, string.Empty);
+                window.Show();
+            }
+            // if (windowFrame != null)
+            // {
+            //     Search(windowFrame, string.Empty);
+            //     windowFrame.Show();
+            // }
+        }
+
+
+        private vsShellInterop.IVsWindowFrame FindExistingWindowFrame(
+			string projectPath)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // TODO: moved from GetService to GetServiceAsync -- work? any other changes?
+            var uiShell = (vsShellInterop.IVsUIShell)_asyncServiceProvider.GetServiceAsync(typeof(vsShellInterop.SVsUIShell));
+            foreach (var windowFrame in VsUtility.GetDocumentWindows(uiShell))
+            {
+                object docView;
+                var hr = windowFrame.GetProperty(
+                    (int)vsShellInterop.__VSFPROPID.VSFPROPID_DocView,
+                    out docView);
+                if (hr == VSConstants.S_OK
+                    && docView is PackageManagerWindowPane)
+                {
+                    var packageManagerWindowPane = (PackageManagerWindowPane)docView;
+                    if (packageManagerWindowPane.Model.IsSolution)
+                    {
+                        // the window is the solution package manager
+                        continue;
+                    }
+
+                    var projects = packageManagerWindowPane.Model.Context.Projects;
+                    if (projects.Count() != 1)
+                    {
+                        continue;
+                    }
+
+                    var existingProject = projects.First();
+                    var projectName = existingProject.GetMetadata<string>(NuGetProjectMetadataKeys.Name);
+                    if (string.Equals(projectName, projectPath, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return windowFrame;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private async Task<System.Windows.Window> CreateNewWindowFrameAsync(string projectPath)
+        // private async Task<vsShellInterop.IVsWindowFrame> CreateNewWindowFrameAsync(string projectPath)
+        {
+            await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            //var vsProject = VsHierarchyUtility.ToVsHierarchy(project);
+            var documentName = projectPath;
+
+            // Find existing hierarchy and item id of the document window if it's
+            // already registered.
+            var rdt = await _asyncServiceProvider.GetServiceAsync(typeof(vsShellInterop.IVsRunningDocumentTable)) as vsShellInterop.IVsRunningDocumentTable;
+            Assumes.Present(rdt);
+            vsShellInterop.IVsHierarchy hier;
+            uint itemId;
+            var docData = IntPtr.Zero;
+            int hr;
+
+            try
+            {
+                uint cookie;
+                hr = rdt.FindAndLockDocument(
+                    (uint)vsShellInterop._VSRDTFLAGS.RDT_NoLock,
+                    documentName,
+                    out hier,
+                    out itemId,
+                    out docData,
+                    out cookie);
+                if (hr != VSConstants.S_OK)
+                {
+                    // the docuemnt window is not registered yet. So use the hierarchy from the current selection.
+                    //hier = vsUIHierarchy;
+                    itemId = (uint)VSConstants.VSITEMID.Root;
+                }
+            }
+            finally
+            {
+                if (docData != IntPtr.Zero)
+                {
+                    Marshal.Release(docData);
+                    docData = IntPtr.Zero;
+                }
+            }
+
+            // Create the doc window using the hiearchy & item id.
+            return await CreateDocWindowAsync(projectPath, documentName, hier, itemId);
+        }
+
+        private async Task<System.Windows.Window> CreateDocWindowAsync(
+        // private async Task<vsShellInterop.IVsWindowFrame> CreateDocWindowAsync(
+            string projectPath,
+            string documentName,
+            vsShellInterop.IVsHierarchy hier,
+            uint itemId)
+        {
+            await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+
+            var uiController = UIFactory.Value.Create();
+
+            var model = new PackageManagerModel(
+                uiController,
+                isSolution: false,
+                editorFactoryGuid: GuidList.NuGetEditorType);
+
+            var vsWindowSearchHostfactory = await _asyncServiceProvider.GetServiceAsync(typeof(vsShellInterop.SVsWindowSearchHostFactory)) as vsShellInterop.IVsWindowSearchHostFactory;
+            var vsShell = await _asyncServiceProvider.GetServiceAsync(typeof(vsShellInterop.SVsShell)) as vsShellInterop.IVsShell4;
+			// TODO: rob to fix assembly loading problem.
+            //var control = new PackageManagerControl(model, Settings.Value, vsWindowSearchHostfactory, vsShell, OutputConsoleLogger.Value);
+
+            //var windowPane = new PackageManagerWindowPane(control);
+            var guidEditorType = GuidList.NuGetEditorType;
+            var guidCommandUI = Guid.Empty;
+            var ppunkDocData = IntPtr.Zero;
+
+            var caption = string.Format(
+                CultureInfo.CurrentCulture,
+                Resx.Label_NuGetWindowCaption,
+                projectPath);
+
+            //workaround assembly loading problem.
+            var control = new Button() { Content = "Package Manager UI - under construction" };
+            var win = new System.Windows.Window();
+            win.Title = caption;
+            win.Content = control;
+
+            // vsShellInterop.IVsWindowFrame windowFrame;
+            // var uiShell = await _asyncServiceProvider.GetServiceAsync(typeof(vsShellInterop.SVsUIShell)) as vsShellInterop.IVsUIShell;
+            // Assumes.Present(uiShell);
+            // var ppunkToolView = IntPtr.Zero;
+            // var hr = 0;
+            // int[] pfDefaultPosition = null;
+
+            // try
+            // {
+            //     ppunkToolView = Marshal.GetIUnknownForObject(windowPane);
+
+            //     hr = uiShell.CreateToolWindow(
+            //         windowFlags,
+            //         0, //dwToolWindowId - singleInstance = 0
+            //         ppunkToolView, // punkTool
+            //         Guid.Empty,  //rclsidTool = GUID_NULL
+            //         Guid.NewGuid(), // TODO: should be projectGuid...so persistance info works.
+            //         Guid.Empty,  //reserved - do not use - GUID_NULL
+            //         null, // IServiceProvider
+            //         caption,
+            //         pfDefaultPosition,
+            //         out windowFrame);
+
+            //     // hr = uiShell.CreateDocumentWindow(
+            //     //     windowFlags,
+            //     //     documentName,
+            //     //     (vsShellInterop.IVsUIHierarchy)hier,
+            //     //     itemId,
+            //     //     ppunkDocView,
+            //     //     ppunkDocData,
+            //     //     ref guidEditorType,
+            //     //     null,
+            //     //     ref guidCommandUI,
+            //     //     null,
+            //     //     caption,
+            //     //     string.Empty,
+            //     //     null,
+            //     //     out windowFrame);
+            //     if (windowFrame != null)
+            //     {
+            //         WindowFrameHelper.AddF1HelpKeyword(windowFrame, keywordValue: F1KeywordValuePmUI);
+            //         WindowFrameHelper.DisableWindowAutoReopen(windowFrame);
+            //     }
+            // }
+            // finally
+            // {
+            //     if (ppunkToolView != IntPtr.Zero)
+            //     {
+            //         Marshal.Release(ppunkDocData);
+            //     }
+
+            //     if (windowPane != null)
+            //     {
+            //         windowPane.Dispose();
+            //     }
+            // }
+
+            // ErrorHandler.ThrowOnFailure(hr);
+            // return windowFrame;
+            win.Tag = F1KeywordValuePmUI;
+            return win;
+        }
+
+        private async Task<vsShellInterop.IVsWindowFrame> FindExistingSolutionWindowFrameAsync()
+        {
+            await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            var uiShell = await _asyncServiceProvider.GetServiceAsync(typeof(vsShellInterop.SVsUIShell)) as vsShellInterop.IVsUIShell;
+            foreach (var windowFrame in VsUtility.GetDocumentWindows(uiShell))
+            {
+                object property;
+                var hr = windowFrame.GetProperty(
+                    (int)vsShellInterop.__VSFPROPID.VSFPROPID_DocData,
+                    out property);
+                var packageManagerControl = VsUtility.GetPackageManagerControl(windowFrame);
+                if (hr == VSConstants.S_OK
+                    &&
+                    property is vsShellInterop.IVsSolution
+                    &&
+                    packageManagerControl != null)
+                {
+                    return windowFrame;
+                }
+            }
+
+            return null;
+        }
+
+        private static string GetSearchText(string parameterString)
+        {
+            if (parameterString == null)
+            {
+                return null;
+            }
+
+            // The parameterString looks like:
+            //     "jquery /searchin:online"
+            // or just "jquery"
+
+            parameterString = parameterString.Trim();
+            var lastIndexOfSearchInSwitch = parameterString.LastIndexOf("/searchin:", StringComparison.OrdinalIgnoreCase);
+
+            if (lastIndexOfSearchInSwitch == -1)
+            {
+                return parameterString;
+            }
+            return parameterString.Substring(0, lastIndexOfSearchInSwitch);
+        }
+
+        /// <summary>
+        /// Search for packages using the searchText.
+        /// </summary>
+        /// <param name="windowFrame">A window frame that hosts the PackageManagerControl.</param>
+        /// <param name="searchText">Search text.</param>
+        private void Search(System.Windows.Window windowFrame, string searchText)
+        // private void Search(vsShellInterop.IVsWindowFrame windowFrame, string searchText)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                return;
+            }
+
+            var packageManagerControl = windowFrame.Content as PackageManagerControl;
+            // var packageManagerControl = VsUtility.GetPackageManagerControl(windowFrame);
+            if (packageManagerControl != null)
+            {
+                packageManagerControl.Search(searchText);
+            }
+        }
+
+
+        private bool ShouldMEFBeInitialized()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            return !_initialized;
+        }
+
     }
 }
