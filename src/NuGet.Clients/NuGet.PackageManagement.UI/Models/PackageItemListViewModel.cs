596c3998ccf0eb36b559fade700d4276bf654db2
Fernando Aguilar <feaguila@microsoft.com>
2020-05-08 12:37:34 -0700

    Updated comment

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/PackageItemListViewModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/PackageItemListViewModel.cs
@@ -438,7 +438,7 @@ namespace NuGet.PackageManagement.UI
 
         public void UpdatePackageStatus(IEnumerable<PackageCollectionItem> installedPackages)
         {
-            // Get the minimum version installed in any target project/solution
+            // Get the maxium version installed in any target project/solution
             InstalledVersion = installedPackages
                 .GetPackageVersions(Id)
                 .MaxOrDefault();
