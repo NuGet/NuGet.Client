1f5a4d9c428055276878352e5e3a3363614749b0
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2020-05-21 23:19:01 -0700

    added conditional compilation

--- src/NuGet.Core/NuGet.Commands/SignCommand/CertificateProvider.cs
+++ src/NuGet.Core/NuGet.Commands/SignCommand/CertificateProvider.cs
@@ -32,11 +32,14 @@ namespace NuGet.Commands
 
         private const int MACOS_INVALID_CERT = -25257;
 
-        // OpenSSL:  error:0D07803A:asn1 encoding routines:ASN1_ITEM_EX_D2I:nested asn1 error
-        private const int OPENSSL_ERR_R_NESTED_ASN1_ERROR = 0x0D07803A;
 
+#if IS_SIGNING_SUPPORTED && IS_CORECLR
         //Generic exception ASN1 corrupted data
         private const int OPENSSL_ASN1_CORRUPTED_DATA_ERROR = unchecked((int)0x80131501);
+#else
+        // OpenSSL:  error:0D07803A:asn1 encoding routines:ASN1_ITEM_EX_D2I:nested asn1 error
+        private const int OPENSSL_ERR_R_NESTED_ASN1_ERROR = 0x0D07803A;
+#endif
 
         /// <summary>
         /// Looks for X509Certificates using the CertificateSourceOptions.
