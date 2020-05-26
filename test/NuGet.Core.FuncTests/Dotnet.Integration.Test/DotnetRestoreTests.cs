a8111a7939edebee6410a560dfab4b57fbc0919d
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2020-05-26 09:25:40 -0700

    removed unused namespaces and sorted them

--- test/NuGet.Core.FuncTests/Dotnet.Integration.Test/DotnetRestoreTests.cs
+++ test/NuGet.Core.FuncTests/Dotnet.Integration.Test/DotnetRestoreTests.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.ConstrainedExecution;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using FluentAssertions;
