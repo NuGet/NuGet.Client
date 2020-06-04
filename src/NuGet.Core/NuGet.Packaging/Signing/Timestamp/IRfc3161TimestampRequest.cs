6946542849ebe4005c4c8139ee3da3635c0fe2e7
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2020-06-04 12:45:18 -0700

    added GetNonce() to IRfc3161TimestampRequest interface (#3427)

--- src/NuGet.Core/NuGet.Packaging/Signing/Timestamp/IRfc3161TimestampRequest.cs
+++ src/NuGet.Core/NuGet.Packaging/Signing/Timestamp/IRfc3161TimestampRequest.cs
@@ -9,5 +9,11 @@ namespace NuGet.Packaging.Signing
     internal interface IRfc3161TimestampRequest
     {
         Task<IRfc3161TimestampToken> SubmitRequestAsync(Uri timestampUri, TimeSpan timeout);
+
+        /// <summary>
+        /// Gets the nonce for this timestamp request.
+        /// </summary>
+        /// <returns>The nonce for this timestamp request as byte[], if one was present; otherwise, null</returns>
+        byte[] GetNonce();
     }
 }
