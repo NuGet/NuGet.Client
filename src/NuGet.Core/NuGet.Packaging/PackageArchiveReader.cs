5e9a03f3bda7010b81449d0e8c0897ed1637d0d0
Heng Liu <liu.heng@microsoft.com>
2020-06-12 20:23:28 -0700

    disable verification during extraction on mono

--- src/NuGet.Core/NuGet.Packaging/PackageArchiveReader.cs
+++ src/NuGet.Core/NuGet.Packaging/PackageArchiveReader.cs
@@ -381,7 +381,16 @@ namespace NuGet.Packaging
         public override bool CanVerifySignedPackages(SignedPackageVerifierSettings verifierSettings)
         {
 #if IS_SIGNING_SUPPORTED
-            return true;
+            // Mono support has been deprioritized, so verification on Mono is not enabled
+            if (RuntimeEnvironmentHelper.IsMono)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
 #else
             return false;
 #endif
