c7ddcf70c26b14ed9bcd678ee1bd3223915c9139
donnie-msft <49205731+donnie-msft@users.noreply.github.com>
2020-06-15 20:46:27 -0400

    Update HttpSourceAuthenticationHandlerTests.cs

--- test/NuGet.Core.Tests/NuGet.Protocol.Tests/HttpSource/HttpSourceAuthenticationHandlerTests.cs
+++ test/NuGet.Core.Tests/NuGet.Protocol.Tests/HttpSource/HttpSourceAuthenticationHandlerTests.cs
@@ -492,7 +492,7 @@ namespace NuGet.Protocol.Tests
             yield return new object[] { multipartFormDataContent };
         }
 
-        [Theory]
+        [Theory(Skip = "https://github.com/NuGet/Home/issues/9685")]
         [MemberData(nameof(GetHttpContent))]
         public async Task SendAsync_RetryWithClonedPostRequest(HttpContent httpContent)
         {
