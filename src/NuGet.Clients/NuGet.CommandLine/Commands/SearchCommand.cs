deabf7e553a2c691b63ba64e61a65a2bb931272c
Advay Tandon <advay.tandon@gmail.com>
2020-06-23 18:48:23 -0400

    Resetting project to build correctly

---
+++ src/NuGet.Clients/NuGet.CommandLine/Commands/SearchCommand.cs
@@ -0,0 +1,166 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Frameworks;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Packaging.Licenses;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+
+namespace NuGet.CommandLine
+{
+
+    [Command(typeof(NuGetCommand), "search", "SearchCommandDescription", MaxArgs = 1,
+            UsageSummaryResourceName = "SearchCommandUsageSummary", UsageExampleResourceName = "SearchCommandUsageExamples")]
+    public class SearchCommand : Command
+    {
+        [Option(typeof(NuGetCommand), "SearchCommandAssemblyPathDescription")]
+        public string AssemblyPath
+        {
+            get;
+            set;
+        }
+
+        [Option(typeof(NuGetCommand), "SearchCommandForceDescription")]
+        public bool Force
+        {
+            get;
+            set;
+        }
+
+        //private static string BoldStart = "\x1B[1m";
+        //private static string BoldEnd = "\x1B[22m";
+
+        //private string Bold(string text)
+        //{
+        //    return BoldStart + text + BoldEnd;
+        //}
+
+        public override async Task ExecuteCommandAsync()
+        {
+
+            ILogger logger = NullLogger.Instance;
+            CancellationToken cancellationToken = CancellationToken.None;
+
+            foreach (var source in SourceProvider.LoadPackageSources())
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.FileName = "cmd";
+                    myProcess.StartInfo.Arguments = "/c more";
+                    myProcess.StartInfo.UseShellExecute = false;
+                    myProcess.StartInfo.RedirectStandardInput = true;
+
+                    myProcess.Start();
+
+                    StreamWriter myStreamWriter = myProcess.StandardInput;
+
+                    var target = source.Source;
+                    var name = source.Name;
+
+                    SourceRepository repository = Repository.Factory.GetCoreV3(target);
+                    PackageSearchResource resource = await repository.GetResourceAsync<PackageSearchResource>();
+
+                    if (resource is null)
+                    {
+                        continue;
+                    }
+
+                    SearchFilter searchFilter = new SearchFilter(includePrerelease: true);
+
+                    IEnumerable<IPackageSearchMetadata> results = await resource.SearchAsync(
+                        Arguments[0],
+                        searchFilter,
+                        skip: 0,
+                        take: 20,
+                        logger,
+                        cancellationToken);
+
+
+                    myStreamWriter.WriteLine(new string('=', 20));
+                    myStreamWriter.WriteLine($"Source: {source.Name}");
+
+                    if (!results.Any())
+                    {
+                        myStreamWriter.WriteLine(new string('-', 20));
+                        myStreamWriter.WriteLine("No results found.");
+                    }
+
+
+                    foreach (IPackageSearchMetadata result in results)
+                    {
+                        myStreamWriter.WriteLine(new string('-', 20));
+                        myStreamWriter.WriteLine($"> {result.Identity.Id} | v{result.Identity.Version} | DLs: {result.DownloadCount}"); // TODO: '>' and 'v' necessary or not?
+                        myStreamWriter.WriteLine($"{result.Description}\n");
+
+                    }
+
+                    myStreamWriter.Close();
+
+                    // Wait for the sort process to write the sorted text lines.
+                    myProcess.WaitForExit();
+                }
+
+            }
+
+            Thread.Sleep(5000);
+        }
+
+
+        public override bool IncludedInHelp(string optionName)
+        {
+            if (string.Equals(optionName, "ConfigFile", StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            return base.IncludedInHelp(optionName);
+        }
+        
+        private static string RemoveSchemaNamespace(string content)
+        {
+            // This seems to be the only way to clear out xml namespaces.
+            return Regex.Replace(content, @"(xmlns:?[^=]*=[""][^""]*[""])", string.Empty, RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.CultureInvariant);
+        }
+
+        private static IList<Resource> Deserialize(string value)
+        {
+            return JsonConvert.DeserializeObject<IndexJson>(value).Resources;
+        }
+
+        private sealed class IndexJson
+        {
+            [JsonProperty("resources")]
+            public IList<Resource> Resources { get; set; }
+        }
+
+        private sealed class Resource
+        {
+            [JsonProperty("@type")]
+            public string Type { get; set; }
+
+            [JsonProperty("@id")]
+            public string Id { get; set; }
+        }
+
+
+    }
+
+    
+}
---
+++ test/NuGet.Clients.FuncTests/NuGet.CommandLine.FuncTest/Commands/SearchCommandTests.cs
@@ -0,0 +1,121 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using FluentAssertions;
+using NuGet.CommandLine.Test;
+using NuGet.Commands;
+using NuGet.Configuration;
+using NuGet.Packaging;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Test.Utility;
+using Test.Utility;
+using Xunit;
+
+namespace NuGet.CommandLine.FuncTest.Commands
+{
+    public class SearchCommandTest
+    {
+        [Fact]
+        public void SearchCommand_BasicTest()
+        {
+            // Arrange
+            var nugetexe = Util.GetNuGetExePath();
+
+            using (var server = new MockServer())
+            {
+                string index = $@"
+{{""version"": ""3.0.0"",
+  ""resources"": [
+    {{
+      ""@id"": ""{server.Uri + "search/query"}"",
+      ""@type"": ""SearchQueryService"",
+      ""comment"": ""Query endpoint of NuGet Search service (primary)""
+    }}
+    ],
+    ""@context"": {{
+    ""@vocab"": ""http://schema.nuget.org/services#"",
+    ""comment"": ""http://www.w3.org/2000/01/rdf-schema#comment""
+  }}
+        }}
+            ";
+
+                server.Get.Add("/v3/index.json", r => index);
+
+
+
+                string queryResult = $@"{{
+""@context"": {{
+""@vocab"": ""http://schema.nuget.org/schema#"",
+""@base"": ""https://api.nuget.org/v3/registration5-semver1/""
+}},
+""totalHits"": 396,
+""data"": [
+{{
+""@id"": ""https://api.nuget.org/v3/registration5-semver1/newtonsoft.json/index.json"",
+""@type"": ""Package"",
+""registration"": ""https://api.nuget.org/v3/registration5-semver1/newtonsoft.json/index.json"",
+""id"": ""Newtonsoft.Json"",
+""version"": ""12.0.3"",
+""description"": ""Json.NET is a popular high-performance JSON framework for .NET"",
+""summary"": """",
+""title"": ""Json.NET"",
+""iconUrl"": ""https://api.nuget.org/v3-flatcontainer/newtonsoft.json/12.0.3/icon"",
+""licenseUrl"": ""https://www.nuget.org/packages/Newtonsoft.Json/12.0.3/license"",
+""projectUrl"": ""https://www.newtonsoft.com/json"",
+""tags"": [
+""json""
+],
+""authors"": [
+""James Newton-King""
+],
+""totalDownloads"": 531607259,
+""verified"": true,
+""packageTypes"": [
+{{
+""name"": ""Dependency""
+}}
+],
+""versions"": [{{
+""version"": ""3.5.8"",
+""downloads"": 461992,
+""@id"": ""https://api.nuget.org/v3/registration5-semver1/newtonsoft.json/3.5.8.json""
+}}]
+}}";
+
+
+                server.Get.Add("/search/query?q=logging&skip=0&take=20&prerelease=false&semVerLevel=2.0.0", r => queryResult
+                );
+
+
+                server.Start();
+
+                // Act
+                var args = new[]
+                {
+                    "search",
+                    "logging",
+                };
+
+                var result = CommandRunner.Run(
+                    nugetexe,
+                    Directory.GetCurrentDirectory(),
+                    string.Join(" ", args),
+                    waitForExit: true);
+
+                server.Stop();
+
+                // Assert
+                Assert.True(0 == result.Item1, $"{result.Item2} {result.Item3}");
+                Assert.Contains("foo", result.Item2);
+            }
+        }
+
+    }
+}
