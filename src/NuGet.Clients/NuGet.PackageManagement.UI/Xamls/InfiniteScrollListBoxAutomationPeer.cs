46b5a735fa7bbda3604f39e128a686f4f8a6a1e2
Donnie Goodson <donnie.goodson@microsoft.com>
2020-06-23 15:48:13 -0400

    AutomationPeer is aware of UI Filtering to indicate correct count

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBoxAutomationPeer.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBoxAutomationPeer.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using System.Windows.Data;
 using NuGet.VisualStudio;
 
 namespace NuGet.PackageManagement.UI
@@ -26,8 +28,19 @@ namespace NuGet.PackageManagement.UI
             {
                 await infiniteScrollListBox.ItemsLock.ExecuteAsync(() =>
                 {
-                    // Don't return the LoadingStatusIndicator as an AutomationPeer, otherwise narrator will report it as an item in the list of packages, even when not visible
-                    value = base.GetChildrenCore()?.Where(lbiap => !(((ListBoxItemAutomationPeer)lbiap).Item is LoadingStatusIndicator)).ToList() ?? new List<AutomationPeer>();
+                    List<AutomationPeer> items = base.GetChildrenCore();
+                    if (items == null)
+                    {
+                        value = new List<AutomationPeer>();
+                    }
+                    else
+                    {
+                        var listCollectionView = (ListCollectionView)CollectionViewSource.GetDefaultView(items);
+                        // Don't return the LoadingStatusIndicator as an AutomationPeer, otherwise narrator will report it as an item in the list of packages, even when not visible
+                        IEnumerable<AutomationPeer> packageItemsInView = items.Where(item => ((ListBoxItemAutomationPeer)item).Item is PackageItemListViewModel
+                                                                                             && listCollectionView.Contains(item));
+                        value = packageItemsInView.ToList();
+                    }
 
                     return Task.CompletedTask;
                 });
