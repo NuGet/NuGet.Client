bd482514a87a0085f8eb8603842ee1cb3ca5f277
Erdembayar Yondon <erdembayaryondon@gmail.com>
2020-07-01 18:23:46 -0500

    Return empty result for signed package. (#3490)
    * Return empty result for signed package.
    
    * Add Unit test for handling Null stream if package not found.
    
    * Keep only 1 concise unit test.
    
    * Add unit test to handle null stream if no content from http response.
    
    * Revert unneeded change.
    
    * Rename after Donnie comment.
    
    Co-authored-by: Erick Yondon <eryondon@microsoft.com>

--- test/NuGet.Core.Tests/NuGet.Protocol.Tests/PackageMetadataResourceV3Tests.cs
+++ test/NuGet.Core.Tests/NuGet.Protocol.Tests/PackageMetadataResourceV3Tests.cs
@@ -4,6 +4,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using NuGet.Packaging;
@@ -225,6 +227,78 @@ namespace NuGet.Protocol.Tests
 
                 Assert.Equal(errorCount, result.LicenseMetadata.WarningsAndErrors.Count);
                 Assert.Contains(errorMessage, result.LicenseMetadata.WarningsAndErrors[0]);
+            }
+        }
+
+        [Fact]
+        public async Task PackageMetadataResourceV3_GetMetadataAsync_NotFoundHandleNullStream()
+        {
+            // Arrange
+            var notExistPackage = "NotExistPackage";
+            var package = new PackageIdentity(notExistPackage, NuGetVersion.Parse("1.0.0"));
+            var source = "http://testsource.com/v3/index.json";
+
+            var responses = new Dictionary<string, Func<HttpRequestMessage, Task<HttpResponseMessage>>>
+            {
+                {
+                    source,
+                    _ => Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new TestContent(JsonData.IndexWithoutFlatContainer)
+                    })
+                },
+                {
+                    $"https://api.nuget.org/v3/registration0/{notExistPackage.ToLowerInvariant()}/index.json",
+                    _ => Task.FromResult(new HttpResponseMessage(HttpStatusCode.NotFound))
+                }
+            };
+
+            var repo = StaticHttpHandler.CreateSource(source, Repository.Provider.GetCoreV3(), responses);
+            var resource = await repo.GetResourceAsync<PackageMetadataResource>();
+
+            //Act
+            using (var sourceCacheContext = new SourceCacheContext())
+            {
+                var metadata = await resource.GetMetadataAsync(package, sourceCacheContext, Common.NullLogger.Instance, CancellationToken.None);
+
+                //Assert
+                Assert.Null(metadata);
+            }
+        }
+
+        [Fact]
+        public async Task PackageMetadataResourceV3_GetMetadataAsync_NoContentHandleNullStream()
+        {
+            // Arrange
+            var noContentPackage = "NoContentPackage";
+            var package = new PackageIdentity(noContentPackage, NuGetVersion.Parse("1.0.0"));
+            var source = "http://testsource.com/v3/index.json";
+
+            var responses = new Dictionary<string, Func<HttpRequestMessage, Task<HttpResponseMessage>>>
+            {
+                {
+                    source,
+                    _ => Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new TestContent(JsonData.IndexWithoutFlatContainer)
+                    })
+                },
+                {
+                    $"https://api.nuget.org/v3/registration0/{noContentPackage.ToLowerInvariant()}/index.json",
+                    _ => Task.FromResult(new HttpResponseMessage(HttpStatusCode.NoContent))
+                }
+            };
+
+            var repo = StaticHttpHandler.CreateSource(source, Repository.Provider.GetCoreV3(), responses);
+            var resource = await repo.GetResourceAsync<PackageMetadataResource>();
+
+            //Act
+            using (var sourceCacheContext = new SourceCacheContext())
+            {
+                var metadata = await resource.GetMetadataAsync(package, sourceCacheContext, Common.NullLogger.Instance, CancellationToken.None);
+
+                //Assert
+                Assert.Null(metadata);
             }
         }
     }
