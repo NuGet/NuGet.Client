8afcb39913cf0eb90c7e80c3af40fddf65fb6ab6
Fernando Aguilar <feaguila@microsoft.com>
2020-08-06 21:08:53 -0700

    Removes dependency from VSIX validation from build share
    - Adds new script arguments
    - Adds task to copy logs to artifacts folder

--- scripts/cibuild/BuildValidator.ps1
+++ scripts/cibuild/BuildValidator.ps1
@@ -5,37 +5,40 @@ Validates the result of the localization process
 .DESCRIPTION
 This script is used to validate the results of localization.
 
-.PARAMETER BuildOutputTargetPath
+.PARAMETER RepoRoot
+Path to NuGet.Client repo root folfer
+
+.PARAMETER OutputLogsBasePath
 Path to the location where the build artifacts are output
 
 .PARAMETER BuildRTM
 True/false depending on whether nupkgs are being with or without the release labels.
 
+.PARAMETER ValidateVsix
+Flag to indicate validation on VSIX artifact. Otherwise, validates binaries under $RepoRoot\artifacts folder (default)
 #>
-
 param
 (
     [Parameter(Mandatory=$True)]
-    [string]$BuildOutputTargetPath,
+    [string]$RepoRoot,
+    [Parameter(Mandatory=$True)]
+    [string]$OutputLogsBasePath,
     [Parameter(Mandatory=$True)]
     [string]$BuildRTM,
+    [string]$TmpPath = $Env:TEMP,
     [switch]$ValidateVsix
 )
 
-
 if ($BuildRTM -eq 'false')
 {   
+    $NuGetValidator = [System.IO.Path]::Combine($RepoRoot, 'packages', 'nugetvalidator', '2.0.2', 'tools', 'NuGetValidator.exe')
+    $LocalizationRepository = [System.IO.Path]::Combine($RepoRoot, 'submodules', 'NuGet.Build.Localization', 'localize', 'comments', '15')
 
-    $NuGetClientRoot = $env:BUILD_REPOSITORY_LOCALPATH
-    $NuGetValidator = [System.IO.Path]::Combine($NuGetClientRoot, 'packages', 'nugetvalidator', '2.0.2', 'tools', 'NuGetValidator.exe')
-    $VsixLogOutputDir = [System.IO.Path]::Combine($BuildOutputTargetPath, 'LocalizationValidation', 'vsix' )
-    $LocalizationRepository = [System.IO.Path]::Combine($NuGetClientRoot, 'submodules', 'NuGet.Build.Localization', 'localize', 'comments', '15')
-    
     if ($ValidateVsix) 
     {
-        $VsixLocation = [System.IO.Path]::Combine($BuildOutputTargetPath, 'artifacts', 'VS15', 'NuGet.Tools.vsix' )
-        $VsixExtractLocation = [System.IO.Path]::Combine($env:SYSTEM_DEFAULTWORKINGDIRECTORY, 'extractedVsix' ) 
-        $VsixLogOutputDir = [System.IO.Path]::Combine($BuildOutputTargetPath, 'LocalizationValidation', 'vsix' )
+        $VsixLocation = [System.IO.Path]::Combine($RepoRoot, 'artifacts', 'VS15', 'NuGet.Tools.vsix' )
+        $VsixExtractLocation = [System.IO.Path]::Combine($TmpPath, 'extractedVsix')
+        $VsixLogOutputDir = [System.IO.Path]::Combine($OutputLogsBasePath, 'LocalizationValidation', 'vsix' )
 
         Write-Host "Validating NuGet.Tools.Vsix localization..."
         Write-Host "Running: $NuGetValidator localization --vsix --vsix-path $VsixLocation --vsix-extract-path $VsixExtractLocation --output-path $VsixLogOutputDir --comments-path $LocalizationRepository"
@@ -43,9 +46,9 @@ if ($BuildRTM -eq 'false')
     }
     else 
     {
-        $ArtifactsLocation = [System.IO.Path]::Combine($NuGetClientRoot, 'artifacts')
-        $ArtifactsLogOutputDir = [System.IO.Path]::Combine($BuildOutputTargetPath, 'LocalizationValidation', 'artifacts' )
-
+        $ArtifactsLocation = [System.IO.Path]::Combine($RepoRoot, 'artifacts')
+        $ArtifactsLogOutputDir = [System.IO.Path]::Combine($OutputLogsBasePath, 'LocalizationValidation', 'artifacts' )
+    
         Write-Host "Validating NuGet.Client repository localization..."
         Write-Host "Running: $NuGetValidator localization --artifacts-path $ArtifactsLocation --output-path $ArtifactsLogOutputDir --comments-path $LocalizationRepository"
         & $NuGetValidator localization --artifacts-path $ArtifactsLocation --output-path $ArtifactsLogOutputDir --comments-path $LocalizationRepository
