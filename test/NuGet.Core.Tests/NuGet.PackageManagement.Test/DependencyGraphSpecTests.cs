8e5b0dd4043f8816ed908e2788e78e8b88fd62fe
Erick Yondon <eryondon@microsoft.com>
2020-08-08 15:35:22 -0700

    Add unit test for DependencyGraphSpec>>WithReplacedPackageSpecs new method I created.

---
+++ test/NuGet.Core.Tests/NuGet.PackageManagement.Test/DependencyGraphSpecTests.cs
@@ -0,0 +1,48 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using NuGet.ProjectModel;
+using Xunit;
+
+namespace NuGet.Test
+{
+    public class DependencyGraphSpecTests
+    {
+        [Fact]
+        public void DependencyGraphSpecTests_WithReplacedPackageSpecs()
+        {
+            // Arrange
+            var packageSpecA = new PackageSpec();
+            packageSpecA.Title = "A";
+            packageSpecA.RestoreMetadata = new ProjectRestoreMetadata() { ProjectUniqueName = "a", CentralPackageVersionsEnabled = false };
+            var dgSpec = new DependencyGraphSpec();
+            dgSpec.AddRestore("a");
+            dgSpec.AddProject(packageSpecA);
+            var packageSpecB = new PackageSpec();
+            packageSpecB.Title = "B";
+            packageSpecB.RestoreMetadata = new ProjectRestoreMetadata()
+            {
+                ProjectUniqueName = "BBB"
+            };
+            var packageSpecC = new PackageSpec();
+            packageSpecC.Title = "C";
+            packageSpecC.RestoreMetadata = new ProjectRestoreMetadata()
+            {
+                ProjectUniqueName = "CCC"
+            };
+            var newNugetPackageSpecs = new List<PackageSpec>()
+            {
+                packageSpecB,
+                packageSpecC
+            };
+
+            // Act
+            dgSpec = dgSpec.WithReplacedPackageSpecs(newNugetPackageSpecs);
+
+            // Assert
+            Assert.Equal(dgSpec.Projects.Count, 3);
+            Assert.Equal(dgSpec.Restore.Count, 2);
+        }
+    }
+}
