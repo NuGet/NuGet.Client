797444abde5e8e3a9fb5737744310173cc7ace3c
Heng Liu <liu.heng@microsoft.com>
2020-08-20 18:45:35 -0700

    change the 2 new tests for net472 only

--- test/NuGet.Core.FuncTests/NuGet.Packaging.FuncTest/SigningTests/SignatureUtilityTests.cs
+++ test/NuGet.Core.FuncTests/NuGet.Packaging.FuncTest/SigningTests/SignatureUtilityTests.cs
@@ -67,7 +67,6 @@ namespace NuGet.Packaging.FuncTest.SigningTests
                 }
             }
         }
-#endif
 
         [Theory]
         [InlineData(SigningCertificateUsage.V1TestShortHash)]
@@ -112,10 +111,64 @@ namespace NuGet.Packaging.FuncTest.SigningTests
         }
 
         [Theory]
+        [InlineData(SigningCertificateUsage.V1TestMismatchedHash)]
+        public async Task GetTimestampCertificateChain_WithMismatchedEssCertIdCertificateHash_ReturnsChain(
+            SigningCertificateUsage signingCertificateUsage)
+        {
+            ISigningTestServer testServer = await _fixture.GetSigningTestServerAsync();
+            CertificateAuthority rootCa = await _fixture.GetDefaultTrustedCertificateAuthorityAsync();
+            var options = new TimestampServiceOptions()
+            {
+                SigningCertificateUsage = signingCertificateUsage
+            };
+            TimestampService timestampService = TimestampService.Create(rootCa, options);
+
+            using (testServer.RegisterResponder(timestampService))
+            {
+                var nupkg = new SimpleTestPackageContext();
+
+                using (var certificate = new X509Certificate2(_fixture.TrustedTestCertificate.Source.Cert))
+                using (var directory = TestDirectory.Create())
+                {
+                    var signedPackagePath = await SignedArchiveTestUtility.AuthorSignPackageAsync(
+                        certificate,
+                        nupkg,
+                        directory,
+                        timestampService.Url);
+
+                    using (FileStream stream = File.OpenRead(signedPackagePath))
+                    using (var reader = new PackageArchiveReader(stream))
+                    {
+                        PrimarySignature signature = await reader.GetPrimarySignatureAsync(CancellationToken.None);
+
+                        using (IX509CertificateChain actualChain = SignatureUtility.GetTimestampCertificateChain(signature))
+                        {
+                            Assert.NotEmpty(actualChain);
+
+                            IReadOnlyList<Org.BouncyCastle.X509.X509Certificate> expectedChain = GetExpectedCertificateChain(timestampService);
+
+                            Assert.Equal(expectedChain.Count, actualChain.Count);
+
+                            for (var i = 0; i < expectedChain.Count; ++i)
+                            {
+                                Org.BouncyCastle.X509.X509Certificate expectedCertificate = expectedChain[i];
+                                X509Certificate2 actualCertificate = actualChain[i];
+
+                                Assert.True(
+                                    expectedCertificate.GetEncoded().SequenceEqual(actualCertificate.RawData),
+                                    $"The certificate at index {i} in the chain is unexpected.");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+#endif
+
+        [Theory]
         [InlineData(SigningCertificateUsage.V1)]
         [InlineData(SigningCertificateUsage.V2)]
         [InlineData(SigningCertificateUsage.V1 | SigningCertificateUsage.V2)]
-        [InlineData(SigningCertificateUsage.V1TestMismatchedHash)]
         public async Task GetTimestampCertificateChain_WithValidSigningCertificateUsage_ReturnsChain(
             SigningCertificateUsage signingCertificateUsage)
         {
