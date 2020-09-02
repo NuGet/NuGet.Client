4172d139e8fba2134b5be4b06f66885375617ce8
Erick Yondon <eryondon@microsoft.com>
2020-09-02 12:52:44 -0700

    Fix unit test after Nikolche review.

--- test/NuGet.Core.Tests/NuGet.PackageManagement.Test/DependencyGraphSpecTests.cs
+++ test/NuGet.Core.Tests/NuGet.PackageManagement.Test/DependencyGraphSpecTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using NuGet.ProjectModel;
 using Xunit;
 
@@ -41,38 +43,63 @@ namespace NuGet.Test
         }
 
         [Fact]
-        public void WithPackageSpecs()
+        public void WithReplacedPackageSpecs_WithASinglePackageSpec_Succeeds()
         {
             // Arrange
-            var packageSpecA = new PackageSpec();
-            packageSpecA.Title = "A";
-            packageSpecA.RestoreMetadata = new ProjectRestoreMetadata() { ProjectUniqueName = "a", CentralPackageVersionsEnabled = false };
+            var packageSpecA = new PackageSpec
+            {
+                Title = "A",
+                RestoreMetadata = new ProjectRestoreMetadata()
+                {
+                    ProjectUniqueName = "a", CentralPackageVersionsEnabled = false
+                }
+            };
+            var packageSpecB = new PackageSpec
+            {
+                Title = "B",
+                RestoreMetadata = new ProjectRestoreMetadata()
+                {
+                    ProjectUniqueName = "BBB"
+                }
+            };
+            var packageSpecC = new PackageSpec
+            {
+                Title = "C",
+                RestoreMetadata = new ProjectRestoreMetadata()
+                {
+                    ProjectUniqueName = "CCC"
+                }
+            };
             var dgSpec = new DependencyGraphSpec();
-            var packageSpecB = new PackageSpec();
-            packageSpecB.Title = "B";
-            packageSpecB.RestoreMetadata = new ProjectRestoreMetadata()
-            {
-                ProjectUniqueName = "BBB"
-            };
-            var packageSpecC = new PackageSpec();
-            packageSpecC.Title = "C";
-            packageSpecC.RestoreMetadata = new ProjectRestoreMetadata()
-            {
-                ProjectUniqueName = "CCC"
-            };
+            dgSpec.AddProject(packageSpecA);
+            dgSpec.AddProject(packageSpecB);
+            dgSpec.AddProject(packageSpecC);
+            dgSpec.AddRestore(packageSpecA.RestoreMetadata.ProjectUniqueName);
+            dgSpec.AddRestore(packageSpecB.RestoreMetadata.ProjectUniqueName);
+            dgSpec.AddRestore(packageSpecC.RestoreMetadata.ProjectUniqueName);
+
+            // Create an updated packageSpecA
+            var updatedPackageA = packageSpecA.Clone();
+            updatedPackageA.RestoreMetadata.ConfigFilePaths.Add("/samplePath");
             var newNugetPackageSpecs = new List<PackageSpec>()
             {
-                packageSpecA,
-                packageSpecB,
-                packageSpecC
+                updatedPackageA
             };
 
+            // Preconditions
+            dgSpec.Projects.Should().HaveCount(3);
+            dgSpec.Restore.Should().HaveCount(3);
+
             // Act
-            dgSpec = dgSpec.WithPackageSpecs(newNugetPackageSpecs);
+            var dgSpecWithReplacedPackageA = dgSpec.WithPackageSpecs(newNugetPackageSpecs);
 
             // Assert
-            Assert.Equal(dgSpec.Projects.Count, 3);
-            Assert.Equal(dgSpec.Restore.Count, 3);
+            dgSpecWithReplacedPackageA.Projects.Should().HaveCount(3);
+            dgSpecWithReplacedPackageA.Restore.Should().HaveCount(1);
+
+            var packageSpecInAFromDgSpec = dgSpecWithReplacedPackageA.Projects.Single(e => e.Title.Equals("A"));
+            packageSpecInAFromDgSpec.Should().Be(updatedPackageA);
+            dgSpecWithReplacedPackageA.Restore.Single().Should().Be(updatedPackageA.RestoreMetadata.ProjectUniqueName);
         }
     }
 }
