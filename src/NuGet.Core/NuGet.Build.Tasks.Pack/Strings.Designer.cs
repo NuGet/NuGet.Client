d1e99c970e52630a67e5b069dcdd537b027d48cf
Kat Marchán <kzm@zkat.tech>
2020-09-08 19:39:41 -0700

    error when TPV is missing from target frameworks (#3616)
    * error when TPV is missing from dependencies
    
    Fixes: https://github.com/nuget/home/issues/9441

--- src/NuGet.Core/NuGet.Build.Tasks.Pack/Strings.Designer.cs
+++ src/NuGet.Core/NuGet.Build.Tasks.Pack/Strings.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace NuGet.Build.Tasks.Pack {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -20,7 +19,7 @@ namespace NuGet.Build.Tasks.Pack {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Strings {
@@ -40,7 +39,7 @@ namespace NuGet.Build.Tasks.Pack {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NuGet.Build.Tasks.Pack.Strings", typeof(Strings).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NuGet.Build.Tasks.Pack.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -157,6 +156,15 @@ namespace NuGet.Build.Tasks.Pack {
         internal static string InvalidPackageVersion {
             get {
                 return ResourceManager.GetString("InvalidPackageVersion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Platform version is missing from &apos;{0}&apos;.
+        /// </summary>
+        internal static string InvalidPlatformVersion {
+            get {
+                return ResourceManager.GetString("InvalidPlatformVersion", resourceCulture);
             }
         }
         
