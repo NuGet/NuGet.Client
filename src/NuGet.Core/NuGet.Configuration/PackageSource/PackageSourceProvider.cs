74ce73f5d335b98cd93e083c8042224e839e71d0
Rob Relyea <rrelyea@microsoft.com>
2020-09-30 17:33:31 -0700

    add pragmas to allow calling obsolete apis for now.

--- src/NuGet.Core/NuGet.Configuration/PackageSource/PackageSourceProvider.cs
+++ src/NuGet.Core/NuGet.Configuration/PackageSource/PackageSourceProvider.cs
@@ -9,7 +9,9 @@ using NuGet.Common;
 
 namespace NuGet.Configuration
 {
+#pragma warning disable CS0618 // Type or member is obsolete
     public class PackageSourceProvider : IPackageSourceProvider, IPackageSourceProvider2
+#pragma warning restore CS0618 // Type or member is obsolete
     {
         public ISettings Settings { get; private set; }
 
@@ -748,8 +750,9 @@ namespace NuGet.Configuration
             {
                 throw new ArgumentNullException(nameof(sources));
             }
-
+#pragma warning disable CS0618 // Type or member is obsolete
             SavePackageSources(sources, PackageSourceUpdateOptions.Default);
+#pragma warning restore CS0618 // Type or member is obsolete
         }
 
         private Dictionary<string, SourceItem> GetExistingSettingsLookup()
