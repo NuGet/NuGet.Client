fa45cd5353659a892293955c0a9097ac5617cf7c
Heng Liu <liu.heng@microsoft.com>
2020-10-05 18:41:57 -0700

    try to split

--- test/EndToEnd/tests/InstallPackageTest.ps1
+++ test/EndToEnd/tests/InstallPackageTest.ps1
@@ -2985,3 +2985,23 @@ function Test-InstallPackageWithRootNamespaceInPPFile {
     $content = [System.IO.File]::ReadAllText($path)
     Assert-True ($content.Contains("namespace testProject"))
 }
+
+function RemoveDirectory {
+    param($dir)
+
+    $iteration = 0
+    while ($iteration++ -lt 10)
+    {
+        if (Test-Path $dir)
+        {
+            # because -Recurse parameter in Remove-Item has a known issue so using Get-ChildItem to
+            # first delete all the children and then delete the folder.
+            Get-ChildItem $dir -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+            Remove-Item -Recurse -Force $dir -ErrorAction SilentlyContinue
+        }
+        else 
+        {
+            break;
+        }
+    }
+}
