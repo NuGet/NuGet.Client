36253329cdc2377cdddeb68cec490b2c9222cbee
Rob Relyea <rrelyea@microsoft.com>
2020-10-15 07:19:19 -0700

    Make package icon loading in PM UI async to prepare for codespaces improvements needed. (#3702)
    When connected to a codespaces sesssion, we are going to need to call the server instance to get embedded license files and embedded icons (for packages not accessed via search to http feeds).
    
    In order to make that work well, we need to change our Icon fetching to be more async.
    
    This change doesn't add the codespaces side of things yet...is just trying to make our current icon loading as good, and better than it is now.
    
    The ViewModel.IconUrl property now has a matching IconBitmap property...and IconBitmapStatus
    
    Since 2016, we've used a ValueConverter (runs on UI thread only) to convert the Url to a BitmapImage that an Image element could show. This approach causes a UI delay when network retrieval of images is slow. We believe this work should fix that.
    
    In order to move off the UI thread...we moved the download into background work that IconBitmap getter kicks off. We rely on INotifyPropertyChanged to notify databinding later, that the IconBitmap value has changed on the background thread. Binding can update UI properties on UI Thread that were modified on a viewmodel in a background thread(s).
    
    ErrorFloodGate.cs - made this thread safe.
    
    Testing/Validation
    Tests Added: Yes
    Validation: testing with instrumented version of UI to see where each image came from (memorycache, download, default, defaultduetonetworkproblem, etc...). tested with clumsy to slow network, drop packets, etc...

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Resources/Images.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Resources/Images.cs
@@ -1,26 +1,50 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
+using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace NuGet.PackageManagement.UI
 {
     internal static class Images
     {
-        public static readonly BitmapImage DefaultPackageIcon;
+        public static readonly BitmapSource DefaultPackageIcon;
 
         static Images()
         {
-            DefaultPackageIcon = new BitmapImage();
-            DefaultPackageIcon.BeginInit();
+            // in VS, look up the icon via pack://application url
+            if (Application.Current != null)
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
 
-            // If the DLL name changes, this URI would need to change to match.
-            DefaultPackageIcon.UriSource = new Uri("pack://application:,,,/NuGet.PackageManagement.UI;component/Resources/packageicon.png");
+                // If the DLL name changes, this URI would need to change to match.
+                image.UriSource = new Uri("pack://application:,,,/NuGet.PackageManagement.UI;component/Resources/packageicon.png");
 
-            // Instead of scaling larger images and keeping larger image in memory, this makes it so we scale it down, and throw away the bigger image.
-            // Only need to set this on one dimension, to preserve aspect ratio
-            DefaultPackageIcon.DecodePixelWidth = 32;
+                // Instead of scaling larger images and keeping larger image in memory, this makes it so we scale it down, and throw away the bigger image.
+                // Only need to set this on one dimension, to preserve aspect ratio
+                image.DecodePixelWidth = PackageItemListViewModel.DecodePixelWidth;
 
-            DefaultPackageIcon.EndInit();
-            DefaultPackageIcon.Freeze();
+                image.EndInit();
+                image.Freeze();
+                DefaultPackageIcon = image;
+            }
+            else // for tests, don't actually load the icon, just use a 1x1 image.
+            {
+                BitmapSource image = BitmapSource.Create(
+                    pixelWidth: 1,
+                    pixelHeight: 1,
+                    dpiX: 96.0,
+                    dpiY: 96.0,
+                    PixelFormats.Bgr32,
+                    palette: null,
+                    pixels: new byte[] { 0, 0, 255, 0 },
+                    stride: 32);
+                image.Freeze();
+                DefaultPackageIcon = image;
+            }
         }
     }
 }
