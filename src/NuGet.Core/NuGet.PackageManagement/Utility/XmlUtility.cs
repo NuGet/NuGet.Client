053969a6dff82e448c9b10e6543f1b37c82509a8
Kartheek Penagamuri <kapenaga@microsoft.com>
2020-10-19 22:58:34 -0700

    added NuGet.ProjectManagement.XmlUtility back to maintain public API

--- src/NuGet.Core/NuGet.PackageManagement/Utility/XmlUtility.cs
+++ src/NuGet.Core/NuGet.PackageManagement/Utility/XmlUtility.cs
@@ -1,22 +1,56 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.IO;
 using System.Xml;
 using System.Xml.Linq;
-using static NuGet.Shared.XmlUtility;
+using XmlUtility = NuGet.Shared.XmlUtility;
 
 namespace NuGet.ProjectManagement
 {
     public static class XmlUtility
     {
+        [Obsolete("Use NuGet.Shared.XmlUtility.Load instead.")]
+        public static XDocument LoadSafe(string filePath)
+        {
+            return Shared.XmlUtility.Load(filePath);
+        }
+
+        [Obsolete("Use NuGet.Shared.XmlUtility.Load instead.")]
+        public static XDocument LoadSafe(Stream input)
+        {
+            return Shared.XmlUtility.Load(input);
+        }
+
+        [Obsolete("Use NuGet.Shared.XmlUtility.Load instead.")]
+        public static XDocument LoadSafe(Stream input, bool ignoreWhiteSpace)
+        {
+            if (ignoreWhiteSpace)
+                Shared.XmlUtility.Load(input);
+
+            return Shared.XmlUtility.Load(input, LoadOptions.PreserveWhitespace);
+        }
+
+        [Obsolete("Use NuGet.Shared.XmlUtility.Load instead.")]
+        public static XDocument LoadSafe(Stream input, LoadOptions options)
+        {
+            return Shared.XmlUtility.Load(input, options);
+        }
+
+        [Obsolete("Use MSBuildNuGetProjectSystemUtility.GetOrCreateDocument instead.")]
+        public static XDocument GetOrCreateDocument(XName rootName, string path, IMSBuildProjectSystem msBuildNuGetProjectSystem)
+        {
+            return MSBuildNuGetProjectSystemUtility.GetOrCreateDocument(rootName, path, msBuildNuGetProjectSystem);
+        }
+
         public static XDocument GetOrCreateDocument(XName rootName, string root, string path, INuGetProjectContext nuGetProjectContext)
         {
             if (File.Exists(Path.Combine(root, path)))
             {
                 try
                 {
-                    return Load(Path.Combine(root, path), LoadOptions.PreserveWhitespace);
+                    return Shared.XmlUtility.Load(Path.Combine(root, path), LoadOptions.PreserveWhitespace);
                 }
                 catch (FileNotFoundException) { }
             }
@@ -26,6 +60,46 @@ namespace NuGet.ProjectManagement
             FileSystemUtility.AddFile(root, path, document.Save, nuGetProjectContext);
 
             return document;
+        }
+
+        public static XDocument CreateDocument(XName rootName, string path, IMSBuildProjectSystem msBuildNuGetProjectSystem)
+        {
+            var document = new XDocument(new XElement(rootName));
+            // Add it to the project system
+            MSBuildNuGetProjectSystemUtility.AddFile(msBuildNuGetProjectSystem, path, document.Save);
+            return document;
+        }
+
+        public static XDocument CreateDocument(XName rootName, string root, string path, INuGetProjectContext nuGetProjectContext)
+        {
+            var document = new XDocument(new XElement(rootName));
+            // Add it to the file system
+            FileSystemUtility.AddFile(root, path, document.Save, nuGetProjectContext);
+            return document;
+        }
+
+        [Obsolete("Use NuGet.Shared.XmlUtility.Load instead.")]
+        public static XDocument GetDocument(string root, string path)
+        {
+            var fullPath = Path.Combine(root, path);
+            using (Stream configStream = File.OpenRead(fullPath))
+            {
+                return LoadSafe(configStream, LoadOptions.PreserveWhitespace);
+            }
+        }
+
+        public static bool TryParseDocument(string content, out XDocument document)
+        {
+            document = null;
+            try
+            {
+                document = XDocument.Parse(content);
+                return true;
+            }
+            catch (XmlException)
+            {
+                return false;
+            }
         }
     }
 }
