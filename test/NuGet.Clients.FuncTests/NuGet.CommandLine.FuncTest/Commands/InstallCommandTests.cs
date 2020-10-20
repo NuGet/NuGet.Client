0b4c3918e62ee3cf99d52d1d03c1aa2e74bca8e5
Heng Liu <liu.heng@microsoft.com>
2020-10-20 16:42:55 -0700

    fix another flaky tests

--- test/NuGet.Clients.FuncTests/NuGet.CommandLine.FuncTest/Commands/InstallCommandTests.cs
+++ test/NuGet.Clients.FuncTests/NuGet.CommandLine.FuncTest/Commands/InstallCommandTests.cs
@@ -195,10 +195,13 @@ namespace NuGet.CommandLine.FuncTest.Commands
 
                 var signedPackagePath = await SignedArchiveTestUtility.AuthorSignPackageAsync(testCertificate, nupkg, context.WorkingDirectory);
                 SignedArchiveTestUtility.TamperWithPackage(signedPackagePath);
+
+                //Making sure the OCSP responce expires
+                await certificateAuthority.OcspResponder.WaitForResponseExpirationAsync(bcCertificate);
                 certificateAuthority.Revoke(
                     bcCertificate,
                     RevocationReason.KeyCompromise,
-                    DateTimeOffset.UtcNow);
+                    DateTimeOffset.UtcNow.AddSeconds(-1));
 
                 var args = new string[]
                 {
