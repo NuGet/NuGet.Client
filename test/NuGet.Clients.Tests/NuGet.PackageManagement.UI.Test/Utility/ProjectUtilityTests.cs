4b6c4ac6d4626d9dc5f7cf40ce3211a317ba6240
Damon Tivel <dtivel@microsoft.com>
2020-10-22 13:57:11 -0700

    VS OE:  cache and reuse IServiceBroker instance in the client (#3724)
    Resolve https://github.com/NuGet/Home/issues/10141.

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Utility/ProjectUtilityTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Utility/ProjectUtilityTests.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.ServiceHub.Framework;
 using Moq;
+using NuGet.PackageManagement.VisualStudio;
 using NuGet.VisualStudio.Internal.Contracts;
 using Xunit;
 
@@ -15,10 +17,25 @@ namespace NuGet.PackageManagement.UI.Test
     public class ProjectUtilityTests
     {
         [Fact]
+        public async Task GetSortedProjectIdsAsync_WhenServiceBrokerIsNull_Throws()
+        {
+            ArgumentNullException exception = await Assert.ThrowsAsync<ArgumentNullException>(
+                () => ProjectUtility.GetSortedProjectIdsAsync(
+                    serviceBroker: null,
+                    projects: null,
+                    CancellationToken.None).AsTask());
+
+            Assert.Equal("serviceBroker", exception.ParamName);
+        }
+
+        [Fact]
         public async Task GetSortedProjectIdsAsync_WhenProjectsIsNull_Throws()
         {
             ArgumentNullException exception = await Assert.ThrowsAsync<ArgumentNullException>(
-                () => ProjectUtility.GetSortedProjectIdsAsync(projects: null, CancellationToken.None).AsTask());
+                () => ProjectUtility.GetSortedProjectIdsAsync(
+                    Mock.Of<IServiceBroker>(),
+                    projects: null,
+                    CancellationToken.None).AsTask());
 
             Assert.Equal("projects", exception.ParamName);
         }
@@ -28,6 +45,7 @@ namespace NuGet.PackageManagement.UI.Test
         {
             await Assert.ThrowsAsync<OperationCanceledException>(
                 () => ProjectUtility.GetSortedProjectIdsAsync(
+                    Mock.Of<IServiceBroker>(),
                     Enumerable.Empty<IProjectContextInfo>(),
                     new CancellationToken(canceled: true)).AsTask());
         }
@@ -35,19 +53,40 @@ namespace NuGet.PackageManagement.UI.Test
         [Fact]
         public async Task GetSortedProjectIdsAsync_WhenProjectsUnsorted_ReturnsSortedProjectIds()
         {
+            string projectIdA = Guid.NewGuid().ToString();
+            string projectIdB = Guid.NewGuid().ToString();
+            string projectIdC = Guid.NewGuid().ToString();
             var projectA = new Mock<IProjectContextInfo>();
             var projectB = new Mock<IProjectContextInfo>();
             var projectC = new Mock<IProjectContextInfo>();
             var projectAMetadata = new Mock<IProjectMetadataContextInfo>();
             var projectBMetadata = new Mock<IProjectMetadataContextInfo>();
             var projectCMetadata = new Mock<IProjectMetadataContextInfo>();
+            var serviceBroker = new Mock<IServiceBroker>();
+            Mock<INuGetProjectManagerService> projectManagerService = SetupProjectManagerService(serviceBroker);
 
-            projectA.Setup(x => x.GetMetadataAsync(It.IsAny<CancellationToken>()))
-                .ReturnsAsync(projectAMetadata.Object);
-            projectB.Setup(x => x.GetMetadataAsync(It.IsAny<CancellationToken>()))
-                .ReturnsAsync(projectBMetadata.Object);
-            projectC.Setup(x => x.GetMetadataAsync(It.IsAny<CancellationToken>()))
-                .ReturnsAsync(projectCMetadata.Object);
+            projectManagerService.Setup(
+                    x => x.GetMetadataAsync(
+                        It.Is<string>(projectId => projectId == projectIdA),
+                        It.IsAny<CancellationToken>()))
+                .Returns(new ValueTask<IProjectMetadataContextInfo>(projectAMetadata.Object));
+            projectManagerService.Setup(
+                    x => x.GetMetadataAsync(
+                        It.Is<string>(projectId => projectId == projectIdB),
+                        It.IsAny<CancellationToken>()))
+                .Returns(new ValueTask<IProjectMetadataContextInfo>(projectBMetadata.Object));
+            projectManagerService.Setup(
+                    x => x.GetMetadataAsync(
+                        It.Is<string>(projectId => projectId == projectIdC),
+                        It.IsAny<CancellationToken>()))
+                .Returns(new ValueTask<IProjectMetadataContextInfo>(projectCMetadata.Object));
+
+            projectA.SetupGet(x => x.ProjectId)
+                .Returns(projectIdA);
+            projectB.SetupGet(x => x.ProjectId)
+                .Returns(projectIdB);
+            projectC.SetupGet(x => x.ProjectId)
+                .Returns(projectIdC);
 
             projectAMetadata.SetupGet(x => x.UniqueName)
                 .Returns("a");
@@ -67,10 +106,27 @@ namespace NuGet.PackageManagement.UI.Test
                 .Returns(expectedResults[2]);
 
             IEnumerable<string> actualResults = await ProjectUtility.GetSortedProjectIdsAsync(
+                serviceBroker.Object,
                 new[] { projectB.Object, projectA.Object, projectC.Object },
                 CancellationToken.None);
 
             Assert.Equal(expectedResults, actualResults.ToArray());
+        }
+
+        private static Mock<INuGetProjectManagerService> SetupProjectManagerService(Mock<IServiceBroker> serviceBroker)
+        {
+            var projectManagerService = new Mock<INuGetProjectManagerService>();
+
+            serviceBroker.Setup(
+#pragma warning disable ISB001 // Dispose of proxies
+                    x => x.GetProxyAsync<INuGetProjectManagerService>(
+                        It.Is<ServiceRpcDescriptor>(s => s == NuGetServices.ProjectManagerService),
+                        It.IsAny<ServiceActivationOptions>(),
+                        It.IsAny<CancellationToken>()))
+#pragma warning restore ISB001 // Dispose of proxies
+                .Returns(new ValueTask<INuGetProjectManagerService>(projectManagerService.Object));
+
+            return projectManagerService;
         }
     }
 }
