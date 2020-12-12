95e850a1d6b3e12083dee7c6c5fc0a7cb15dfac4
Donnie Goodson <donnie.goodson@microsoft.com>
2020-12-12 01:30:56 -0500

    UpgradeLogger

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Actions/UpgradeLogger.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Actions/UpgradeLogger.cs
@@ -80,7 +80,7 @@ namespace NuGet.PackageManagement.UI
         {
             if (string.IsNullOrEmpty(reportName))
             {
-                throw new ArgumentException(nameof(reportName));
+                throw new ArgumentNullException(nameof(reportName));
             }
 
             if (string.IsNullOrEmpty(backupPath) || !Directory.Exists(backupPath))
