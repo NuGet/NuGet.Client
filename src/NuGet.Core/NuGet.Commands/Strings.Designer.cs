c2a8d540db7983f5c72a552bd122f21cd7c71e77
Erick Yondon <eryondon@microsoft.com>
2020-12-14 10:34:27 -0800

    Fix wording after @JonDouglas review.

--- src/NuGet.Core/NuGet.Commands/Strings.Designer.cs
+++ src/NuGet.Core/NuGet.Commands/Strings.Designer.cs
@@ -2645,7 +2645,7 @@ namespace NuGet.Commands {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Project dependency {0} does not have version specified or does not contain an inclusive lower bound. Specify version or include a lower bound in the dependency version to ensure consistent restore results..
+        ///   Looks up a localized string similar to Project dependency {0} does not contain an inclusive lower bound or specified version. Include a lower bound or specify a version in the dependency version to ensure consistent restore results..
         /// </summary>
         internal static string Warning_ProjectDependencyMissing_Version_LowerBound {
             get {
