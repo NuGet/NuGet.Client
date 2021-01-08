38bea250c93b5f17a8f527443059ed1ae6d20697
Donnie Goodson <donnie.goodson@microsoft.com>
2021-01-08 01:59:27 -0500

    Create Toggleable Automation Peer for ListBox

---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Automation/ToggleableItemAutomationPeer.cs
@@ -0,0 +1,62 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Windows.Automation;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+
+namespace NuGet.PackageManagement.UI
+{
+    internal class ToggleableItemAutomationPeer : ListBoxItemAutomationPeer, IToggleProvider
+    {
+        private readonly PackageItemListViewModel _owner;
+
+        public ToggleableItemAutomationPeer(object item, SelectorAutomationPeer selectorAutomationPeer)
+            : base(item, selectorAutomationPeer)
+        {
+            _owner = item as PackageItemListViewModel;
+        }
+
+        /// <summary>
+        /// TODO: really necessary?
+        /// </summary>
+        /// <param name="patternInterface"></param>
+        /// <returns></returns>
+        public override object GetPattern(PatternInterface patternInterface)
+        {
+            if (patternInterface == PatternInterface.Toggle)
+            {
+                return this;
+            }
+
+            return base.GetPattern(patternInterface);
+        }
+
+        /// <summary>
+        /// The default will be assumed to be toggled off (eg, Checked is false), therefore translating to `ToggleState.Off`.
+        /// </summary>
+        public ToggleState ToggleState
+        {
+            get
+            {
+                return _owner?.Selected == true ? ToggleState.On : ToggleState.Off;
+            }
+            set
+            {
+                if (_owner != null)
+                {
+                    _owner.Selected = value == ToggleState.On;
+                }
+            }
+        }
+
+        public void Toggle()
+        {
+            if (_owner != null)
+            {
+                _owner.Selected = !_owner.Selected;
+            }
+        }
+    }
+}
--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageItemControl.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageItemControl.xaml.cs
@@ -1,7 +1,11 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace NuGet.PackageManagement.UI
 {
@@ -14,6 +18,57 @@ namespace NuGet.PackageManagement.UI
         public PackageItemControl()
         {
             InitializeComponent();
+        }
+
+        // Whenever the checkbox in the item is checked, the containing item will raise a 
+        // UIA event to convey the fact that the toggle state of the item has changed. The 
+        // event must be raised regardless of whether the toggle state of the item changed 
+        // in response to keyboard, mouse, or programmatic input.
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var itemCheckBox = sender as CheckBox;
+            if (itemCheckBox == null || itemCheckBox.Visibility != Visibility.Visible)
+            {
+                return;
+            }
+
+            ListBoxItem itemContainer = FindParent<ListBoxItem>(this);
+            if (itemContainer == null)
+            {
+                return;
+            }
+
+            var isChecked = (e.RoutedEvent == CheckBox.CheckedEvent);
+
+            AutomationPeer itemAutomationPeer = UIElementAutomationPeer.FromElement(itemContainer);
+            if (itemAutomationPeer != null)
+            {
+                itemAutomationPeer.RaisePropertyChangedEvent(
+                    TogglePatternIdentifiers.ToggleStateProperty,
+                    isChecked ? ToggleState.Off : ToggleState.On, // Assume the state has actually toggled.
+                    isChecked ? ToggleState.On : ToggleState.Off);
+            }
+        }
+
+        private static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentDepedencyObject = VisualTreeHelper.GetParent(child);
+
+            if (parentDepedencyObject == null)
+            {
+                return null;
+            }
+
+            T parent = parentDepedencyObject as T;
+
+            if (parent != null)
+            {
+                return parent;
+            }
+            else // Keep looking up the visual tree.
+            {
+                return FindParent<T>(parentDepedencyObject);
+            }
         }
     }
 }
