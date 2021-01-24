59792e4733ced999af222a8ba7adb7a4ee8f5a08
Andy Zivkovic <zivkan@users.noreply.github.com>
2021-01-24 06:56:17 -0800

    Add package source to "installing <package> <version>" message at normal verbosity (#3849)

---
+++ src/NuGet.Core/NuGet.Packaging/StringFormatter.cs
@@ -0,0 +1,24 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Globalization;
+
+namespace NuGet.Packaging
+{
+    internal static class StringFormatter
+    {
+        internal static string Log_InstalledPackage(
+            string packageId,
+            string packageVersion,
+            string source,
+            string contentHash)
+        {
+            return string.Format(CultureInfo.CurrentCulture,
+                Strings.Log_InstalledPackage,
+                packageId,
+                packageVersion,
+                source,
+                contentHash);
+        }
+    }
+}
