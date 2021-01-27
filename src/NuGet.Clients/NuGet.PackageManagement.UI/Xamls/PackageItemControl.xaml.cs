15b15d0647a3761a3415563be8a6c04453c9d9c0
donnie-msft <49205731+donnie-msft@users.noreply.github.com>
2021-01-26 22:25:07 -0500

    PMUI Packages List Update selection checkboxes support Screen-Reader Toggle Pattern (#3857)
    * Create Toggleable Automation Peer for ListBox

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageItemControl.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageItemControl.xaml.cs
@@ -1,7 +1,11 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace NuGet.PackageManagement.UI
 {
@@ -14,6 +18,48 @@ namespace NuGet.PackageManagement.UI
         public PackageItemControl()
         {
             InitializeComponent();
+        }
+
+        // Whenever the checkbox in the item is checked, the containing item will raise a 
+        // UIA event to convey the fact that the toggle state of the item has changed. The 
+        // event must be raised regardless of whether the toggle state of the item changed 
+        // in response to keyboard, mouse, or programmatic input.
+        private void CheckBox_Toggled(object sender, RoutedEventArgs e)
+        {
+            var itemCheckBox = sender as CheckBox;
+            if (itemCheckBox is null || itemCheckBox.Visibility != Visibility.Visible)
+            {
+                return;
+            }
+
+            var parentContentPresenter = VisualTreeHelper.GetParent(this) as ContentPresenter;
+            if (parentContentPresenter is null)
+            {
+                return;
+            }
+
+            var parentBorder = VisualTreeHelper.GetParent(parentContentPresenter) as Border;
+            if (parentBorder is null)
+            {
+                return;
+            }
+
+            var itemContainer = VisualTreeHelper.GetParent(parentBorder) as ListBoxItem;
+            if (itemContainer is null)
+            {
+                return;
+            }
+
+            var isChecked = (e.RoutedEvent == CheckBox.CheckedEvent);
+
+            AutomationPeer itemAutomationPeer = UIElementAutomationPeer.FromElement(itemContainer);
+            if (itemAutomationPeer != null)
+            {
+                itemAutomationPeer.RaisePropertyChangedEvent(
+                    TogglePatternIdentifiers.ToggleStateProperty,
+                    isChecked ? ToggleState.Off : ToggleState.On, // Assume the state has actually toggled.
+                    isChecked ? ToggleState.On : ToggleState.Off);
+            }
         }
     }
 }
