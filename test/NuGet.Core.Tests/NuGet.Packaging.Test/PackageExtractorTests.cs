dea0eef958463c72c8c75aee4734cfb02f280c2e
Erick Yondon <eryondon@microsoft.com>
2021-01-29 13:22:08 -0800

    Revert accidental changes.

--- test/NuGet.Core.Tests/NuGet.Packaging.Test/PackageExtractorTests.cs
+++ test/NuGet.Core.Tests/NuGet.Packaging.Test/PackageExtractorTests.cs
@@ -3766,7 +3766,7 @@ namespace NuGet.Packaging.Test
         }
 
         [Fact]
-        public async Task InstallFromSourceAsync_LogsSourceOnNormalLevel()
+        public async Task InstallFromSourceAsync_LogsSourceOnVerboseLevel()
         {
             // Arrange
             using (var testDirectory = TestDirectory.Create())
@@ -3804,7 +3804,7 @@ namespace NuGet.Packaging.Test
                     // Assert
                     File.Exists(resolver.GetPackageFilePath(identity.Id, identity.Version)).Should().BeTrue();
                     var nupkgMetadata = NupkgMetadataFileFormat.Read(resolver.GetNupkgMetadataPath(identity.Id, identity.Version));
-                    testLogger.InformationMessages.Should().Contain($"Installed {identity.Id} {identity.Version} from {source} with content hash {nupkgMetadata.ContentHash}.");
+                    testLogger.VerboseMessages.Should().Contain($"Completed installation of {identity.Id} {identity.Version} from {source} with content hash {nupkgMetadata.ContentHash}");
                 }
             }
         }
@@ -3855,7 +3855,7 @@ namespace NuGet.Packaging.Test
         }
 
         [Fact]
-        public async Task InstallFromSourceAsync_WithPackageDownloader_LogsSourceOnNormalLevel()
+        public async Task InstallFromSourceAsync_WithPackageDownloader_LogsSourceOnVerboseLevel()
         {
             // Arrange
             using (var testDirectory = TestDirectory.Create())
@@ -3899,7 +3899,7 @@ namespace NuGet.Packaging.Test
                     // Assert
                     File.Exists(resolver.GetPackageFilePath(identity.Id, identity.Version)).Should().BeTrue();
                     var nupkgMetadata = NupkgMetadataFileFormat.Read(resolver.GetNupkgMetadataPath(identity.Id, identity.Version));
-                    testLogger.InformationMessages.Should().Contain($"Installed {identity.Id} {identity.Version} from {source} with content hash {nupkgMetadata.ContentHash}.");
+                    testLogger.VerboseMessages.Should().Contain($"Completed installation of {identity.Id} {identity.Version} from {source} with content hash {nupkgMetadata.ContentHash}");
                 }
             }
         }
