bf0e23858ee6702bcdb0bc479663f4f9095807f6
Erick Yondon <eryondon@microsoft.com>
2021-02-04 17:14:12 -0800

    Add dotnet functional test for same name/version package/project reference works.

--- test/NuGet.Core.FuncTests/Dotnet.Integration.Test/DotnetRestoreTests.cs
+++ test/NuGet.Core.FuncTests/Dotnet.Integration.Test/DotnetRestoreTests.cs
@@ -1122,6 +1122,83 @@ EndGlobal";
             }
         }
 
+
+        [PlatformFact(Platform.Windows)]
+        public async Task DotnetRestore_SameNameSameKeyProjectPackageReferencing_Succeeds()
+        {
+            using (var pathContext = _msbuildFixture.CreateSimpleTestPathContext())
+            {
+                // Set up solution, and project
+                var solution = new SimpleTestSolutionContext(pathContext.SolutionRoot);
+                var projFramework = FrameworkConstants.CommonFrameworks.Net462;
+                var projectPackageName = "projectA";
+                var projectA = SimpleTestProjectContext.CreateNETCore(
+                   projectPackageName,
+                   pathContext.SolutionRoot,
+                   projFramework);
+                var projectIntermed = SimpleTestProjectContext.CreateNETCore(
+                   "projectIntermed",
+                   pathContext.SolutionRoot,
+                   projFramework);
+                var projectMain = SimpleTestProjectContext.CreateNETCore(
+                   "projectMain",
+                   pathContext.SolutionRoot,
+                   projFramework);
+
+                //Setup packages and feed
+                var packageA = new SimpleTestPackageContext()
+                {
+                    Id = projectPackageName,
+                    Version = "1.0.0"
+                };
+
+                await SimpleTestPackageUtility.CreateFolderFeedV3Async(
+                    pathContext.PackageSource,
+                    PackageSaveMode.Defaultv3,
+                    packageA);
+
+                //add the packe to the project
+                projectIntermed.AddPackageToAllFrameworks(packageA);
+                projectMain.AddProjectToAllFrameworks(projectA);
+                projectMain.AddProjectToAllFrameworks(projectIntermed);
+                solution.Projects.Add(projectA);
+                solution.Projects.Add(projectIntermed);
+                solution.Projects.Add(projectMain);
+                solution.Create(pathContext.SolutionRoot);
+
+                // Act
+                var args = $" --source \"{pathContext.PackageSource}\" ";
+                var reader = new LockFileFormat();
+
+                var projdir = Path.GetDirectoryName(projectA.ProjectPath);
+                var projfilename = Path.GetFileNameWithoutExtension(projectA.ProjectName);
+                _msbuildFixture.RestoreProject(projdir, projfilename, args);
+                Assert.True(File.Exists(projectA.AssetsFileOutputPath));
+
+                projdir = Path.GetDirectoryName(projectIntermed.ProjectPath);
+                projfilename = Path.GetFileNameWithoutExtension(projectIntermed.ProjectName);
+                _msbuildFixture.RestoreProject(projdir, projfilename, args);
+                Assert.True(File.Exists(projectIntermed.AssetsFileOutputPath));
+                var lockFile = reader.Read(projectIntermed.AssetsFileOutputPath);
+                IList<LockFileTargetLibrary> libraries = lockFile.Targets[0].Libraries;
+                Assert.True(libraries.Any(l => l.Type == "package" && l.Name == projectA.ProjectName));
+
+                projdir = Path.GetDirectoryName(projectMain.ProjectPath);
+                projfilename = Path.GetFileNameWithoutExtension(projectMain.ProjectName);
+                _msbuildFixture.RestoreProject(projdir, projfilename, args);
+                Assert.True(File.Exists(projectMain.AssetsFileOutputPath));
+                lockFile = reader.Read(projectMain.AssetsFileOutputPath);
+                var errors = lockFile.LogMessages.Where(m => m.Level == LogLevel.Error);
+                var warnings = lockFile.LogMessages.Where(m => m.Level == LogLevel.Warning);
+                Assert.Equal(0, errors.Count());
+                Assert.Equal(0, warnings.Count());
+                libraries = lockFile.Targets[0].Libraries;
+                Assert.Equal(2, libraries.Count);
+                Assert.True(libraries.Any(l => l.Type == "project" && l.Name == projectA.ProjectName));
+                Assert.True(libraries.Any(l => l.Type == "project" && l.Name == projectIntermed.ProjectName));
+            }
+        }
+
         private static SimpleTestPackageContext CreateNetstandardCompatiblePackage(string id, string version)
         {
             var pkgX = new SimpleTestPackageContext(id, version);
