a67bc30b5f3c96a31a9cd51d9b28bd3b437c5c19
Jean-Pierre Briedé <jebriede@microsoft.com>
2021-03-15 13:23:03 -0700

    Update the background and foreground colors of the PMUI under experimentation flag (#3947)

---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Experimentation/ExperimentationConstants.cs
@@ -0,0 +1,18 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGet.PackageManagement.UI
+{
+    internal static class ExperimentationConstants
+    {
+        internal static class FlightFlags
+        {
+            public const string PackageManagerBackgroundColor = "nuGetPackageManagerBackgroundColor";
+        }
+
+        internal static class EnvironmentVariables
+        {
+            public const string PackageManagerBackgroundColor = "NUGET_PACKAGE_MANAGER_BACKGROUND_COLOR";
+        }
+    }
+}
