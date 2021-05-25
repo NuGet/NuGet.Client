c4d3b4eccdc7d74b3bf847342c154c27c1fd0392
Nikolche Kolev <nikolev@microsoft.com>
2021-05-25 13:36:53 -0700

    Improve description

--- src/NuGet.Clients/NuGet.SolutionRestoreManager.Interop/IVsProjectRestoreInfoSource.cs
+++ src/NuGet.Clients/NuGet.SolutionRestoreManager.Interop/IVsProjectRestoreInfoSource.cs
@@ -33,7 +33,7 @@ namespace NuGet.SolutionRestoreManager
         /// If the project has no pending restore data, it will return a completed task.
         /// Otherwise, the task will be completed once the project nominates.
         /// The task will be cancelled, if the source decide it no longer needs to nominate (for example: the restore state has no change)
-        /// The task will be failed, if the source runs into a problem, so it cannot get correct data to nominate (for exammple: DT build failed)
+        /// The task will be failed, if the source runs into a problem, and it cannot get the correct data to nominate (for example: DT build failed)
         /// </summary>
         /// <param name="cancellationToken">Cancellation token.</param>
         Task WhenNominatedAsync(CancellationToken cancellationToken);
