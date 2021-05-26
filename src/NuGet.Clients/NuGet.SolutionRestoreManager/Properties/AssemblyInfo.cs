0108911e8620c21217aa421651148406070721b7
Andy Zivkovic <Andy.Zivkovic@microsoft.com>
2021-05-26 10:01:37 -0700

    Fix N.SRM.I binding redirect codebase syntax

--- src/NuGet.Clients/NuGet.SolutionRestoreManager/Properties/AssemblyInfo.cs
+++ src/NuGet.Clients/NuGet.SolutionRestoreManager/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using Microsoft.VisualStudio.Shell;
 [assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\NuGet.VisualStudio.dll")]
 [assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\NuGet.VisualStudio.Internal.Contracts.dll")]
 
-[assembly: ProvideBindingRedirection(CodeBase = "NuGet.SolutionRestoreManager.Interop", OldVersionLowerBound = "0.0.0.0")]
+[assembly: ProvideBindingRedirection(CodeBase = @"$PackageFolder$\NuGet.SolutionRestoreManager.Interop.dll", OldVersionLowerBound = "0.0.0.0")]
 
 #if SIGNED_BUILD
 [assembly: InternalsVisibleTo("NuGet.SolutionRestoreManager.Test, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
