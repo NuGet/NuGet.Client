3dbf6778634714ced1d80455ca86c465aa0c057b
Erick Yondon <eryondon@microsoft.com>
2021-07-07 12:25:05 -0700

    Add back tests.

--- test/EndToEnd/tests/PackageNameSpaceTests.ps1
+++ test/EndToEnd/tests/PackageNameSpaceTests.ps1
@@ -1,3 +1,170 @@
+function Test-PackageNamespaceRestore-WithSingleFeed
+{
+    param($context)
+
+    # Arrange
+    $repoDirectory = $context.RepositoryRoot
+    $nugetConfigPath = Join-Path $OutputPath 'nuget.config'
+
+    $settingFileContent =@"
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <packageSources>
+    <clear />
+    <add key="ReadyPackages" value="{0}" />
+    </packageSources>
+    <packageNamespaces>
+        <packageSource key="ReadyPackages">
+            <namespace id="Soluti*" />
+        </packageSource>
+    </packageNamespaces>
+</configuration>
+"@
+  
+    try {
+        # We have to create config file before creating solution, otherwise it's not effective for new solutions.
+        $settingFileContent -f $repoDirectory | Out-File -Encoding "UTF8" $nugetConfigPath
+    
+        $p = New-ConsoleApplication
+    
+        $projectDirectoryPath = $p.Properties.Item("FullPath").Value
+        $packagesConfigPath = Join-Path $projectDirectoryPath 'packages.config'
+        $projectDirectoryPath = $p.Properties.Item("FullPath").Value
+        $solutionDirectory = Split-Path -Path $projectDirectoryPath -Parent   
+        # Write a file to disk, but do not add it to project
+        '<packages>
+            <package id="SolutionLevelPkg" version="1.0.0" targetFramework="net461" />
+    </packages>' | out-file $packagesConfigPath
+    
+        # Act
+        Build-Solution
+    
+        # Assert
+        $packagesFolder = Join-Path $solutionDirectory "packages"
+        $solutionLevelPkgNupkgFolder = Join-Path $packagesFolder "SolutionLevelPkg.1.0.0"
+        Assert-PathExists(Join-Path $solutionLevelPkgNupkgFolder "SolutionLevelPkg.1.0.0.nupkg")
+        
+        $errorlist = Get-Errors
+        Assert-AreEqual 0 $errorlist.Count
+    }
+    finally {
+        Remove-Item $nugetConfigPath
+    }
+}
+
+function Test-PackageNamespaceRestore-WithMultipleFeedsWithIdenticalPackages-RestoresCorrectPackage
+{
+    param($context)
+
+    # Arrange
+    $repoDirectory = Join-Path $OutputPath "CustomPackages"
+    $opensourceRepo = Join-Path $repoDirectory "opensourceRepo"
+    $privateRepo = Join-Path $repoDirectory "privateRepo"
+    $nugetConfigPath = Join-Path $OutputPath 'nuget.config'
+
+	$settingFileContent =@"
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <packageSources>
+    <clear />
+    <add key="OpensourceRepository" value="{0}" />
+    <add key="PrivateRepository" value="{1}" />
+    </packageSources>
+    <packageNamespaces>
+        <packageSource key="PrivateRepository">
+            <namespace id="Contoso.MVC.*" />
+        </packageSource>
+    </packageNamespaces>
+</configuration>
+"@
+    try {
+        # We have to create config file before creating solution, otherwise it's not effective for new solutions.
+        $settingFileContent -f $opensourceRepo,$privateRepo | Out-File -Encoding "UTF8" $nugetConfigPath
+
+        $p = New-ConsoleApplication
+
+        $projectDirectoryPath = $p.Properties.Item("FullPath").Value
+        $packagesConfigPath = Join-Path $projectDirectoryPath 'packages.config'
+        $projectDirectoryPath = $p.Properties.Item("FullPath").Value
+        $solutionDirectory = Split-Path -Path $projectDirectoryPath -Parent   
+        # Write a file to disk, but do not add it to project
+        '<packages>
+            <package id="Contoso.MVC.ASP" version="1.0.0" targetFramework="net461" />
+    </packages>' | out-file $packagesConfigPath
+
+        CreateCustomTestPackage "Contoso.MVC.ASP" "1.0.0" $privateRepo "Thisisfromprivaterepo.txt"
+        CreateCustomTestPackage "Contoso.MVC.ASP" "1.0.0" $opensourceRepo "Thisisfromopensourcerepo.txt"
+
+        # Act
+        Build-Solution
+
+        # Assert   
+        $packagesFolder = Join-Path $solutionDirectory "packages"
+        $contosoNupkgFolder = Join-Path $packagesFolder "Contoso.MVC.ASP.1.0.0"
+        Assert-PathExists(Join-Path $contosoNupkgFolder "Contoso.MVC.ASP.1.0.0.nupkg")
+        # Make sure name squatting package from public repo not restored.
+        $contentFolder = Join-Path $contosoNupkgFolder "content"
+        Assert-PathExists(Join-Path $contentFolder "Thisisfromprivaterepo.txt")
+
+        $errorlist = Get-Errors
+        Assert-AreEqual 0 $errorlist.Count
+    }
+    finally {
+        Remove-Item -Recurse -Force $repoDirectory
+        Remove-Item $nugetConfigPath
+    }
+}
+
+function Test-PackageNamespaceInstall-WithSingleFeed
+{
+    param($context)
+
+    # Arrange
+    $repoDirectory = $context.RepositoryRoot
+    $nugetConfigPath = Join-Path $OutputPath 'nuget.config'
+
+    $settingFileContent =@"
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <packageSources>
+    <clear />
+    <add key="ReadyPackages" value="{0}" />
+    </packageSources>
+    <packageNamespaces>
+        <packageSource key="ReadyPackages">
+            <namespace id="Soluti*" />
+        </packageSource>
+    </packageNamespaces>
+</configuration>
+"@
+  
+    try {
+        # We have to create config file before creating solution, otherwise it's not effective for new solutions.
+        $settingFileContent -f $repoDirectory | Out-File -Encoding "UTF8" $nugetConfigPath
+    
+        $p = New-ConsoleApplication
+    
+        $projectDirectoryPath = $p.Properties.Item("FullPath").Value
+        $packagesConfigPath = Join-Path $projectDirectoryPath 'packages.config'
+        $projectDirectoryPath = $p.Properties.Item("FullPath").Value
+        $solutionDirectory = Split-Path -Path $projectDirectoryPath -Parent   
+
+        # Act
+        $p | Install-Package SolutionLevelPkg -version 1.0.0
+    
+        # Assert
+        $packagesFolder = Join-Path $solutionDirectory "packages"
+        $solutionLevelPkgNupkgFolder = Join-Path $packagesFolder "SolutionLevelPkg.1.0.0"
+        Assert-PathExists(Join-Path $solutionLevelPkgNupkgFolder "SolutionLevelPkg.1.0.0.nupkg")
+        
+        $errorlist = Get-Errors
+        Assert-AreEqual 0 $errorlist.Count
+    }
+    finally {
+        Remove-Item $nugetConfigPath
+    }
+}
+
 function Test-PackageNamespaceRestore-WithMultipleFeedsWithIdenticalPackages-InstallCorrectPackage
 {
     param($context)
