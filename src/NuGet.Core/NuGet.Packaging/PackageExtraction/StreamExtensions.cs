ed00f60f37c7e84478d8cba3c7d6721c4c6cf289
David Kean <davkean@microsoft.com>
2021-07-09 09:14:59 +1000

    Avoid byte array allocations to reduce allocations by about 16% during clean restore (#4134)
    This avoid unnecessary during signed package reads & writes. BinaryReader.ReadBytes ends returning an instance of new array that signed package reading and writing only turns around writes it immediately to another stream or hash function.

--- src/NuGet.Core/NuGet.Packaging/PackageExtraction/StreamExtensions.cs
+++ src/NuGet.Core/NuGet.Packaging/PackageExtraction/StreamExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Buffers;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 
@@ -58,15 +59,37 @@ namespace NuGet.Packaging
                     using (MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(fileFullPath, FileMode.Open, mapName: null, (long)size))
                     using (MemoryMappedViewStream mmstream = mmf.CreateViewStream())
                     {
-                        inputStream.CopyTo(mmstream);
+                        CopyTo(inputStream, mmstream);
                     }
                 }
                 else
                 {
-                    inputStream.CopyTo(outputStream);
+                    CopyTo(inputStream, outputStream);
                 }
             }
             return fileFullPath;
+        }
+
+        private static void CopyTo(Stream inputStream, Stream outputStream)
+        {
+            // .NET Framework allocates an unavoidable byte[] when using
+            // Stream.CopyTo. Reimplement it, pulling from the pool similar
+            // to .NET 5.
+
+#if NETFRAMEWORK || NETSTANDARD2_0
+            const int bufferSize = 81920; // Same as Stream.CopyTo
+            byte[] buffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+
+            int bytesRead;
+            while ((bytesRead = inputStream.Read(buffer, offset: 0, buffer.Length)) != 0)
+            {
+                outputStream.Write(buffer, offset: 0, bytesRead);
+            }
+
+            ArrayPool<byte>.Shared.Return(buffer);
+#else
+            inputStream.CopyTo(outputStream);
+#endif
         }
     }
 }
