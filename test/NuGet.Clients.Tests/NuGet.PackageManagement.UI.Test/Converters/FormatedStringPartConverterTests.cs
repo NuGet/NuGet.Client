b010f1989daf41da0dae467cce07fc83017bc226
Fernando Aguilar <feaguila@microsoft.com>
2021-07-25 15:59:21 -0700

    Fixed tests

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Converters/FormatedStringPartConverterTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Converters/FormatedStringPartConverterTests.cs
@@ -10,7 +10,7 @@ namespace NuGet.PackageManagement.UI.Test.Converters
         [Theory]
         [InlineData("Package deprecated. Alternative {0}", "Package deprecated. Alternative ", "")]
         [InlineData("Deprecated. Use {0} instead", "Deprecated. Use ", " instead")]
-        [InlineData("{0} will replace the deprecated package", "", " will replace deprecated package")]
+        [InlineData("{0} will replace the deprecated package", "", " will replace the deprecated package")]
         [InlineData("Пакет устарел. Вместо этого используйте {0}.", "Пакет устарел. Вместо этого используйте ", ".")]
         [InlineData("包装被弃用。改用{0}", "包装被弃用。改用", "")]
         [InlineData("No placeholder ", null, null)]
