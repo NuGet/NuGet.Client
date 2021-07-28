d158b74acc25fc206b5a091647d8d78e40ec86b6
Drew Noakes <git@drewnoakes.com>
2021-07-28 11:26:04 +1000

    Remove assignments to MSBuildAllProjects
    In the past, MSBuildAllProjects was used to support incremental build. Every project file would have to append its path to this property, and an up-to-date check could use that information to scan timestamps of all consituent files.
    
    Nowadays, MSBuild tracks which of the project files has the newest timestamp and automatically prepends that value to MSBuildAllProjects. The up-to-date check in VS only needs to check the first item from this property, and the rest are ignored.
    
    For more information see http://www.panopticoncentral.net/2019/07/12/msbuildallprojects-considered-harmful/

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/Utility/BuildAssetsUtils.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/Utility/BuildAssetsUtils.cs
@@ -190,9 +190,7 @@ namespace NuGet.Commands
 
                 new XElement(Namespace + "Project",
                     new XAttribute("ToolsVersion", "14.0"),
-                    new XAttribute("xmlns", Namespace.NamespaceName),
-                    new XElement(Namespace + "PropertyGroup",
-                        new XElement(Namespace + "MSBuildAllProjects", "$(MSBuildAllProjects);$(MSBuildThisFileFullPath)"))));
+                    new XAttribute("xmlns", Namespace.NamespaceName)));
 
             return doc;
         }
