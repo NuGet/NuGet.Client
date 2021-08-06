fd87dec162390b44847acd3334cd1088bd66c48c
Erick Yondon <eryondon@microsoft.com>
2021-08-06 09:39:34 -0700

    Address PR comments

--- src/NuGet.Core/NuGet.ProjectModel/ToolPathResolver.cs
+++ src/NuGet.Core/NuGet.ProjectModel/ToolPathResolver.cs
@@ -112,9 +112,8 @@ namespace NuGet.ProjectModel
                 var version = Path.GetFileName(versionDirectory);
 
                 NuGetVersion nugetVersion = null;
-                _ = NuGetVersion.TryParse(version, out nugetVersion);
 
-                if (nugetVersion != null)
+                if (NuGetVersion.TryParse(version, out var nugetVersion))
                 {
                     availableVersions.Add(nugetVersion);
                 }
