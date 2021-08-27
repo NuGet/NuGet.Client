d4d41c81a3b6f48ec5dc56726b583ef45c276985
Jean-Pierre Briede <jebriede@microsoft.com>
2021-08-27 11:24:25 -0700

    Remove reference to Net472 in unit test because it is not avaible in commit 820bfe9

--- test/NuGet.Core.Tests/NuGet.ProjectModel.Test/PackagesLockFileTargetTests.cs
+++ test/NuGet.Core.Tests/NuGet.ProjectModel.Test/PackagesLockFileTargetTests.cs
@@ -9,7 +9,6 @@ namespace NuGet.ProjectModel.Test
     public class PackagesLockFileTargetTests
     {
         [Theory]
-        [InlineData("net472", null, ".NETFramework,Version=v4.7.2")]
         [InlineData("netstandard2.0", null, ".NETStandard,Version=v2.0")]
         [InlineData("netcoreapp3.1", null, ".NETCoreApp,Version=v3.1")]
         [InlineData("netcoreapp3.1", "win-x64", ".NETCoreApp,Version=v3.1/win-x64")]
