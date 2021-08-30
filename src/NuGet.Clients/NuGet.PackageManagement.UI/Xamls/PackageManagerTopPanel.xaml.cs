9f3b813861b4675b93b41d5cda20631fee5580e4
Jean-Pierre Briede <jebriede@microsoft.com>
2021-08-30 15:34:23 -0700

    Make Consolidate tab textblock not hit testable

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageManagerTopPanel.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageManagerTopPanel.xaml.cs
@@ -52,6 +52,7 @@ namespace NuGet.PackageManagement.UI
             var textConsolidate = new TextBlock();
             textConsolidate.Name = nameof(textConsolidate);
             textConsolidate.Text = Resx.Action_Consolidate;
+            textConsolidate.IsHitTestVisible = false;
             sp.Children.Add(textConsolidate);
 
             SetConsolidationAutomationProperties(tabConsolidate, count: 0);
