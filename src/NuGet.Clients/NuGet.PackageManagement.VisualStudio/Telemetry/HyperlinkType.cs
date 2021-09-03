34de922ff89a8c0686c0576fa66f5efb78303d6e
Fernando Aguilar <feaguila@microsoft.com>
2021-09-02 20:36:30 -0700

    Fixed Uri mapping

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Telemetry/HyperlinkType.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Telemetry/HyperlinkType.cs
@@ -1,16 +1,11 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace NuGet.PackageManagement.Telemetry
 {
     public enum HyperlinkType
     {
+        Header,
         License,
         ProjectUri,
         ReportAbuse,
