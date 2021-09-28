446ec3cce9cd4414a7a4bee11632f8213558145f
Heng Liu <liu.heng@microsoft.com>
2021-09-28 16:27:21 -0700

    address comments

--- src/NuGet.Core/NuGet.Protocol/Plugins/ProtocolException.cs
+++ src/NuGet.Core/NuGet.Protocol/Plugins/ProtocolException.cs
@@ -6,7 +6,7 @@ using System;
 namespace NuGet.Protocol.Plugins
 {
     /// <summary>
-    /// A plugin  protocol exception.
+    /// A plugin protocol exception.
     /// </summary>
     [Serializable]
     public sealed class ProtocolException : Exception
