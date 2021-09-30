171085dc2d52c2dc87768f4c6f66050553d38029
Kartheek Penagamuri <kapenaga@microsoft.com>
2021-09-30 11:03:00 -0700

    remove unnecessary suppressions.cs files

--- test/NuGet.Core.Tests/NuGet.Build.Tasks.Pack.Test/GlobalSuppressions2.cs
+++
@@ -1,9 +0,0 @@
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.Build.Tasks.Pack.Test.PackTaskLogicTests.TestContext.#ctor(NuGet.Test.Utility.TestDirectory,System.String)")]
-[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.Build.Tasks.Pack.Test.PackTaskTests.GetRequest(NuGet.Build.Tasks.Pack.PackTask)~NuGet.Build.Tasks.Pack.IPackTaskRequest{NuGet.Commands.IMSBuildItem}")]
