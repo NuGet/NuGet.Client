a93cf5a5e95404da6dff454a2c4b4e674b699ad6
Erick Yondon <eryondon@microsoft.com>
2021-10-01 11:23:50 -0700

    Use "-Channel x.0 -Quality Daily" option with dotnet-install for configure.ps1 (#4289)
    * Add -Quality Daily into dotnet-install.ps1 parameters

--- build/common.ps1
+++ build/common.ps1
@@ -240,6 +240,20 @@ Function Install-DotnetCLI {
 
         #If "-force" is specified, or folder with specific version doesn't exist, the download command will run"
         if ($Force -or -not (Test-Path $probeDotnetPath)) {
+            $channelMainVersion = ""
+            foreach($channelPart in $cli.Channel.Split('/'))
+            {
+                if ($channelPart -match "\d+.*")
+                {
+                    $channelMainVersion = $channelPart.Split('.')[0]
+                    Break
+                }
+            }
+
+            if ([string]::IsNullOrEmpty($channelMainVersion)) {
+                Error-Log "Unable to detect channel version for dotnetinstall.ps1. The CLI install cannot be initiated." -Fatal
+            }
+
             Trace-Log "$DotNetInstall -Channel $($channelMainVersion) -InstallDir $($cli.Root) -Version $($cli.Version) -Architecture $arch -NoPath"
             # dotnet-install might make http requests that fail, but it handles those errors internally
             # However, Invoke-BuildStep checks if any error happened, ever. Hence we need to run dotnet-install
