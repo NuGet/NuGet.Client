5f4b4644ae31033f350418671cb4a480748d6e2e
Fernando Aguilar <feaguila@microsoft.com>
2021-10-05 19:27:01 -0700

    Reduced test

--- test/NuGet.Core.Tests/NuGet.Common.Test/DateTimeUtilityTests.cs
+++ test/NuGet.Core.Tests/NuGet.Common.Test/DateTimeUtilityTests.cs
@@ -13,26 +13,26 @@ namespace NuGet.Common.Test
         {
             return new[]
             {
-                new object[] { "1.23 sec", TimeSpan.FromSeconds(1.23d) },
-                new object[] { "0 ms", TimeSpan.FromMilliseconds(0.01d) }, // round down
-                new object[] { "1 ms", TimeSpan.FromMilliseconds(0.56d) }, // round up
-                new object[] { "1 ms", TimeSpan.FromMilliseconds(1.21d) }, // round down
-                new object[] { "92183.91 hr", TimeSpan.FromHours(92183.91d) },
-                new object[] { "1 hr", TimeSpan.FromSeconds(3600.0d) },
-                new object[] { "3.6 min", TimeSpan.FromMinutes(3.6d) },
-                new object[] { "72 hr", TimeSpan.FromDays(3.0d) },
+                new object[] { "1.23", TimeSpan.FromSeconds(1.23d) },
+                new object[] { "0", TimeSpan.FromMilliseconds(0.01d) }, // round down
+                new object[] { "1", TimeSpan.FromMilliseconds(0.56d) }, // round up
+                new object[] { "1", TimeSpan.FromMilliseconds(1.21d) }, // round down
+                new object[] { "92183.91", TimeSpan.FromHours(92183.91d) },
+                new object[] { "1", TimeSpan.FromSeconds(3600.0d) },
+                new object[] { "3.6", TimeSpan.FromMinutes(3.6d) },
+                new object[] { "72", TimeSpan.FromDays(3.0d) },
             };
         }
 
         [Theory]
         [MemberData(nameof(GetData))]
-        public void ToReadableTimeFormat_CultureInvariant_Succeeds(string expected, TimeSpan time)
+        public void ToReadableTimeFormat_Localized_AssumesEnglishLocale_ContainsTimeNumber_Succeeds(string timeNumber, TimeSpan time)
         {
             // Act
             string actual = DatetimeUtility.ToReadableTimeFormat(time);
 
             // Assert
-            Assert.Equal(expected, actual);
+            Assert.Contains(timeNumber, actual);
         }
     }
 }
