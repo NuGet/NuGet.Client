37cd184ad67f2ca0cc02b7e9da0617a2d4fd42f7
Kartheek Penagamuri <kapenaga@microsoft.com>
2021-10-09 19:16:51 -0700

    remove and sort usings

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/Utility/CommonUtility.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/Utility/CommonUtility.cs
@@ -8,7 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using EnvDTE;
