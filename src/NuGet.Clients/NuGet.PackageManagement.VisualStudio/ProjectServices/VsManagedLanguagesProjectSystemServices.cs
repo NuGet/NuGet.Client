bce6e4c62d71338a057d6c03f5bd37603c0e1e8e
Nikolche Kolev <nikolev@microsoft.com>
2021-10-29 13:35:27 -0700

    Remove unneeded changes

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/ProjectServices/VsManagedLanguagesProjectSystemServices.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/ProjectServices/VsManagedLanguagesProjectSystemServices.cs
@@ -147,7 +147,7 @@ namespace NuGet.PackageManagement.VisualStudio
             }
 
             var references = new List<ProjectRestoreReference>();
-            foreach (Reference6 r in AsVSProject4.References.Cast<Reference6>()) // this is not considering things.
+            foreach (Reference6 r in AsVSProject4.References.Cast<Reference6>())
             {
                 if (r.SourceProject != null && await EnvDTEProjectUtility.IsSupportedAsync(r.SourceProject))
                 {
