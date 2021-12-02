7479ce098a7f2d848267539f3f75587b8a1c8b7d
Fernando Aguilar <feaguila@microsoft.com>
2021-12-02 09:47:52 -0600

    Added test to N.PM.Test

---
+++ test/NuGet.Core.Tests/NuGet.PackageManagement.Test/Utility/MSBuildNuGetProjectSystemUtilityTests.cs
@@ -0,0 +1,28 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NuGet.ProjectManagement;
+using Xunit;
+
+namespace NuGet.PackageManagement.Test
+{
+    public class MSBuildNuGetProjectSystemUtilityTests
+    {
+        [Fact]
+        public async Task TryAddFileAsync_WithCancellationToken_SucceedsAsync()
+        {
+            await Assert.ThrowsAsync<OperationCanceledException>(async () => {
+                await MSBuildNuGetProjectSystemUtility.TryAddFileAsync(
+                    It.IsAny<IMSBuildProjectSystem>(),
+                    path: "",
+                    streamTaskFactory: async () => await Task.FromResult(It.IsAny<Stream>()),
+                    new CancellationToken(canceled: true));
+            });
+        }
+    }
+}
