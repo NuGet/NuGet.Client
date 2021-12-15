6378ab332f97324dac28be0b566e89d9cd3edd34
Marcin Krystianc <marcin.krystianc@gmail.com>
2021-12-15 11:31:37 +0100

    make mmap optionable

--- src/NuGet.Core/NuGet.Packaging/PackageExtraction/StreamExtensions.cs
+++ src/NuGet.Core/NuGet.Packaging/PackageExtraction/StreamExtensions.cs
@@ -1,13 +1,25 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Buffers;
+using System.Diagnostics;
 using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace NuGet.Packaging
 {
     public static class StreamExtensions
     {
+        /**
+        Only files smaller than this value will be mmap'ed
+        */
+        private const long MAX_MMAP_SIZE = 10 * 1024 * 1024; // 10 MB
+
+        private static bool IsMMapEnabled { get; } = string.Equals(Environment.GetEnvironmentVariable("NUGET_MMAP_PACKAGE_EXTRACTION"), "1", StringComparison.OrdinalIgnoreCase);
+
         public static string CopyToFile(this Stream inputStream, string fileFullPath)
         {
             if (Path.GetFileName(fileFullPath).Length == 0)
@@ -28,11 +40,38 @@ namespace NuGet.Packaging
                 return fileFullPath;
             }
 
+            // For files of a certain size, we can do some Cleverness and mmap
+            // them instead of writing directly to disk. This can improve
+            // performance by a lot on some operating systems and hardware,
+            // particularly Windows
+            long? size = null;
+            try
+            {
+                size = inputStream.Length;
+            }
+            catch (NotSupportedException)
+            {
+                // If we can't get Length, just move on.
+            }
             using (var outputStream = NuGetExtractionFileIO.CreateFile(fileFullPath))
             {
-                CopyTo(inputStream, outputStream);
+                if (IsMMapEnabled && size > 0 && size <= MAX_MMAP_SIZE)
+                {
+                    // NOTE: Linux can't create a mmf from outputStream, so we
+                    // need to close the file (which now has the desired
+                    // perms), and then re-open it as a memory-mapped file.
+                    outputStream.Dispose();
+                    using (MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(fileFullPath, FileMode.Open, mapName: null, (long)size))
+                    using (MemoryMappedViewStream mmstream = mmf.CreateViewStream())
+                    {
+                        CopyTo(inputStream, mmstream);
+                    }
+                }
+                else
+                {
+                    CopyTo(inputStream, outputStream);
+                }
             }
-
             return fileFullPath;
         }
 
