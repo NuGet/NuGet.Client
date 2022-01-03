b1aa3bc3e9e6806cf134a021e4ba938e057b5139
Erick Yondon <eryondon@microsoft.com>
2022-01-03 09:17:01 -0800

    Address latest comment

--- src/NuGet.Core/NuGet.Build.Tasks.Pack/PackTask.cs
+++ src/NuGet.Core/NuGet.Build.Tasks.Pack/PackTask.cs
@@ -8,6 +8,8 @@ using NuGet.Commands;
 using NuGet.Common;
 using NuGet.Packaging;
 using ILogger = NuGet.Common.ILogger;
+using PackageSpecificWarningProperties = NuGet.Commands.PackCommand.PackageSpecificWarningProperties;
+using WarningPropertiesCollection = NuGet.Commands.PackCommand.WarningPropertiesCollection;
 
 namespace NuGet.Build.Tasks.Pack
 {
@@ -115,6 +117,7 @@ namespace NuGet.Build.Tasks.Pack
                 var request = GetRequest();
                 var logic = PackTaskLogic;
                 PackageBuilder packageBuilder = null;
+                var packArgs = logic.GetPackArgs(request);
 
                 // If packing using a Nuspec file, we don't need to build a PackageBuilder here
                 // as the package builder is built by reading the manifest file later in the code path.
@@ -122,9 +125,16 @@ namespace NuGet.Build.Tasks.Pack
                 if (string.IsNullOrEmpty(request.NuspecFile))
                 {
                     packageBuilder = logic.GetPackageBuilder(request);
+
+                    if (packageBuilder?.PackageSpecificNoWarnProperties.Keys.Count > 0)
+                    {
+                        var warningPropertiesCollection = new WarningPropertiesCollection(PackageSpecificWarningProperties
+                            .CreatePackageSpecificWarningProperties(packageBuilder.PackageSpecificNoWarnProperties));
+                        // Override logger with warning properties
+                        packArgs = logic.GetPackArgs(request, warningPropertiesCollection);
+                    }
                 }
 
-                PackArgs packArgs = logic.GetPackArgs(request);
                 var packRunner = logic.GetPackCommandRunner(request, packArgs, packageBuilder);
 
                 return logic.BuildPackage(packRunner);
--- src/NuGet.Core/NuGet.Build.Tasks.Pack/PackTaskLogic.cs
+++ src/NuGet.Core/NuGet.Build.Tasks.Pack/PackTaskLogic.cs
@@ -15,7 +15,6 @@ using NuGet.Packaging.Core;
 using NuGet.Packaging.Licenses;
 using NuGet.ProjectModel;
 using NuGet.Versioning;
-using PackageSpecificWarningProperties = NuGet.Commands.PackCommand.PackageSpecificWarningProperties;
 using WarningPropertiesCollection = NuGet.Commands.PackCommand.WarningPropertiesCollection;
 
 namespace NuGet.Build.Tasks.Pack
@@ -23,9 +22,13 @@ namespace NuGet.Build.Tasks.Pack
     public class PackTaskLogic : IPackTaskLogic
     {
         private const string IdentityProperty = "Identity";
-        private PackageBuilder _packageBuilder;
 
         public PackArgs GetPackArgs(IPackTaskRequest<IMSBuildItem> request)
+        {
+            return GetPackArgs(request, warningPropertiesCollection: null);
+        }
+
+        public PackArgs GetPackArgs(IPackTaskRequest<IMSBuildItem> request, WarningPropertiesCollection warningPropertiesCollection)
         {
             var packArgs = new PackArgs
             {
@@ -42,14 +45,6 @@ namespace NuGet.Build.Tasks.Pack
                 WarningProperties = WarningProperties.GetWarningProperties(request.TreatWarningsAsErrors, request.WarningsAsErrors, request.NoWarn),
                 PackTargetArgs = new MSBuildPackTargetArgs()
             };
-
-            WarningPropertiesCollection warningPropertiesCollection = null;
-
-            if (_packageBuilder?.PackageSpecificNoWarnProperties.Keys.Count > 0)
-            {
-                warningPropertiesCollection = new WarningPropertiesCollection(PackageSpecificWarningProperties
-                    .CreatePackageSpecificWarningProperties(_packageBuilder?.PackageSpecificNoWarnProperties));
-            }
 
             packArgs.Logger = new PackCollectorLogger(request.Logger, packArgs.WarningProperties, warningPropertiesCollection);
 
@@ -268,7 +263,6 @@ namespace NuGet.Build.Tasks.Pack
             PopulateFrameworkAssemblyReferences(builder, request);
             PopulateFrameworkReferences(builder, assetsFile);
 
-            _packageBuilder = builder;
             return builder;
         }
 
