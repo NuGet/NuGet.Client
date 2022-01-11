3d640b6f2cc20986ff4b508a80556ec87b634921
Nikolche Kolev <nikolev@microsoft.com>
2022-01-10 18:57:58 -0800

    Add a null check

--- src/NuGet.Clients/NuGet.SolutionRestoreManager/VsRestoreProgressEvents.cs
+++ src/NuGet.Clients/NuGet.SolutionRestoreManager/VsRestoreProgressEvents.cs
@@ -30,13 +30,16 @@ namespace NuGet.SolutionRestoreManager
                 throw new ArgumentNullException(nameof(updatedFiles));
             }
 
-            foreach (var handler in ProjectUpdateFinished?.GetInvocationList())
+            if (ProjectUpdateFinished != null)
             {
-                try
+                foreach (var handler in ProjectUpdateFinished.GetInvocationList())
                 {
-                    handler.DynamicInvoke(projectName, updatedFiles);
+                    try
+                    {
+                        handler.DynamicInvoke(projectName, updatedFiles);
+                    }
+                    catch { }
                 }
-                catch { }
             }
         }
 
@@ -52,13 +55,16 @@ namespace NuGet.SolutionRestoreManager
                 throw new ArgumentNullException(nameof(updatedFiles));
             }
 
-            foreach (var handler in ProjectUpdateStarted?.GetInvocationList())
+            if (ProjectUpdateStarted != null)
             {
-                try
+                foreach (var handler in ProjectUpdateStarted.GetInvocationList())
                 {
-                    handler.DynamicInvoke(projectName, updatedFiles);
+                    try
+                    {
+                        handler.DynamicInvoke(projectName, updatedFiles);
+                    }
+                    catch { }
                 }
-                catch { }
             }
         }
 
@@ -69,13 +75,16 @@ namespace NuGet.SolutionRestoreManager
                 throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Resources.Argument_Cannot_Be_Null_Or_Empty, nameof(projects)));
             }
 
-            foreach (var handler in SolutionRestoreStarted?.GetInvocationList())
+            if (SolutionRestoreStarted != null)
             {
-                try
+                foreach (var handler in SolutionRestoreStarted.GetInvocationList())
                 {
-                    handler.DynamicInvoke(projects);
+                    try
+                    {
+                        handler.DynamicInvoke(projects);
+                    }
+                    catch { }
                 }
-                catch { }
             }
         }
 
@@ -86,13 +95,16 @@ namespace NuGet.SolutionRestoreManager
                 throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, Resources.Argument_Cannot_Be_Null_Or_Empty, nameof(projects)));
             }
 
-            foreach (var handler in SolutionRestoreFinished?.GetInvocationList())
+            if (SolutionRestoreFinished != null)
             {
-                try
+                foreach (var handler in SolutionRestoreFinished.GetInvocationList())
                 {
-                    handler.DynamicInvoke(projects);
+                    try
+                    {
+                        handler.DynamicInvoke(projects);
+                    }
+                    catch { }
                 }
-                catch { }
             }
         }
     }
