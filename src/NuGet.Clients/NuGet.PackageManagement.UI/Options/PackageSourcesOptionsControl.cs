ad4017d3c1945c7a7c257a9427da061b0473dd8d
Andy Zivkovic <zivkan@users.noreply.github.com>
2022-01-26 14:32:34 -0800

    Reset checkboxes in VS options when adding/removing package source (#4416)

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Options/PackageSourcesOptionsControl.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Options/PackageSourcesOptionsControl.cs
@@ -180,10 +180,7 @@ namespace NuGet.Options
                 _packageSources.CurrentChanged += OnSelectedPackageSourceChanged;
                 PackageSourcesListBox.GotFocus += PackageSourcesListBox_GotFocus;
                 PackageSourcesListBox.DataSource = _packageSources;
-                for (int i = 0; i < packageSources.Count; i++)
-                {
-                    PackageSourcesListBox.SetItemChecked(i, packageSources[i].IsEnabled);
-                }
+                ResetItemsCheckedState(PackageSourcesListBox, _packageSources);
 
                 if (machineWidePackageSources.Count > 0)
                 {
@@ -191,10 +188,7 @@ namespace NuGet.Options
                     _machineWidepackageSources.CurrentChanged += OnSelectedMachineWidePackageSourceChanged;
                     MachineWidePackageSourcesListBox.GotFocus += MachineWidePackageSourcesListBox_GotFocus;
                     MachineWidePackageSourcesListBox.DataSource = _machineWidepackageSources;
-                    for (int i = 0; i < machineWidePackageSources.Count; i++)
-                    {
-                        MachineWidePackageSourcesListBox.SetItemChecked(i, machineWidePackageSources[i].IsEnabled);
-                    }
+                    ResetItemsCheckedState(MachineWidePackageSourcesListBox, _machineWidepackageSources);
                 }
                 else
                 {
@@ -222,6 +216,17 @@ namespace NuGet.Options
             {
                 MessageHelper.ShowErrorMessage(Resources.ShowError_SettingActivatedFailed, Resources.ErrorDialogBoxTitle);
                 ActivityLog.LogError(NuGetUI.LogEntrySource, ex.ToString());
+            }
+        }
+
+        private void ResetItemsCheckedState(PackageSourceCheckedListBox checkedListBox, BindingSource bindingSource)
+        {
+            var list = (IList<PackageSourceContextInfo>)bindingSource.List;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var isEnabled = list[i].IsEnabled;
+                checkedListBox.SetItemChecked(i, isEnabled);
             }
         }
 
@@ -343,6 +348,10 @@ namespace NuGet.Options
                 return;
             }
             _packageSources.Remove(PackageSourcesListBox.SelectedItem);
+
+            // changing _packageSources appears to clear all the checked items, so now we have to reset all the checkbox states
+            ResetItemsCheckedState(PackageSourcesListBox, _packageSources);
+
             UpdateUI();
         }
 
@@ -354,6 +363,9 @@ namespace NuGet.Options
             }
 
             _packageSources.Add(CreateNewPackageSource());
+
+            // changing _packageSources appears to clear all the checked items, so now we have to reset all the checkbox states
+            ResetItemsCheckedState(PackageSourcesListBox, _packageSources);
 
             // auto-select the newly-added item
             PackageSourcesListBox.SelectedIndex = PackageSourcesListBox.Items.Count - 1;
