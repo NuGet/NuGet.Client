673a144080ad349797919e8a76946652a0f547ea
Fernando Aguilar <feaguila@microsoft.com>
2022-02-01 06:53:04 -0600

    Fixed source code format

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Models/DetailControlModelTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Models/DetailControlModelTests.cs
@@ -1,17 +1,12 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.ServiceHub.Framework;
-using NuGet.VisualStudio.Internal.Contracts;
 using Moq;
-using Xunit;
 using NuGet.Versioning;
+using NuGet.VisualStudio.Internal.Contracts;
+using Xunit;
 
 namespace NuGet.PackageManagement.UI.Test
 {
