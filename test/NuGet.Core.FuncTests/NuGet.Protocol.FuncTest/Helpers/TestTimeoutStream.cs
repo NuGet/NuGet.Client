7965f3cfe0533fcb2029210881bca4aa9ed9161b
Andy Zivkovic <zivkan@users.noreply.github.com>
2022-02-02 13:35:07 -0800

    CopyNupkgToStream to reset stream on retries (#4425)

---
+++ test/NuGet.Core.FuncTests/NuGet.Protocol.FuncTest/Helpers/TestTimeoutStream.cs
@@ -0,0 +1,65 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+
+namespace NuGet.Protocol.FuncTest.Helpers
+{
+    internal class TestTimeoutStream : Stream
+    {
+        private Stream _stream;
+
+        public TestTimeoutStream(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public override bool CanRead => _stream.CanRead;
+
+        public override bool CanSeek => _stream.CanSeek;
+
+        public override bool CanWrite => _stream.CanWrite;
+
+        public override long Length => _stream.Length;
+
+        public override long Position
+        {
+            get => _stream.Position;
+            set => _stream.Position = value;
+        }
+
+        public override void Flush()
+        {
+            _stream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            // Don't time out immediately, because we want to test what happens when some data is sent successfully,
+            // then the timeout happens, and NuGet.Protocol has to retry and handle the previously partial data.
+            var read = _stream.Read(buffer, offset, count);
+            if (read > 0)
+            {
+                return read;
+            }
+
+            throw new TimeoutException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return _stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            _stream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _stream.Write(buffer, offset, count);
+        }
+    }
+}
