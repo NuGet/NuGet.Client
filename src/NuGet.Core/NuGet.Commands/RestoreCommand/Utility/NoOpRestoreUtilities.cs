ccdf3e3142fd14b4c7a1ba29ad44b2e29ee31e17
Martin Finkel <me@martinfinkel.com>
2022-02-25 12:11:48 +0700

    remove unused using

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/Utility/NoOpRestoreUtilities.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/Utility/NoOpRestoreUtilities.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using NuGet.Common;
 using NuGet.Packaging;
 using NuGet.ProjectModel;
