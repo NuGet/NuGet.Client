e96651f2b3a764993667ac9e712359ca85b3a07f
Kartheek Penagamuri <kapenaga@microsoft.com>
2022-03-22 20:50:43 -0700

    fix tests

--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/MsBuildToolsetTest.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/MsBuildToolsetTest.cs
@@ -3,28 +3,25 @@
 
 using System;
 using NuGet.Common;
+using NuGet.Test.Utility;
 using Xunit;
 
 namespace NuGet.CommandLine.Test
 {
     public class MsBuildToolsetTest
     {
-        [Fact]
+        [PlatformFact(Platform.Windows)]
         public void WhenNullIsPassedForVersionParameterThenMsBuildVersionIsFetchedFromPath_Success()
         {
             //Arrange
             var msbuildPath = Util.GetMsbuildPathOnWindows();
-            if (RuntimeEnvironmentHelper.IsMono && RuntimeEnvironmentHelper.IsMacOSX)
-            {
-                msbuildPath = @"/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/msbuild/15.0/bin/";
-            }
 
             //Act
             var toolset = new MsBuildToolset(version: null, path: msbuildPath);
 
             //Assert
             Assert.Equal(msbuildPath, toolset.Path);
-            Assert.False(toolset.ParsedVersion.CompareTo(new Version(15, 5)) < 0);
+            Assert.True(toolset.ParsedVersion.CompareTo(new Version()) > 0);
         }
     }
 }
