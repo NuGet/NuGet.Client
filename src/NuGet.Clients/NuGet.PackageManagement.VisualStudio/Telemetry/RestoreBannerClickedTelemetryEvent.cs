924901bbb10764b31cc1536ccba56b4a34db4d7f
Martin Ruiz <martin.ruiz.mares@gmail.com>
2022-04-08 15:57:23 -0700

    renamed variable

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Telemetry/RestoreBannerClickedTelemetryEvent.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Telemetry/RestoreBannerClickedTelemetryEvent.cs
@@ -10,7 +10,7 @@ namespace NuGet.PackageManagement.Telemetry
     {
         internal const string RestoreBannerClickedEventName = "RestoreBannerClicked";
         internal const string RestoreButtonActionName = "RestoreButtonAction";
-        internal const string RestoreButtonOriginPropertyName = "IsSolutionView";
+        internal const string RestoreButtonOriginPropertyName = "ProjectLevel";
 
         public RestoreBannerClickedTelemetryEvent(RestoreButtonAction restoreButtonAction, RestoreButtonOrigin restoreButtonOrigin)
             : base(RestoreBannerClickedEventName)
