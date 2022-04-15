472ea6c47e05c23cc80d6e2c796e2b47b2252983
Fernando Aguilar <feaguila@microsoft.com>
2022-04-15 16:35:48 -0700

    Added tests to counterfactual

---
+++ test/NuGet.Clients.Tests/NuGet.VisualStudio.Common.Test/Telemetry/TransitiveDependenciesCounterfactualEvent.cs
@@ -0,0 +1,35 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Moq;
+using NuGet.Common;
+using Xunit;
+
+namespace NuGet.VisualStudio.Common.Test
+{
+    public class TransitiveDependenciesCounterfactualEventTests
+    {
+
+        [Fact]
+        public void EmitEvent_Succeeds()
+        {
+            // Arrange
+            var telemetrySession = new Mock<ITelemetrySession>();
+            TelemetryEvent lastTelemetryEvent = null;
+            _ = telemetrySession
+                .Setup(x => x.PostEvent(It.IsAny<TelemetryEvent>()))
+                .Callback<TelemetryEvent>(x => lastTelemetryEvent = x);
+
+            var service = new NuGetVSTelemetryService(telemetrySession.Object);
+
+            var evt = new TransitiveDependenciesCounterfactualEvent();
+
+            // Act
+            service.EmitTelemetryEvent(evt);
+
+            // Assert
+            Assert.NotNull(lastTelemetryEvent);
+            Assert.Equal(evt.Name, lastTelemetryEvent.Name);
+        }
+    }
+}
