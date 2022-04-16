264f87bafa108e946a31b9d5e16951220dae7e84
Erick Yondon <eryondon@microsoft.com>
2022-04-15 17:45:48 -0700

    Fix binding redirect (#4575)

--- test/EndToEnd/tests/InstallPackageTest.ps1
+++ test/EndToEnd/tests/InstallPackageTest.ps1
@@ -764,15 +764,14 @@ function Test-SimpleBindingRedirectsClassLibraryUpdatePackage {
 }
 
 function Test-SimpleBindingRedirectsClassLibraryReference {
-    [SkipTest('https://github.com/NuGet/Home/issues/10890')]
     param(
         $context
     )
     # Arrange
     $a = New-WebApplication
     $b = New-WebSite
-    $d = New-ClassLibrary
-    $e = New-ClassLibrary
+    $d = New-ClassLibraryNET46
+    $e = New-ClassLibraryNET46
 
     Add-ProjectReference $a $d
     Add-ProjectReference $b $e
