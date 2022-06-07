c7e91f6d352d90088d4037daa4b7a69692ce5159
Fernando Aguilar <feaguila@microsoft.com>
2022-06-06 21:14:15 -0700

    Test: Added Comparer tests

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.VisualStudio.Test/Utility/GetPackageReferenceUtilityTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.VisualStudio.Test/Utility/GetPackageReferenceUtilityTests.cs
@@ -170,6 +170,50 @@ namespace NuGet.PackageManagement.VisualStudio.Test
             Assert.Equal(0, result);
         }
 
+        [Fact]
+        public void PackageReferenceMergeComparer_AnyNullArgument_ReturnsExpectedResult()
+        {
+            // Arrange
+            PackageReference testPkgReference = CreatePackageReference("Abc", "1.0", "net6.0");
+
+            // Act and Assert 1: Second is null
+            int result1 = GetPackageReferenceUtility.PackageReferenceMergeComparer.Compare(testPkgReference, null);
+            Assert.True(result1 > 0);
+
+            // Act and Assert 2: First is null
+            int result2 = GetPackageReferenceUtility.PackageReferenceMergeComparer.Compare(null, testPkgReference);
+            Assert.True(result2 < 0);
+
+            // Act and Assert 3: Both are null
+            int result3 = GetPackageReferenceUtility.PackageReferenceMergeComparer.Compare(null, null);
+            Assert.True(result3 == 0);
+
+            var prWithNullIdentity = new PackageReference(null, null);
+
+            // Act and Assert 4: Second with null PackageIdentity
+            int result4 = GetPackageReferenceUtility.PackageReferenceMergeComparer.Compare(testPkgReference, prWithNullIdentity);
+            Assert.True(result4 > 0);
+
+            // Act and Assert 5: First with null PackageIdentity
+            int result5 = GetPackageReferenceUtility.PackageReferenceMergeComparer.Compare(prWithNullIdentity, testPkgReference);
+            Assert.True(result5 < 0);
+
+            // Act and Assert 6: First with null PackageIdentity and null
+            int result6 = GetPackageReferenceUtility.PackageReferenceMergeComparer.Compare(prWithNullIdentity, null);
+            Assert.Equal(0, result6);
+
+            PackageReference pkgRefWithNullVersion1 = CreatePackageReference("nullVersion", null, fw: null);
+            PackageReference pkgRefWithNullVersion2 = CreatePackageReference("otherVersion", null, fw: null);
+
+            // Act and Assert 7: First Null PackageIdentity and Second with null version, compare only with package IDs
+            int result7 = GetPackageReferenceUtility.PackageReferenceMergeComparer.Compare(prWithNullIdentity, pkgRefWithNullVersion1);
+            Assert.True(result7 < 0);
+
+            // Act and Assert 8: Only package ID's on both
+            int result8 = GetPackageReferenceUtility.PackageReferenceMergeComparer.Compare(pkgRefWithNullVersion1, pkgRefWithNullVersion2);
+            Assert.True(result8 < 0);
+        }
+
         private static PackageReference CreatePackageReference(string id, string version, NuGetFramework fw) => new PackageReference(CreatePackageIdentity(id, version), fw);
 
         private static PackageReference CreatePackageReference(string id, string version, string framework)
