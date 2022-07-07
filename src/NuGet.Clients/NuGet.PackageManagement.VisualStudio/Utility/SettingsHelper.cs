e358676c21e19bd28ab60b7092665e8a4dcec08e
Nikolche Kolev <nikolev@microsoft.com>
2022-07-07 13:27:44 -0700

    cleanup

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Utility/SettingsHelper.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Utility/SettingsHelper.cs
@@ -12,7 +12,7 @@ namespace NuGet.PackageManagement.VisualStudio
     /// <summary>
     /// This class is used by functional tests to change NuGet user settings.
     /// </summary>
-    public static class SettingsHelper // move this?
+    public static class SettingsHelper
     {
         /// <summary>
         /// Sets a NuGet user settings property.
