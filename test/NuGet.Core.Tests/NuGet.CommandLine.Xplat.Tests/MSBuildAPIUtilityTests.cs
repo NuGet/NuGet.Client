c7aef21df1fab875518949b4ae66bf26f092e719
Pragnya Pandrate <t-ppandrate@microsoft.com>
2022-07-08 11:52:14 -0700

    Revert "[skip ci] Attempt to modify unit test, still need to debug"
    This reverts commit 4e49d9360afb400af008066d5683c76ea266b30d.

--- test/NuGet.Core.Tests/NuGet.CommandLine.Xplat.Tests/MSBuildAPIUtilityTests.cs
+++ test/NuGet.Core.Tests/NuGet.CommandLine.Xplat.Tests/MSBuildAPIUtilityTests.cs
@@ -3,7 +3,6 @@
 
 using System.IO;
 using System.Linq;
-using Microsoft.Build.Construction;
 using Microsoft.Build.Definition;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Locator;
@@ -117,8 +116,8 @@ namespace NuGet.CommandLine.Xplat.Tests
             project.Save();
 
             // Assert
-            ProjectItem packageReference = project.Xml.Items.LastOrDefault(i => i.ItemType == "PackageReference" && i.EvaluatedInclude.Equals("X"));
-            Assert.True(packageReference != null, packageReference.ItemType);
+            Assert.Contains(@$"<PackageReference Include=""X"" />", File.ReadAllText(Path.Combine(testDirectory, "projectA.csproj")));
+            Assert.DoesNotContain(@$"<Version = ""1.0.0"" />", File.ReadAllText(Path.Combine(testDirectory, "projectA.csproj")));
         }
 
         [Fact]
