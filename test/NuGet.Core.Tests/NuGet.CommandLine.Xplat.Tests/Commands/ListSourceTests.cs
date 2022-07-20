9b7236790263764d637acd567cdde88e418babf0
Fernando Aguilar <feaguila@microsoft.com>
2022-07-19 23:11:27 -0500

    Refactored tests. Reverted project file

--- test/NuGet.Core.Tests/NuGet.CommandLine.Xplat.Tests/Commands/ListSourceTests.cs
+++ test/NuGet.Core.Tests/NuGet.CommandLine.Xplat.Tests/Commands/ListSourceTests.cs
@@ -12,20 +12,24 @@ namespace NuGet.CommandLine.Xplat.Tests
     public class ListSourceTests
     {
         [Fact]
-        public void ListSource_BothCommands_Runs()
+        public void ListSource_RunSameCommandInBothCommandLineInterfaces_CommandOutputEqual()
         {
+            // Arrange
             var cmd = new[] { "list", "source" };
    
             var currentCli = new CommandLineApplication();
             var testLoggerCurrent = new TestLogger();
             ListVerbParser.Register(currentCli, () => testLoggerCurrent);
-            currentCli.Execute(cmd);
 
             var newCli = new RootCommand();
             var testLoggerNew = new TestLogger();
             NuGet.CommandLine.XPlat.Commands.ListVerbParser.Register(newCli, () => testLoggerNew);
+
+            // Act
+            currentCli.Execute(cmd);
             newCli.Invoke(cmd);
 
+            // Assert
             Assert.False(testLoggerCurrent.Messages.IsEmpty);
             Assert.False(testLoggerNew.Messages.IsEmpty);
             Assert.Equal(testLoggerCurrent.Messages, testLoggerNew.Messages);
