dd003afaac8ee2c0d1fc54ce137655204e6221a4
Jeff Kluge <jeffkl@microsoft.com>
2022-08-11 09:26:29 -0700

    Optimize global property retrieval in RestoreTaskEx
    Fixes https://github.com/NuGet/Client.Engineering/issues/1814

--- test/NuGet.Core.Tests/NuGet.Build.Tasks.Test/RestoreTaskExTests.cs
+++ test/NuGet.Core.Tests/NuGet.Build.Tasks.Test/RestoreTaskExTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -70,6 +71,42 @@ namespace NuGet.Build.Tasks.Test
                     projectPath,
                         $"Property1=Value1;Property2=  Value2  ;ExcludeRestorePackageImports=true;OriginalMSBuildStartupDirectory={testDirectory}");
                 }
+            }
+        }
+
+        /// <summary>
+        /// Verifies that the <see cref="RestoreTaskEx.GetGlobalProperties" /> returns the global properties plus any extra ones set by NuGet.
+        /// </summary>
+        [Fact]
+        public void GetGlobalProperties_ExtraGlobalProperties_AreSetCorrectly()
+        {
+            const string MSBuildStartupDirectory = @"C:\something";
+
+            Dictionary<string, string> globalProperties = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                ["Property1"] = "Value1",
+                ["Property2"] = "Value2"
+            };
+
+            using (var task = new RestoreTaskEx()
+            {
+                BuildEngine = new TestBuildEngine(globalProperties),
+                MSBuildStartupDirectory = MSBuildStartupDirectory
+            })
+            {
+                var actual = task.GetGlobalProperties().ToDictionary(i => i.Key, i => i.Value, StringComparer.OrdinalIgnoreCase);
+
+                actual.TryGetValue("Property1", out string value1).Should().BeTrue();
+                value1.Should().Be("Value1");
+
+                actual.TryGetValue("Property2", out string value2).Should().BeTrue();
+                value2.Should().Be("Value2");
+
+                actual.TryGetValue("ExcludeRestorePackageImports", out string excludeRestorePackageImports).Should().BeTrue();
+                excludeRestorePackageImports.Should().Be(bool.TrueString);
+
+                actual.TryGetValue("OriginalMSBuildStartupDirectory", out string originalMSBuildStartupDirectory).Should().BeTrue();
+                originalMSBuildStartupDirectory.Should().Be(MSBuildStartupDirectory);
             }
         }
 
