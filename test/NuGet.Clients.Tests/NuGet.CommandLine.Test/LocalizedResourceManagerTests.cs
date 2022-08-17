a97d8ed26e6b40a71f6c3ff063bead113da6cd49
Fernando Aguilar <feaguila@microsoft.com>
2022-08-17 17:04:41 -0500

    Tests: More tests
    Tests: Added tests
    
    Tests: modified tests
    
    Fixed tests
    
    Fixed more tests

---
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/LocalizedResourceManagerTests.cs
@@ -0,0 +1,73 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using System.Resources;
+using Moq;
+using Xunit;
+
+namespace NuGet.CommandLine.Test
+{
+    public class LocalizedResourceManagerTests
+    {
+        [Theory]
+        [InlineData("A_String_With_No_Name")]
+        [InlineData("An_unknown_String")]
+        public void GetString_NonExistentResource_ReturnsNull(string stringResourceName)
+        {
+            // Arrange & Act
+            string resource = LocalizedResourceManager.GetString(stringResourceName);
+
+            // Assert
+            Assert.Null(resource);
+        }
+
+        [Theory]
+        [InlineData("SpecCommandCreatedNuSpec")]
+        public void GetString_ExistingResourceInNuGetResources_ReturnsStringResource(string stringResourceName)
+        {
+            // Arrange & Act
+            string resource = LocalizedResourceManager.GetString(stringResourceName);
+
+            // Assert
+            Assert.NotEmpty(resource);
+        }
+
+        [Theory]
+        [InlineData("SpecCommandCreatedNuSpec", typeof(NuGetResources) )]
+        [InlineData("UpdateCommandPrerelease", typeof(NuGetCommand))]
+        public void GetString_ExistingResourcesInOtherResources_ReturnsStringResource(string resourceName, Type resourceType)
+        {
+            // Arrange
+            PropertyInfo property = resourceType.GetProperty("ResourceManager", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+            ResourceManager resourceManager = (ResourceManager)property.GetGetMethod(nonPublic: true).Invoke(obj: null, parameters: null);
+
+            // Act
+            string resource = LocalizedResourceManager.GetString(resourceName, resourceManager);
+
+            // Assert
+            Assert.NotEmpty(resource);
+        }
+
+        [Fact]
+        public void GetString_ExistingResourceInNuGetResources_ReturnsSameValueFromResourceClass()
+        {
+            // Arrange & Act
+            string resource = LocalizedResourceManager.GetString(nameof(NuGetResources.ListCommandNoPackages));
+
+            // Assert
+            Assert.Equal(NuGetResources.ListCommandNoPackages, resource);
+        }
+
+        [Fact]
+        public void GetString_NullArgument_Throws()
+        {
+            // Arrange, Act & Assert
+            Assert.Throws<ArgumentException>(() => LocalizedResourceManager.GetString(resourceName: null));
+            Assert.Throws<ArgumentException>(() => LocalizedResourceManager.GetString(resourceName: null, resourceManager: null));
+            Assert.Throws<ArgumentException>(() => LocalizedResourceManager.GetString(resourceName: "", resourceManager: It.IsAny<ResourceManager>()));
+            Assert.Throws<ArgumentNullException>(() => LocalizedResourceManager.GetString(resourceName: "e", resourceManager: null));
+        }
+    }
+}
