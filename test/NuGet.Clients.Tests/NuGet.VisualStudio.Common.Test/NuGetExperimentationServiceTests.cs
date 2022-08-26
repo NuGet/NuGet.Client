19e99bea750273f1c961858283fa728953dfcf73
donnie-msft <49205731+donnie-msft@users.noreply.github.com>
2022-08-26 12:06:03 -0700

    NuGetExperimentationServiceTests use utility

--- test/NuGet.Clients.Tests/NuGet.VisualStudio.Common.Test/NuGetExperimentationServiceTests.cs
+++ test/NuGet.Clients.Tests/NuGet.VisualStudio.Common.Test/NuGetExperimentationServiceTests.cs
@@ -113,83 +113,83 @@ namespace NuGet.VisualStudio.Common.Test
             OutputMessages.Should().NotContainMatch($"*{constant.FlightFlag}*{constant.FlightEnvironmentVariable}*");
         }
 
-        //[Fact]
-        //public void IsEnabled_WithEnvVarEnabled_WithExperimentalServiceDisabled_ReturnsTrue()
-        //{
-        //    var constant = ExperimentationConstants.PackageManagerBackgroundColor;
-        //    var flightsEnabled = new Dictionary<string, bool>()
-        //    {
-        //        { constant.FlightFlag, false },
-        //    };
+        [Fact]
+        public void IsEnabled_WithEnvVarEnabled_WithExperimentalServiceDisabled_ReturnsTrue()
+        {
+            var constant = ExperimentationConstants.PackageManagerBackgroundColor;
+            var flightsEnabled = new Dictionary<string, bool>()
+            {
+                { constant.FlightFlag, false },
+            };
 
-        //    var envVars = new Dictionary<string, string>()
-        //    {
-        //        { constant.FlightEnvironmentVariable, "1" },
-        //    };
-        //    var envVarWrapper = new TestEnvironmentVariableReader(envVars);
+            var envVars = new Dictionary<string, string>()
+            {
+                { constant.FlightEnvironmentVariable, "1" },
+            };
+            var envVarWrapper = new TestEnvironmentVariableReader(envVars);
 
-        //    var service = new NuGetExperimentationService(envVarWrapper, new TestVisualStudioExperimentalService(flightsEnabled), _outputConsoleProvider);
+            var service = new NuGetExperimentationService(envVarWrapper, NuGetExperimentationServiceUtility.GetMock(flightsEnabled), _outputConsoleProvider);
 
-        //    service.IsExperimentEnabled(ExperimentationConstants.PackageManagerBackgroundColor).Should().BeTrue();
-        //    OutputMessages.Should().ContainMatch($"*{constant.FlightFlag}*{constant.FlightEnvironmentVariable}*1*");
-        //}
+            service.IsExperimentEnabled(ExperimentationConstants.PackageManagerBackgroundColor).Should().BeTrue();
+            OutputMessages.Should().ContainMatch($"*{constant.FlightFlag}*{constant.FlightEnvironmentVariable}*1*");
+        }
 
-        //[Fact]
-        //public void IsEnabled_WithEnvVarDisabled_WithExperimentalServiceEnabled_ReturnsFalse()
-        //{
-        //    var constant = ExperimentationConstants.PackageManagerBackgroundColor;
-        //    var flightsEnabled = new Dictionary<string, bool>()
-        //    {
-        //        { constant.FlightFlag, true },
-        //    };
+        [Fact]
+        public void IsEnabled_WithEnvVarDisabled_WithExperimentalServiceEnabled_ReturnsFalse()
+        {
+            var constant = ExperimentationConstants.PackageManagerBackgroundColor;
+            var flightsEnabled = new Dictionary<string, bool>()
+            {
+                { constant.FlightFlag, true },
+            };
 
-        //    var envVars = new Dictionary<string, string>()
-        //    {
-        //        { constant.FlightEnvironmentVariable, "0" },
-        //    };
-        //    var envVarWrapper = new TestEnvironmentVariableReader(envVars);
+            var envVars = new Dictionary<string, string>()
+            {
+                { constant.FlightEnvironmentVariable, "0" },
+            };
+            var envVarWrapper = new TestEnvironmentVariableReader(envVars);
 
-        //    var service = new NuGetExperimentationService(envVarWrapper, new TestVisualStudioExperimentalService(flightsEnabled), _outputConsoleProvider);
+            var service = new NuGetExperimentationService(envVarWrapper, NuGetExperimentationServiceUtility.GetMock(flightsEnabled), _outputConsoleProvider);
 
-        //    service.IsExperimentEnabled(ExperimentationConstants.PackageManagerBackgroundColor).Should().BeFalse();
-        //    OutputMessages.Should().ContainMatch($"*{constant.FlightFlag}*{constant.FlightEnvironmentVariable}*0*");
-        //}
+            service.IsExperimentEnabled(ExperimentationConstants.PackageManagerBackgroundColor).Should().BeFalse();
+            OutputMessages.Should().ContainMatch($"*{constant.FlightFlag}*{constant.FlightEnvironmentVariable}*0*");
+        }
 
-        //[Fact]
-        //public void IsEnabled_WithNullEnvironmentVariableForConstant_HandlesGracefully()
-        //{
-        //    var service = new NuGetExperimentationService(new EnvironmentVariableWrapper(), new TestVisualStudioExperimentalService(), _outputConsoleProvider);
-        //    var constant = new ExperimentationConstants("flag", null);
-        //    service.IsExperimentEnabled(constant).Should().BeFalse();
-        //    OutputMessages.Should().NotContainMatch($"*{constant.FlightFlag}*{constant.FlightEnvironmentVariable}*");
-        //}
+        [Fact]
+        public void IsEnabled_WithNullEnvironmentVariableForConstant_HandlesGracefully()
+        {
+            var service = new NuGetExperimentationService(Mock.Of<IEnvironmentVariableReader>(), NuGetExperimentationServiceUtility.GetMock(), _outputConsoleProvider);
+            var constant = new ExperimentationConstants("flag", null);
+            service.IsExperimentEnabled(constant).Should().BeFalse();
+            OutputMessages.Should().NotContainMatch($"*{constant.FlightFlag}*{constant.FlightEnvironmentVariable}*");
+        }
 
-        //[Fact]
-        //public void IsEnabled_MultipleExperimentsOverriddenWithDifferentEnvVars_DoNotConflict()
-        //{
-        //    var forcedOffExperiment = new ExperimentationConstants("TestExp1", "TEST_EXP_1");
-        //    var forcedOnExperiment = new ExperimentationConstants("TestExp2", "TEST_EXP_2");
-        //    var noOverrideExperiment = new ExperimentationConstants("TestExp3", "TEST_EXP_3");
-        //    var flightsEnabled = new Dictionary<string, bool>()
-        //    {
-        //        { forcedOffExperiment.FlightFlag, true },
-        //        { forcedOnExperiment.FlightFlag, true },
-        //        { noOverrideExperiment.FlightFlag, true },
-        //    };
-        //    var envVars = new Dictionary<string, string>()
-        //    {
-        //        { forcedOnExperiment.FlightEnvironmentVariable, "1" },
-        //        { forcedOffExperiment.FlightEnvironmentVariable, "0" },
-        //    };
-        //    var envVarWrapper = new TestEnvironmentVariableReader(envVars);
-        //    var service = new NuGetExperimentationService(envVarWrapper, new TestVisualStudioExperimentalService(flightsEnabled), _outputConsoleProvider);
+        [Fact]
+        public void IsEnabled_MultipleExperimentsOverriddenWithDifferentEnvVars_DoNotConflict()
+        {
+            var forcedOffExperiment = new ExperimentationConstants("TestExp1", "TEST_EXP_1");
+            var forcedOnExperiment = new ExperimentationConstants("TestExp2", "TEST_EXP_2");
+            var noOverrideExperiment = new ExperimentationConstants("TestExp3", "TEST_EXP_3");
+            var flightsEnabled = new Dictionary<string, bool>()
+            {
+                { forcedOffExperiment.FlightFlag, true },
+                { forcedOnExperiment.FlightFlag, true },
+                { noOverrideExperiment.FlightFlag, true },
+            };
+            var envVars = new Dictionary<string, string>()
+            {
+                { forcedOnExperiment.FlightEnvironmentVariable, "1" },
+                { forcedOffExperiment.FlightEnvironmentVariable, "0" },
+            };
+            var envVarWrapper = new TestEnvironmentVariableReader(envVars);
+            var service = new NuGetExperimentationService(envVarWrapper, NuGetExperimentationServiceUtility.GetMock(flightsEnabled), _outputConsoleProvider);
 
-        //    service.IsExperimentEnabled(forcedOffExperiment).Should().BeFalse();
-        //    service.IsExperimentEnabled(forcedOnExperiment).Should().BeTrue();
-        //    service.IsExperimentEnabled(noOverrideExperiment).Should().BeTrue();
-        //    OutputMessages.Should().ContainMatch($"*{forcedOffExperiment.FlightFlag}*{forcedOffExperiment.FlightEnvironmentVariable}*0*");
-        //    OutputMessages.Should().ContainMatch($"*{forcedOnExperiment.FlightFlag}*{forcedOnExperiment.FlightEnvironmentVariable}*1*");
-        //    OutputMessages.Should().NotContainMatch($"*{noOverrideExperiment.FlightFlag}*{noOverrideExperiment.FlightEnvironmentVariable}*");
-        //}
+            service.IsExperimentEnabled(forcedOffExperiment).Should().BeFalse();
+            service.IsExperimentEnabled(forcedOnExperiment).Should().BeTrue();
+            service.IsExperimentEnabled(noOverrideExperiment).Should().BeTrue();
+            OutputMessages.Should().ContainMatch($"*{forcedOffExperiment.FlightFlag}*{forcedOffExperiment.FlightEnvironmentVariable}*0*");
+            OutputMessages.Should().ContainMatch($"*{forcedOnExperiment.FlightFlag}*{forcedOnExperiment.FlightEnvironmentVariable}*1*");
+            OutputMessages.Should().NotContainMatch($"*{noOverrideExperiment.FlightFlag}*{noOverrideExperiment.FlightEnvironmentVariable}*");
+        }
     }
 }
