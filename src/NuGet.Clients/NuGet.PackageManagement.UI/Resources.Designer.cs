99097f8eadde4c65c92d27379f00399a9ac31bca
Fernando Aguilar <feaguila@microsoft.com>
2022-09-13 16:26:10 -0700

    Removed unused strings in PM UI

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Resources.Designer.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Resources.Designer.cs
@@ -1015,15 +1015,6 @@ namespace NuGet.PackageManagement.UI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Filter:.
-        /// </summary>
-        public static string Label_Filter {
-            get {
-                return ResourceManager.GetString("Label_Filter", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Installed.
         /// </summary>
         public static string Label_Installed {
@@ -1200,15 +1191,6 @@ namespace NuGet.PackageManagement.UI {
         public static string Label_Repository {
             get {
                 return ResourceManager.GetString("Label_Repository", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Solution &apos;{0}&apos;.
-        /// </summary>
-        public static string Label_Solution {
-            get {
-                return ResourceManager.GetString("Label_Solution", resourceCulture);
             }
         }
         
