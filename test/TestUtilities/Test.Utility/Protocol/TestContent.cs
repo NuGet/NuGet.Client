4752b7f0ac0e707a8a027ee7265738c0916d5ad5
Fernando Aguilar <feaguila@microsoft.com>
2022-09-26 15:04:18 -0700

    Added roslyn issue

--- test/TestUtilities/Test.Utility/Protocol/TestContent.cs
+++ test/TestUtilities/Test.Utility/Protocol/TestContent.cs
@@ -15,7 +15,8 @@ namespace Test.Utility
     {
 #pragma warning disable CA2213
         // Need to disable this rule because the analyzer thinks We are not disposing _stream
-        // when in reality, it is
+        // when in reality, it is.
+        // See https://github.com/dotnet/roslyn-analyzers/issues/6172
         private readonly MemoryStream _stream;
 #pragma warning restore CA2213
         private bool _isDisposed = false; // internal for testing purposes
