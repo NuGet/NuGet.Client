22faa5ea20b19c0e2e12c8df4abae3ed4d5244b7
Fernando Aguilar <feaguila@microsoft.com>
2022-09-30 02:04:27 +0000

    Removed TODOs

--- test/TestUtilities/Test.Utility/SlowStream.cs
+++ test/TestUtilities/Test.Utility/SlowStream.cs
@@ -10,10 +10,7 @@ namespace Test.Utility
 {
     public class SlowStream : Stream
     {
-#pragma warning disable CA2213
-        // TODO: https://github.com/NuGet/Home/issues/12116
         private readonly Stream _innerStream;
-#pragma warning restore CA2213
         private readonly CancellationToken _cancellationToken;
         internal bool _isDisposed = false; // internal for testing purposes
 
