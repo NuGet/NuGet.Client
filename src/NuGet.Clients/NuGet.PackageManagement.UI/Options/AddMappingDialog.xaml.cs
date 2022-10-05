e562222663f97ca00fc90625df50b6398bb2e8e3
donnie-msft <49205731+donnie-msft@users.noreply.github.com>
2022-10-05 14:14:27 -0700

    Add Mapping list Toggles appropriately with screen readers

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Options/AddMappingDialog.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Options/AddMappingDialog.xaml.cs
@@ -3,6 +3,10 @@
 
 using System.Collections.Generic;
 using System.Threading;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using Microsoft.ServiceHub.Framework;
 using Microsoft.VisualStudio.PlatformUI;
@@ -58,7 +62,7 @@ namespace NuGet.Options
             SourcesCollection.Clear();
             foreach (PackageSourceContextInfo source in _originalPackageSources)
             {
-                var viewModel = new PackageSourceViewModel(source, false);
+                var viewModel = new PackageSourceViewModel(source);
                 SourcesCollection.Add(viewModel);
             }
         }
@@ -79,7 +83,7 @@ namespace NuGet.Options
             List<PackageSourceContextInfo> packageSources = new List<PackageSourceContextInfo>();
             foreach (PackageSourceViewModel source in _sourcesListBox.Items)
             {
-                if (source.IsChecked)
+                if (source.IsSelected)
                 {
                     packageSources.Add(source.SourceInfo);
                 }
@@ -102,7 +106,7 @@ namespace NuGet.Options
 
             foreach (PackageSourceViewModel source in _sourcesListBox.Items)
             {
-                if (source.IsChecked)
+                if (source.IsSelected)
                 {
                     return true;
                 }
@@ -117,14 +121,44 @@ namespace NuGet.Options
             DragMove();
         }
 
-        private void CheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var itemCheckBox = sender as CheckBox;
+            var itemContainer = itemCheckBox?.FindAncestor<ListViewItem>();
+            if (itemContainer is null)
+            {
+                return;
+            }
+
+            var newValue = (e.RoutedEvent == CheckBox.CheckedEvent);
+            var oldValue = !newValue; // Assume the state has actually toggled.
+            AutomationPeer peer = UIElementAutomationPeer.FromElement(itemContainer);
+            peer?.RaisePropertyChangedEvent(
+                TogglePatternIdentifiers.ToggleStateProperty,
+                oldValue ? ToggleState.On : ToggleState.Off,
+                newValue ? ToggleState.On : ToggleState.Off);
+
+            (AddMappingCommand as DelegateCommand).RaiseCanExecuteChanged();
+        }
+
+        private void PackageID_TextChanged(object sender, TextChangedEventArgs e)
         {
             (AddMappingCommand as DelegateCommand).RaiseCanExecuteChanged();
         }
 
-        private void PackageID_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void SourcesListBox_PreviewKeyUp(object sender, KeyEventArgs e)
         {
-            (AddMappingCommand as DelegateCommand).RaiseCanExecuteChanged();
+            if (e.Key == Key.Space
+                && ((ListViewItem)(_sourcesListBox.ItemContainerGenerator.ContainerFromItem(_sourcesListBox.SelectedItem))).IsFocused)
+            {
+                // toggle the selection state when user presses the space bar when focus is on the ListViewItem
+                var viewModel = _sourcesListBox.SelectedItem as PackageSourceViewModel;
+                if (viewModel != null)
+                {
+                    viewModel.IsSelected = !viewModel.IsSelected;
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
--- src/NuGet.Clients/NuGet.PackageManagement.UI/Options/PackageSourceViewModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Options/PackageSourceViewModel.cs
@@ -1,19 +1,40 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using NuGet.PackageManagement.UI;
+using System.ComponentModel;
 using NuGet.VisualStudio.Internal.Contracts;
+using System.Runtime.CompilerServices;
 
 namespace NuGet.Options
 {
-    public class PackageSourceViewModel
+    public class PackageSourceViewModel : INotifyPropertyChanged, ISelectableItem
     {
-        public PackageSourceContextInfo SourceInfo { get; private set; }
-        public bool IsChecked { get; set; }
+        private bool _isSelected;
 
-        public PackageSourceViewModel(PackageSourceContextInfo sourceInfo, bool isChecked)
+        public PackageSourceViewModel(PackageSourceContextInfo sourceInfo)
         {
             SourceInfo = sourceInfo;
-            IsChecked = isChecked;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public PackageSourceContextInfo SourceInfo { get; private set; }
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
