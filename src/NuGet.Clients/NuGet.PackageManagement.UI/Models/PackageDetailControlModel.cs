49ecb357c86c58e248f7e22c926bfccb8bdb9cbd
Martin Ruiz <martin.ruiz.mares@gmail.com>
2022-10-05 00:51:25 -0700

    remove unnecesary guard

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/PackageDetailControlModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/PackageDetailControlModel.cs
@@ -318,7 +318,7 @@ namespace NuGet.PackageManagement.UI
             {
                 return SelectedVersion != null
                     && InstalledVersion != null
-                    && ((IsProjectPackageReference && InstalledVersionRange?.OriginalString != null) ? SelectedVersion?.Range?.OriginalString == InstalledVersionRange?.OriginalString : true)
+                    && ((IsProjectPackageReference && InstalledVersionRange?.OriginalString != null) ? SelectedVersion?.Range?.OriginalString == InstalledVersionRange.OriginalString : true)
                     && SelectedVersion.Version == InstalledVersion;
             }
         }
