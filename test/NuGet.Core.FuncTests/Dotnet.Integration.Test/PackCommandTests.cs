39651126c90acab817b2581b276ba3a8e1f5f650
Damon Tivel <dtivel@microsoft.com>
2022-10-10 17:11:04 -0700

    Skip test while it fails on .NET 7

--- test/NuGet.Core.FuncTests/Dotnet.Integration.Test/PackCommandTests.cs
+++ test/NuGet.Core.FuncTests/Dotnet.Integration.Test/PackCommandTests.cs
@@ -1488,7 +1488,7 @@ namespace Dotnet.Integration.Test
             }
         }
 
-        [PlatformTheory(Platform.Windows)]
+        [PlatformTheory(Platform.Windows, Skip = "https://github.com/dotnet/sdk/issues/28131")]
         // Command line : /p:NuspecProperties=\"id=MyPackage;version=1.2.3;tags=tag1;description="hello world"\"
         [InlineData("/p:NuspecProperties=\\\"id=MyPackage;version=1.2.3;tags=tag1;description=\"hello world\"\\\"", "MyPackage",
             "1.2.3", "hello world", "tag1")]
