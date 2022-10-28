3c4c648622d10043ae825e92786bcd3175f0730d
Erick Yondon <eryondon@microsoft.com>
2022-10-28 09:41:10 -0700

    Skip failing tests from official pipeline to reduce CI test flakiness (#4878)
    * Skip failing tests from official pipeline to reduce CI test flakiness

--- test/NuGet.Core.FuncTests/Dotnet.Integration.Test/PackCommandTests.cs
+++ test/NuGet.Core.FuncTests/Dotnet.Integration.Test/PackCommandTests.cs
@@ -436,7 +436,7 @@ namespace Dotnet.Integration.Test
             }
         }
 
-        [PlatformTheory(Platform.Windows)]
+        [PlatformTheory(Platform.Windows, Skip = "https://github.com/NuGet/Home/issues/12194")]
         [InlineData(true)]
         [InlineData(false)]
         public void PackCommand_PackConsoleAppWithRID_NupkgValid(bool includeSymbols)
