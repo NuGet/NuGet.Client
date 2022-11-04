e286d72f3efb9013cb9372bc574d0d1841a1a64b
Kartheek Penagamuri <kartheek.penagamuri@microsoft.com>
2022-11-03 17:57:53 -0700

    MigrationRunnerTests are only applicable to net472 code path on nuget 4.9.x

--- test/NuGet.Core.Tests/NuGet.Common.Test/MigrationRunnerTests.cs
+++ test/NuGet.Core.Tests/NuGet.Common.Test/MigrationRunnerTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#if IS_DESKTOP
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -64,3 +65,4 @@ namespace NuGet.Common.Test
         }
     }
 }
+#endif
