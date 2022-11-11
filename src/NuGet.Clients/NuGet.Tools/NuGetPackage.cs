f1d69f1639fcb4a03b245061ce42be12367ca261
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2022-11-11 14:57:39 -0800

    update release branding to 5.11.4 (release-5.11.x) (#4911)

--- src/NuGet.Clients/NuGet.Tools/NuGetPackage.cs
+++ src/NuGet.Clients/NuGet.Tools/NuGetPackage.cs
@@ -83,7 +83,7 @@ namespace NuGetVSExtension
     public sealed class NuGetPackage : AsyncPackage, IVsPackageExtensionProvider, IVsPersistSolutionOpts
     {
         // It is displayed in the Help - About box of Visual Studio
-        public const string ProductVersion = "5.11.3";
+        public const string ProductVersion = "5.11.4";
         private const string F1KeywordValuePmUI = "VS.NuGet.PackageManager.UI";
 
         private AsyncLazy<IVsMonitorSelection> _vsMonitorSelection;
