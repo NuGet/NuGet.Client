dcb355d1a989be369e5ac3e3cd6eeea449a4b5eb
Kartheek Penagamuri <52756182+kartheekp-ms@users.noreply.github.com>
2022-11-22 15:59:38 -0800

    Fix code analysis warning in a test utility method (#4936)

--- test/TestUtilities/Test.Utility/ZipExtensions.cs
+++ test/TestUtilities/Test.Utility/ZipExtensions.cs
@@ -63,6 +63,7 @@ namespace NuGet.Test.Utility
 
         public static void ExtractAll(this ZipArchive archive, string targetPath)
         {
+            string fullDestDirPath = Path.GetFullPath(targetPath + Path.DirectorySeparatorChar);
             foreach (var entry in archive.Entries)
             {
                 var entryFullName = entry.FullName;
@@ -73,8 +74,8 @@ namespace NuGet.Test.Utility
 
                 entryFullName = Uri.UnescapeDataString(entryFullName.Replace('/', Path.DirectorySeparatorChar));
 
-                var targetFile = Path.Combine(targetPath, entryFullName);
-                if (!targetFile.StartsWith(targetPath, StringComparison.OrdinalIgnoreCase))
+                string targetFile = Path.GetFullPath(Path.Combine(targetPath, entryFullName));
+                if (!targetFile.StartsWith(fullDestDirPath, StringComparison.OrdinalIgnoreCase))
                 {
                     continue;
                 }
