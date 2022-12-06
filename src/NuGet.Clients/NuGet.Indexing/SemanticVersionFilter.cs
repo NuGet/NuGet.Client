ef300e2c46db4b6c266b7ae10fd477e2363bbe3a
Gary Ewan Park <gep13@gep13.co.uk>
2022-12-06 21:40:32 +0000

    Fixed spacing before/after copyright header
    Ensured that copyright header was the first thing in each .cs file, and
    also that there was only 1 line after the header and the first
    using/namespace line.

--- src/NuGet.Clients/NuGet.Indexing/SemanticVersionFilter.cs
+++ src/NuGet.Clients/NuGet.Indexing/SemanticVersionFilter.cs
@@ -1,5 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using Lucene.Net.Analysis;
 using Lucene.Net.Analysis.Tokenattributes;
 using NuGet.Versioning;
--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetResponseFileTests.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetResponseFileTests.cs
@@ -1,6 +1,7 @@
-#if IS_DESKTOP
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#if IS_DESKTOP
 
 using System;
 using System.IO;
