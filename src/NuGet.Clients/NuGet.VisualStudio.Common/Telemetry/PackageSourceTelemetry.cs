a0e534265462309cc64a19361f0a219315df5f46
Erick Yondon <eryondon@microsoft.com>
2023-01-20 12:40:48 -0800

    Improve VSfeedback data (#5017) (#5023)

--- src/NuGet.Clients/NuGet.VisualStudio.Common/Telemetry/PackageSourceTelemetry.cs
+++ src/NuGet.Clients/NuGet.VisualStudio.Common/Telemetry/PackageSourceTelemetry.cs
@@ -323,7 +323,7 @@ namespace NuGet.VisualStudio.Telemetry
             return subevent;
         }
 
-        private static string GetMsFeed(PackageSource source)
+        internal static string GetMsFeed(PackageSource source)
         {
             if (source.IsHttp)
             {
