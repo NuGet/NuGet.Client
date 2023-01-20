3933be86abd50b66991791480d4d48ae1375cb28
Erick Yondon <eryondon@microsoft.com>
2023-01-20 12:40:48 -0800

    Improve VSfeedback data (#5017) (#5023)

--- src/NuGet.Clients/NuGet.VisualStudio.Common/Telemetry/PackageSourceTelemetry.cs
+++ src/NuGet.Clients/NuGet.VisualStudio.Common/Telemetry/PackageSourceTelemetry.cs
@@ -322,7 +322,7 @@ namespace NuGet.VisualStudio.Telemetry
             return subevent;
         }
 
-        private static string GetMsFeed(PackageSource source)
+        internal static string GetMsFeed(PackageSource source)
         {
             if (source.IsHttp)
             {
--- test/NuGet.Clients.Tests/NuGet.VisualStudio.Common.Test/NuGetFeedbackDiagnosticFileProviderTests.cs
+++ test/NuGet.Clients.Tests/NuGet.VisualStudio.Common.Test/NuGetFeedbackDiagnosticFileProviderTests.cs
@@ -10,11 +10,22 @@ using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using Moq;
+using Newtonsoft.Json.Linq;
+using NuGet.Commands;
 using NuGet.Common;
 using NuGet.Configuration;
+using NuGet.Configuration.Test;
 using NuGet.PackageManagement;
+using NuGet.PackageManagement.Test;
+using NuGet.Packaging.Core;
 using NuGet.ProjectManagement;
+using NuGet.ProjectManagement.Projects;
+using NuGet.ProjectModel;
+using NuGet.Protocol.Core.Types;
+using NuGet.Test.Utility;
+using NuGet.Versioning;
 using NuGet.VisualStudio.Telemetry;
+using Test.Utility;
 using Xunit;
 
 namespace NuGet.VisualStudio.Common.Test
@@ -156,7 +167,7 @@ namespace NuGet.VisualStudio.Common.Test
         }
 
         [Fact]
-        public async Task WriteToZipAsync_MefImportsNotAvailable_AddsMefErrorsFie()
+        public async Task WriteToZipAsync_MefImportsNotAvailable_AddsMefErrorsFile()
         {
             // Arrange
             _target.SolutionManager = null;
@@ -171,6 +182,148 @@ namespace NuGet.VisualStudio.Common.Test
             {
                 IEnumerable<string> zipFiles = zip.Entries.Select(e => e.FullName);
                 Assert.Contains("mef-errors.txt", zipFiles);
+            }
+        }
+
+        [Fact]
+        public async Task WriteToZipAsync_WithMSSource_SourceRemainsStill()
+        {
+            // Arrange
+            var projectName = "testproj";
+            var dgSpecFileName = "dgspec.json";
+
+            using (var solutionManager = new TestSolutionManager())
+            {
+                var projectFolder = new DirectoryInfo(Path.Combine(solutionManager.SolutionDirectory, projectName));
+                projectFolder.Create();
+                var projectConfig = new FileInfo(Path.Combine(projectFolder.FullName, "project.json"));
+                var msbuildProjectPath = new FileInfo(Path.Combine(projectFolder.FullName, $"{projectName}.csproj"));
+                string extractPath = Path.Combine(solutionManager.SolutionDirectory);
+
+                BuildIntegrationTestUtility.CreateConfigJson(projectConfig.FullName);
+                var json = JObject.Parse(File.ReadAllText(projectConfig.FullName));
+
+                JsonConfigUtility.AddDependency(json, new PackageDependency("nuget.versioning", VersionRange.Parse("1.0.7")));
+
+                using (var writer = new StreamWriter(projectConfig.FullName))
+                {
+                    writer.Write(json.ToString());
+                }
+
+                var sources = new List<SourceRepository> { };
+                var testLogger = new TestLogger();
+                var settings = Settings.LoadSpecificSettings(solutionManager.SolutionDirectory, "NuGet.Config");
+                var project = new ProjectJsonNuGetProject(projectConfig.FullName, msbuildProjectPath.FullName);
+
+                solutionManager.NuGetProjects.Add(project);
+
+                var restoreContext = new DependencyGraphCacheContext(testLogger, settings);
+                var providersCache = new RestoreCommandProvidersCache();
+                DependencyGraphSpec dgSpec = await DependencyGraphRestoreUtility.GetSolutionRestoreSpec(solutionManager, restoreContext);
+
+                using var stream = new MemoryStream();
+                _target.Settings = settings;
+                _target.SolutionManager = solutionManager;
+
+                // Act
+                await _target.WriteToZipAsync(stream);
+
+                // Assert
+                using (var zip = new ZipArchive(stream))
+                {
+                    IEnumerable<string> zipFiles = zip.Entries.Select(e => e.FullName);
+                    var expectedFiles = new[] { dgSpecFileName };
+
+                    Assert.Equal(zipFiles.OrderBy(f => f), expectedFiles);
+
+                    foreach (ZipArchiveEntry entry in zip.Entries)
+                    {
+                        string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FullName));
+                        if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+                            entry.ExtractToFile(destinationPath);
+                    }
+                }
+
+                DependencyGraphSpec vsFeedbackDgSpec = DependencyGraphSpec.Load(Path.Combine(extractPath, dgSpecFileName));
+                Assert.Equal(dgSpec.Projects.Count, vsFeedbackDgSpec.Projects.Count);
+                Assert.Equal(dgSpec.Projects[0].RestoreMetadata.Sources.Count, vsFeedbackDgSpec.Projects[0].RestoreMetadata.Sources.Count);
+                Assert.Equal(dgSpec.Projects[0].RestoreMetadata.Sources[0].Source, vsFeedbackDgSpec.Projects[0].RestoreMetadata.Sources[0].Source);
+                // dgSpec.Save replaces source name with source.
+                Assert.Equal(dgSpec.Projects[0].RestoreMetadata.Sources[0].Source, vsFeedbackDgSpec.Projects[0].RestoreMetadata.Sources[0].Name);
+            }
+        }
+
+        [Fact]
+        public async Task WriteToZipAsync_WithNonMSSource_SourceHashed()
+        {
+            // Arrange
+            var projectName = "testproj";
+            var dgSpecFileName = "dgspec.json";
+
+            using (var solutionManager = new TestSolutionManager())
+            {
+                var privateRepositoryPath = Path.Combine(solutionManager.TestDirectory, "SharedRepository");
+                Directory.CreateDirectory(privateRepositoryPath);
+
+                var configPath = Path.Combine(solutionManager.TestDirectory, "nuget.config");
+                SettingsTestUtils.CreateConfigurationFile(configPath, $@"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+    <packageSources>
+    <!--To inherit the global NuGet package sources remove the <clear/> line below -->
+    <clear />
+    <add key=""PrivateRepository"" value=""{privateRepositoryPath}"" />
+    </packageSources>
+</configuration>");
+
+                var projectFolder = new DirectoryInfo(Path.Combine(solutionManager.SolutionDirectory, projectName));
+                projectFolder.Create();
+                var projectConfig = new FileInfo(Path.Combine(projectFolder.FullName, "project.json"));
+                var msbuildProjectPath = new FileInfo(Path.Combine(projectFolder.FullName, $"{projectName}.csproj"));
+                string extractPath = Path.Combine(solutionManager.SolutionDirectory);
+
+                BuildIntegrationTestUtility.CreateConfigJson(projectConfig.FullName);
+
+                var sources = new List<SourceRepository> { };
+                var testLogger = new TestLogger();
+                var settings = Settings.LoadSpecificSettings(solutionManager.SolutionDirectory, "NuGet.Config");
+                var project = new ProjectJsonNuGetProject(projectConfig.FullName, msbuildProjectPath.FullName);
+
+                solutionManager.NuGetProjects.Add(project);
+
+                var restoreContext = new DependencyGraphCacheContext(testLogger, settings);
+                var providersCache = new RestoreCommandProvidersCache();
+                DependencyGraphSpec dgSpec = await DependencyGraphRestoreUtility.GetSolutionRestoreSpec(solutionManager, restoreContext);
+
+                using var stream = new MemoryStream();
+                _target.Settings = settings;
+                _target.SolutionManager = solutionManager;
+
+                // Act
+                await _target.WriteToZipAsync(stream);
+
+                // Assert
+                using (var zip = new ZipArchive(stream))
+                {
+                    IEnumerable<string> zipFiles = zip.Entries.Select(e => e.FullName);
+                    var expectedFiles = new[] { dgSpecFileName };
+
+                    Assert.Equal(zipFiles.OrderBy(f => f), expectedFiles);
+
+                    foreach (ZipArchiveEntry entry in zip.Entries)
+                    {
+                        string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FullName));
+                        if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+                            entry.ExtractToFile(destinationPath);
+                    }
+                }
+
+                DependencyGraphSpec vsFeedbackDgSpec = DependencyGraphSpec.Load(Path.Combine(extractPath, dgSpecFileName));
+                Assert.Equal(dgSpec.Projects.Count, vsFeedbackDgSpec.Projects.Count);
+                Assert.Equal(dgSpec.Projects[0].RestoreMetadata.Sources.Count, vsFeedbackDgSpec.Projects[0].RestoreMetadata.Sources.Count);
+                string hashedSource = CryptoHashUtility.GenerateUniqueToken(privateRepositoryPath);
+                Assert.Equal(hashedSource, vsFeedbackDgSpec.Projects[0].RestoreMetadata.Sources[0].Source);
+                // dgSpec.Save replaces source name with source.
+                Assert.Equal(hashedSource, vsFeedbackDgSpec.Projects[0].RestoreMetadata.Sources[0].Name);
             }
         }
     }
