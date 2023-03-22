3d3f57dd8b3fc88aa1e40f64ae5fdfe2874250e2
dainizzz <dainiz.almazan@gmail.com>
2023-03-22 12:03:10 -0700

    Enables nullable for config path args

--- src/NuGet.Core/NuGet.CommandLine.XPlat/Commands/ConfigCommands/ConfigArgs.cs
+++ src/NuGet.Core/NuGet.CommandLine.XPlat/Commands/ConfigCommands/ConfigArgs.cs
@@ -1,10 +1,11 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+#nullable enable
 
 namespace NuGet.CommandLine.XPlat
 {
     internal partial class ConfigPathsArgs
     {
-        public string WorkingDirectory { get; set; }
+        public string? WorkingDirectory { get; set; }
     }
 }
