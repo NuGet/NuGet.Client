6be90069287b0d117359cf7ec3fb030a69f666c7
John Brimley <johnbrimley@gmail.com>
2023-03-23 13:56:22 -0600

    Added fix so that TryCreate does not throw an exception and instead returns false. Added supporting test.

---
+++ test/NuGet.Core.Tests/NuGet.Protocol.Tests/Resources/RegistrationResourceV3ProviderTests.cs
@@ -0,0 +1,44 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Newtonsoft.Json.Linq;
+using NuGet.Protocol.Core.Types;
+using Xunit;
+
+namespace NuGet.Protocol.Tests.Resources
+{
+    public class RegistrationResourceV3ProviderTests
+    {
+        [Fact]
+        public async Task TryCreate_When_RegistrationsBaseUrl_Version_Is_Unuspported_Returns_False()
+        {
+            //Arrange
+            var rawServiceIndex = @"{
+              ""version"": ""3.0.0"",
+              ""resources"": [
+                {
+                  ""@id"": ""https://api.nuget.org/v5/registrations-gz-semver3"",
+                  ""@type"": ""RegistrationsBaseUrl"",
+                  ""comment"": ""Fancy new unsupported semver 3""
+                }]}";
+
+            var serviceIndexJObject = JObject.Parse(rawServiceIndex);
+            var serviceIndexResource = new ServiceIndexResourceV3(serviceIndexJObject, DateTime.Now);
+            var sourceRepositoryMock = Mock.Of<SourceRepository>(mock => mock.GetResourceAsync<ServiceIndexResourceV3>(It.IsAny<CancellationToken>()) == Task.FromResult(serviceIndexResource));
+
+            var sut = new RegistrationResourceV3Provider();
+
+            //Act
+            var actual = await sut.TryCreate(sourceRepositoryMock, CancellationToken.None);
+
+            //Assert
+            actual.Item1.Should().BeFalse();
+            actual.Item2.Should().BeNull();
+        }
+    }
+}
