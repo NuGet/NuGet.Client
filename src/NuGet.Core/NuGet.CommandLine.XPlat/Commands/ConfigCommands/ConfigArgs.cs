f077ac2efd7b770e80e1729f370dca2013d2a00f
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2023-03-29 12:16:54 -0700

    Dotnet NuGet Config CLI Config Paths Command (#5088)
    Added DotNet Nuget Config Paths command

---
+++ src/NuGet.Core/NuGet.CommandLine.XPlat/Commands/ConfigCommands/ConfigArgs.cs
@@ -0,0 +1,11 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+#nullable enable
+
+namespace NuGet.CommandLine.XPlat
+{
+    internal class ConfigPathsArgs
+    {
+        public string? WorkingDirectory { get; set; }
+    }
+}
---
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XPlatConfigTests.cs
@@ -0,0 +1,176 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Globalization;
+using System.IO;
+using NuGet.CommandLine.XPlat;
+using NuGet.Test.Utility;
+using Xunit;
+
+namespace NuGet.XPlat.FuncTest
+{
+    public class XPlatConfigTests
+    {
+        private static readonly string XplatDll = DotnetCliUtil.GetXplatDll();
+        private static readonly string DotnetCli = TestFileSystemUtility.GetDotnetCli();
+
+        [Fact]
+        public void ConfigPathsCommand_ListConfigPathsWithArgs_Success()
+        {
+            // Arrange & Act
+            using var testInfo = new TestInfo("NuGet.Config");
+            {
+                var result = CommandRunner.Run(
+                      DotnetCli,
+                      Directory.GetCurrentDirectory(),
+                      $"{XplatDll} config paths {testInfo.WorkingPath}",
+                      waitForExit: true
+                      );
+
+                // Assert
+                DotnetCliUtil.VerifyResultSuccess(result, Path.Combine(testInfo.WorkingPath.Path, "NuGet.Config"));
+            }
+        }
+
+        [Fact]
+        public void ConfigPathsCommand_NoDirectoryArg_Success()
+        {
+            // Arrange & Act
+            using var testInfo = new TestInfo("NuGet.Config");
+            {
+                var result = CommandRunner.Run(
+                      DotnetCli,
+                      testInfo.WorkingPath,
+                      $"{XplatDll} config paths",
+                      waitForExit: true
+                      );
+
+                // Assert
+                DotnetCliUtil.VerifyResultSuccess(result, Path.Combine(testInfo.WorkingPath.Path, "NuGet.Config"));
+            }
+        }
+
+        [Fact]
+        public void ConfigPathsCommand_HelpMessage_Success()
+        {
+            // Arrange
+            var helpMessage = string.Format(CultureInfo.CurrentCulture, Strings.ConfigPathsWorkingDirectoryDescription);
+
+            // Act
+            var result = CommandRunner.Run(
+                DotnetCli,
+                Directory.GetCurrentDirectory(),
+                $"{XplatDll} config paths --help",
+                waitForExit: true
+                );
+
+            // Assert
+            DotnetCliUtil.VerifyResultSuccess(result, helpMessage);
+        }
+
+        [Fact]
+        public void ConfigCommand_HelpMessage_Success()
+        {
+            // Arrange
+            var helpMessage = string.Format(CultureInfo.CurrentCulture, Strings.ConfigPathsCommandDescription);
+
+            // Act
+            var result = CommandRunner.Run(
+                DotnetCli,
+                Directory.GetCurrentDirectory(),
+                $"{XplatDll} config --help",
+                waitForExit: true
+                );
+
+            // Assert
+            DotnetCliUtil.VerifyResultSuccess(result, helpMessage);
+        }
+
+        [Fact]
+        public void ConfigPathsCommand_ListConfigPathsNonExistingDirectory_Fail()
+        {
+            // Arrange & Act
+            using var testInfo = new TestInfo("NuGet.Config");
+            {
+                var nonExistingDirectory = Path.Combine(testInfo.WorkingPath.Path, @"\NonExistingRepos");
+                var result = CommandRunner.Run(
+                    DotnetCli,
+                    Directory.GetCurrentDirectory(),
+                    $"{XplatDll} config paths {nonExistingDirectory}",
+                    waitForExit: true
+                    );
+                var expectedError = string.Format(CultureInfo.CurrentCulture, Strings.Error_PathNotFound, nonExistingDirectory);
+
+                // Assert
+                DotnetCliUtil.VerifyResultFailure(result, expectedError);
+            }
+        }
+
+        [Fact]
+        public void ConfigPathsCommand_NullArgs_Fail()
+        {
+            // Arrange
+            var log = new TestCommandOutputLogger();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => ConfigPathsRunner.Run(null, () => log));
+        }
+
+        [Fact]
+        public void ConfigPathsCommand_NullGetLogger_Fail()
+        {
+            // Arrange
+            var args = new ConfigPathsArgs()
+            {
+            };
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => ConfigPathsRunner.Run(args, null));
+        }
+
+        internal class TestInfo : IDisposable
+        {
+            public static void CreateFile(string directory, string fileName, string fileContent)
+            {
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                var fileFullName = Path.Combine(directory, fileName);
+                CreateFile(fileFullName, fileContent);
+            }
+
+            public static void CreateFile(string fileFullName, string fileContent)
+            {
+                using var writer = new StreamWriter(fileFullName);
+                {
+                    writer.Write(fileContent);
+                }
+            }
+
+            public TestInfo(string configPath)
+            {
+                WorkingPath = TestDirectory.Create();
+                ConfigFile = configPath;
+                CreateFile(WorkingPath.Path,
+                           Path.GetFileName(ConfigFile),
+                           $@"
+<configuration>
+    <packageSources>
+        <add key=""Foo"" value=""https://contoso.test/v3/index.json"" />
+    </packageSources>
+</configuration>
+");
+            }
+
+            public TestDirectory WorkingPath { get; }
+            public string ConfigFile { get; }
+            public void Dispose()
+            {
+                WorkingPath.Dispose();
+            }
+        }
+    }
+}
