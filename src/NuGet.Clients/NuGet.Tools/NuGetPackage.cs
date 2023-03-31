dca9c3c7fafe73f05a07946a0bd609b88b97a9a4
Heng Liu <henli@microsoft.com>
2023-03-31 17:45:39 +0000

    Merged PR 462113: update NuGet patch version (6.0.4 to 6.0.5)
    update patch version

--- src/NuGet.Clients/NuGet.Tools/NuGetPackage.cs
+++ src/NuGet.Clients/NuGet.Tools/NuGetPackage.cs
@@ -83,7 +83,7 @@ namespace NuGetVSExtension
     public sealed class NuGetPackage : AsyncPackage, IVsPackageExtensionProvider, IVsPersistSolutionOpts
     {
         // It is displayed in the Help - About box of Visual Studio
-        public const string ProductVersion = "6.0.4";
+        public const string ProductVersion = "6.0.5";
         private const string F1KeywordValuePmUI = "VS.NuGet.PackageManager.UI";
 
         private AsyncLazy<IVsMonitorSelection> _vsMonitorSelection;
