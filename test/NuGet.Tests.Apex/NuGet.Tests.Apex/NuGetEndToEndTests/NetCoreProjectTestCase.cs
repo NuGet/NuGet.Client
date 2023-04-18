9fa3d35e3fcf82b71b42195da69ed648e8704629
Jamie Zhang <jamie-zhang@163.com>
2023-04-18 16:19:52 +0800

    Add building and waiting for auto-restore steps after updating/uninstalling package to fix bug 12441 (#5128)
    * Add building and waiting for auto-restore steps after updating/uninstalling package to fix bug 12441
    
    * Add building and restoring steps after package installation
    
    ---------
    
    Co-authored-by: Jamie Zhang <v-luzh@microsoft.com>

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NetCoreProjectTestCase.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NetCoreProjectTestCase.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
-using EnvDTE;
 using Microsoft.Test.Apex.VisualStudio.Solution;
 using NuGet.StaFact;
 using NuGet.Test.Utility;
@@ -281,7 +278,7 @@ namespace NuGet.Tests.Apex
             }
         }
 
-        [NuGetWpfTheory(Skip = "https://github.com/NuGet/Home/issues/12441")]
+        [NuGetWpfTheory]
         [MemberData(nameof(GetNetCoreTemplates))]
         public async Task UpdatePackageToNetCoreProjectFromUI(ProjectTemplate projectTemplate)
         {
@@ -306,8 +303,13 @@ namespace NuGet.Tests.Apex
                     var nugetTestService = GetNuGetTestService();
                     var uiwindow = nugetTestService.GetUIWindowfromProject(testContext.Project);
                     uiwindow.InstallPackageFromUI(packageName, packageVersion1);
+                    testContext.SolutionService.Build();
+                    testContext.NuGetApexTestService.WaitForAutoRestore();
                     uiwindow.SwitchTabToUpdate();
+
                     uiwindow.UpdatePackageFromUI(packageName, packageVersion2);
+                    testContext.SolutionService.Build();
+                    testContext.NuGetApexTestService.WaitForAutoRestore();
 
                     // Assert
                     VisualStudio.AssertNuGetOutputDoesNotHaveErrors();
@@ -316,7 +318,7 @@ namespace NuGet.Tests.Apex
             }
         }
 
-        [NuGetWpfTheory(Skip = "https://github.com/NuGet/Home/issues/12441")]
+        [NuGetWpfTheory]
         [MemberData(nameof(GetNetCoreTemplates))]
         public async Task UninstallPackageFromNetCoreProjectFromUI(ProjectTemplate projectTemplate)
         {
@@ -339,8 +341,13 @@ namespace NuGet.Tests.Apex
                     var nugetTestService = GetNuGetTestService();
                     var uiwindow = nugetTestService.GetUIWindowfromProject(testContext.Project);
                     uiwindow.InstallPackageFromUI(packageName, packageVersion);
+                    testContext.SolutionService.Build();
+                    testContext.NuGetApexTestService.WaitForAutoRestore();
                     uiwindow.SwitchTabToInstalled();
+
                     uiwindow.UninstallPackageFromUI(packageName);
+                    testContext.SolutionService.Build();
+                    testContext.NuGetApexTestService.WaitForAutoRestore();
 
                     // Assert
                     VisualStudio.AssertNuGetOutputDoesNotHaveErrors();
