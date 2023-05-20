db3e0e731b84ee598311f73d842f99d6b9c241d4
dainizzz <dainiz.almazan@gmail.com>
2023-05-19 17:56:00 -0700

    Removes extra whitespace

--- test/NuGet.Core.Tests/NuGet.CommandLine.Xplat.Tests/XPlatConfigUnitTests.cs
+++ test/NuGet.Core.Tests/NuGet.CommandLine.Xplat.Tests/XPlatConfigUnitTests.cs
@@ -12,18 +12,18 @@ namespace NuGet.CommandLine.Xplat.Tests
         [Theory]
         [InlineData(true, false)]
         [InlineData(false, true)]
-        public void ConfigPathsCommand_WithNullArguments_ThrowsArgumentNullException(bool argsIsNull, bool loggerIsNull )
+        public void ConfigPathsCommand_WithNullArguments_ThrowsArgumentNullException(bool argsIsNull, bool loggerIsNull)
         {
             // Arrange
             ConfigPathsArgs args = null;
             CommandOutputLogger getLogger = null;
 
-            if ( !argsIsNull )
+            if (!argsIsNull)
             {
                 args = new ConfigPathsArgs();
             }
 
-            if ( !loggerIsNull )
+            if (!loggerIsNull)
             {
                 getLogger = new CommandOutputLogger(Common.LogLevel.Debug);
             }
@@ -35,18 +35,18 @@ namespace NuGet.CommandLine.Xplat.Tests
         [Theory]
         [InlineData(true, false)]
         [InlineData(false, true)]
-        public void ConfigGetCommand_WithNullArguments_ThrowsArgumentNullException(bool argsIsNull, bool loggerIsNull )
+        public void ConfigGetCommand_WithNullArguments_ThrowsArgumentNullException(bool argsIsNull, bool loggerIsNull)
         {
             // Arrange
             ConfigGetArgs args = null;
             CommandOutputLogger getLogger = null;
 
-            if ( !argsIsNull )
+            if (!argsIsNull)
             {
                 args = new ConfigGetArgs();
             }
 
-            if ( !loggerIsNull )
+            if (!loggerIsNull)
             {
                 getLogger = new CommandOutputLogger(Common.LogLevel.Debug);
             }
@@ -58,18 +58,18 @@ namespace NuGet.CommandLine.Xplat.Tests
         [Theory]
         [InlineData(true, false)]
         [InlineData(false, true)]
-        public void ConfigSetCommand_WithNullArguments_ThrowsArgumentNullException(bool argsIsNull, bool loggerIsNull )
+        public void ConfigSetCommand_WithNullArguments_ThrowsArgumentNullException(bool argsIsNull, bool loggerIsNull)
         {
             // Arrange
             ConfigSetArgs args = null;
             CommandOutputLogger getLogger = null;
 
-            if ( !argsIsNull )
+            if (!argsIsNull)
             {
                 args = new ConfigSetArgs();
             }
 
-            if ( !loggerIsNull )
+            if (!loggerIsNull)
             {
                 getLogger = new CommandOutputLogger(Common.LogLevel.Debug);
             }
@@ -81,18 +81,18 @@ namespace NuGet.CommandLine.Xplat.Tests
         [Theory]
         [InlineData(true, false)]
         [InlineData(false, true)]
-        public void ConfigUnsetCommand_WithNullArguments_ThrowsArgumentNullException(bool argsIsNull, bool loggerIsNull )
+        public void ConfigUnsetCommand_WithNullArguments_ThrowsArgumentNullException(bool argsIsNull, bool loggerIsNull)
         {
             // Arrange
             ConfigUnsetArgs args = null;
             CommandOutputLogger getLogger = null;
 
-            if ( !argsIsNull )
+            if (!argsIsNull)
             {
                 args = new ConfigUnsetArgs();
             }
 
-            if ( !loggerIsNull )
+            if (!loggerIsNull)
             {
                 getLogger = new CommandOutputLogger(Common.LogLevel.Debug);
             }
