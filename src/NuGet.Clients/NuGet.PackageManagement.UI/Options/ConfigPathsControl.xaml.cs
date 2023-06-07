0856568a68df224f0821e754de67e6a1de1dc105
Martin Ruiz <martin.ruiz.mares@gmail.com>
2023-06-07 13:36:04 -0700

    fix initialize component issue

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Options/ConfigPathsControl.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Options/ConfigPathsControl.xaml.cs
@@ -1,16 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Windows.Controls;
 using System.Windows.Input;
-using Microsoft.Internal.VisualStudio.PlatformUI;
-using Microsoft.VisualStudio.ComponentModelHost;
-using Microsoft.VisualStudio.PlatformUI;
-using Microsoft.VisualStudio.Shell;
-using NuGet.Configuration;
-using NuGet.VisualStudio;
-using NuGet.VisualStudio.Internal.Contracts;
 
 namespace NuGet.PackageManagement.UI.Options
 {
