d02d5913e0435dfa551d61f1f359ccce3fcb9c64
Martin Ruiz <martin.ruiz.mares@gmail.com>
2023-06-07 14:02:43 -0700

    rendering the listbox with elements

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Options/ConfigPathsControl.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Options/ConfigPathsControl.xaml.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using System.Threading;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Microsoft.VisualStudio.Services.Common;
 
 namespace NuGet.PackageManagement.UI.Options
 {
@@ -10,15 +12,23 @@ namespace NuGet.PackageManagement.UI.Options
     public partial class ConfigPathsControl : UserControl
     {
         //private AddMappingDialog _addMappingDialog;
+        public System.Collections.Generic.IEnumerable<string> ConfigPaths { get; set; }
 
         public ConfigPathsControl()
         {
+            var list = new System.Collections.Generic.List<string>();
+            list.Add("1");
+            list.Add("2");
+            ConfigPaths = list;
             DataContext = this;
             InitializeComponent();
         }
 
         internal void InitializeOnActivated(CancellationToken cancellationToken)
         {
+            // should caclculate the config files and create the view models
+            // array of view models
+            // each view model should represent a config file
         }
 
         public ICommand ShowAddDialogCommand { get; set; }
