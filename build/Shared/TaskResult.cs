3f7f85902145c473a5c17bc7ef08e5215b305f8e
Drew Noakes <git@drewnoakes.com>
2023-06-16 12:27:36 +1000

    Avoid allocation for tasks having interger values

--- build/Shared/TaskResult.cs
+++ build/Shared/TaskResult.cs
@@ -31,6 +31,29 @@ internal static class TaskResult
     }
 
     /// <summary>
+    /// Gets a <see cref="Task{TResult}"/> that's completed successfully with the result of <see langword="true"/>.
+    /// </summary>
+    public static Task<int> Zero { get; } = Task.FromResult(0);
+
+    /// <summary>
+    /// Gets a <see cref="Task{TResult}"/> that's completed successfully with the result of <see langword="false"/>.
+    /// </summary>
+    public static Task<int> One { get; } = Task.FromResult(1);
+
+    /// <summary>
+    /// Returns a <see cref="Task{TResult}"/> that's completed successfully with the result of <paramref name="i"/>.
+    /// </summary>
+    public static Task<int> Integer(int i)
+    {
+        return i switch
+        {
+            0 => Zero,
+            1 => One,
+            _ => Task.FromResult(i)
+        };
+    }
+
+    /// <summary>
     /// Returns a <see cref="Task{TResult}"/> of type <typeparamref name="T" /> that's completed successfully with the result of <see langword="null"/>.
     /// </summary>
     public static Task<T?> Null<T>() where T : class => NullTaskResult<T>.Instance;
