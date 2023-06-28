e70410d3f8d6c6e24350abfec94f5415c31812a4
Drew Noakes <git@drewnoakes.com>
2023-06-29 05:07:09 +1000

    Reduce allocations in ContentFileUtils.GetContentFileGroup (#5252)
    * Add unit test for ContentFileUtils.GetContentFileGroup
    
    Capture the behaviour ahead of rewriting the function to allocate less.
    
    * Reduce allocations in ContentFileUtils.GetContentFileGroup
    
    Avoids a bunch of temporary string allocations, arrays and enumerators by using `ReadOnlySpan<char>` instead of `string`.
    
    * Document SuppressAsserts class

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/ContentFiles/ContentFileUtils.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/ContentFiles/ContentFileUtils.cs
@@ -206,7 +206,7 @@ namespace NuGet.Commands
 
                 if (copyToOutput)
                 {
-                    string destination = null;
+                    string destination;
 
                     if (flatten)
                     {
@@ -216,7 +216,7 @@ namespace NuGet.Commands
                     {
                         // Find path relative to the TxM
                         // Ex: contentFiles/cs/net45/config/config.xml -> config/config.xml
-                        destination = GetContentFileFolderRelativeToFramework(file);
+                        destination = GetContentFileFolderRelativeToFramework(file.AsSpan());
                     }
 
                     if (isPP)
@@ -231,8 +231,8 @@ namespace NuGet.Commands
                 // Add the pp transform file if one exists
                 if (isPP)
                 {
-                    var destination = lockFileItem.Path.Substring(0, lockFileItem.Path.Length - 3);
-                    destination = GetContentFileFolderRelativeToFramework(destination);
+                    var destination = GetContentFileFolderRelativeToFramework(
+                        lockFileItem.Path.AsSpan().Slice(0, lockFileItem.Path.Length - 3));
 
                     lockFileItem.PPOutputPath = destination;
                 }
@@ -259,17 +259,36 @@ namespace NuGet.Commands
         // Find path relative to the TxM
         // Ex: contentFiles/cs/net45/config/config.xml -> config/config.xml
         // Ex: contentFiles/any/any/config/config.xml -> config/config.xml
-        private static string GetContentFileFolderRelativeToFramework(string itemPath)
+        internal static string GetContentFileFolderRelativeToFramework(ReadOnlySpan<char> itemPath)
         {
-            var parts = itemPath.Split('/');
+            ReadOnlySpan<char> span = itemPath;
 
-            if (parts.Length > 3)
+            int found = 0;
+
+            while (true)
             {
-                return string.Join("/", parts.Skip(3));
+                int slashIndex = span.IndexOf('/');
+
+                if (slashIndex == -1)
+                {
+                    // Didn't find enough parts
+                    break;
+                }
+
+                span = span.Slice(slashIndex + 1);
+
+                found++;
+
+                if (found == 3)
+                {
+                    // We have skipped three levels. Return what remains.
+                    return span.ToString();
+                }
             }
 
-            Debug.Fail("Unable to get relative path: " + itemPath);
-            return itemPath;
+            var path = itemPath.ToString();
+            Debug.Fail("Unable to get relative path: " + path);
+            return path;
         }
     }
 }
