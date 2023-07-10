751180b2148daa757da2c41886e102be18f98af2
Drew Noakes <git@drewnoakes.com>
2023-07-10 16:52:21 +1000

    Use CombinedHash over GetHashCode
    This avoids a boxed (allocating) call to ValueType.GetHashCode.

--- src/NuGet.Core/NuGet.Packaging/Signing/Verification/TrustedSignerAllowListEntry.cs
+++ src/NuGet.Core/NuGet.Packaging/Signing/Verification/TrustedSignerAllowListEntry.cs
@@ -1,7 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using NuGet.Common;
@@ -62,7 +61,7 @@ namespace NuGet.Packaging.Signing
             combiner.AddStruct(FingerprintAlgorithm);
             combiner.AddSequence(Owners);
 
-            return combiner.GetHashCode();
+            return combiner.CombinedHash;
         }
     }
 }
