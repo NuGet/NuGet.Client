6338adb81210f93a6ab960c0c55c4cef866137c0
Nikolche Kolev <nikolev@microsoft.com>
2023-08-25 15:42:12 -0700

    add all the tests

--- test/NuGet.Core.Tests/NuGet.PackageManagement.Test/AuditUtilityTests.cs
+++ test/NuGet.Core.Tests/NuGet.PackageManagement.Test/AuditUtilityTests.cs
@@ -1,14 +1,21 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#nullable enable
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using FluentAssertions;
+using Moq;
 using NuGet.Common;
+using NuGet.Configuration;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
 using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
 using NuGet.Protocol.Model;
 using NuGet.Versioning;
 using Xunit;
@@ -18,13 +25,6 @@ namespace NuGet.PackageManagement.Test
 {
     public class AuditUtilityTests
     {
-
-        [Fact]
-        public void GetAllVulnerabilityDataAsync_MergesVulnerabilities()
-        {
-
-        }
-
         [Fact]
         public void GetKnownVulnerability_WithPackageNotVulnerable_ReturnsNull()
         {
@@ -94,10 +94,10 @@ namespace NuGet.PackageManagement.Test
                     }
                 };
 
-            var vulnerabilities = AuditUtility.GetKnownVulnerabilities("packageId", new NuGetVersion(2, 0, 0), knownVulnerabilities);
+            List<PackageVulnerabilityInfo>? vulnerabilities = AuditUtility.GetKnownVulnerabilities("packageId", new NuGetVersion(2, 0, 0), knownVulnerabilities);
 
-            vulnerabilities.Should().HaveCount(2);
-            var moderateVulnerability = vulnerabilities[0];
+            vulnerabilities!.Should().HaveCount(2);
+            var moderateVulnerability = vulnerabilities![0];
             moderateVulnerability.Severity.Should().Be(PackageVulnerabilitySeverity.Moderate);
             moderateVulnerability.Url.Should().Be(new Uri("https://contoso.com/random-vulnerability2"));
             moderateVulnerability.Versions.Should().Be(VersionRange.Parse("[2.0.0, 2.0.0]"));
@@ -146,10 +146,10 @@ namespace NuGet.PackageManagement.Test
                     }
                 };
 
-            List<PackageVulnerabilityInfo> vulnerabilities = AuditUtility.GetKnownVulnerabilities("packageId", new NuGetVersion(2, 0, 0), knownVulnerabilities);
+            List<PackageVulnerabilityInfo>? vulnerabilities = AuditUtility.GetKnownVulnerabilities("packageId", new NuGetVersion(2, 0, 0), knownVulnerabilities);
 
-            vulnerabilities.Should().HaveCount(2);
-            var moderateVulnerability = vulnerabilities[0];
+            vulnerabilities!.Should().HaveCount(2);
+            var moderateVulnerability = vulnerabilities![0];
             moderateVulnerability.Severity.Should().Be(PackageVulnerabilitySeverity.Moderate);
             moderateVulnerability.Url.Should().Be(new Uri("https://contoso.com/random-vulnerability2"));
             moderateVulnerability.Versions.Should().Be(VersionRange.Parse("[2.0.0, 2.0.0]"));
@@ -198,10 +198,10 @@ namespace NuGet.PackageManagement.Test
                     }
                 };
 
-            var vulnerabilities = AuditUtility.GetKnownVulnerabilities("packageId", new NuGetVersion(2, 0, 0), knownVulnerabilities);
+            List<PackageVulnerabilityInfo>? vulnerabilities = AuditUtility.GetKnownVulnerabilities("packageId", new NuGetVersion(2, 0, 0), knownVulnerabilities);
 
             vulnerabilities.Should().HaveCount(1);
-            var moderateVulnerability = vulnerabilities[0];
+            var moderateVulnerability = vulnerabilities![0];
             moderateVulnerability.Severity.Should().Be(PackageVulnerabilitySeverity.Moderate);
             moderateVulnerability.Url.Should().Be(new Uri("https://contoso.com/random-vulnerability2"));
             moderateVulnerability.Versions.Should().Be(VersionRange.Parse("[2.0.0, 2.0.0]"));
@@ -334,6 +334,187 @@ namespace NuGet.PackageManagement.Test
         public void GetSeverityLabelAndCode_ReturnCorrectLabelAndCode(PackageVulnerabilitySeverity severity, string expectedLabel, NuGetLogCode expectedCode)
         {
             AuditUtility.GetSeverityLabelAndCode(severity).Should().Be((expectedLabel, expectedCode));
+        }
+
+        internal class VulnerabilityInfoResourceImplementation : IVulnerabilityInfoResource
+        {
+            internal GetVulnerabilityInfoResult Result { get; }
+            internal SourceRepository SourceRepository { get; }
+            public VulnerabilityInfoResourceImplementation(SourceRepository sourceRepository, GetVulnerabilityInfoResult result)
+            {
+                SourceRepository = sourceRepository;
+                Result = result;
+            }
+            public Task<GetVulnerabilityInfoResult> GetVulnerabilityInfoAsync(SourceCacheContext cacheContext, ILogger logger, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(Result);
+            }
+        }
+
+        internal class VulnerabilityInfoResourceProvider : ResourceProvider
+        {
+            private readonly Dictionary<string, GetVulnerabilityInfoResult> _vulnerabilityInfoResults;
+
+            public VulnerabilityInfoResourceProvider(Dictionary<string, GetVulnerabilityInfoResult> vulnerabilityInfoResults)
+                : base(typeof(IVulnerabilityInfoResource), nameof(VulnerabilityInfoResourceProvider))
+            {
+                _vulnerabilityInfoResults = vulnerabilityInfoResults ?? throw new ArgumentNullException(nameof(vulnerabilityInfoResults));
+            }
+
+            public override Task<Tuple<bool, INuGetResource?>> TryCreate(SourceRepository source, CancellationToken token)
+            {
+                if (_vulnerabilityInfoResults.TryGetValue(source.PackageSource.Source, out GetVulnerabilityInfoResult? value))
+                {
+                    var resource = new VulnerabilityInfoResourceImplementation(source, value);
+                    var result = new Tuple<bool, INuGetResource?>(true, resource);
+                    return Task.FromResult(result);
+                }
+                return Task.FromResult(new Tuple<bool, INuGetResource?>(false, null));
+            }
+        }
+
+        [Fact]
+        public async Task GetAllVulnerabilityDataAsync_WithNoSourcesProvidingVulnerabilities_ReturnsNull()
+        {
+            Dictionary<string, GetVulnerabilityInfoResult> vulnerabilityResults = new();
+
+            var sourceRepositories = new List<SourceRepository>
+            {
+                new SourceRepository(new PackageSource("https://contoso.com/v3/index.json"), new List<INuGetResourceProvider>{new VulnerabilityInfoResourceProvider(vulnerabilityResults) })
+            };
+            GetVulnerabilityInfoResult? vulnerabilityData = await AuditUtility.GetAllVulnerabilityDataAsync(sourceRepositories, Mock.Of<SourceCacheContext>(), NullLogger.Instance, CancellationToken.None);
+            vulnerabilityData.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetAllVulnerabilityDataAsync_WithMultipleSources_AndSingleVulnerabilityInfo_ProvidesAllData()
+        {
+            List<IReadOnlyDictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>> knownVulnerabilities = new List<IReadOnlyDictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>>()
+            {
+                new Dictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>
+                {
+                    {
+                        "A",
+                        new PackageVulnerabilityInfo[] {
+                            new PackageVulnerabilityInfo(new Uri("https://vulnerability1"), PackageVulnerabilitySeverity.Low, VersionRange.Parse("[1.0.0,2.0.0)"))
+                        }
+                    }
+                }
+            };
+
+            string sourceWithVulnerabilityData = "https://contoso.com/vulnerability/v3/index.json";
+            Dictionary<string, GetVulnerabilityInfoResult> vulnerabilityResults = new()
+            {
+                { sourceWithVulnerabilityData, new GetVulnerabilityInfoResult(knownVulnerabilities, exceptions: null) }
+            };
+
+            var providers = new List<INuGetResourceProvider> { new VulnerabilityInfoResourceProvider(vulnerabilityResults) };
+            var sourceRepositories = new List<SourceRepository>
+            {
+                new SourceRepository(new PackageSource("https://contoso.com/v3/index.json"), providers),
+                new SourceRepository(new PackageSource(sourceWithVulnerabilityData), providers)
+            };
+
+            GetVulnerabilityInfoResult? vulnerabilityData = await AuditUtility.GetAllVulnerabilityDataAsync(sourceRepositories, Mock.Of<SourceCacheContext>(), NullLogger.Instance, CancellationToken.None);
+            vulnerabilityData.Should().NotBeNull();
+            vulnerabilityData!.Exceptions.Should().BeNull();
+            vulnerabilityData.KnownVulnerabilities.Should().HaveCount(1);
+            vulnerabilityData.KnownVulnerabilities.Single().Keys.Should().Contain("A");
+            vulnerabilityData.KnownVulnerabilities.Single().Values.Single().Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async Task GetAllVulnerabilityDataAsync_WithMultipleSources_MergesDataAndExceptions()
+        {
+            List<IReadOnlyDictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>> knownVulnerabilities = new List<IReadOnlyDictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>>()
+            {
+                new Dictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>
+                {
+                    {
+                        "A",
+                        new PackageVulnerabilityInfo[] {
+                            new PackageVulnerabilityInfo(new Uri("https://vulnerability1"), PackageVulnerabilitySeverity.Low, VersionRange.Parse("[1.0.0,2.0.0)"))
+                        }
+                    }
+                }
+            };
+
+            string sourceWithVulnerabilityData = "https://contoso.com/vulnerability/v3/index.json";
+            string sourceWithBadVulnerabilityData = "https://contoso.com/vulnerability/broken/v3/index.json";
+            string failureMessage = "Failed getting vulnerability data";
+            Dictionary<string, GetVulnerabilityInfoResult> vulnerabilityResults = new()
+            {
+                { sourceWithVulnerabilityData, new GetVulnerabilityInfoResult(knownVulnerabilities, exceptions: null) },
+                { sourceWithBadVulnerabilityData, new GetVulnerabilityInfoResult(null, exceptions: new AggregateException(new Exception(failureMessage))) }
+            };
+
+            var providers = new List<INuGetResourceProvider> { new VulnerabilityInfoResourceProvider(vulnerabilityResults) };
+            var sourceRepositories = new List<SourceRepository>
+            {
+                new SourceRepository(new PackageSource(sourceWithBadVulnerabilityData), providers),
+                new SourceRepository(new PackageSource(sourceWithVulnerabilityData), providers)
+            };
+
+            GetVulnerabilityInfoResult? vulnerabilityData = await AuditUtility.GetAllVulnerabilityDataAsync(sourceRepositories, Mock.Of<SourceCacheContext>(), NullLogger.Instance, CancellationToken.None);
+            vulnerabilityData.Should().NotBeNull();
+            vulnerabilityData!.KnownVulnerabilities.Should().HaveCount(1);
+            vulnerabilityData.KnownVulnerabilities.Single().Keys.Should().Contain("A");
+            vulnerabilityData.KnownVulnerabilities.Single().Values.Single().Should().HaveCount(1);
+            vulnerabilityData.Exceptions.Should().NotBeNull();
+            vulnerabilityData.Exceptions!.InnerException.Should().NotBeNull();
+            vulnerabilityData.Exceptions.InnerException!.Message.Should().Be(failureMessage);
+        }
+
+        [Fact]
+        public async Task GetAllVulnerabilityDataAsync_WithMultipleSources_MergesDataInOneResult()
+        {
+            List<IReadOnlyDictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>> firstKnownVulnerabilities = new List<IReadOnlyDictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>>()
+            {
+                new Dictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>
+                {
+                    {
+                        "A",
+                        new PackageVulnerabilityInfo[] {
+                            new PackageVulnerabilityInfo(new Uri("https://vulnerability1"), PackageVulnerabilitySeverity.Low, VersionRange.Parse("[1.0.0,2.0.0)"))
+                        }
+                    }
+                }
+            };
+            List<IReadOnlyDictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>> secondKnownVulnerabilities = new List<IReadOnlyDictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>>()
+            {
+                new Dictionary<string, IReadOnlyList<PackageVulnerabilityInfo>>
+                {
+                    {
+                        "B",
+                        new PackageVulnerabilityInfo[] {
+                            new PackageVulnerabilityInfo(new Uri("https://vulnerability2"), PackageVulnerabilitySeverity.Low, VersionRange.Parse("[2.0.0,3.0.0)"))
+                        }
+                    }
+                }
+            };
+
+            string sourceWithVulnerabilityData = "https://contoso.com/vulnerability/v3/index.json";
+            string secondarySourceWithVulnerability = "https://contoso.com/vulnerability/secondary/v3/index.json";
+            Dictionary<string, GetVulnerabilityInfoResult> vulnerabilityResults = new()
+            {
+                { sourceWithVulnerabilityData, new GetVulnerabilityInfoResult(firstKnownVulnerabilities, exceptions: null) },
+                { secondarySourceWithVulnerability, new GetVulnerabilityInfoResult(secondKnownVulnerabilities, exceptions: null) }
+            };
+
+            var providers = new List<INuGetResourceProvider> { new VulnerabilityInfoResourceProvider(vulnerabilityResults) };
+            var sourceRepositories = new List<SourceRepository>
+            {
+                new SourceRepository(new PackageSource(secondarySourceWithVulnerability), providers),
+                new SourceRepository(new PackageSource(sourceWithVulnerabilityData), providers)
+            };
+
+            GetVulnerabilityInfoResult? vulnerabilityData = await AuditUtility.GetAllVulnerabilityDataAsync(sourceRepositories, Mock.Of<SourceCacheContext>(), NullLogger.Instance, CancellationToken.None);
+            vulnerabilityData.Should().NotBeNull();
+            vulnerabilityData!.KnownVulnerabilities.Should().HaveCount(2);
+            vulnerabilityData.KnownVulnerabilities.First().Keys.Should().Contain("B");
+            vulnerabilityData.KnownVulnerabilities.First().Values.Single().Should().HaveCount(1);
+            vulnerabilityData.KnownVulnerabilities.Last().Keys.Should().Contain("A");
+            vulnerabilityData.KnownVulnerabilities.Last().Values.Single().Should().HaveCount(1);
         }
     }
 }
