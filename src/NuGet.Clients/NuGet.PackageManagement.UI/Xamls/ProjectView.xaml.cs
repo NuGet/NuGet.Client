d75faf3ed8752194a55015b93be3ecae8990580f
Martin Ruiz <martin.ruiz.mares@gmail.com>
2023-08-29 16:03:13 -0700

    Disable tab navigation in combobox items and project lists in PM UI

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/ProjectView.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/ProjectView.xaml.cs
@@ -85,7 +85,7 @@ namespace NuGet.PackageManagement.UI
             }
         }
 
-        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        private void Versions_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (PackageDetailControlModel.IsProjectPackageReference)
             {
@@ -140,11 +140,20 @@ namespace NuGet.PackageManagement.UI
             }
             else
             {
-                base.OnPreviewKeyDown(e);
+                switch (e.Key)
+                {
+                    case Key.Tab:
+                        _versions.IsDropDownOpen = false;
+                        e.Handled = true;
+                        break;
+                    default:
+                        base.OnPreviewKeyDown(e);
+                        break;
+                }
             }
         }
 
-        protected override void OnKeyUp(KeyEventArgs e)
+        private void Versions_KeyUp(object sender, KeyEventArgs e)
         {
             if (PackageDetailControlModel.IsProjectPackageReference)
             {
