1915b78b899e06c8bfe1e7783eee5bc1517c65ea
Nigusu Yenework <nyenework@microsoft.com>
2023-09-21 13:47:15 -0700

    add more test

--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/CommandLineParserTests.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/CommandLineParserTests.cs
@@ -3,6 +3,8 @@
 
 using System.Collections.Generic;
 using Xunit;
+using NuGet.Commands;
+using System;
 
 namespace NuGet.CommandLine.Test
 {
@@ -36,6 +38,24 @@ namespace NuGet.CommandLine.Test
 
             // Assert
             Assert.False(command.NoHttpCache);
+        }
+
+        [Fact]
+        public void ExtractOptions_WhenPassingOptionNoHttpCacheToHelpCommand_ExtractOptionsShouldRaseCommandException()
+        {
+            // Arrange
+            DeleteCommand command = new DeleteCommand();
+            string option = "-NoHttpCache";
+            List<string> args = new() { option };
+            CommandLineParser commandLineParser = new CommandLineParser(new CommandManager());
+            CommandException expectedException = new CommandException(LocalizedResourceManager.GetString("UnknownOptionError"), option);
+
+            // Act
+            Action action = () => commandLineParser.ExtractOptions(command, args.GetEnumerator());
+
+            // Assert
+            CommandException exception = Assert.Throws<CommandException>(action);
+            Assert.Equal(expectedException.Message, exception.Message);
         }
     }
 }
