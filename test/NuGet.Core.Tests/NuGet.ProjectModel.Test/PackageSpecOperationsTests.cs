e2aa2fb381c902094d33a961daa4a9fe15b22e12
Andy Zivkovic <Andy.Zivkovic@microsoft.com>
2023-09-21 08:56:23 +0200

    Fix new require member errors

--- test/NuGet.Core.Tests/NuGet.ProjectModel.Test/PackageSpecOperationsTests.cs
+++ test/NuGet.Core.Tests/NuGet.ProjectModel.Test/PackageSpecOperationsTests.cs
@@ -173,8 +173,10 @@ namespace NuGet.ProjectModel.Test
             {
                 FrameworkName = FrameworkConstants.CommonFrameworks.Net45
             };
-            var ld = new LibraryDependency();
-            ld.LibraryRange = new LibraryRange(packageId, new VersionRange(oldVersion), LibraryDependencyTarget.Package);
+            var ld = new LibraryDependency()
+            {
+                LibraryRange = new LibraryRange(packageId, new VersionRange(oldVersion), LibraryDependencyTarget.Package)
+            };
             var frameworkB = new TargetFrameworkInformation
             {
                 FrameworkName = FrameworkConstants.CommonFrameworks.NetStandard16,
@@ -219,8 +221,10 @@ namespace NuGet.ProjectModel.Test
             {
                 FrameworkName = FrameworkConstants.CommonFrameworks.Net45
             };
-            var ld = new LibraryDependency();
-            ld.LibraryRange = new LibraryRange(packageId, new VersionRange(oldVersion), LibraryDependencyTarget.Package);
+            var ld = new LibraryDependency()
+            {
+                LibraryRange = new LibraryRange(packageId, new VersionRange(oldVersion), LibraryDependencyTarget.Package)
+            };
             var frameworkB = new TargetFrameworkInformation
             {
                 FrameworkName = FrameworkConstants.CommonFrameworks.NetStandard16,
