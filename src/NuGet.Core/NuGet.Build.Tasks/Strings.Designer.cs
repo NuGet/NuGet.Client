4fafad6b2c6ab05b85efb62218fd8cd8e2214937
Nigusu Yenework <nyenework@microsoft.com>
2023-09-27 15:29:11 -0700

    modify log

--- src/NuGet.Core/NuGet.Build.Tasks/Strings.Designer.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/Strings.Designer.cs
@@ -188,7 +188,7 @@ namespace NuGet.Build.Tasks {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to RestoreNoCache is deprecated in favor of the appropriately named RestoreNoHttpCache..
+        ///   Looks up a localized string similar to RestoreNoCache is deprecated and has been renamed to NoHttpCache. Please use RestoreNoHttpCache instead..
         /// </summary>
         public static string Log_RestoreNoCacheInformation {
             get {
