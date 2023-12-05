d1e4d8ac47d4342f6e0ebe732d0ad432a6021ffc
Jonatan Gonzalez (HE/HIM) (from Dev Box) <jongonza@microsoft.com>
2023-12-05 12:52:17 -0800

    Moved files over and addressed some PR comments

---
+++ src/NuGet.Core/NuGet.ProjectModel/Utf8JsonReaderExtensions.cs
@@ -0,0 +1,231 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Text.Json;
+
+namespace NuGet.ProjectModel
+{
+    internal static class Utf8JsonReaderExtensions
+    {
+        //private static readonly char[] DelimitedStringDelimiters = new char[] { ' ', ',' };
+        //internal static readonly Utf8JsonStreamLockFileConverter LockFileConverter = new Utf8JsonStreamLockFileConverter();
+        //internal static readonly Utf8JsonStreamLockFileItemConverter<LockFileItem> LockFileItemConverter = new Utf8JsonStreamLockFileItemConverter<LockFileItem>();
+        //internal static readonly Utf8JsonStreamLockFileItemConverter<LockFileContentFile> LockFileContentFileConverter = new Utf8JsonStreamLockFileItemConverter<LockFileContentFile>();
+        //internal static readonly Utf8JsonStreamLockFileItemConverter<LockFileRuntimeTarget> LockFileRuntimeTargetConverter = new Utf8JsonStreamLockFileItemConverter<LockFileRuntimeTarget>();
+        //internal static readonly Utf8JsonStreamLockFileTargetLibraryConverter LockFileTargetLibraryConverter = new Utf8JsonStreamLockFileTargetLibraryConverter();
+        //internal static readonly Utf8JsonStreamLockFileLibraryConverter LockFileLibraryConverter = new Utf8JsonStreamLockFileLibraryConverter();
+        //internal static readonly Utf8JsonStreamLockFileTargetConverter LockFileTargetConverter = new Utf8JsonStreamLockFileTargetConverter();
+        //internal static readonly Utf8JsonStreamProjectFileDependencyGroupConverter ProjectFileDepencencyGroupConverter = new Utf8JsonStreamProjectFileDependencyGroupConverter();
+        //internal static readonly Utf8JsonStreamAssetsLogMessageConverter AssetsLogMessageConverter = new Utf8JsonStreamAssetsLogMessageConverter();
+
+        //internal static IReadOnlyList<string> ReadDelimitedString(this ref Utf8JsonReader reader)
+        //{
+        //    if (ReadNextToken(ref reader))
+        //    {
+        //        switch (reader.TokenType)
+        //        {
+        //            case JsonTokenType.String:
+        //                var value = reader.GetString();
+
+        //                return value.Split(DelimitedStringDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        //            default:
+        //                var invalidCastException = new InvalidCastException();
+        //                throw new JsonException(invalidCastException.Message, invalidCastException);
+        //        }
+        //    }
+
+        //    return null;
+        //}
+
+        //internal static bool ReadNextToken(this ref Utf8JsonReader reader)
+        //{
+        //    bool wasRead;
+
+        //    while ((wasRead = reader.Read()) && reader.TokenType == JsonTokenType.Comment)
+        //    {
+        //    }
+
+        //    return wasRead;
+        //}
+
+        //internal static bool ReadNextTokenAsBoolOrFalse(this ref Utf8JsonReader reader)
+        //{
+        //    if (reader.ReadNextToken() && (reader.TokenType == JsonTokenType.False || reader.TokenType == JsonTokenType.True))
+        //    {
+        //        return reader.GetBoolean();
+        //    }
+        //    return false;
+        //}
+
+        //internal static string ReadNextTokenAsString(this ref Utf8JsonReader reader)
+        //{
+        //    if (ReadNextToken(ref reader))
+        //    {
+        //        return ReadTokenAsString(ref reader);
+        //    }
+
+        //    return null;
+        //}
+
+        //internal static IList<T> ReadObjectAsList<T>(this ref Utf8JsonReader reader, JsonSerializerOptions options)
+        //{
+        //    if (reader.TokenType != JsonTokenType.StartObject)
+        //    {
+        //        return new List<T>(0);
+
+        //    }
+        //    //We use JsonObjects for the arrays so we advance to the first property in the object which is the name/ver of the first library
+        //    reader.ReadNextToken();
+
+        //    if (reader.TokenType == JsonTokenType.EndObject)
+        //    {
+        //        return new List<T>(0);
+        //    }
+
+        //    var objectConverter = (JsonConverter<T>)options.GetConverter(typeof(T));
+        //    var listObjects = new List<T>();
+        //    do
+        //    {
+        //        listObjects.Add(objectConverter.Read(ref reader, typeof(T), options));
+        //        //At this point we're looking at the EndObject token for the object, need to advance.
+        //        reader.ReadNextToken();
+        //    }
+        //    while (reader.TokenType != JsonTokenType.EndObject);
+        //    return listObjects;
+        //}
+
+        //internal static List<string> ReadNextStringArrayAsList(this ref Utf8JsonReader reader, List<string> strings = null)
+        //{
+        //    ReadNextToken(ref reader);
+        //    return ReadStringArrayAsList(ref reader, strings);
+        //}
+
+        //internal static List<string> ReadStringArrayAsList(this ref Utf8JsonReader reader, List<string> strings = null)
+        //{
+        //    if (reader.TokenType == JsonTokenType.StartArray)
+        //    {
+        //        while (ReadNextToken(ref reader) && reader.TokenType != JsonTokenType.EndArray)
+        //        {
+        //            string value = ReadTokenAsString(ref reader);
+
+        //            strings = strings ?? new List<string>();
+
+        //            strings.Add(value);
+        //        }
+        //    }
+
+        //    return strings;
+        //}
+
+
+        //internal static IList<string> ReadStringArrayAsIList(this ref Utf8JsonReader reader, IList<string> strings = null)
+        //{
+        //    if (reader.TokenType == JsonTokenType.StartArray)
+        //    {
+        //        while (ReadNextToken(ref reader) && reader.TokenType != JsonTokenType.EndArray)
+        //        {
+        //            string value = ReadTokenAsString(ref reader);
+
+        //            strings = strings ?? new List<string>();
+
+        //            strings.Add(value);
+        //        }
+        //    }
+
+        //    return strings;
+        //}
+
+        //internal static void ReadArrayOfObjects<T1, T2>(this ref Utf8JsonReader reader, JsonSerializerOptions options, IList<T2> objectList) where T1 : T2
+        //{
+        //    if (objectList is null)
+        //    {
+        //        return;
+        //    }
+
+        //    var type = typeof(T1);
+        //    var objectConverter = (JsonConverter<T1>)options.GetConverter(type);
+
+        //    if (ReadNextToken(ref reader) && reader.TokenType == JsonTokenType.StartArray)
+        //    {
+        //        while (ReadNextToken(ref reader) && reader.TokenType != JsonTokenType.EndArray)
+        //        {
+        //            var convertedObject = objectConverter.Read(ref reader, type, options);
+        //            if (convertedObject != null)
+        //            {
+        //                objectList.Add(convertedObject);
+        //            }
+        //        }
+        //    }
+
+        //}
+
+        //internal static IReadOnlyList<string> ReadNextStringOrArrayOfStringsAsReadOnlyList(this ref Utf8JsonReader reader)
+        //{
+        //    if (ReadNextToken(ref reader))
+        //    {
+        //        switch (reader.TokenType)
+        //        {
+        //            case JsonTokenType.String:
+        //                return new[] { (string)reader.GetString() };
+
+        //            case JsonTokenType.StartArray:
+        //                return ReadStringArrayAsReadOnlyListFromArrayStart(ref reader);
+
+        //            case JsonTokenType.StartObject:
+        //                return null;
+        //        }
+        //    }
+
+        //    return null;
+        //}
+
+        //internal static IReadOnlyList<string> ReadStringArrayAsReadOnlyListFromArrayStart(this ref Utf8JsonReader reader)
+        //{
+        //    List<string> strings = null;
+
+        //    while (ReadNextToken(ref reader) && reader.TokenType != JsonTokenType.EndArray)
+        //    {
+        //        string value = ReadTokenAsString(ref reader);
+
+        //        strings = strings ?? new List<string>();
+
+        //        strings.Add(value);
+        //    }
+
+        //    return (IReadOnlyList<string>)strings ?? Array.Empty<string>();
+        //}
+
+        internal static string ReadTokenAsString(this ref Utf8JsonReader reader)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.True:
+                case JsonTokenType.False:
+                    return reader.GetBoolean().ToString();
+                case JsonTokenType.Number:
+                    if (reader.TryGetInt16(out short shortValue))
+                    {
+                        return shortValue.ToString();
+                    }
+                    if (reader.TryGetInt32(out int intValue))
+                    {
+                        return intValue.ToString();
+                    }
+                    else if (reader.TryGetInt64(out long longValue))
+                    {
+                        return longValue.ToString();
+                    }
+                    return reader.GetDouble().ToString();
+                case JsonTokenType.String:
+                    return reader.GetString();
+                case JsonTokenType.None:
+                case JsonTokenType.Null:
+                    return null;
+                default:
+                    throw new InvalidCastException();
+            }
+        }
+    }
+}
---
+++ src/NuGet.Core/NuGet.ProjectModel/Utf8JsonStreamReader.cs
@@ -0,0 +1,338 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace NuGet.ProjectModel
+{
+    /// <summary>
+    /// This struct is used to read over a memeory stream in parts, in order to avoid reading the entire stream into memory.
+    /// It functions as a wrapper around <see cref="Utf8JsonStreamReader"/>, while maintaining a stream and a buffer to read from.
+    /// must call <see cref="CompleteRead"/> to return the buffer to the pool when completed.
+    /// </summary>
+    internal ref struct Utf8JsonStreamReader
+    {
+        private static readonly char[] DelimitedStringDelimiters = new char[] { ' ', ',' };
+
+        // The UT8 BOM characters which may appear at the start of a JSON file.
+        private ReadOnlySpan<byte> _utf8Bom = new byte[] { 0xEF, 0xBB, 0xBF };
+        private Utf8JsonReader _reader;
+        // The buffer is used to read from the stream in chunks.
+        private byte[] _buffer;
+        // The stream is the source of the JSON data.
+        private Stream _stream;
+        private bool _complete;
+
+        internal Utf8JsonStreamReader(Stream stream)
+        {
+            if (stream is null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            _complete = false;
+            _stream = stream;
+            _buffer = ArrayPool<byte>.Shared.Rent(1024);
+            _stream.Read(_buffer, 0, 3);
+            var offset = 0;
+            if (!_utf8Bom.SequenceEqual(_buffer.AsSpan(0, 3)))
+            {
+                offset = 3;
+            }
+            _stream.Read(_buffer, offset, _buffer.Length - offset);
+            _reader = new Utf8JsonReader(_buffer, isFinalBlock: false, state: new JsonReaderState(new JsonReaderOptions
+            {
+                AllowTrailingCommas = true,
+                CommentHandling = JsonCommentHandling.Skip,
+            }));
+            _reader.Read();
+        }
+
+        internal bool IsFinalBlock => _reader.IsFinalBlock;
+
+        internal JsonTokenType TokenType => _reader.TokenType;
+
+        internal int BufferSize()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+            return _buffer.Length;
+        }
+
+        internal bool ValueTextEquals(ReadOnlySpan<byte> utf8Text) => _reader.ValueTextEquals(utf8Text);
+
+        internal bool ValueTextEquals(string text) => _reader.ValueTextEquals(text);
+
+        internal bool TryGetInt32(out int value) => _reader.TryGetInt32(out value);
+
+        internal string GetString() => _reader.GetString();
+
+        internal bool GetBoolean() => _reader.GetBoolean();
+
+        internal int GetInt32() => _reader.GetInt32();
+
+        internal void CompleteRead()
+        {
+            _complete = true;
+            ArrayPool<byte>.Shared.Return(_buffer);
+        }
+
+        internal bool Read()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            bool wasRead;
+            while (!(wasRead = _reader.Read()) && !_reader.IsFinalBlock)
+            {
+                GetMoreBytesFromStream();
+            }
+            return wasRead;
+        }
+
+        internal bool TrySkip()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            bool wasSkipped;
+            while (!(wasSkipped = _reader.TrySkip()) && !_reader.IsFinalBlock)
+            {
+                GetMoreBytesFromStream();
+            }
+            return wasSkipped;
+        }
+
+        //internal IList<T> ReadObjectAsList<T>(Utf8JsonStreamReaderConverter<T> streamReaderConverter)
+        //{
+        //    if (TokenType != JsonTokenType.StartObject)
+        //    {
+        //        return Array.Empty<T>();
+
+        //    }
+        //    //We use JsonObjects for the arrays so we advance to the first property in the object which is the name/ver of the first library
+        //    Read();
+
+        //    if (TokenType == JsonTokenType.EndObject)
+        //    {
+        //        return Array.Empty<T>();
+        //    }
+
+        //    var listObjects = new List<T>();
+        //    do
+        //    {
+        //        listObjects.Add(streamReaderConverter.Read(ref this));
+        //        //At this point we're looking at the EndObject token for the object, need to advance.
+        //        Read();
+        //    }
+        //    while (TokenType != JsonTokenType.EndObject);
+        //    return listObjects;
+        //}
+
+        //internal void ReadArrayOfObjects<T1, T2>(IList<T2> objectList, Utf8JsonStreamReaderConverter<T1> streamReaderConverter) where T1 : T2
+        //{
+        //    if (objectList is null)
+        //    {
+        //        return;
+        //    }
+
+        //    if (Read() && TokenType == JsonTokenType.StartArray)
+        //    {
+        //        while (Read() && TokenType != JsonTokenType.EndArray)
+        //        {
+        //            var convertedObject = streamReaderConverter.Read(ref this);
+        //            if (convertedObject != null)
+        //            {
+        //                objectList.Add(convertedObject);
+        //            }
+        //        }
+        //    }
+        //}
+
+        internal string ReadNextTokenAsString()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            if (Read())
+            {
+                return _reader.ReadTokenAsString();
+            }
+
+            return null;
+        }
+
+        internal string GetCurrentBufferAsString()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            return Encoding.UTF8.GetString(_buffer);
+        }
+
+        internal IList<string> ReadStringArrayAsIList(IList<string> strings = null)
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            if (TokenType == JsonTokenType.StartArray)
+            {
+                while (Read() && TokenType != JsonTokenType.EndArray)
+                {
+                    string value = _reader.ReadTokenAsString();
+
+                    strings = strings ?? new List<string>();
+
+                    strings.Add(value);
+                }
+            }
+
+            return strings;
+        }
+
+        internal List<string> ReadStringArrayAsList(List<string> strings = null)
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            if (TokenType == JsonTokenType.StartArray)
+            {
+                while (Read() && TokenType != JsonTokenType.EndArray)
+                {
+                    string value = _reader.ReadTokenAsString();
+
+                    strings = strings ?? new List<string>();
+
+                    strings.Add(value);
+                }
+            }
+
+            return strings;
+        }
+
+        internal IReadOnlyList<string> ReadDelimitedString()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            if (Read())
+            {
+                switch (TokenType)
+                {
+                    case JsonTokenType.String:
+                        var value = GetString();
+
+                        return value.Split(DelimitedStringDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                    default:
+                        var invalidCastException = new InvalidCastException();
+                        throw new JsonException(invalidCastException.Message, invalidCastException);
+                }
+            }
+
+            return null;
+        }
+
+        internal bool ReadNextTokenAsBoolOrFalse()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            if (Read() && (TokenType == JsonTokenType.False || TokenType == JsonTokenType.True))
+            {
+                return GetBoolean();
+            }
+            return false;
+        }
+
+        internal IReadOnlyList<string> ReadNextStringOrArrayOfStringsAsReadOnlyList()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            if (Read())
+            {
+                switch (_reader.TokenType)
+                {
+                    case JsonTokenType.String:
+                        return new[] { (string)_reader.GetString() };
+
+                    case JsonTokenType.StartArray:
+                        return ReadStringArrayAsReadOnlyListFromArrayStart();
+
+                    case JsonTokenType.StartObject:
+                        return null;
+                }
+            }
+
+            return null;
+        }
+
+        internal IReadOnlyList<string> ReadStringArrayAsReadOnlyListFromArrayStart()
+        {
+            if (_complete)
+            {
+                throw new InvalidOperationException("Cannot read from completed Utf8JsonStreamReader");
+            }
+
+            List<string> strings = null;
+
+            while (Read() && _reader.TokenType != JsonTokenType.EndArray)
+            {
+                string value = _reader.ReadTokenAsString();
+
+                strings = strings ?? new List<string>();
+
+                strings.Add(value);
+            }
+
+            return (IReadOnlyList<string>)strings ?? Array.Empty<string>();
+        }
+
+        private void GetMoreBytesFromStream()
+        {
+            if (_reader.BytesConsumed < _buffer.Length)
+            {
+                ReadOnlySpan<byte> leftover = _buffer.AsSpan((int)_reader.BytesConsumed);
+                if (leftover.Length == _buffer.Length)
+                {
+                    ArrayPool<byte>.Shared.Return(_buffer);
+                    _buffer = ArrayPool<byte>.Shared.Rent(_buffer.Length * 2);
+                }
+                leftover.CopyTo(_buffer);
+                _stream.Read(_buffer, leftover.Length, _buffer.Length - leftover.Length);
+            }
+            else
+            {
+                _stream.Read(_buffer, 0, _buffer.Length);
+            }
+            _reader = new Utf8JsonReader(_buffer, isFinalBlock: _stream.Length == _stream.Position, _reader.CurrentState);
+        }
+    }
+}
