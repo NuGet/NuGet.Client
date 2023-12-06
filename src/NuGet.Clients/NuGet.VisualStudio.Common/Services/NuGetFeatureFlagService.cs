a31e442c6b9d4a208a4a86f1066459a7ab551a1b
Jonatan Gonzalez (HE/HIM) (from Dev Box) <jongonza@microsoft.com>
2023-12-06 11:35:28 -0800

    Add another check to see if value is in cache

--- src/NuGet.Clients/NuGet.VisualStudio.Common/Services/NuGetFeatureFlagService.cs
+++ src/NuGet.Clients/NuGet.VisualStudio.Common/Services/NuGetFeatureFlagService.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Threading.Tasks;
 using Microsoft.Internal.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Shell;
 using NuGet.Common;
 
@@ -44,8 +45,11 @@ namespace NuGet.VisualStudio
             {
                 var featureFlagService = await _ivsFeatureFlags.GetValueAsync();
                 await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                featureEnabled = featureFlagService.IsFeatureEnabled(featureFlag.Name, defaultValue: featureFlag.DefaultState);
-                _featureFlagCache[featureFlag.Name] = featureEnabled;
+                if (!_featureFlagCache.TryGetValue(featureFlag.Name, out featureEnabled))
+                {
+                    featureEnabled = featureFlagService.IsFeatureEnabled(featureFlag.Name, defaultValue: featureFlag.DefaultState);
+                    _featureFlagCache.TryAdd(featureFlag.Name, featureEnabled);
+                }
             }
             return !isFeatureForcedDisabled && (isFeatureForcedEnabled || featureEnabled);
         }
