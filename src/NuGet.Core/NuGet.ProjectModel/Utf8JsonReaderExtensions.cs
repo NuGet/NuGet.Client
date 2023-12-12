17319520bb7878dabc757dcea641e66d87552970
Jonatan Gonzalez <jgonz120@gmail.com>
2023-12-12 10:30:55 -0800

    Create class for reading Json files in chunks (#5530)
    * Moved files over and addressed some PR comments
    
    * added comment
    
    * switched to true and false strings
    
    * Added ctr to specify buffer for testing purposes.
    
    * remove commented code
    
    * switch to use Utf8 preamble for BOM
    
    * Create method for checking complete
    
    * combined code for ReadStringArray
    
    * Updated buffer size to match STJ's default buffer size
    
    * Switch Utf8JsonStreamReader to be disposable.
    
    * Switch to read the value for numbers into a string directly
    
    * revert back to using private var for utf8Bom
    
    * Remove ReadStringArrayAsList
    
    * Avoid referencing buffer after  returning
    
    * Actually avoid referencing _buffer after returning
    
    * Update how buffers are fed into Utf8JsonReader to avoid feeding extra empty data.
    
    * remove extra line
    
    * Reverted back to using try get int for ReadTokenAsString
    
    * Update src/NuGet.Core/NuGet.ProjectModel/Utf8JsonStreamReader.cs
    
    Co-authored-by: Andy Zivkovic <zivkan@users.noreply.github.com>
    
    * Remove ValueTextEquals taking in string
    
    * Switched to Skip instead of TrySkip
    
    * Update src/NuGet.Core/NuGet.ProjectModel/Utf8JsonStreamReader.cs
    
    Co-authored-by: Andy Zivkovic <zivkan@users.noreply.github.com>
    
    * Added some unit tests
    
    * fix Bom
    
    * Switched to using Moq
    
    * Update src/NuGet.Core/NuGet.ProjectModel/Utf8JsonStreamReader.cs
    
    Co-authored-by: Andy Zivkovic <zivkan@users.noreply.github.com>
    
    * loop through stream when reading to ensure reading full bytes or to the end
    
    * update signature comment
    
    * Switched stream back to field and supress warning
    
    ---------
    
    Co-authored-by: Andy Zivkovic <zivkan@users.noreply.github.com>

---
+++ src/NuGet.Core/NuGet.ProjectModel/Utf8JsonReaderExtensions.cs
@@ -0,0 +1,40 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Text.Json;
+
+namespace NuGet.ProjectModel
+{
+    internal static class Utf8JsonReaderExtensions
+    {
+        internal static string ReadTokenAsString(this ref Utf8JsonReader reader)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.True:
+                    return bool.TrueString;
+                case JsonTokenType.False:
+                    return bool.FalseString;
+                case JsonTokenType.Number:
+                    return reader.ReadNumberAsString();
+                case JsonTokenType.String:
+                    return reader.GetString();
+                case JsonTokenType.None:
+                case JsonTokenType.Null:
+                    return null;
+                default:
+                    throw new InvalidCastException();
+            }
+        }
+
+        private static string ReadNumberAsString(this ref Utf8JsonReader reader)
+        {
+            if (reader.TryGetInt64(out long value))
+            {
+                return value.ToString();
+            }
+            return reader.GetDouble().ToString();
+        }
+    }
+}
