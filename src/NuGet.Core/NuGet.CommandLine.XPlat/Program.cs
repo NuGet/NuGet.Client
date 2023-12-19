ef0453bc362d0407096abecc23609d2aab24a0fd
Martin Ruiz <martin.ruiz.mares@gmail.com>
2023-12-19 12:11:38 -0800

    cleaning

--- src/NuGet.Core/NuGet.CommandLine.XPlat/Program.cs
+++ src/NuGet.Core/NuGet.CommandLine.XPlat/Program.cs
@@ -39,7 +39,7 @@ namespace NuGet.CommandLine.XPlat
         {
 #if DEBUG
             // Uncomment the following when debugging. Also uncomment the PackageReference for Microsoft.Build.Locator.
-            try
+            /*try
             {
                 // .NET JIT compiles one method at a time. If this method calls `MSBuildLocator` directly, the
                 // try block is never entered if Microsoft.Build.Locator.dll can't be found. So, run it in a
@@ -50,7 +50,7 @@ namespace NuGet.CommandLine.XPlat
             {
                 // MSBuildLocator is used only to enable Visual Studio debugging.
                 // It's not needed when using a patched dotnet sdk, so it doesn't matter if it fails.
-            }
+            }*/
 
             var debugNuGetXPlat = Environment.GetEnvironmentVariable("DEBUG_NUGET_XPLAT");
 
