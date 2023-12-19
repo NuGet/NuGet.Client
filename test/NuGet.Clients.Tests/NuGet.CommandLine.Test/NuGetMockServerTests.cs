cabdb9886f3bc99c7a342ccc1661d393b14a0d1d
Jeff Kluge <jeffkl@microsoft.com>
2023-12-19 11:23:40 -0800

    Try to fix MockServer_RestorePCVerifySessionId (#5545)

--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetMockServerTests.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/NuGetMockServerTests.cs
@@ -2,11 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Linq;
 using FluentAssertions;
 using NuGet.Frameworks;
+using NuGet.ProjectModel;
 using NuGet.Protocol;
 using NuGet.Test.Utility;
 using Xunit;
@@ -15,63 +16,43 @@ namespace NuGet.CommandLine.Test
 {
     public class NuGetMockServerTests
     {
-        [Fact]
-        public void MockServer_RestorePRVerifySessionId()
+        [Theory]
+        [InlineData(ProjectStyle.PackageReference)]
+        [InlineData(ProjectStyle.PackagesConfig)]
+        public void MockServer_VerifySessionId(ProjectStyle projectStyle)
         {
             // Arrange
             using (var pathContext = new SimpleTestPathContext())
             {
-                var nugetexe = Util.GetNuGetExePath();
-
                 var packageA = new FileInfo(Util.CreateTestPackage("a", "1.0.0", pathContext.PackageSource));
                 var packageB = new FileInfo(Util.CreateTestPackage("b", "1.0.0", pathContext.PackageSource));
 
-                var project = SimpleTestProjectContext.CreateNETCore("proj", pathContext.SolutionRoot, NuGetFramework.Parse("net46"));
+                string inputPath = null;
 
-                project.AddPackageToAllFrameworks(new SimpleTestPackageContext("a", "1.0.0"));
-                project.AddPackageToAllFrameworks(new SimpleTestPackageContext("b", "1.0.0"));
-
-                project.Save();
-
-                var ids = new List<string>();
-
-                using (var server = Util.CreateMockServer(new[] { packageA, packageB }))
+                switch (projectStyle)
                 {
-                    server.RequestObserver = context =>
-                    {
-                        ids.Add(context.Request.Headers.Get(ProtocolConstants.SessionId));
-                    };
-
-                    server.Start();
-
-                    var result = Util.Restore(pathContext, project.ProjectPath, 0, "-Source", server.Uri + "nuget");
-
-                    result.Success.Should().BeTrue();
-
-                    ids.Distinct().Count().Should().Be(1, "all requests should be in the same session");
-                    ids.All(s => !string.IsNullOrEmpty(s) && Guid.TryParse(s, out var r)).Should().BeTrue("the values should guids");
-                }
-            }
-        }
-
-        [Fact]
-        public void MockServer_RestorePCVerifySessionId()
-        {
-            // Arrange
-            using (var pathContext = new SimpleTestPathContext())
-            {
-                var nugetexe = Util.GetNuGetExePath();
-
-                var packageA = new FileInfo(Util.CreateTestPackage("a", "1.0.0", pathContext.PackageSource));
-                var packageB = new FileInfo(Util.CreateTestPackage("b", "1.0.0", pathContext.PackageSource));
-
-                Util.CreateFile(pathContext.SolutionRoot, "packages.config",
+                    case ProjectStyle.PackagesConfig:
+                        inputPath = Util.CreateFile(
+                            pathContext.SolutionRoot,
+                            "packages.config",
 @"<packages>
   <package id=""a"" version=""1.0.0"" targetFramework=""net45"" />
   <package id=""b"" version=""1.0.0"" targetFramework=""net45"" />
 </packages>");
+                        break;
 
-                var ids = new List<string>();
+                    case ProjectStyle.PackageReference:
+                        var project = SimpleTestProjectContext.CreateNETCore("proj", pathContext.SolutionRoot, NuGetFramework.Parse("net46"));
+
+                        project.AddPackageToAllFrameworks(new SimpleTestPackageContext("a", "1.0.0"), new SimpleTestPackageContext("b", "1.0.0"));
+
+                        project.Save();
+
+                        inputPath = project.ProjectPath;
+                        break;
+                }
+
+                var ids = new ConcurrentBag<string>();
 
                 using (var server = Util.CreateMockServer(new[] { packageA, packageB }))
                 {
@@ -82,13 +63,11 @@ namespace NuGet.CommandLine.Test
 
                     server.Start();
 
-                    var result = Util.Restore(pathContext, Path.Combine(pathContext.SolutionRoot, "packages.config"), 0, "-Source", server.Uri + "nuget");
+                    var result = Util.Restore(pathContext, inputPath, 0, "-Source", server.Uri + "nuget");
 
                     result.Success.Should().BeTrue();
-
-                    // Not all ids will be in the same session, this is due to how cache contexts are shared for packages.config
-                    ids.Distinct().Count().Should().BeLessThan(ids.Count(), "Verify some requests share the same cache context and session id.");
                     ids.All(s => !string.IsNullOrEmpty(s) && Guid.TryParse(s, out var r)).Should().BeTrue("the values should guids");
+                    ids.GroupBy(i => i).Any(i => i.Count() > 1).Should().BeTrue("At least one session should have been reused");
                 }
             }
         }
