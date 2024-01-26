3acce053933b3d0c4e462535ae58ece4ed8caf46
Andy Zivkovic <Andy.Zivkovic@microsoft.com>
2024-01-26 02:34:35 +0100

    Improve VS project API telemetry to include property name

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Telemetry/IVsProjectBuildPropertiesTelemetry.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Telemetry/IVsProjectBuildPropertiesTelemetry.cs
@@ -1,11 +1,13 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#nullable enable
+
 namespace NuGet.PackageManagement.VisualStudio.Telemetry
 {
     internal interface IVsProjectBuildPropertiesTelemetry
     {
-        void OnPropertyStorageUsed(string[] projectTypeGuids);
-        void OnDteUsed(string[] projectTypeGuids);
+        void OnPropertyStorageUsed(string propertyName, string[] projectTypeGuids);
+        void OnDteUsed(string propertyName, string[] projectTypeGuids);
     }
 }
