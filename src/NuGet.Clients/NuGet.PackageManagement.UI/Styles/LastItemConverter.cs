be9507c929accce043989ceaecb1e3fe280c3d5b
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2024-01-30 11:25:31 -0800

    Create LastItemConverter for comma

---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Styles/LastItemConverter.cs
@@ -0,0 +1,33 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NuGet.PackageManagement.UI
+{
+    internal class LastItemToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Length == 2
+                && values[0] is int currentIndex
+                && values[1] is int length)
+            {
+                int lastIndex = length - 1;
+                return Equals(currentIndex, lastIndex) ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            Debug.Fail("Not Implemented");
+            return null;
+        }
+    }
+}
