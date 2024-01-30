e520f969f09411cdbf024349cbae3a1f60c4c73c
Eric Arndt <60519722+Erarndt@users.noreply.github.com>
2024-01-30 11:55:29 -0800

    Avoid intermediate string allocation caused by StringBuilder Append(int) (#5600)

--- src/NuGet.Core/NuGet.Versioning/VersionFormatter.cs
+++ src/NuGet.Core/NuGet.Versioning/VersionFormatter.cs
@@ -86,16 +86,16 @@ namespace NuGet.Versioning
                     builder.Append(version.Metadata);
                     return;
                 case 'x':
-                    builder.Append(version.Major);
+                    AppendInt(builder, version.Major);
                     return;
                 case 'y':
-                    builder.Append(version.Minor);
+                    AppendInt(builder, version.Minor);
                     return;
                 case 'z':
-                    builder.Append(version.Patch);
+                    AppendInt(builder, version.Patch);
                     return;
                 case 'r':
-                    builder.Append(version is NuGetVersion nuGetVersion && nuGetVersion.IsLegacyVersion ? nuGetVersion.Version.Revision : 0);
+                    AppendInt(builder, version is NuGetVersion nuGetVersion && nuGetVersion.IsLegacyVersion ? nuGetVersion.Version.Revision : 0);
                     return;
 
                 default:
@@ -135,16 +135,79 @@ namespace NuGet.Versioning
 
         private static void AppendVersion(StringBuilder builder, SemanticVersion version)
         {
-            builder.Append(version.Major);
+            AppendInt(builder, version.Major);
             builder.Append('.');
-            builder.Append(version.Minor);
+            AppendInt(builder, version.Minor);
             builder.Append('.');
-            builder.Append(version.Patch);
+            AppendInt(builder, version.Patch);
 
             if (version is NuGetVersion nuGetVersion && nuGetVersion.IsLegacyVersion)
             {
                 builder.Append('.');
-                builder.Append(nuGetVersion.Version.Revision);
+                AppendInt(builder, nuGetVersion.Version.Revision);
+            }
+        }
+
+        /// <summary>
+        /// Helper function to append an <see cref="int"/> to a <see cref="StringBuilder"/>. Calling
+        /// <see cref="StringBuilder.Append(int)"/> directly causes an allocation by first converting the
+        /// <see cref="int"/> to a string and then appending that result:
+        /// <code>
+        /// public StringBuilder Append(int value)
+        /// {
+        ///     return Append(value.ToString(CultureInfo.CurrentCulture));
+        /// }
+        /// </code>
+        ///
+        /// Note that this uses the current culture to do the conversion while <see cref="AppendInt(StringBuilder, int)"/> does
+        /// not do any cultural sensitive conversion.
+        /// </summary>
+        /// <param name="sb">The <see cref="StringBuilder"/> to append to.</param>
+        /// <param name="value">The <see cref="int"/> to append.</param>
+        private static void AppendInt(StringBuilder sb, int value)
+        {
+            if (value == 0)
+            {
+                sb.Append('0');
+                return;
+            }
+
+            // special case min value since it'll overflow if we negate it
+            if (value == int.MinValue)
+            {
+                sb.Append("-2147483648");
+                return;
+            }
+
+            // do all math with positive integers
+            if (value < 0)
+            {
+                sb.Append('-');
+                value = -value;
+            }
+
+            // upper range of int is 1 billion, so we start dividing by that to get the digit at that position
+            int divisor = 1_000_000_000;
+
+            // remember when we found our first digit so we can keep adding intermediate zeroes
+            bool digitFound = false;
+            while (divisor > 0)
+            {
+                if (digitFound || value >= divisor)
+                {
+                    digitFound = true;
+                    int digit = value / divisor;
+                    value -= digit * divisor;
+
+                    // convert the digit to char by adding the value to '0'.
+                    // '0' + 0 = 48 + 0 = 48 = '0'
+                    // '0' + 1 = 48 + 1 = 49 = '1'
+                    // '0' + 2 = 48 + 2 = 50 = '2'
+                    // etc...
+                    sb.Append((char)('0' + digit));
+                }
+
+                divisor /= 10;
             }
         }
     }
