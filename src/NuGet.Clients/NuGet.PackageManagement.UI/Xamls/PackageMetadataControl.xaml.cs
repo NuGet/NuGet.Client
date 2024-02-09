c4ed6b72ef8baf09c145b037900c4ed02045b1b2
Jonatan Gonzalez (HE/HIM) <jongonza@microsoft.com>
2024-02-08 16:54:14 -0800

    Added very ugly way of getting readme from disk

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageMetadataControl.xaml.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageMetadataControl.xaml.cs
@@ -25,36 +25,9 @@ namespace NuGet.PackageManagement.UI
             DataContextChanged += PackageMetadataControl_DataContextChanged;
         }
 
-        private void ViewLicense_Click(object sender, RoutedEventArgs e)
-        {
-            if (DataContext is DetailedPackageMetadata metadata)
-            {
-                var window = new LicenseFileWindow()
-                {
-                    DataContext = new LicenseFileData
-                    {
-                        LicenseHeader = string.Format(CultureInfo.CurrentCulture, UI.Resources.WindowTitle_LicenseFileWindow, metadata.Id),
-                        LicenseText = new FlowDocument(new Paragraph(new Run(UI.Resources.LicenseFile_Loading)))
-                    }
-                };
-
-                NuGetUIThreadHelper.JoinableTaskFactory.RunAsync(async () =>
-                {
-                    string content = await PackageLicenseUtilities.GetEmbeddedLicenseAsync(new Packaging.Core.PackageIdentity(metadata.Id, metadata.Version), CancellationToken.None);
-
-                    var flowDoc = new FlowDocument();
-                    flowDoc.Blocks.AddRange(PackageLicenseUtilities.GenerateParagraphs(content));
-                    await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                    (window.DataContext as LicenseFileData).LicenseText = flowDoc;
-                }).PostOnFailure(nameof(PackageMetadataControl), nameof(ViewLicense_Click));
-
-                window.ShowModal();
-            }
-        }
-
         private void PackageMetadataControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if (DataContext is DetailedPackageMetadata)
+            if (DataContext is DetailControlModel)
             {
                 Visibility = Visibility.Visible;
             }
@@ -62,25 +35,6 @@ namespace NuGet.PackageManagement.UI
             {
                 Visibility = Visibility.Collapsed;
             }
-        }
-
-        // capture each item as it is selected, so we can unselect when treeview lostfocus
-        private void OnItemSelected(object sender, RoutedEventArgs e)
-        {
-            //_dependencies.Tag = e.OriginalSource;
-        }
-
-        private void TreeView_LostFocus(object sender, RoutedEventArgs e)
-        {
-            // hide focus highlight when treeview lostfocus
-            //if (_dependencies.SelectedItem != null)
-            //{
-            //    TreeViewItem selectedTVI = _dependencies.Tag as TreeViewItem;
-            //    if (selectedTVI != null)
-            //    {
-            //        selectedTVI.IsSelected = false;
-            //    }
-            //}
         }
     }
 }
