887ccb03da8606a30ba733633de3b51e8e86c99e
Jonatan Gonzalez (HE/HIM) (from Dev Box) <jongonza@microsoft.com>
2024-02-14 12:54:13 -0800

    Update widths and heights to allow view to show without scrolling out of view

--- src/NuGet.Clients/NuGet.PackageManagement.UI/ViewModels/ReadMePreviewViewModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/ViewModels/ReadMePreviewViewModel.cs
@@ -24,20 +24,7 @@ namespace NuGet.PackageManagement.UI.ViewModels
             _markdownPreview = new PreviewBuilder().Build();
 #pragma warning restore CS0618 // Type or member is obsolete
             _ = UpdateMarkdownAsync("");
-            PathString = "Hello";
             MarkdownPreviewControl = _markdownPreview.VisualElement;
-        }
-
-        private string _path;
-
-        public string PathString
-        {
-            get => _path;
-            private set
-            {
-                _path = value;
-                RaisePropertyChanged(nameof(PathString));
-            }
         }
 
         private FrameworkElement _markdownPreviewControl;
@@ -56,23 +43,22 @@ namespace NuGet.PackageManagement.UI.ViewModels
 
         public async Task UpdateMarkdownAsync(string packagePath)
         {
-            //var markDown = string.Empty;
-            var markDown = "# About xUnit.net\r\n\r\n[<img align=\"right\" width=\"100px\" src=\"https://raw.githubusercontent.com/xunit/media/main/dotnet-foundation.svg\" />](https://dotnetfoundation.org/projects/project-detail/xunit)\r\n\r\nxUnit.net is a free, open source, community-focused unit testing tool for the .NET Framework. Written by the original inventor of NUnit v2, xUnit.net is the latest technology for unit testing C# and F# (other .NET languages may work as well, but are unsupported). xUnit.net works with Visual Studio, Visual Studio Code, ReSharper, CodeRush, and TestDriven.NET. It is part of the [.NET Foundation](https://www.dotnetfoundation.org/), and operates under their [code of conduct](https://www.dotnetfoundation.org/code-of-conduct). It is licensed under [Apache 2](https://opensource.org/licenses/Apache-2.0) (an OSI approved license)";
-            //if (!string.IsNullOrEmpty(packagePath))
-            //{
-            //    var fileInfo = new FileInfo(packagePath);
-            //    if (fileInfo.Exists)
-            //    {
-            //        using var pfr = new PackageArchiveReader(fileInfo.OpenRead());
-            //        var files = await pfr.GetFilesAsync(CancellationToken.None);
-            //        var readmeFile = files.FirstOrDefault(file => file.IndexOf("readme.md", System.StringComparison.OrdinalIgnoreCase) >= 0);
-            //        if (!string.IsNullOrEmpty(readmeFile))
-            //        {
-            //            using var stream = new StreamReader(await pfr.GetStreamAsync(readmeFile, CancellationToken.None));
-            //            markDown = await stream.ReadToEndAsync();
-            //        }
-            //    }
-            //}
+            var markDown = string.Empty;
+            if (!string.IsNullOrEmpty(packagePath))
+            {
+                var fileInfo = new FileInfo(packagePath);
+                if (fileInfo.Exists)
+                {
+                    using var pfr = new PackageArchiveReader(fileInfo.OpenRead());
+                    var files = await pfr.GetFilesAsync(CancellationToken.None);
+                    var readmeFile = files.FirstOrDefault(file => file.IndexOf("readme.md", System.StringComparison.OrdinalIgnoreCase) >= 0);
+                    if (!string.IsNullOrEmpty(readmeFile))
+                    {
+                        using var stream = new StreamReader(await pfr.GetStreamAsync(readmeFile, CancellationToken.None));
+                        markDown = await stream.ReadToEndAsync();
+                    }
+                }
+            }
             await _markdownPreview.UpdateContentAsync(markDown, ScrollHint.None);
         }
 
