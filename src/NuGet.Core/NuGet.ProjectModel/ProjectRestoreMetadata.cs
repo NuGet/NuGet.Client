5fd2187b3fe6e08424b9922280b366cb11cbfbd3
Nikolche Kolev <nikolce1kolev@gmail.com>
2024-03-04 12:46:25 -0800

    Cleanup

--- src/NuGet.Core/NuGet.ProjectModel/ProjectRestoreMetadata.cs
+++ src/NuGet.Core/NuGet.ProjectModel/ProjectRestoreMetadata.cs
@@ -212,7 +212,11 @@ namespace NuGet.ProjectModel
 
         private HashSet<string> GetSources(IList<PackageSource> sources)
         {
+#if NETSTANDARD2_0
+            var setSources = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+#else
             var setSources = new HashSet<string>(sources.Count, StringComparer.OrdinalIgnoreCase);
+#endif
             for (var i = 0; i < sources.Count; i++)
             {
                 setSources.Add(sources[i].Source);
