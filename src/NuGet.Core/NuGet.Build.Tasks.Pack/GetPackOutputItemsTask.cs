6e59d96ebaa88ffb34d6d0887942e211e59db7d3
Jeff Tyson <jetyson@microsoft.com>
2024-03-28 09:50:53 -0700

    Fix nit

--- src/NuGet.Core/NuGet.Build.Tasks.Pack/GetPackOutputItemsTask.cs
+++ src/NuGet.Core/NuGet.Build.Tasks.Pack/GetPackOutputItemsTask.cs
@@ -33,7 +33,7 @@ namespace NuGet.Build.Tasks.Pack
 
         public string SymbolPackageFormat { get; set; }
 
-        public bool OutputFileNamesWithoutVersion  { get; set; }
+        public bool OutputFileNamesWithoutVersion { get; set; }
 
         /// <summary>
         /// Output items
