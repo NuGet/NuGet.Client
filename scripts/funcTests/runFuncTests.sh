09c6d883a5b68140d791c80c05f70204aa8c5a6b
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2024-04-08 16:26:33 -0700

    Update runFuncTests.sh

--- scripts/funcTests/runFuncTests.sh
+++ scripts/funcTests/runFuncTests.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+UNIT_TESTS=0
+FUNCTIONAL_TESTS=0
+MONO_TESTS=0
+
+while true ; do
+    case "$1" in
+        -u|--unit-tests) UNIT_TESTS=1 ; shift ;;
+        -f|--functional-tests) FUNCTIONAL_TESTS=1 ; shift ;;
+        -m|--mono-tests) MONO_TESTS=1 ; shift ;;
+        --) shift ; break ;;
+        *) shift ; break ;;
+    esac
+done
+
 # move up to the repo root
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR=$SCRIPTDIR/../..
@@ -21,22 +35,25 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "=============== Build and run unit tests started at `date -u +"%Y-%m-%dT%H:%M:%S"` ================="
-echo "dotnet msbuild build/build.proj /restore:false /target:CoreUnitTests /property:Configuration=Release /property:ReleaseLabel=beta /bl:$LOG_DIRECTORY/binlog/03.CoreUnitTests.binlog"
-dotnet msbuild build/build.proj /restore:false /target:CoreUnitTests /property:Configuration=Release /property:ReleaseLabel=beta /bl:$LOG_DIRECTORY/binlog/03.CoreUnitTests.binlog
-UNIT_TEST_RESULT=$?
-echo "=============== Build and run unit tests finished at `date -u +"%Y-%m-%dT%H:%M:%S"`================="
-echo ""
+EXIT_CODE=0
 
-echo "============ Build and run functional tests started at `date -u +"%Y-%m-%dT%H:%M:%S"` =============="
-echo "dotnet msbuild build/build.proj /restore:false /target:CoreFuncTests /property:Configuration=Release /property:ReleaseLabel=beta /bl:$LOG_DIRECTORY/binlog/04.CoreFuncTests.binlog"
-dotnet msbuild build/build.proj /restore:false /target:CoreFuncTests /property:Configuration=Release /property:ReleaseLabel=beta /bl:$LOG_DIRECTORY/binlog/04.CoreFuncTests.binlog
-FUNC_TEST_RESULT=$?
-echo "============== Build and run functional tests finished at `date -u +"%Y-%m-%dT%H:%M:%S"`============"
-echo ""
+if [ "$UNIT_TESTS" == "1" ]; then
+    echo "=============== Build and run unit tests started at `date -u +"%Y-%m-%dT%H:%M:%S"` ================="
+    echo "dotnet msbuild build/build.proj /restore:false /target:CoreUnitTests /property:Configuration=Release /property:ReleaseLabel=beta /bl:$LOG_DIRECTORY/binlog/03.CoreUnitTests.binlog"
+    dotnet msbuild build/build.proj /restore:false /target:CoreUnitTests /property:Configuration=Release /property:ReleaseLabel=beta /bl:$LOG_DIRECTORY/binlog/03.CoreUnitTests.binlog
+    EXIT_CODE=$?
+    echo "=============== Build and run unit tests finished at `date -u +"%Y-%m-%dT%H:%M:%S"`================="
+fi
 
-MONO_TEST_RESULT=-1
-if [ "$CI" == "true" ]; then
+if [ "$FUNCTIONAL_TESTS" == "1" ]; then
+    echo "============ Build and run functional tests started at `date -u +"%Y-%m-%dT%H:%M:%S"` =============="
+    echo "dotnet msbuild build/build.proj /restore:false /target:CoreFuncTests /property:Configuration=Release /property:ReleaseLabel=beta /bl:$LOG_DIRECTORY/binlog/04.CoreFuncTests.binlog"
+    dotnet msbuild build/build.proj /restore:false /target:CoreFuncTests /property:Configuration=Release /property:ReleaseLabel=beta /bl:$LOG_DIRECTORY/binlog/04.CoreFuncTests.binlog
+    EXIT_CODE=$?
+    echo "============== Build and run functional tests finished at `date -u +"%Y-%m-%dT%H:%M:%S"`============"
+fi
+
+if [ "$MONO_TESTS" == "1" ]; then
     # Run mono test
     TestDir="$DIR/artifacts/NuGet.CommandLine.Test/"
     VsTestConsole="$DIR/artifacts/NuGet.CommandLine.Test/vstest/vstest.console.exe"
@@ -55,14 +72,14 @@ if [ "$CI" == "true" ]; then
 			    # We are not testing Mono on linux currently, so comment it out.
 			    #echo "mono $VsTestConsole $TestDir/NuGet.CommandLine.Test.dll --TestCaseFilter:Platform!=Windows&Platform!=Darwin --logger:console;verbosity=$VsTestVerbosity --logger:"trx" --ResultsDirectory:$TestResultsDir"
 			    #mono $VsTestConsole "$TestDir/NuGet.CommandLine.Test.dll" --TestCaseFilter:"Platform!=Windows&Platform!=Darwin" --logger:"console;verbosity=$VsTestVerbosity" --logger:"trx" --ResultsDirectory:"$TestResultsDir"
-			    #MONO_TEST_RESULT=$?
+			    #EXIT_CODE=$?
 			    ;;
 		    Darwin)
                 echo "==================== Run mono tests started at `date -u +"%Y-%m-%dT%H:%M:%S"` ======================"
 			    echo "mono $VsTestConsole $TestDir/NuGet.CommandLine.Test.dll --TestCaseFilter:Platform!=Windows&Platform!=Linux --logger:console;verbosity=$VsTestVerbosity --logger:"trx" --ResultsDirectory:$TestResultsDir"
 			    mono $VsTestConsole "$TestDir/NuGet.CommandLine.Test.dll" --TestCaseFilter:"Platform!=Windows&Platform!=Linux" --logger:"console;verbosity=$VsTestVerbosity" --logger:"trx" --ResultsDirectory:"$TestResultsDir"
-			    MONO_TEST_RESULT=$?
-                echo "================== mono tests finished started at `date -u +"%Y-%m-%dT%H:%M:%S"` ==================="
+			    EXIT_CODE=$?
+                echo "================== mono tests finished at `date -u +"%Y-%m-%dT%H:%M:%S"` ==================="
                 echo ""
 			    ;;
 		    *) ;;
@@ -71,30 +88,10 @@ fi
 
 popd
 
-EXITCODE=0
-
-echo "Test Results:"
-if [ $UNIT_TEST_RESULT -eq 0 ]; then
-    echo "  Unit tests:       Passed"
+if [ $EXIT_CODE -eq 0 ]; then
+    echo "SUCCESS!"
 else
-    EXITCODE=$UNIT_TEST_RESULT
-    echo "  Unit tests:       Failed"
+    echo "FAILED!"
 fi
 
-if [ $FUNC_TEST_RESULT -eq 0 ]; then
-    echo "  Functional tests: Passed"
-else
-    EXITCODE=$FUNC_TEST_RESULT
-    echo "  Functional tests: Failed"
-fi
-
-if [ $MONO_TEST_RESULT -eq -1 ]; then
-    echo "  Mono tests:       Not Run"
-elif [ $MONO_TEST_RESULT -eq 0 ]; then
-    echo "  Mono tests:       Passed"
-else
-    EXITCODE=$MONO_TEST_RESULT
-    echo "  Mono tests:       Failed"
-fi
-
-exit $EXITCODE
+exit $EXIT_CODE
