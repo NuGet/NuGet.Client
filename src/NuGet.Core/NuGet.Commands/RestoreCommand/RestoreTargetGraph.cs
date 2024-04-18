2543a863cbdf478a39cf0449f2802856aaff4ced
Scott Wadsworth <bwadswor@microsoft.com>
2024-04-18 13:11:28 -0700

    Setup basic envvar to enable prototype. SubProtoNug=1 means use prototype SubProtoNug=2 means use flattened graph from Prototype, but everything else from original any other value means use original.
    Also turned off analyzers to allow eaiser public api manipulation in Nuget.Commands.csproj

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/RestoreTargetGraph.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/RestoreTargetGraph.cs
@@ -1,7 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using NuGet.Client;
@@ -41,7 +40,7 @@ namespace NuGet.Commands
         public IEnumerable<GraphNode<RemoteResolveResult>> Graphs { get; }
 
         public ISet<RemoteMatch> Install { get; }
-        public ISet<GraphItem<RemoteResolveResult>> Flattened { get; }
+        public ISet<GraphItem<RemoteResolveResult>> Flattened { get; set; } //BSW added set to allow merging partial work
         public ISet<LibraryRange> Unresolved { get; }
         public bool InConflict { get; }
 
