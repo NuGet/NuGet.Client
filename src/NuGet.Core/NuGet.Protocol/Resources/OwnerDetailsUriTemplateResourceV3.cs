2fe816750a2da8e66790bd6c098cd08b2b4dd96e
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2024-04-23 12:29:11 -0700

    Create Owner Details URI Template resources

---
+++ src/NuGet.Core/NuGet.Protocol/Resources/OwnerDetailsUriTemplateResourceV3.cs
@@ -0,0 +1,76 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#nullable enable
+
+using System;
+using NuGet.Protocol.Core.Types;
+
+namespace NuGet.Protocol.Resources
+{
+    /// <summary>Owner Details Uri Template for NuGet V3 HTTP feeds.</summary>
+    /// <remarks>Not intended to be created directly. Use <see cref="SourceRepository.GetResourceAsync{T}(CancellationToken)"/>
+    /// with <see cref="OwnerDetailsUriTemplateResourceV3"/> for T, and typecast to this class.
+    public class OwnerDetailsUriTemplateResourceV3 : INuGetResource
+    {
+        private readonly string _template;
+
+        public OwnerDetailsUriTemplateResourceV3(string template)
+        {
+            _template = template ?? throw new ArgumentNullException(nameof(template));
+        }
+
+        /// <summary>
+        /// Creates the specified Owner Details Uri template provided by the server if it exists and is valid.
+        /// </summary>
+        /// <param name="uriTemplate">The Absolute Uri template provided by the server.</param>
+        /// <returns>A valid Owner Details Uri template, or null.</returns>
+        public static OwnerDetailsUriTemplateResourceV3? CreateOrNull(Uri? uriTemplate)
+        {
+            if (uriTemplate == null || uriTemplate.OriginalString.Length == 0 || !uriTemplate.IsAbsoluteUri)
+            {
+                return null;
+            }
+
+            string absoluteUri = uriTemplate.OriginalString;
+            if (string.IsNullOrWhiteSpace(absoluteUri)
+                || !IsValidUriTemplate(absoluteUri))
+            {
+                return null;
+            }
+
+            return new OwnerDetailsUriTemplateResourceV3(absoluteUri);
+        }
+
+        private static bool IsValidUriTemplate(string absoluteUri)
+        {
+            Uri? uri;
+            var isValidUri = Uri.TryCreate(absoluteUri, UriKind.Absolute, out uri);
+
+            // Only allow HTTPS owner details URLs.
+            if (isValidUri && uri?.Scheme.Equals("https", StringComparison.OrdinalIgnoreCase) != true)
+            {
+                return false;
+            }
+
+            return isValidUri;
+        }
+
+        /// <summary>
+        /// Gets a URL for viewing package Owner URL outside of Visual Studio. The URL will not be verified to exist.
+        /// </summary>
+        /// <param name="owner">The owner username.</param>
+        /// <returns>The first URL from the resource, with the URI template applied.</returns>
+        public Uri GetUri(string owner)
+        {
+            var uriString = _template
+#if NETCOREAPP
+               .Replace("{owner}", owner, StringComparison.OrdinalIgnoreCase);
+#else
+               .Replace("{owner}", owner);
+#endif
+
+            return new Uri(uriString);
+        }
+    }
+}
--- src/NuGet.Core/NuGet.Protocol/ServiceTypes.cs
+++ src/NuGet.Core/NuGet.Protocol/ServiceTypes.cs
@@ -17,6 +17,7 @@ namespace NuGet.Protocol
         public static readonly string Version500 = "/5.0.0";
         public static readonly string Version510 = "/5.1.0";
         internal const string Version670 = "/6.7.0";
+        internal const string Version6110 = "/6.11.0";
 
         public static readonly string[] SearchQueryService = { "SearchQueryService" + Versioned, "SearchQueryService" + Version340, "SearchQueryService" + Version300beta };
         public static readonly string[] RegistrationsBaseUrl = { $"RegistrationsBaseUrl{Versioned}", $"RegistrationsBaseUrl{Version360}", $"RegistrationsBaseUrl{Version340}", $"RegistrationsBaseUrl{Version300rc}", $"RegistrationsBaseUrl{Version300beta}", "RegistrationsBaseUrl" };
@@ -29,5 +30,6 @@ namespace NuGet.Protocol
         public static readonly string[] RepositorySignatures = { "RepositorySignatures" + Version500, "RepositorySignatures" + Version490, "RepositorySignatures" + Version470 };
         public static readonly string[] SymbolPackagePublish = { "SymbolPackagePublish" + Version490 };
         internal static readonly string[] VulnerabilityInfo = { "VulnerabilityInfo" + Version670 };
+        internal static readonly string[] OwnerDetailsUriTemplate = { "OwnerDetailsUriTemplate" + Version6110 };
     }
 }
