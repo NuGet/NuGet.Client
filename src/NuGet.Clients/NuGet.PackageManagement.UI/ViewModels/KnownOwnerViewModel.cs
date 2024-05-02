988fff9e4cd4bac01da982727386abc0b47efb66
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2024-05-02 13:33:42 -0400

    Enable Nullable throw in KnownOwnerViewModel (#5784)

--- src/NuGet.Clients/NuGet.PackageManagement.UI/ViewModels/KnownOwnerViewModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/ViewModels/KnownOwnerViewModel.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#nullable enable
+
 using System;
 using NuGet.VisualStudio.Internal.Contracts;
 
@@ -10,6 +12,11 @@ namespace NuGet.PackageManagement.UI.ViewModels
     {
         public KnownOwnerViewModel(KnownOwner knownOwner)
         {
+            if (knownOwner is null)
+            {
+                throw new ArgumentNullException(nameof(knownOwner));
+            }
+
             Name = knownOwner.Name;
             Link = knownOwner.Link;
         }
