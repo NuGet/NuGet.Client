c5d096799d4c6e54ab288006b1ddbc3d86d1e441
Nigusu Yenework <nyenework@microsoft.com>
2024-06-14 09:55:21 -0700

    fix mistake

--- src/NuGet.Core/NuGet.Protocol/Providers/ReportAbuseResourceV3Provider.cs
+++ src/NuGet.Core/NuGet.Protocol/Providers/ReportAbuseResourceV3Provider.cs
@@ -9,35 +9,28 @@ using NuGet.Protocol.Core.Types;
 
 namespace NuGet.Protocol
 {
-    public class RegistrationResourceV3Provider : ResourceProvider
+    public class ReportAbuseResourceV3Provider : ResourceProvider
     {
-        public RegistrationResourceV3Provider()
-            : base(typeof(RegistrationResourceV3),
-                  nameof(RegistrationResourceV3),
+        public ReportAbuseResourceV3Provider()
+            : base(typeof(ReportAbuseResourceV3),
+                  nameof(ReportAbuseResourceV3),
                   NuGetResourceProviderPositions.Last)
         {
         }
 
         public override async Task<Tuple<bool, INuGetResource>> TryCreate(SourceRepository source, CancellationToken token)
         {
-            RegistrationResourceV3 regResource = null;
+            ReportAbuseResourceV3 resource = null;
             var serviceIndex = await source.GetResourceAsync<ServiceIndexResourceV3>(token);
-
             if (serviceIndex != null)
             {
-                //This will come back as null if there are no matching RegistrationsBaseUrl types
-                var baseUrl = serviceIndex.GetServiceEntryUri(source.PackageSource.AllowInsecureConnections, ServiceTypes.RegistrationsBaseUrl);
+                var uriTemplate = serviceIndex.GetServiceEntryUri(source.PackageSource.AllowInsecureConnections, ServiceTypes.ReportAbuse)?.AbsoluteUri;
 
-                if (baseUrl != null)
-                {
-                    var httpSourceResource = await source.GetResourceAsync<HttpSourceResource>(token);
-
-                    // construct a new resource
-                    regResource = new RegistrationResourceV3(httpSourceResource.HttpSource, baseUrl);
-                }
+                // construct a new resource
+                resource = new ReportAbuseResourceV3(uriTemplate);
             }
 
-            return new Tuple<bool, INuGetResource>(regResource != null, regResource);
+            return new Tuple<bool, INuGetResource>(resource != null, resource);
         }
     }
 }
