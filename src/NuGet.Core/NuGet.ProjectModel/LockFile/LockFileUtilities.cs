384d10049f44b8c75e167ff2b4d2677a1e925e07
Todd Grunke <toddgrun@microsoft.com>
2024-06-17 12:35:02 -0700

    Fallout from LockFileReadFlags rename Remove some diagnostic info added

--- src/NuGet.Core/NuGet.ProjectModel/LockFile/LockFileUtilities.cs
+++ src/NuGet.Core/NuGet.ProjectModel/LockFile/LockFileUtilities.cs
@@ -1,8 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.IO;
 using NuGet.Common;
 
@@ -10,8 +8,6 @@ namespace NuGet.ProjectModel
 {
     public static class LockFileUtilities
     {
-        private static readonly List<string> LockFiles = new List<string>();
-
         public static LockFile GetLockFile(string lockFilePath, Common.ILogger logger)
         {
             return GetLockFile(lockFilePath, logger, LockFileReadFlags.All);
@@ -23,8 +19,6 @@ namespace NuGet.ProjectModel
 
             if (File.Exists(lockFilePath))
             {
-                LockFiles.Add(lockFilePath);
-
                 var format = new LockFileFormat();
 
                 // A corrupt lock file will log errors and return null
