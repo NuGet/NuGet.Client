98c68c475801444774fcf515cea3f539dee33087
Jonatan Gonzalez (HE/HIM) <jongonza@microsoft.com>
2024-06-18 16:40:20 -0700

    Add .editorconfig

--- src/NuGet.Clients/NuGet.CommandLine/CommandLineMachineWideSettings.cs
+++ src/NuGet.Clients/NuGet.CommandLine/CommandLineMachineWideSettings.cs
@@ -8,23 +8,17 @@ using NuGet.Common;
 namespace NuGet.CommandLine
 {
     [Export(typeof(Configuration.IMachineWideSettings))]
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public class CommandLineMachineWideSettings : Configuration.IMachineWideSettings
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     {
         Lazy<Configuration.ISettings> _settings;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public CommandLineMachineWideSettings()
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             var baseDirectory = NuGetEnvironment.GetFolderPath(NuGetFolderPath.MachineWideConfigDirectory);
             _settings = new Lazy<Configuration.ISettings>(
                 () => Configuration.Settings.LoadMachineWideSettings(baseDirectory));
         }
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public Configuration.ISettings Settings => _settings.Value;
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 }
