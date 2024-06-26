5299aceffa07764920cfa400ffb0d63b325b49fa
Martin Ruiz <martin.ruiz.mares@gmail.com>
2024-06-25 20:56:43 -0700

    remove unnecesary imports

--- src/NuGet.Core/NuGet.Protocol/Converters/PackageDependencyGroupConverter.cs
+++ src/NuGet.Core/NuGet.Protocol/Converters/PackageDependencyGroupConverter.cs
@@ -3,9 +3,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using NuGet.Frameworks;
 using NuGet.Packaging;
 using NuGet.Versioning;
