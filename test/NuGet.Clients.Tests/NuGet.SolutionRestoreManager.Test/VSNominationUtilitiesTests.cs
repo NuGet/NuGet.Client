4933379bcd70c1f5732d2976824f3f510f96dbae
Nigusu Yenework <nyenework@microsoft.com>
2024-07-09 15:07:22 -0700

    test nominations

--- test/NuGet.Clients.Tests/NuGet.SolutionRestoreManager.Test/VSNominationUtilitiesTests.cs
+++ test/NuGet.Clients.Tests/NuGet.SolutionRestoreManager.Test/VSNominationUtilitiesTests.cs
@@ -7,6 +7,7 @@ using System;
 using Xunit;
 using FluentAssertions;
 using System.Collections.Immutable;
+using NuGet.Versioning;
 
 namespace NuGet.SolutionRestoreManager.Test
 {
@@ -250,6 +251,104 @@ namespace NuGet.SolutionRestoreManager.Test
             // Act & Assert
             InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => VSNominationUtilities.GetRestoreAuditProperties(targetFrameworks));
             exception.Message.Should().Contain(ProjectItems.NuGetAuditSuppress);
+        }
+
+        private VsTargetFrameworkInfo4[] TargetFrameworkWithSdkAnalysisLevel(string sdkAnalysisLevel)
+        {
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            keyValuePairs["SdkAnalysisLevel"] = sdkAnalysisLevel;
+            var targetFrameworks = new VsTargetFrameworkInfo4[]
+            {
+                new VsTargetFrameworkInfo4(
+                    items: new Dictionary<string, IReadOnlyList<IVsReferenceItem2>>(StringComparer.OrdinalIgnoreCase),
+                    properties: new Dictionary<string, string>
+                    {
+                        { ProjectBuildProperties.SdkAnalysisLevel, sdkAnalysisLevel }
+                    })
+            };
+
+            return targetFrameworks;
+        }
+
+        private VsTargetFrameworkInfo4[] TargetFrameworkWithUsingMicrosoftNetSdk(string UsingMicrosoftNetSdk)
+        {
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            keyValuePairs["UsingMicrosoftNETSdk"] = UsingMicrosoftNetSdk;
+            var targetFrameworks = new VsTargetFrameworkInfo4[]
+            {
+                new VsTargetFrameworkInfo4(
+                    items: new Dictionary<string, IReadOnlyList<IVsReferenceItem2>>(StringComparer.OrdinalIgnoreCase),
+                    properties: new Dictionary<string, string>
+                    {
+                        { ProjectBuildProperties.UsingMicrosoftNETSdk, UsingMicrosoftNetSdk }
+                    })
+            };
+
+            return targetFrameworks;
+        }
+
+        [Theory]
+        [InlineData("9.0.100")]
+        [InlineData("7.0.100")]
+        [InlineData("9.1.100")]
+        [InlineData("9.2.101")]
+        public void GetSdkAnalysisLevel_WithValidVersions_ReturnsNuGetVersion(string sdkAnalysisLevel)
+        {
+            // Arrange
+            var targetFrameworks = TargetFrameworkWithSdkAnalysisLevel(sdkAnalysisLevel);
+            NuGetVersion expected = new NuGetVersion(sdkAnalysisLevel);
+
+            //Act
+            NuGetVersion actual = VSNominationUtilities.GetSdkAnalysisLevel(targetFrameworks);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("invalid")]
+        [InlineData("1.3e")]
+        public void GetSdkAnalysisLevel_WithInvalidVersions_ThrowsException(string sdkAnalysisLevel)
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => VSNominationUtilities.GetSdkAnalysisLevel(TargetFrameworkWithSdkAnalysisLevel(sdkAnalysisLevel)));
+        }
+
+        [Theory]
+        [InlineData("true")]
+        [InlineData("True")]
+        [InlineData("trUe")]
+        [InlineData("TrUe")]
+        public void GetUsingMicrosoftNETSdk_WithTrueValue_ReturnsTrue(string usingMicrosoftNETSdk)
+        {
+            // Act
+            bool actual = VSNominationUtilities.GetUsingMicrosoftNETSdk(TargetFrameworkWithUsingMicrosoftNetSdk(usingMicrosoftNETSdk));
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [InlineData("false")]
+        [InlineData("False")]
+        [InlineData("falSe")]
+        [InlineData("FalsE")]
+        public void GetUsingMicrosoftNETSdk_WithFalseValue_ReturnsFalse(string usingMicrosoftNETSdk)
+        {
+            // Act
+            bool actual = VSNominationUtilities.GetUsingMicrosoftNETSdk(TargetFrameworkWithUsingMicrosoftNetSdk(usingMicrosoftNETSdk));
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        [Theory]
+        [InlineData("t")]
+        [InlineData("1.3e")]
+        [InlineData("1")]
+        public void GetUsingMicrosoftNETSdk_WithInvalidValue_ThrowsException(string usingMicrosoftNETSdk)
+        {
+            Assert.Throws<ArgumentException>(() => VSNominationUtilities.GetUsingMicrosoftNETSdk(TargetFrameworkWithUsingMicrosoftNetSdk(usingMicrosoftNETSdk)));
         }
 
     }
