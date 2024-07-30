fceda864af642b96e0fc6cef42896ba6aa11264b
Jonatan Gonzalez (HE/HIM) <jongonza@microsoft.com>
2024-07-29 21:22:29 -0700

    enable generate docs for all projects and supress warnings

--- build/TestShared/CommonAssemblyInfo.cs
+++ build/TestShared/CommonAssemblyInfo.cs
@@ -1,6 +1,2 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-using System.Reflection;
-using System.Resources;
-using System.Runtime.InteropServices;
