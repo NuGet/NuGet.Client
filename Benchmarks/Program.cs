1cd48e32886136f2d90dd6f3d0cc213918ebf21c
Martin Ruiz <martin.ruiz.mares@gmail.com>
2024-08-08 15:04:15 -0700

    Add converter to PackageDependency

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,4 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+Console.WriteLine("Hello, World!");
