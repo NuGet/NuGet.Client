2748a72ec93626386c9fddadf52d02a5d6461a89
Andy Zivkovic <Andy.Zivkovic@microsoft.com>
2024-08-15 10:39:01 +0930

    Allow CLI parser config to be garbage collected

--- src/NuGet.Core/NuGet.CommandLine.XPlat/Commands/Why/WhyCommand.cs
+++ src/NuGet.Core/NuGet.CommandLine.XPlat/Commands/Why/WhyCommand.cs
@@ -12,46 +12,6 @@ namespace NuGet.CommandLine.XPlat.Commands.Why
 {
     internal static class WhyCommand
     {
-        private static CliArgument<string> Path = new CliArgument<string>("PROJECT|SOLUTION")
-        {
-            Description = Strings.WhyCommand_PathArgument_Description,
-            // We really want this to be zero or one, however, because this is the first argument, it doesn't work.
-            // Instead, we need to use a CustomParser to choose if the argument is the path, or the package.
-            // In order for the parser to tell us there's more than 1 argument available, we need to tell CliArgument
-            // that it supports more than one, but then in the custom parser we'll make sure we only take at most 1.
-            Arity = ArgumentArity.ZeroOrMore,
-            CustomParser = ar =>
-            {
-                if (ar.Tokens.Count > 1)
-                {
-                    var value = ar.Tokens[0];
-                    ar.OnlyTake(1);
-                    return value.Value;
-                }
-
-                ar.OnlyTake(0);
-                var currentDirectory = Directory.GetCurrentDirectory();
-                return currentDirectory;
-            }
-        };
-
-        private static CliArgument<string> Package = new CliArgument<string>("PACKAGE")
-        {
-            Description = Strings.WhyCommand_PackageArgument_Description,
-            Arity = ArgumentArity.ExactlyOne
-        };
-
-        private static CliOption<List<string>> Frameworks = new CliOption<List<string>>("--framework", "-f")
-        {
-            Description = Strings.WhyCommand_FrameworksOption_Description,
-            Arity = ArgumentArity.OneOrMore
-        };
-
-        private static HelpOption Help = new HelpOption()
-        {
-            Arity = ArgumentArity.Zero
-        };
-
         internal static void Register(CommandLineApplication app)
         {
             app.Command("why", whyCmd =>
@@ -69,10 +29,50 @@ namespace NuGet.CommandLine.XPlat.Commands.Why
         {
             var whyCommand = new CliCommand("why", Strings.WhyCommand_Description);
 
-            whyCommand.Arguments.Add(Path);
-            whyCommand.Arguments.Add(Package);
-            whyCommand.Options.Add(Frameworks);
-            whyCommand.Options.Add(Help);
+            CliArgument<string> path = new CliArgument<string>("PROJECT|SOLUTION")
+            {
+                Description = Strings.WhyCommand_PathArgument_Description,
+                // We really want this to be zero or one, however, because this is the first argument, it doesn't work.
+                // Instead, we need to use a CustomParser to choose if the argument is the path, or the package.
+                // In order for the parser to tell us there's more than 1 argument available, we need to tell CliArgument
+                // that it supports more than one, but then in the custom parser we'll make sure we only take at most 1.
+                Arity = ArgumentArity.ZeroOrMore,
+                CustomParser = ar =>
+                {
+                    if (ar.Tokens.Count > 1)
+                    {
+                        var value = ar.Tokens[0];
+                        ar.OnlyTake(1);
+                        return value.Value;
+                    }
+
+                    ar.OnlyTake(0);
+                    var currentDirectory = Directory.GetCurrentDirectory();
+                    return currentDirectory;
+                }
+            };
+
+            CliArgument<string> package = new CliArgument<string>("PACKAGE")
+            {
+                Description = Strings.WhyCommand_PackageArgument_Description,
+                Arity = ArgumentArity.ExactlyOne
+            };
+
+            CliOption<List<string>> frameworks = new CliOption<List<string>>("--framework", "-f")
+            {
+                Description = Strings.WhyCommand_FrameworksOption_Description,
+                Arity = ArgumentArity.OneOrMore
+            };
+
+            HelpOption help = new HelpOption()
+            {
+                Arity = ArgumentArity.Zero
+            };
+
+            whyCommand.Arguments.Add(path);
+            whyCommand.Arguments.Add(package);
+            whyCommand.Options.Add(frameworks);
+            whyCommand.Options.Add(help);
 
             whyCommand.SetAction((parseResult) =>
             {
@@ -81,9 +81,9 @@ namespace NuGet.CommandLine.XPlat.Commands.Why
                 try
                 {
                     var whyCommandArgs = new WhyCommandArgs(
-                        parseResult.GetValue(Path),
-                        parseResult.GetValue(Package),
-                        parseResult.GetValue(Frameworks),
+                        parseResult.GetValue(path),
+                        parseResult.GetValue(package),
+                        parseResult.GetValue(frameworks),
                         logger);
 
                     int exitCode = action(whyCommandArgs);
