c4638c470d1f8f83304322d9eecd61ff86155d03
Nigusu Yenework <nyenework@microsoft.com>
2024-09-05 10:52:41 -0700

    Comment

--- test/NuGet.Core.Tests/NuGet.Protocol.Tests/Plugins/PluginDiscovererTests.cs
+++ test/NuGet.Core.Tests/NuGet.Protocol.Tests/Plugins/PluginDiscovererTests.cs
@@ -381,7 +381,7 @@ namespace NuGet.Protocol.Plugins.Tests
 
                 using (var process = new Process())
                 {
-                    // Use a shell command to make the file executable
+                    // Use a shell command to make the file not executable
                     process.StartInfo.FileName = "sh";
                     process.StartInfo.Arguments = $"-c \"chmod -x {myPlugin}\"";
                     process.StartInfo.UseShellExecute = false;
