cd1e75e799e9518d0ef3b3ed68d55b937d713798
Kirill Osenkov <KirillOsenkov@users.noreply.github.com>
2024-09-09 15:12:52 -0700

    Always debug RestoreTask and RestoreEx when environment variable is set (#6008)

--- src/NuGet.Core/NuGet.Build.Tasks/RestoreTask.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/RestoreTask.cs
@@ -101,13 +101,13 @@ namespace NuGet.Build.Tasks
 
         public override bool Execute()
         {
-#if DEBUG
             var debugRestoreTask = Environment.GetEnvironmentVariable("DEBUG_RESTORE_TASK");
-            if (!string.IsNullOrEmpty(debugRestoreTask) && debugRestoreTask.Equals(bool.TrueString, StringComparison.OrdinalIgnoreCase))
+            if (!string.IsNullOrEmpty(debugRestoreTask) &&
+                (debugRestoreTask.Equals(bool.TrueString, StringComparison.OrdinalIgnoreCase) || debugRestoreTask == "1"))
             {
                 Debugger.Launch();
             }
-#endif
+
             var log = new MSBuildLogger(Log);
 
             NuGet.Common.Migrations.MigrationRunner.Run();
