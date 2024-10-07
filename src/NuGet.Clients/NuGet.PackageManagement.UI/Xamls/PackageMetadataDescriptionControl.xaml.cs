2889e80a5e8d07d22b5b20e4d1ef33d3d50f743d
Jonatan Gonzalez (HE/HIM) <jongonza@microsoft.com>
2024-10-07 13:43:24 -0700

    Added new user controls, updated details

---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageMetadataDescriptionControl.xaml.cs
@@ -0,0 +1,84 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using NuGet.VisualStudio;
+using NuGet.VisualStudio.Telemetry;
+
+namespace NuGet.PackageManagement.UI
+{
+    /// <summary>
+    /// Interaction logic for PackageMetadataDescriptionControl.xaml
+    /// </summary>
+    public partial class PackageMetadataDescriptionControl : UserControl
+    {
+        public PackageMetadataDescriptionControl()
+        {
+            InitializeComponent();
+
+            Visibility = Visibility.Collapsed;
+            DataContextChanged += PackageMetadataDescriptionControl_DataContextChanged;
+        }
+
+        private void ViewLicense_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is DetailedPackageMetadata metadata)
+            {
+                var window = new LicenseFileWindow()
+                {
+                    DataContext = new LicenseFileData
+                    {
+                        LicenseHeader = string.Format(CultureInfo.CurrentCulture, UI.Resources.WindowTitle_LicenseFileWindow, metadata.Id),
+                        LicenseText = new FlowDocument(new Paragraph(new Run(UI.Resources.LicenseFile_Loading)))
+                    }
+                };
+
+                NuGetUIThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+                {
+                    string content = await PackageLicenseUtilities.GetEmbeddedLicenseAsync(new Packaging.Core.PackageIdentity(metadata.Id, metadata.Version), CancellationToken.None);
+                    var flowDoc = new FlowDocument();
+                    flowDoc.Blocks.AddRange(PackageLicenseUtilities.GenerateParagraphs(content));
+                    await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                    (window.DataContext as LicenseFileData).LicenseText = flowDoc;
+                }).PostOnFailure(nameof(PackageMetadataDescriptionControl), nameof(ViewLicense_Click));
+
+                window.ShowModal();
+            }
+        }
+
+        private void PackageMetadataDescriptionControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (DataContext is DetailedPackageMetadata)
+            {
+                Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Visibility = Visibility.Collapsed;
+            }
+        }
+
+        // capture each item as it is selected, so we can unselect when treeview lostfocus
+        private void OnItemSelected(object sender, RoutedEventArgs e)
+        {
+            _dependencies.Tag = e.OriginalSource;
+        }
+
+        private void TreeView_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // hide focus highlight when treeview lostfocus
+            if (_dependencies.SelectedItem != null)
+            {
+                TreeViewItem selectedTVI = _dependencies.Tag as TreeViewItem;
+                if (selectedTVI != null)
+                {
+                    selectedTVI.IsSelected = false;
+                }
+            }
+        }
+    }
+}
---
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/PackageMetadataReadmeControl.xaml.cs
@@ -0,0 +1,169 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.Markdown.Platform;
+using NuGet.PackageManagement.UI.ViewModels;
+using NuGet.VisualStudio;
+using NuGet.VisualStudio.Telemetry;
+
+namespace NuGet.PackageManagement.UI
+{
+
+    /// <summary>
+    /// Interaction logic for PackageMetadataReadMeControl.xaml
+    /// </summary>
+    public partial class PackageMetadataReadMeControl : UserControl, IDisposable
+    {
+        internal CancellationTokenSource _loadCts;
+
+        public static readonly DependencyProperty PackageMetadataProperty =
+            DependencyProperty.Register(
+                nameof(PackageMetadata),
+                typeof(DetailedPackageMetadata),
+                typeof(PackageMetadataReadMeControl),
+                new PropertyMetadata(OnPropertyChanged));
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        private IMarkdownPreview _markdownPreview;
+#pragma warning restore CS0618 // Type or member is obsolete
+        private bool _disposed = false;
+
+        private ReadMePreviewViewModel ReadMeViewModel { get => (ReadMePreviewViewModel)DataContext; }
+
+        public PackageMetadataReadMeControl()
+        {
+            InitializeComponent();
+            _loadCts = new CancellationTokenSource();
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public DetailedPackageMetadata PackageMetadata
+        {
+            get
+            {
+                return (DetailedPackageMetadata)GetValue(PackageMetadataProperty);
+            }
+            set
+            {
+                UpdateControl(PackageMetadata, value);
+                SetValue(PackageMetadataProperty, value);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                _loadCts.Cancel();
+                _loadCts.Dispose();
+                _markdownPreview?.Dispose();
+                var viewModel = (ReadMePreviewViewModel)DataContext;
+                viewModel.PropertyChanged -= ViewModel_PropertyChangedAsync;
+            }
+
+            _disposed = true;
+        }
+
+        private static void OnPropertyChanged(
+           DependencyObject dependencyObject,
+           DependencyPropertyChangedEventArgs e)
+        {
+            if (e.Property == PackageMetadataProperty && dependencyObject is PackageMetadataReadMeControl control)
+            {
+                control?.UpdateControl((DetailedPackageMetadata)e.OldValue, (DetailedPackageMetadata)e.NewValue);
+            }
+        }
+
+        private void UpdateControl(DetailedPackageMetadata oldValue, DetailedPackageMetadata newValue)
+        {
+            if (newValue is not null &&
+                (oldValue?.Id != newValue.Id ||
+                oldValue?.Version != newValue.Version))
+            {
+                var loadCts = new CancellationTokenSource();
+                var oldCts = Interlocked.Exchange(ref _loadCts, loadCts);
+                oldCts?.Cancel();
+                oldCts?.Dispose();
+
+                NuGetUIThreadHelper.JoinableTaskFactory
+                .RunAsync(async () =>
+                {
+                    await ReadMeViewModel.LoadReadmeAsync(newValue.ReadmeFileUrl, _loadCts.Token);
+                })
+                .PostOnFailure(nameof(PackageMetadataReadMeControl));
+            }
+        }
+
+        private void PackageMetadataReadMeControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue is ReadMePreviewViewModel oldViewModel && oldViewModel is not null)
+            {
+                oldViewModel.PropertyChanged -= ViewModel_PropertyChangedAsync;
+            }
+            ReadMeViewModel.PropertyChanged += ViewModel_PropertyChangedAsync;
+        }
+
+#pragma warning disable VSTHRD100 // Avoid async void methods
+        private async void ViewModel_PropertyChangedAsync(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+#pragma warning restore VSTHRD100 // Avoid async void methods
+        {
+            var markdown = string.Empty;
+            if (ReadMeViewModel is not null)
+            {
+                if (ReadMeViewModel.CanDetermineReadMeDefined)
+                {
+                    markdown = string.IsNullOrWhiteSpace(ReadMeViewModel.ReadMeMarkdown)
+                        ? UI.Resources.Text_NoReadme
+                        : ReadMeViewModel.ReadMeMarkdown;
+                }
+            }
+
+#pragma warning disable CA1031 // Do not catch general exception types
+            try
+            {
+                await UpdateMarkdownAsync(markdown);
+            }
+            catch (Exception ex)
+            {
+                ReadMeViewModel.IsErrorWithReadMe = true;
+                descriptionMarkdownPreview.Visibility = Visibility.Collapsed;
+                await TelemetryUtility.PostFaultAsync(ex, nameof(ReadMePreviewViewModel));
+            }
+#pragma warning restore CA1031 // Do not catch general exception types
+        }
+
+        private async Task UpdateMarkdownAsync(string markDown)
+        {
+            if (_markdownPreview is null)
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                _markdownPreview = new PreviewBuilder().Build();
+                descriptionMarkdownPreview.Content = _markdownPreview.VisualElement;
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
+            if (DataContext is ReadMePreviewViewModel viewModel)
+            {
+                if (markDown is not null)
+                {
+                    await _markdownPreview.UpdateContentAsync(markDown, ScrollHint.None);
+                    descriptionMarkdownPreview.Visibility = string.IsNullOrEmpty(markDown) ? Visibility.Collapsed : Visibility.Visible;
+                }
+            }
+        }
+    }
+}
