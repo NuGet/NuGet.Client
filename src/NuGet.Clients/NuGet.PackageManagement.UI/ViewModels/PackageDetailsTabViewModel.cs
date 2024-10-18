e4d713b59226af789287388f33002ba3544dc1f7
Jonatan Gonzalez (HE/HIM) <jongonza@microsoft.com>
2024-10-18 12:34:30 -0700

    Adjust how selected tab is set

--- src/NuGet.Clients/NuGet.PackageManagement.UI/ViewModels/PackageDetailsTabViewModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/ViewModels/PackageDetailsTabViewModel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Shell;
@@ -88,7 +87,7 @@ namespace NuGet.PackageManagement.UI.ViewModels
             {
                 if (!ReadmePreviewViewModel.IsVisible)
                 {
-                    SelectedTab = Tabs.Where(t => t is DetailControlModel).FirstOrDefault();
+                    SelectedTab = DetailControlModel;
                 }
             }
         }
