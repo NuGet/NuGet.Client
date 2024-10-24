a3a888bf9bb82158ab43cc2958afc883f7428f78
Nigusu Yenework <nyenework@microsoft.com>
2024-10-24 15:24:46 -0700

    PluginManager checks plugin type

--- src/NuGet.Core/NuGet.Protocol/Plugins/PluginManager.cs
+++ src/NuGet.Core/NuGet.Protocol/Plugins/PluginManager.cs
@@ -212,12 +212,26 @@ namespace NuGet.Protocol.Plugins
                 {
                     if (result.PluginFile.State.Value == PluginFileState.Valid)
                     {
-                        var plugin = await _pluginFactory.GetOrCreateAsync(
+                        IPlugin plugin;
+
+                        if (result.PluginFile.IsDotnetToolsPlugin)
+                        {
+                            plugin = await _pluginFactory.GetOrCreateNetToolsPluginAsync(
                             result.PluginFile.Path,
                             PluginConstants.PluginArguments,
                             new RequestHandlers(),
                             _connectionOptions,
                             cancellationToken);
+                        }
+                        else
+                        {
+                            plugin = await _pluginFactory.GetOrCreateAsync(
+                            result.PluginFile.Path,
+                            PluginConstants.PluginArguments,
+                            new RequestHandlers(),
+                            _connectionOptions,
+                            cancellationToken);
+                        }
 
                         var utilities = await PerformOneTimePluginInitializationAsync(plugin, cancellationToken);
 
