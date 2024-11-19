6a4f0b2f4d68cdcae19b46c595ff8156aeeeacc0
Andy Zivkovic <Andy.Zivkovic@microsoft.com>
2024-11-20 08:58:43 +1030

    NuGet.exe workaround & test no longer needed

--- test/NuGet.Core.FuncTests/Msbuild.Integration.Test/NuGetAuditDefaultsTests.cs
+++ test/NuGet.Core.FuncTests/Msbuild.Integration.Test/NuGetAuditDefaultsTests.cs
@@ -51,34 +51,5 @@ namespace Msbuild.Integration.Test
             // Assert
             resultText.Should().Be(expected);
         }
-
-        [Fact]
-        // Some customers run the latest Nuget.exe with old versions of MSBuild. MSBuild only added intrinsic functions
-        // needed for SdkAnalysisVersion comparisons in dev16, so NuGet.exe detects the MSBuild version and adds a
-        // global property when invoking MSBuild, so that our MSBuild script can avoid calling intrinstic functions
-        // that don't exist. In order to test this with a "real" integration test, we'd need an old version of MSBuild
-        // installed, which is not worth the effort, assuming it's even feasible. Therefore, while this test can't
-        // validate that the intrinstic functions are not called, it can set the same global property and validate that
-        // the default values are what we expect.
-        public void NuGetAuditModeDefaults_NuGetExeWithOldMSBuildEmulation()
-        {
-            // Arrange
-            using var testDirectory = TestDirectory.Create();
-
-            string projectText = @"<Project>
-    <Import Project=""$(NuGetRestoreTargets)"" />
-</Project>";
-            var projectFilePath = Path.Combine(testDirectory, "my.proj");
-            File.WriteAllText(projectFilePath, projectText);
-
-            string args = $"{projectFilePath} -getProperty:NuGetAuditMode -p:NuGetExeSkipSdkAnalysisLevelCheck=true";
-
-            // Act
-            var result = _fixture.RunMsBuild(testDirectory, args);
-            var resultText = result.Output.Trim();
-
-            // Assert
-            resultText.Should().Be("all");
-        }
     }
 }
