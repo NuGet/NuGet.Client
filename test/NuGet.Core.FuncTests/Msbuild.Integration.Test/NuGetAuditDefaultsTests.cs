d97289ecfb7b33e52d34d222b6ec9118185ccb32
Andy Zivkovic <zivkan@users.noreply.github.com>
2024-11-20 15:57:58 -0800

    Revert NuGetAuditMode to .NET 8 defaults (#6151)

--- test/NuGet.Core.FuncTests/Msbuild.Integration.Test/NuGetAuditDefaultsTests.cs
+++ test/NuGet.Core.FuncTests/Msbuild.Integration.Test/NuGetAuditDefaultsTests.cs
@@ -22,11 +22,11 @@ namespace Msbuild.Integration.Test
 
         [Theory]
         // .NET 9 SDK
-        [InlineData("9.0.100", "true", "all")]
+        [InlineData("9.0.100", "true", "direct")]
         // .NET 8 SDK
         [InlineData(null, "true", "direct")]
         // non-SDK style project
-        [InlineData(null, null, "all")]
+        [InlineData(null, null, "direct")]
         // non-SDK style project explicit opt-out
         [InlineData("8.0", null, "direct")]
         public void NuGetAuditModeDefaults(string SdkAnalysisLevel, string UsingMicrosoftNETSdk, string expected)
@@ -50,35 +50,6 @@ namespace Msbuild.Integration.Test
 
             // Assert
             resultText.Should().Be(expected);
-        }
-
-        [Fact]
-        // Some customers run the latest Nuget.exe with old versions of MSBuild. MSBuild only added intrinsic functions
-        // needed for SdkAnalysisVersion comparisons in dev16, so NuGet.exe detects the MSBuild version and adds a
-        // global property when invoking MSBuild, so that our MSBuild script can avoid calling intrinstic functions
-        // that don't exist. In order to test this with a "real" integration test, we'd need an old version of MSBuild
-        // installed, which is not worth the effort, assuming it's even feasible. Therefore, while this test can't
-        // validate that the intrinstic functions are not called, it can set the same global property and validate that
-        // the default values are what we expect.
-        public void NuGetAuditModeDefaults_NuGetExeWithOldMSBuildEmulation()
-        {
-            // Arrange
-            using var testDirectory = TestDirectory.Create();
-
-            string projectText = @"<Project>
-    <Import Project=""$(NuGetRestoreTargets)"" />
-</Project>";
-            var projectFilePath = Path.Combine(testDirectory, "my.proj");
-            File.WriteAllText(projectFilePath, projectText);
-
-            string args = $"{projectFilePath} -getProperty:NuGetAuditMode -p:NuGetExeSkipSdkAnalysisLevelCheck=true";
-
-            // Act
-            var result = _fixture.RunMsBuild(testDirectory, args);
-            var resultText = result.Output.Trim();
-
-            // Assert
-            resultText.Should().Be("all");
         }
     }
 }
