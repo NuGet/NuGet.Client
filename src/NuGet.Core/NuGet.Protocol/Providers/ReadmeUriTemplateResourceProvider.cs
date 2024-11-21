9e561cfb08253c8e7f36bc005d861dc5b1d3381c
Jonatan Gonzalez (He/Him) <jongonza@microsoft.com>
2024-11-21 13:50:51 -0800

    add nullable enabled

--- src/NuGet.Core/NuGet.Protocol/Providers/ReadmeUriTemplateResourceProvider.cs
+++ src/NuGet.Core/NuGet.Protocol/Providers/ReadmeUriTemplateResourceProvider.cs
@@ -1,5 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+#nullable enable
 
 using System;
 using System.Threading;
@@ -17,9 +18,9 @@ namespace NuGet.Protocol
         {
         }
 
-        public override async Task<Tuple<bool, INuGetResource>> TryCreate(SourceRepository source, CancellationToken token)
+        public override async Task<Tuple<bool, INuGetResource?>> TryCreate(SourceRepository source, CancellationToken token)
         {
-            ReadmeUriTemplateResource resource = null;
+            ReadmeUriTemplateResource? resource = null;
             var serviceIndex = await source.GetResourceAsync<ServiceIndexResourceV3>(token);
             if (serviceIndex != null)
             {
@@ -29,7 +30,7 @@ namespace NuGet.Protocol
                 resource = string.IsNullOrWhiteSpace(uriTemplate) ? null : new ReadmeUriTemplateResource(uriTemplate);
             }
 
-            return new Tuple<bool, INuGetResource>(resource != null, resource);
+            return new Tuple<bool, INuGetResource?>(resource != null, resource);
         }
     }
 }
