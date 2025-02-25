66db33f3dd0f062fb2ce0bf857864237907ddb16
Nigusu Yenework <nyenework@microsoft.com>
2025-02-25 08:43:59 -0800

    test

--- test/NuGet.Core.Tests/NuGet.Protocol.Tests/Plugins/PluginDiscovererTests.cs
+++ test/NuGet.Core.Tests/NuGet.Protocol.Tests/Plugins/PluginDiscovererTests.cs
@@ -546,7 +546,7 @@ namespace NuGet.Protocol.Plugins.Tests
             // Arrange
             using TestDirectory testDirectory = TestDirectory.Create();
             var workingPath = testDirectory.Path;
-            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin.exe");
+            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin-.exe");
             File.Create(pluginFilePath);
             var fileInfo = new FileInfo(pluginFilePath);
 
@@ -563,7 +563,7 @@ namespace NuGet.Protocol.Plugins.Tests
             // Arrange
             using TestDirectory testDirectory = TestDirectory.Create();
             var workingPath = testDirectory.Path;
-            var nonPluginFilePath = Path.Combine(workingPath, "nuget-plugin.txt");
+            var nonPluginFilePath = Path.Combine(workingPath, "nuget-plugin-.txt");
             File.Create(nonPluginFilePath);
             var fileInfo = new FileInfo(nonPluginFilePath);
 
@@ -580,7 +580,7 @@ namespace NuGet.Protocol.Plugins.Tests
             // Arrange
             using TestDirectory testDirectory = TestDirectory.Create();
             var workingPath = testDirectory.Path;
-            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin");
+            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin-");
             File.Create(pluginFilePath).Dispose();
 
 #if NET8_0_OR_GREATER
@@ -606,7 +606,7 @@ namespace NuGet.Protocol.Plugins.Tests
             // Arrange
             using TestDirectory testDirectory = TestDirectory.Create();
             var workingPath = testDirectory.Path;
-            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin");
+            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin-");
             File.Create(pluginFilePath);
 
             // Set execute permissions
@@ -627,7 +627,7 @@ namespace NuGet.Protocol.Plugins.Tests
             // Arrange
             using TestDirectory testDirectory = TestDirectory.Create();
             var workingPath = testDirectory.Path;
-            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin");
+            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin-");
             File.Create(pluginFilePath);
 
             // Remove execute permissions
@@ -648,7 +648,7 @@ namespace NuGet.Protocol.Plugins.Tests
             // Arrange
             using TestDirectory testDirectory = TestDirectory.Create();
             var workingPath = testDirectory.Path;
-            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin with space");
+            var pluginFilePath = Path.Combine(workingPath, "nuget-plugin- with space");
             File.Create(pluginFilePath).Dispose();
 
             // Set execute permissions
