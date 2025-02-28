9a7cf2a39ef48dfa3f1928401404aa3339c9c7ff
Vishav Pandhi <vipandhi@microsoft.com>
2025-02-28 11:17:00 -0800

    use 1ES build cache

--- build/cachebuild/cache_build.ps1
+++ build/cachebuild/cache_build.ps1
@@ -1,16 +1,3 @@
-# The following is used to obtain the access token when we will upload the msbuild cache data to a private build cache
-Write-Host "Accessing Access Token"
-$accessToken = az account get-access-token --resource https://storage.azure.com --query "accessToken" --output tsv
-if (-not $accessToken) {
-    Write-Error "Unable to get access token"
-    exit 1
-}
-
-# Set the access token as an environment variable for the plugin - This is the case where we are using the Service Connection access Lifeng's Storage Account
-$Env:MSBCACHE_ACCESSTOKEN=$accessToken
-$b = $Env:MSBCACHE_ACCESSTOKEN
-Write-Host "$($b.Substring(1, 11))"
-
 $CacheEnlistmentRoot = "$PSScriptRoot\..\.."
 $MSBuildPath = & "$CacheEnlistmentRoot\scripts\Get-MSBuildPath.ps1" -SixtyFourBit
 
@@ -32,6 +19,9 @@ Copy-Item -Path "$PSScriptRoot\cache_build.targets" -Destination $ImportAfterFol
 # Set the cache log directory so we tell it where to go
 $Env:MSBuildCacheLogDirectory = "$Env:Agent_TempDirectory\MSBuildCacheLogs"
 $Env:MSBuildCacheLocalCacheRootPath = "$Env:Agent_TempDirectory\MSBuildCacheLocalRoot"
+
+# Set the location for the cache auth file
+$Env:MSBuildCacheBuildCacheConfigurationFile = 'c:\buildcacheconfig.json'
 
 Set-Location -Path "$CacheEnlistmentRoot"
 Write-Host "Running $MSBuildPath\msbuild.exe /m:1 /graph /reportfileaccesses /property:Configuration=Debug /property:Platform='Any CPU' `"Nuget.sln`" /v:minimal /bl:`"$Env:Agent_TempDirectory\MSBuildBuildBinLog\build.binlog`""
