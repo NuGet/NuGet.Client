6f4a052c5f37b3ed4f761395e0f5dee957466ff9
Nigusu Yenework <nyenework@microsoft.com>
2025-03-05 11:01:27 -0800

    Show resolved versions in dotnet list package when AuditSources are used

--- src/NuGet.Core/NuGet.CommandLine.XPlat/ListPackage/ListReportPackage.cs
+++ src/NuGet.Core/NuGet.CommandLine.XPlat/ListPackage/ListReportPackage.cs
@@ -94,11 +94,11 @@ namespace NuGet.CommandLine.XPlat.ListPackage
                   autoReference: false)
         { }
 
-        public ListReportPackage(string packageId, string version, List<PackageVulnerabilityMetadata> vulnerabilities)
+        public ListReportPackage(string packageId, string requestedVersion, string resolvedVersion, List<PackageVulnerabilityMetadata> vulnerabilities)
             : this(
                   packageId: packageId,
-                  requestedVersion: version,
-                  resolvedVersion: null,
+                  requestedVersion: requestedVersion,
+                  resolvedVersion: resolvedVersion,
                   latestVersion: null,
                   vulnerabilities: vulnerabilities.Count == 0 ? null : vulnerabilities)
         { }
