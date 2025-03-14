114dcb0e9fd560ef9686ba5c68476b492f3b6a06
Jonatan Gonzalez <jgonz120@gmail.com>
2025-03-14 12:07:04 -0700

    Add known owners capability to remote model (#6318)
    * Added known owners to RemotePackageModel
    
    * Update known owners capability to be private var

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Models/Package/RemotePackageModel.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Models/Package/RemotePackageModel.cs
@@ -12,14 +12,16 @@ using NuGet.VisualStudio.Internal.Contracts;
 
 namespace NuGet.PackageManagement.UI
 {
-    public class RemotePackageModel : PackageModel, IVulnerable
+    public class RemotePackageModel : PackageModel, IVulnerable, IKnownOwnersCapable
     {
         private readonly IVulnerable _vulnerableCapability;
+        private readonly IKnownOwnersCapable _knownOwnersCapability;
 
         public RemotePackageModel(
             PackageIdentity identity,
             IVulnerable vulnerableCapability,
             IEmbeddedResources embeddedResources,
+            IKnownOwnersCapable knownOwnersCapability,
             string? title = null,
             string? description = null,
             string? authors = null,
@@ -43,6 +45,7 @@ namespace NuGet.PackageManagement.UI
             PackageDetailsUrl = packageDetailsUrl;
             DownloadCount = downloadCount;
             _vulnerableCapability = vulnerableCapability;
+            _knownOwnersCapability = knownOwnersCapability;
             ReadmeUrl = readmeUrl;
         }
 
@@ -50,6 +53,7 @@ namespace NuGet.PackageManagement.UI
         public Uri? PackageDetailsUrl { get; }
         public long? DownloadCount { get; }
         public Uri? ReadmeUrl { get; }
+        public IReadOnlyList<KnownOwner>? KnownOwners => _knownOwnersCapability?.KnownOwners;
 
         public IReadOnlyList<PackageVulnerabilityMetadataContextInfo> Vulnerabilities => _vulnerableCapability.Vulnerabilities;
 
