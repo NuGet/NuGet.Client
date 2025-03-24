59ebf217c8bb9a914e3a7ea4400084277ddbc90f
Jean-Pierre Briedé <jebriede@microsoft.com>
2025-03-24 10:25:33 -0700

    Fixed Vulnerability indicator not showing up by Transitive Packages and refactored to reduce code duplication. (#6342)

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Models/Package/RecommendedPackageModelTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Models/Package/RecommendedPackageModelTests.cs
@@ -20,7 +20,6 @@ namespace NuGet.PackageManagement.UI.Test.Models.Package
             var identity = new PackageIdentity("TestPackage", new NuGetVersion("1.0.0"));
             var vulnerabilityCapability = new Mock<IVulnerableCapable>();
             var embeddedCapability = new Mock<IEmbeddedResources>();
-            var knownOwnersCapability = new Mock<IKnownOwnersCapable>();
             var deprecatedCapability = new Mock<IDeprecationCapable>();
             (string modelVersion, string vsixVersion) recommenderVersion = ("1.0.0", "1.0.0");
 
