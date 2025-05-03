242b5997208eec832d3794351803910eb9ad960f
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2025-05-02 17:06:31 -0700

    Update comment for RestoreBarLogger

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Utility/RestoreBarLogger.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Utility/RestoreBarLogger.cs
@@ -18,8 +18,7 @@ namespace NuGet.PackageManagement.UI.Utility
         /// Creates a <see cref="LoggerBase"/> with verbosity level <see cref="Common.LogLevel.Warning"/>
         /// so that only critical log messages are shown in the <see cref="PackageRestoreBar"/>.
         /// </summary>
-        /// <param name="nuGetProjectContext"></param>
-        /// <exception cref="ArgumentNullException"></exception>
+        /// <param name="nuGetProjectContext">Underlying <see cref="ILogger"/> implementation to invoke.</param>
         public RestoreBarLogger(INuGetProjectContext nuGetProjectContext)
             : base(verbosityLevel: LogLevel.Warning)
         {
