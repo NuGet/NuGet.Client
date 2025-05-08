022877614117cc94a4b28231124e25b182c3a706
Nikolche Kolev <nikolce1kolev@gmail.com>
2025-05-07 17:37:26 -0700

    cleanup

--- src/NuGet.Core/NuGet.Build.Tasks/GetReferenceNearestTargetFrameworkTask.cs
+++ src/NuGet.Core/NuGet.Build.Tasks/GetReferenceNearestTargetFrameworkTask.cs
@@ -166,7 +166,7 @@ namespace NuGet.Build.Tasks
                         currentProjectTargetFramework.DotNetFrameworkName,
                         projectNuGetFramework.DotNetFrameworkName);
 
-                    var warning = RestoreLogMessage.CreateWarning(NuGetLogCode.NU1702, message, referencedProjectFile, currentProjectTargetFramework.DotNetFrameworkName);
+                    var warning = RestoreLogMessage.CreateWarning(NuGetLogCode.NU1702, message);
                     warning.LibraryId = referencedProjectFile;
                     warning.ProjectPath = CurrentProjectName;
 
