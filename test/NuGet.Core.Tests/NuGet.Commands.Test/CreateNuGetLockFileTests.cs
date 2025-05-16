7c3448c6542492ea790d96a457a1b2dab60020d4
bcull <brycecullen64@gmail.com>
2025-05-16 13:10:26 -0700

    Align tests with repo conventions & make test simpler

--- test/NuGet.Core.Tests/NuGet.Commands.Test/CreateNuGetLockFileTests.cs
+++ test/NuGet.Core.Tests/NuGet.Commands.Test/CreateNuGetLockFileTests.cs
@@ -3,9 +3,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using NuGet.Frameworks;
 using NuGet.LibraryModel;
 using NuGet.ProjectModel;
+using NuGet.Test.Utility;
 using NuGet.Versioning;
 using Test.Utility.Commands;
 using Xunit;
@@ -17,86 +20,84 @@ public sealed class CreateNuGetLockFileTests
     [Fact]
     public void EnsureNugetLockFileDependenciesOrderedAlphabetically()
     {
-        var lockFile = new LockFile()
+        // Arrange
+        using (var pathContext = new SimpleTestPathContext())
         {
-            Version = 3
-        };
-
-        var target = new LockFileTarget()
-        {
-            TargetFramework = FrameworkConstants.CommonFrameworks.NetCoreApp30
-        };
-
-        // Ordered non-alphabetically
-        List<string> testNames = new List<string> { "C.Package", "A.Package", "B.Package" };
-
-        // Create LockFileTargetLibrary
-        foreach (var name in testNames)
-        {
-            var targetLib = new LockFileTargetLibrary()
+            var lockFile = new LockFile()
             {
-                Name = name,
-                Version = NuGetVersion.Parse("1.0.0"),
-                Type = LibraryType.Package
+                Version = 3
             };
-            targetLib.CompileTimeAssemblies.Add(new LockFileItem("lib/netcoreapp3.0/a.dll"));
-            targetLib.FrameworkReferences.Add("Microsoft.Windows.Desktop|WindowsForms");
-            targetLib.FrameworkReferences.Add("Microsoft.Windows.Desktop|WPF");
-            target.Libraries.Add(targetLib);
-        }
 
-        lockFile.Targets.Add(target);
-
-        //Create LockFileLibrary
-        foreach (var name in testNames)
-        {
-            var lib = new LockFileLibrary()
+            var target = new LockFileTarget()
             {
-                Name = name,
-                Version = NuGetVersion.Parse("1.0.0"),
-                Type = LibraryType.Package
+                TargetFramework = FrameworkConstants.CommonFrameworks.NetCoreApp30
             };
-            lib.Files.Add("lib/netcoreapp3.0/a.dll");
-            lib.Files.Add(name + ".nuspec");
-            lockFile.Libraries.Add(lib);
-        }
 
-        lockFile.PackageSpec = PackageReferenceSpecBuilder.Create("Project", @"X:\Path\Project.csproj")
-            .WithTargetFrameworks(new[]
-                    {
-                        new TargetFrameworkInformation
+            // Ordered non-alphabetically
+            var testNames = new List<string> { "C.Package", "A.Package", "B.Package" };
+
+            // Create LockFileTargetLibrary
+            foreach (var name in testNames)
+            {
+                var targetLib = new LockFileTargetLibrary()
+                {
+                    Name = name,
+                    Version = NuGetVersion.Parse("1.0.0"),
+                    Type = LibraryType.Package
+                };
+                target.Libraries.Add(targetLib);
+            }
+
+            lockFile.Targets.Add(target);
+
+            // Create LockFileLibrary
+            foreach (var name in testNames)
+            {
+                var lib = new LockFileLibrary()
+                {
+                    Name = name,
+                    Version = NuGetVersion.Parse("1.0.0"),
+                    Type = LibraryType.Package
+                };
+                lockFile.Libraries.Add(lib);
+            }
+
+            var projectName = "TestProject";
+            var projectDirectory = Path.Combine(pathContext.SolutionRoot, projectName);
+            lockFile.PackageSpec = PackageReferenceSpecBuilder.Create(projectName, projectDirectory)
+                .WithTargetFrameworks(new[]
                         {
-                            FrameworkName = FrameworkConstants.CommonFrameworks.NetCoreApp30,
-                            Dependencies = [
-                                new LibraryDependency
-                                {
-                                    LibraryRange = new LibraryRange(
-                                        "Microsoft.NETCore.App",
-                                        new VersionRange(
-                                            minVersion: new NuGetVersion("1.0.1"),
-                                            originalString: "1.0.1"),
-                                        LibraryDependencyTarget.Package)
-                                }
-                            ],
-                            CentralPackageVersions =  new Dictionary<string, CentralPackageVersion>(StringComparer.OrdinalIgnoreCase)
+                            new TargetFrameworkInformation
                             {
-                                { "B.Package", new CentralPackageVersion("B.Package", VersionRange.Parse("1.0.0")) }
+                                FrameworkName = FrameworkConstants.CommonFrameworks.NetCoreApp30,
+                                Dependencies = [
+                                    new LibraryDependency
+                                    {
+                                        LibraryRange = new LibraryRange(
+                                            "Microsoft.NETCore.App",
+                                            new VersionRange(
+                                                minVersion: new NuGetVersion("1.0.1"),
+                                                originalString: "1.0.1"),
+                                            LibraryDependencyTarget.Package)
+                                    }
+                                ],
+                                CentralPackageVersions =  new Dictionary<string, CentralPackageVersion>(StringComparer.OrdinalIgnoreCase)
+                                {
+                                    { "B.Package", new CentralPackageVersion("B.Package", VersionRange.Parse("1.0.0")) }
+                                }
                             }
-                        }
-                    })
-                    .WithCentralPackageVersionsEnabled()
-                    .WithCentralPackageTransitivePinningEnabled()
-                    .Build()
-                    .WithTestRestoreMetadata();
+                        })
+                        .WithCentralPackageVersionsEnabled()
+                        .WithCentralPackageTransitivePinningEnabled()
+                        .Build()
+                        .WithTestRestoreMetadata();
 
+            // Act
+            var builder = new PackagesLockFileBuilder();
+            var nuGetLockFile = builder.CreateNuGetLockFile(lockFile);
 
-        var builder = new PackagesLockFileBuilder();
-        var nuGetLockFile = builder.CreateNuGetLockFile(lockFile);
-
-        // Assert that the dependencies in the lockfile are ordered alphabetically by name/id
-        Assert.Equal("A.Package", nuGetLockFile.Targets[0].Dependencies[0].Id);
-        Assert.Equal("B.Package", nuGetLockFile.Targets[0].Dependencies[1].Id);
-        Assert.Equal("C.Package", nuGetLockFile.Targets[0].Dependencies[2].Id);
-
+            // Assert
+            Assert.Equal(["A.Package", "B.Package", "C.Package"], nuGetLockFile.Targets.Single().Dependencies.Select(d => d.Id));
+        }
     }
 }
