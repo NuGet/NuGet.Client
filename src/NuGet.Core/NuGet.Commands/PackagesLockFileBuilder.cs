b2142c9b3b0f4882ce671da1fa85fcd3d2a2ae8b
bcull <brycecullen64@gmail.com>
2025-05-16 13:09:42 -0700

    Use StringComparator for clarity

--- src/NuGet.Core/NuGet.Commands/PackagesLockFileBuilder.cs
+++ src/NuGet.Core/NuGet.Commands/PackagesLockFileBuilder.cs
@@ -108,7 +108,7 @@ namespace NuGet.Commands
                     nuGettarget.Dependencies.Add(dependency);
                 }
 
-                nuGettarget.Dependencies = nuGettarget.Dependencies.OrderBy(d => d.Id).ToList();
+                nuGettarget.Dependencies = nuGettarget.Dependencies.OrderBy(d => d.Id, StringComparer.InvariantCulture).ToList();
 
                 lockFile.Targets.Add(nuGettarget);
             }
