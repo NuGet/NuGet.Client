822ed2912568d230d44435046aefd291999ac203
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2025-07-14 18:52:06 -0700

    RestoreCommand_ProjectJsonProjectType_LogsNU1016ErrorAsync

--- test/NuGet.Core.Tests/NuGet.Commands.Test/RestoreCommandTests/RestoreCommandTests.cs
+++ test/NuGet.Core.Tests/NuGet.Commands.Test/RestoreCommandTests/RestoreCommandTests.cs
@@ -455,6 +455,61 @@ namespace NuGet.Commands.Test.RestoreCommandTests
         }
 
         [Fact]
+        public async Task RestoreCommand_ProjectJsonProjectType_LogsNU1016ErrorAsync()
+        {
+            // Arrange
+            var project1Json = @"
+            {
+              ""version"": ""1.0.0"",
+              ""description"": """",
+              ""authors"": [ ""author"" ],
+              ""tags"": [ """" ],
+              ""projectUrl"": """",
+              ""licenseUrl"": """",
+              ""frameworks"": {
+                ""netstandard1.3"": {
+                    ""dependencies"": {
+                    }
+                }
+              }
+            }";
+
+            using var workingDir = TestDirectory.Create();
+
+            var packagesDir = new DirectoryInfo(Path.Combine(workingDir, "globalPackages"));
+            var sourceDir = new DirectoryInfo(Path.Combine(workingDir, "packageSource"));
+            var projectDir = new DirectoryInfo(Path.Combine(workingDir, "projects", "project1"));
+            packagesDir.Create();
+            sourceDir.Create();
+            projectDir.Create();
+
+            File.WriteAllText(Path.Combine(projectDir.FullName, "project.json"), project1Json);
+
+            var specPath1 = Path.Combine(projectDir.FullName, "project.json");
+            var spec1 = JsonPackageSpecReader.GetPackageSpec(project1Json, "project1", specPath1).WithTestRestoreMetadata();
+            spec1.RestoreMetadata.ProjectStyle = ProjectStyle.ProjectJson;
+
+            var logger = new TestLogger();
+            var request = new TestRestoreRequest(spec1, sources: Enumerable.Empty<PackageSource>(), packagesDirectory: packagesDir.FullName, logger)
+            {
+                LockFilePath = Path.Combine(projectDir.FullName, "project.lock.json")
+            };
+
+            // Act
+            var command = new RestoreCommand(request);
+            var result = await command.ExecuteAsync();
+            await result.CommitAsync(logger, CancellationToken.None);
+
+            // Assert
+            result.Success.Should().BeFalse();
+            result.LockFile.LogMessages.Count.Should().BeGreaterThanOrEqualTo(1);
+
+            IAssetsLogMessage resultLogMessage = result.LockFile.LogMessages.SingleOrDefault(logMessage => logMessage.Code == NuGetLogCode.NU1016);
+            resultLogMessage.Level.Should().Be(LogLevel.Error);
+            resultLogMessage.Message.Should().Be(Strings.Error_ProjectJson_Deprecated);
+        }
+
+        [Fact]
         public async Task RestoreCommand_FileUriV3FolderAsync()
         {
             // Arrange
