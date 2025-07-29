ad006b7f3ddb147a14ac75cdd74f6a7b009fbd0b
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2025-07-29 12:42:40 -0700

    Revert "[dev] Source code updates from dotnet/dotnet (#6482)"
    This reverts commit 4b55ffef5d6cc86e0f19919cfc4bebbab5dde349.

--- eng/dotnet-build/build.sh
+++ eng/dotnet-build/build.sh
@@ -5,18 +5,11 @@ git config --global protocol.file.allow always
 
 source="${BASH_SOURCE[0]}"
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-
-args=()
-
 configuration='Release'
 verbosity='minimal'
 source_build=false
 product_build=false
-from_vmr=false
-ci=false
-warn_as_error=true
-node_reuse=true
-binary_log=false
+properties=''
 
 # resolve $SOURCE until the file is no longer a symlink
 while [[ -h $source ]]; do
@@ -32,45 +25,31 @@ repo_root=`cd -P "$scriptroot/../.." && pwd`
 repo_root="${repo_root}/"
 
 while [[ $# > 0 ]]; do
-  lowerI="$(echo $1 | awk '{print tolower($0)}')"
-  case $lowerI in
-    --verbosity|-v)
-      verbosity=$2
-      shift
-      ;;
-    --binarylog|-bl)
-      binary_log=true
-      ;;
-    --ci)
-      ci=true
-      ;;
-    --configuration|-c)
-      configuration=$2
-      shift
-      ;;
-    --source-build|--sourcebuild|-sb)
-      source_build=true
-      product_build=true
-      ;;
-    --product-build|--productbuild|-pb)
-      product_build=true
-      ;;
-    --from-vmr|--fromvmr)
-      from_vmr=true
-      ;;
-    --warnaserror)
-      warn_as_error=$2
-      shift
-      ;;
-    --nodereuse)
-      node_reuse=$2
-      shift
-      ;;
-    *)
-      args+=("$1")
-      ;;
+    lowerI="$(echo $1 | awk '{print tolower($0)}')"
+    case $lowerI in
+        --verbosity|-v)
+            verbosity=$2
+            shift
+            ;;
+        --configuration|-c)
+            configuration=$2
+            shift
+            ;;
+        --source-build|--sourcebuild|-sb)
+            source_build=true
+            product_build=true
+            ;;
+        --product-build|--productbuild|-pb)
+            product_build=true
+            ;;
+        -*)
+            # just eat this so we don't try to pass it along to MSBuild
+            export DOTNET_CORESDK_NOPRETTYPRINT=1
+            ;;
+        *)
+            args="$args $1"
+            ;;
     esac
-
     shift
 done
 
@@ -110,32 +89,14 @@ else
 fi
 
 ReadGlobalVersion Microsoft.DotNet.Arcade.Sdk
-
-# Environment variables
 export ARCADE_VERSION=$_ReadGlobalVersion
 export NUGET_PACKAGES=${repo_root}artifacts/.packages/
 
-# MSBuild arguments
-dotnetArguments=()
-dotnetArguments+=("$scriptroot/dotnet-build.proj")
-dotnetArguments+=("/p:RepoRoot=$repo_root")
-dotnetArguments+=("/p:Configuration=$configuration")
-dotnetArguments+=("/p:DotNetBuild=$product_build")
-dotnetArguments+=("/p:DotNetBuildSourceOnly=$source_build")
-dotnetArguments+=("/p:DotNetBuildFromVMR=$from_vmr")
+properties="$properties /p:DotNetBuildRepo=$product_build"
+properties="$properties /p:DotNetBuildSourceOnly=$source_build"
 
-local bl=""
-if [[ "$binary_log" == true ]]; then
-  bl="/bl:\"${repo_root}artifacts/log/${configuration}/Build.binlog\""
-fi
+properties="$properties /p:Configuration=$configuration"
+properties="$properties /p:DotNetBuildRepo=true"
+properties="$properties /p:RepoRoot=$repo_root"
 
-if [[ "$ci" == true ]]; then
-  node_reuse=false
-fi
-
-local warnaserror_switch=""
-if [[ $warn_as_error == true ]]; then
-  warnaserror_switch="/warnaserror"
-fi
-
-"$DOTNET" msbuild /m /nologo /clp:Summary /v:$verbosity /nr:$node_reuse $warnaserror_switch /p:TreatWarningsAsErrors=$warn_as_error /p:ContinuousIntegrationBuild=$ci $bl ${dotnetArguments[@]+"${dotnetArguments[@]}"} ${args[@]+"${args[@]}"}
+"$DOTNET" msbuild -v:$verbosity "$scriptroot/dotnet-build.proj" "/bl:${repo_root}artifacts/log/${configuration}/Build.binlog" $properties $args
