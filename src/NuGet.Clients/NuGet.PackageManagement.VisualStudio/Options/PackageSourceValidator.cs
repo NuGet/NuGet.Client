fd92334fe496a9a46749f5f40d537bf3bd9ceba4
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2025-08-19 11:45:28 -0700

    Remove using

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Options/PackageSourceValidator.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Options/PackageSourceValidator.cs
@@ -6,7 +6,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.Package;
 using NuGet.Configuration;
 
 namespace NuGet.PackageManagement.VisualStudio.Options
