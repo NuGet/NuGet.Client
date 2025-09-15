76369434b2f06bd67b00f7dbd633532315b1c1c3
Andy Zivkovic <Andy.Zivkovic@microsoft.com>
2025-09-16 08:02:28 +0930

    Fix dotnet package search table output

--- test/NuGet.Core.Tests/NuGet.CommandLine.Xplat.Tests/NullLoggerWithColor.cs
+++ test/NuGet.Core.Tests/NuGet.CommandLine.Xplat.Tests/NullLoggerWithColor.cs
@@ -17,6 +17,10 @@ namespace NuGet.CommandLine.Xplat.Tests
         {
         }
 
+        public void LogRaw(string data, ConsoleColor color)
+        {
+        }
+
         public LogLevel LogLevel
         {
             get => VerbosityLevel;
