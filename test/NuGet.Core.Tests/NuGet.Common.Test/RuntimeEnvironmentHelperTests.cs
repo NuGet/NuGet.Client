f8424deb3fff75a6ee5ef54c9132d1c3a702dfd8
Nikolche Kolev <nikolce1kolev@gmail.com>
2025-09-18 16:46:41 -0700

    Fiox what copilot couldn';t

--- test/NuGet.Core.Tests/NuGet.Common.Test/RuntimeEnvironmentHelperTests.cs
+++ test/NuGet.Core.Tests/NuGet.Common.Test/RuntimeEnvironmentHelperTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using NuGet.Test.Utility;
 using Xunit;
 
 namespace NuGet.Common.Test
@@ -51,11 +50,11 @@ namespace NuGet.Common.Test
 
             // Assert - Only one platform should be true
             var trueCount = (isWindows ? 1 : 0) + (isMacOS ? 1 : 0) + (isLinux ? 1 : 0);
-            Assert.True(trueCount <= 1, 
+            Assert.True(trueCount <= 1,
                 $"Multiple platforms detected as true: Windows={isWindows}, macOS={isMacOS}, Linux={isLinux}");
 
             // At least one should be detected (we're running on some platform)
-            Assert.True(trueCount >= 1, 
+            Assert.True(trueCount >= 1,
                 $"No platform detected as true: Windows={isWindows}, macOS={isMacOS}, Linux={isLinux}");
         }
 
