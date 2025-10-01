e84e1c1954d82fc47b08d38b40e563d47672740a
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2025-10-01 12:27:59 -0700

    Migrator remove PackageSpec for Candidate

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Migrate/ProjectJsonToPackageRefMigrator.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Migrate/ProjectJsonToPackageRefMigrator.cs
@@ -12,8 +12,8 @@ using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Threading;
 using NuGet.Common;
 using NuGet.LibraryModel;
+using NuGet.PackageManagement.VisualStudio.Migrate;
 using NuGet.ProjectManagement.Projects;
-using NuGet.ProjectModel;
 using NuGet.VisualStudio;
 using Task = System.Threading.Tasks.Task;
 
@@ -35,7 +35,7 @@ namespace NuGet.PackageManagement.VisualStudio
                 throw new FileNotFoundException(string.Format(CultureInfo.CurrentCulture, Strings.Error_FileNotExists, projectJsonFilePath));
             }
 
-            var packageSpec = JsonPackageSpecReader.GetPackageSpec(
+            PackageSpecProjectJsonMigrationCandidate packageSpec = JsonPackageSpecReader.GetPackageSpec(
                 Path.GetFileNameWithoutExtension(project.MSBuildProjectPath),
                 projectJsonFilePath);
 
@@ -57,9 +57,9 @@ namespace NuGet.PackageManagement.VisualStudio
                 projectJsonFilePath);
         }
 
-        private static async Task MigrateDependenciesAsync(BuildIntegratedNuGetProject project, PackageSpec packageSpec)
+        private static async Task MigrateDependenciesAsync(BuildIntegratedNuGetProject project, PackageSpecProjectJsonMigrationCandidate packageSpec)
         {
-            if (packageSpec.TargetFrameworks.Count > 1)
+            if (packageSpec.TargetFrameworkDependencies.Length > 1)
             {
                 throw new InvalidOperationException(
                     string.Format(
@@ -69,9 +69,9 @@ namespace NuGet.PackageManagement.VisualStudio
             }
 
             var dependencies = new List<LibraryDependency>();
-            foreach (var targetFramework in packageSpec.TargetFrameworks)
+            foreach (var targetFrameworkDependency in packageSpec.TargetFrameworkDependencies)
             {
-                dependencies.AddRange(targetFramework.Dependencies);
+                dependencies.Add(targetFrameworkDependency);
             }
 
             await NuGetUIThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
@@ -83,7 +83,7 @@ namespace NuGet.PackageManagement.VisualStudio
             }
         }
 
-        private static void MigrateRuntimes(PackageSpec packageSpec, Microsoft.Build.Evaluation.Project buildProject)
+        private static void MigrateRuntimes(PackageSpecProjectJsonMigrationCandidate packageSpec, Microsoft.Build.Evaluation.Project buildProject)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
             var runtimes = packageSpec.RuntimeGraph.Runtimes;
