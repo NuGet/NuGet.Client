dc1afa36749b805344f71c78ac5dfca6c152ddc9
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2025-10-02 14:14:34 -0700

    Revert NuGet.ProjectModel change

--- src/NuGet.Core/NuGet.ProjectModel/JsonPackageSpecReader.Utf8JsonStreamReader.cs
+++ src/NuGet.Core/NuGet.ProjectModel/JsonPackageSpecReader.Utf8JsonStreamReader.cs
@@ -53,6 +53,7 @@ namespace NuGet.ProjectModel
         private static readonly byte[] OutputPathPropertyName = Encoding.UTF8.GetBytes("outputPath");
         private static readonly byte[] PackagesConfigPathPropertyName = Encoding.UTF8.GetBytes("packagesConfigPath");
         private static readonly byte[] PackagesPathPropertyName = Encoding.UTF8.GetBytes("packagesPath");
+        private static readonly byte[] ProjectJsonPathPropertyName = Encoding.UTF8.GetBytes("projectJsonPath");
         private static readonly byte[] ProjectNamePropertyName = Encoding.UTF8.GetBytes("projectName");
         private static readonly byte[] ProjectPathPropertyName = Encoding.UTF8.GetBytes("projectPath");
         private static readonly byte[] ProjectStylePropertyName = Encoding.UTF8.GetBytes("projectStyle");
@@ -850,6 +851,10 @@ namespace NuGet.ProjectModel
                     else if (jsonReader.ValueTextEquals(PackagesPathPropertyName))
                     {
                         packagesPath = ExtractMacro(jsonReader.ReadNextTokenAsString(), userSettingsDirectory, useMacros);
+                    }
+                    else if (jsonReader.ValueTextEquals(ProjectJsonPathPropertyName))
+                    {
+                        projectJsonPath = jsonReader.ReadNextTokenAsString();
                     }
                     else if (jsonReader.ValueTextEquals(ProjectNamePropertyName))
                     {
