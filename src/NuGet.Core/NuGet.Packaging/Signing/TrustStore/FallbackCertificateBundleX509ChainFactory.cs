7f24ad61d4462cb8991141936e2868b4af6492df
Nigusu Yenework <nyenework@microsoft.com>
2026-03-12 16:46:38 -0700

    use BaseDirectory

--- src/NuGet.Core/NuGet.Packaging/Signing/TrustStore/FallbackCertificateBundleX509ChainFactory.cs
+++ src/NuGet.Core/NuGet.Packaging/Signing/TrustStore/FallbackCertificateBundleX509ChainFactory.cs
@@ -6,9 +6,9 @@
 #if NET5_0_OR_GREATER
 
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 
 namespace NuGet.Packaging.Signing
 {
@@ -18,6 +18,8 @@ namespace NuGet.Packaging.Signing
         internal const string SubdirectoryName = "trustedroots";
         internal const string CodeSigningFileName = "codesignctl.pem";
         internal const string TimestampingFileName = "timestampctl.pem";
+
+        private static readonly Lazy<string> ThisAssemblyDirectoryPath = new(GetThisAssemblyDirectoryPath, LazyThreadSafetyMode.ExecutionAndPublication);
 
         private FallbackCertificateBundleX509ChainFactory(X509Certificate2Collection certificates, string filePath)
             : base(certificates, filePath)
@@ -31,11 +33,6 @@ namespace NuGet.Packaging.Signing
         {
             factory = null;
 
-            if (!TryGetThisAssemblyDirectoryPath(out string assemblyDirectoryPath))
-            {
-                return false;
-            }
-
             if (string.IsNullOrEmpty(fileName))
             {
                 fileName = storePurpose switch
@@ -47,7 +44,7 @@ namespace NuGet.Packaging.Signing
             }
 
             string fullFilePath = Path.Combine(
-                assemblyDirectoryPath,
+                ThisAssemblyDirectoryPath.Value,
                 SubdirectoryName,
                 fileName);
 
@@ -61,22 +58,9 @@ namespace NuGet.Packaging.Signing
             return false;
         }
 
-        [UnconditionalSuppressMessage(
-            "SingleFile",
-            "IL3000",
-            Justification = "Assembly.Location may be empty in single file scenarios. When empty, TryCreate will return false.")]
-        private static bool TryGetThisAssemblyDirectoryPath(out string directoryPath)
+        private static string GetThisAssemblyDirectoryPath()
         {
-            string location = typeof(FallbackCertificateBundleX509ChainFactory).Assembly.Location;
-
-            if (string.IsNullOrEmpty(location))
-            {
-                directoryPath = null;
-                return false;
-            }
-
-            directoryPath = Path.GetDirectoryName(location);
-            return true;
+            return AppContext.BaseDirectory;
         }
     }
 }
