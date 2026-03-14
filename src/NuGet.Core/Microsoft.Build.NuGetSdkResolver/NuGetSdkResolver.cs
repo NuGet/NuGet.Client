197be931926017ed931882effebf10a483b84fa2
Nikolche Kolev <nikolce1kolev@gmail.com>
2026-03-14 00:55:06 -0700

    Replace GetInstalled with result.LockFile.Libraries

--- src/NuGet.Core/Microsoft.Build.NuGetSdkResolver/NuGetSdkResolver.cs
+++ src/NuGet.Core/Microsoft.Build.NuGetSdkResolver/NuGetSdkResolver.cs
@@ -220,11 +220,11 @@ namespace Microsoft.Build.NuGetSdkResolver
                             foreach (RestoreResult restoreResult in results.Select(i => i.Result).Where(i => i.Success))
                             {
                                 // Find the information about the package that was installed.  In some cases, the version can be different than what was specified (like you specify 1.0 but get 1.0.0)
-                                var installedPackage = restoreResult.GetAllInstalled().FirstOrDefault(i => i == libraryIdentity);
+                                var installedPackage = restoreResult.LockFile.Libraries.FirstOrDefault(i => i.Name.Equals(libraryIdentity.Name, StringComparison.OrdinalIgnoreCase) && i.Version.Equals(libraryIdentity.Version));
 
                                 if (installedPackage != null)
                                 {
-                                    if (TryGetMSBuildSdkPackageInfo(fallbackPackagePathResolver, installedPackage, out installedPath, out installedVersion))
+                                    if (TryGetMSBuildSdkPackageInfo(fallbackPackagePathResolver, libraryIdentity, out installedPath, out installedVersion))
                                     {
                                         break;
                                     }
