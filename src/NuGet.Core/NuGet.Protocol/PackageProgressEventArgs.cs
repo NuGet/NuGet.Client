2da363ddaa799b78f3932689145023f390a3a96e
Nikolche Kolev <nikolev@microsoft.com>
2026-04-08 17:29:11 -0700

    nullable

--- src/NuGet.Core/NuGet.Protocol/PackageProgressEventArgs.cs
+++ src/NuGet.Core/NuGet.Protocol/PackageProgressEventArgs.cs
@@ -1,18 +1,17 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-#nullable disable
-
 using System;
 using NuGet.Configuration;
 using NuGet.Packaging.Core;
 
 namespace NuGet.Protocol.Core.Types
 {
+    [Obsolete("This type is unused and will be removed in a future version.")]
     public class PackageProgressEventArgs : EventArgs
     {
         private readonly PackageIdentity _identity;
-        private readonly PackageSource _source;
+        private readonly PackageSource? _source;
         private readonly double _complete;
 
         /// <summary>
@@ -21,7 +20,7 @@ namespace NuGet.Protocol.Core.Types
         /// <param name="identity">package identity</param>
         /// <param name="source">repository source or null</param>
         /// <param name="complete">0.0 - 1.0</param>
-        public PackageProgressEventArgs(PackageIdentity identity, PackageSource source, double complete)
+        public PackageProgressEventArgs(PackageIdentity identity, PackageSource? source, double complete)
         {
             if (complete < 0
                 || complete > 1)
@@ -44,7 +43,7 @@ namespace NuGet.Protocol.Core.Types
             get { return _identity; }
         }
 
-        public PackageSource PackageSource
+        public PackageSource? PackageSource
         {
             get { return _source; }
         }
