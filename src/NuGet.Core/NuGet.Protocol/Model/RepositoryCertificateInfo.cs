ce853cf744e91971225baad48d23156968874768
Nigusu Yenework <nyenework@microsoft.com>
2026-04-17 13:08:14 -0700

    constractor

--- src/NuGet.Core/NuGet.Protocol/Model/RepositoryCertificateInfo.cs
+++ src/NuGet.Core/NuGet.Protocol/Model/RepositoryCertificateInfo.cs
@@ -12,26 +12,45 @@ namespace NuGet.Protocol
     {
         [JsonProperty(PropertyName = JsonProperties.Fingerprints, Required = Required.Always)]
         [JsonPropertyName(JsonProperties.Fingerprints)]
-        public Fingerprints Fingerprints { get; internal init; } = null!;
+        public Fingerprints Fingerprints { get; private set; } = null!;
 
         [JsonProperty(PropertyName = JsonProperties.Subject, Required = Required.Always)]
         [JsonPropertyName(JsonProperties.Subject)]
-        public string Subject { get; internal init; } = null!;
+        public string Subject { get; private set; } = null!;
 
         [JsonProperty(PropertyName = JsonProperties.Issuer, Required = Required.Always)]
         [JsonPropertyName(JsonProperties.Issuer)]
-        public string Issuer { get; internal init; } = null!;
+        public string Issuer { get; private set; } = null!;
 
         [JsonProperty(PropertyName = JsonProperties.NotBefore, Required = Required.Always)]
         [JsonPropertyName(JsonProperties.NotBefore)]
-        public DateTimeOffset NotBefore { get; internal init; }
+        public DateTimeOffset NotBefore { get; private set; }
 
         [JsonProperty(PropertyName = JsonProperties.NotAfter, Required = Required.Always)]
         [JsonPropertyName(JsonProperties.NotAfter)]
-        public DateTimeOffset NotAfter { get; internal init; }
+        public DateTimeOffset NotAfter { get; private set; }
 
         [JsonProperty(PropertyName = JsonProperties.ContentUrl, Required = Required.Always)]
         [JsonPropertyName(JsonProperties.ContentUrl)]
-        public string ContentUrl { get; internal init; } = null!;
+        public string ContentUrl { get; private set; } = null!;
+
+        public RepositoryCertificateInfo() { }
+
+        [System.Text.Json.Serialization.JsonConstructor]
+        internal RepositoryCertificateInfo(
+            Fingerprints fingerprints,
+            string subject,
+            string issuer,
+            DateTimeOffset notBefore,
+            DateTimeOffset notAfter,
+            string contentUrl)
+        {
+            Fingerprints = fingerprints;
+            Subject = subject;
+            Issuer = issuer;
+            NotBefore = notBefore;
+            NotAfter = notAfter;
+            ContentUrl = contentUrl;
+        }
     }
 }
