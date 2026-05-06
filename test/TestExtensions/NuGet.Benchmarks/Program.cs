e5f80adcbe80b62adddfe4af84ced8bb3f133e52
Nikolche Kolev <nikolce1kolev@gmail.com>
2026-05-06 16:53:34 -0700

    Add a benchmarking project to make it easier to write micro benchmarks from within our repo.

---
+++ test/TestExtensions/NuGet.Benchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<LambdaAllocationBenchmarks>();
+
+public class Item
+{
+    public Item(string value) => Value = value;
+    public string Value { get; }
+}
+
+[MemoryDiagnoser]
+public class LambdaAllocationBenchmarks
+{
+    [Benchmark]
+    public void StaticLambda()
+    {
+        Create(static path => new Item(path));
+    }
+
+    [Benchmark]
+    public void Lambda()
+    {
+        string pre = "my prefix";
+        Create(path => new Item(path + pre));
+    }
+
+    [Benchmark]
+    public void MethodGroup()
+    {
+        Create(Factory);
+    }
+
+    [Benchmark]
+    public void DirectCall()
+    {
+        CreateItem("hello");
+    }
+
+    private static Item Factory(string path) => new Item(path);
+
+    private static void Create<T>(Func<string, T> factory)
+    {
+    }
+
+    private static Item CreateItem(string path)
+    {
+        return new Item(path);
+    }
+}
