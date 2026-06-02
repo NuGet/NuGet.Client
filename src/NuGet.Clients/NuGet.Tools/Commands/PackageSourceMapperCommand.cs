a1127bfa687d186f60ac60e8388e3471d7b0c6ac
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2026-06-02 09:42:34 -0700

    WIP todo

--- src/NuGet.Clients/NuGet.Tools/Commands/PackageSourceMapperCommand.cs
+++ src/NuGet.Clients/NuGet.Tools/Commands/PackageSourceMapperCommand.cs
@@ -4,28 +4,15 @@
 using System;
 using System.ComponentModel.Design;
 using System.Threading;
-using System.Threading.Tasks;
-using Microsoft;
-using Microsoft.ServiceHub.Framework;
-using Microsoft.VisualStudio.Copilot;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Threading;
-using NuGet.PackageManagement.VisualStudio;
 using NuGet.VisualStudio;
+using NuGet.VisualStudio.Telemetry;
+using NuGetVSExtension;
 
 namespace NuGet.Tools.Commands
 {
     internal sealed class PackageSourceMapperCommand
     {
-        private const string AgentModeResponderServiceMoniker = "Microsoft.VisualStudio.Copilot.AgentModeResponder";
-        private const string ServiceName = "Microsoft.VisualStudio.Copilot.SolutionContextProvider";
-
-        private static readonly ServiceRpcDescriptor ProviderDescriptor = CopilotDescriptors.CreateContextProviderDescriptor(ServiceName);
-        private static readonly CopilotContextDescriptor ContextDescriptor = new CopilotContextDescriptor(
-                    "SolutionFile",
-                    "solution file context",
-                    CopilotDefaultTypes.StringName);
-
         /// <summary>
         /// Command ID.
         /// </summary>
@@ -37,17 +24,14 @@ namespace NuGet.Tools.Commands
         public static readonly Guid CommandSet = GuidList.guidOnboardPackageSourceMappingCmdSet;
 
         private readonly OleMenuCommandService _oleMenuCommandService;
-        private readonly ICopilotToolInvocationService _toolInvocationService;
-        private readonly IVsSolutionManager _solutionManager;
+        private readonly IPackageSourceMappingService _packageSourceMappingService;
 
         public PackageSourceMapperCommand(
             OleMenuCommandService oleMenuCommandService,
-            ICopilotToolInvocationService toolInvocationService,
-            IVsSolutionManager solutionManager)
+            IPackageSourceMappingService packageSourceMappingService)
         {
             _oleMenuCommandService = oleMenuCommandService ?? throw new ArgumentNullException(nameof(oleMenuCommandService));
-            _toolInvocationService = toolInvocationService ?? throw new ArgumentNullException(nameof(toolInvocationService));
-            _solutionManager = solutionManager ?? throw new ArgumentNullException(nameof(solutionManager));
+            _packageSourceMappingService = packageSourceMappingService ?? throw new ArgumentNullException(nameof(packageSourceMappingService));
         }
 
         public void Initialize()
@@ -59,41 +43,9 @@ namespace NuGet.Tools.Commands
 
         private void ExecutePackageSourceMapperCommand(object sender, EventArgs e)
         {
-            NuGetUIThreadHelper.JoinableTaskFactory.RunAsync(() => ExecuteAsync(CancellationToken.None))
+            NuGetUIThreadHelper.JoinableTaskFactory.RunAsync(() =>
+                _packageSourceMappingService.LaunchOnboardPackageSourceMappingAsync(CancellationToken.None))
                 .PostOnFailure(nameof(NuGetPackage), nameof(ExecutePackageSourceMapperCommand));
         }
-
-        private async Task ExecuteAsync(CancellationToken cancellationToken)
-        {
-            CopilotClientId clientId = new("Microsoft.VisualStudio.NuGet.PackageSourceMapper");
-
-            CopilotRequest request = new("Onboard this repo to package source mapping")
-            {
-                Guidance = "Use absolute paths when invoking MCP Tools.",
-                DirectedResponders = [new(AgentModeResponderServiceMoniker, new(CopilotDescriptors.CurrentResponderVersion))]
-            };
-
-            CopilotToolSessionResult result = await _toolInvocationService.TryCreateToolSessionAsync(
-                clientId,
-                request.CorrelationId,
-                McpServerConstants.PackageSourceMappingFullyQualifiedToolName,
-                cancellationToken);
-
-            if (!result.IsSuccess)
-            {
-                // TODO: Add telemetry and user-facing error messages
-                return;
-            }
-
-            await using CopilotToolSession session = result.Session!;
-
-            string solutionPathContext = $"The current solution file path is: {GetSolutionPath()}.";
-            CopilotContext context = new CopilotContext(ProviderDescriptor.Moniker, ContextDescriptor, request.CorrelationId, solutionPathContext);
-            CopilotRequest requestWithFunctionsAndContext = request.WithFunctions(session.Functions).WithContext(context);
-
-            _ = await session.Thread.Session.SendRequestAsync(requestWithFunctionsAndContext, cancellationToken);
-        }
-
-        private string GetSolutionPath() => _solutionManager?.SolutionDirectory ?? string.Empty;
     }
 }
