9d8be96a63cb74c438068d486527be1053dc219a
Donnie Goodson <49205731+donnie-msft@users.noreply.github.com>
2026-06-02 09:42:34 -0700

    WIP todo

---
+++ src/NuGet.Clients/NuGet.Tools/PackageSourceMappingService.cs
@@ -0,0 +1,83 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft;
+using Microsoft.ServiceHub.Framework;
+using Microsoft.VisualStudio.Copilot;
+using NuGet.Common;
+using NuGet.PackageManagement.VisualStudio;
+using NuGet.VisualStudio;
+
+namespace NuGetVSExtension
+{
+    [Export(typeof(IPackageSourceMappingService))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    internal class PackageSourceMappingService : IPackageSourceMappingService
+    {
+        private const string AgentModeResponderServiceMoniker = "Microsoft.VisualStudio.Copilot.AgentModeResponder";
+        private const string ServiceName = "Microsoft.VisualStudio.Copilot.SolutionContextProvider";
+
+        private static readonly ServiceRpcDescriptor ProviderDescriptor = CopilotDescriptors.CreateContextProviderDescriptor(ServiceName);
+        private static readonly CopilotContextDescriptor ContextDescriptor = new CopilotContextDescriptor(
+                    "SolutionFile",
+                    "solution file context",
+                    CopilotDefaultTypes.StringName);
+
+        [Import(typeof(ICopilotToolInvocationService))]
+        public ICopilotToolInvocationService? ToolInvocationService { get; set; }
+
+        [Import(typeof(IVsSolutionManager))]
+        public IVsSolutionManager? SolutionManager { get; set; }
+
+        [Import(typeof(VisualStudioActivityLogger))]
+        public ILogger? ActivityLogger { get; set; }
+
+        public async Task LaunchOnboardPackageSourceMappingAsync(CancellationToken cancellationToken)
+        {
+            CopilotClientId clientId = new("Microsoft.VisualStudio.NuGet.PackageSourceMapper");
+
+            CopilotRequest request = new("Onboard this repository to package source mapping.")
+            {
+                Guidance = "Use absolute paths when invoking MCP Tools.",
+                DirectedResponders = [new(AgentModeResponderServiceMoniker, new(CopilotDescriptors.CurrentResponderVersion))]
+            };
+
+            Assumes.Present(ToolInvocationService);
+
+            CopilotToolSessionResult result = await ToolInvocationService.TryCreateToolSessionAsync(
+                clientId,
+                request.CorrelationId,
+                McpServerConstants.PackageSourceMappingFullyQualifiedToolName,
+                cancellationToken);
+
+            if (!result.IsSuccess)
+            {
+                // TODO: Add telemetry and user-facing error messages
+                return;
+            }
+
+            await using CopilotToolSession session = result.Session!;
+
+            string solutionPathContext = $"The current solution file path is: {GetSolutionPath()}.";
+            CopilotContext context = new CopilotContext(ProviderDescriptor.Moniker, ContextDescriptor, request.CorrelationId, solutionPathContext);
+            CopilotRequest requestWithFunctionsAndContext = request.WithFunctions(session.Functions).WithContext(context);
+
+            try
+            {
+                _ = await session.Thread.Session.SendRequestAsync(requestWithFunctionsAndContext, cancellationToken);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                ActivityLogger?.LogError(ex.Message);
+            }
+        }
+
+        //TODO: Telemetry
+
+        private string GetSolutionPath() => SolutionManager?.SolutionDirectory ?? string.Empty;
+    }
+}
