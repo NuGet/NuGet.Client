f8e90b5ac640701ae925efe2c9b8c8f74070b282
Nigusu Yenework <nyenework@microsoft.com>
2026-06-02 16:39:48 -0700

    Remove package ID validation

--- src/NuGet.Core/NuGet.Protocol/Utility/PackageIdValidator.cs
+++ src/NuGet.Core/NuGet.Protocol/Utility/PackageIdValidator.cs
@@ -1,20 +1,13 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Globalization;
-using NuGet.Common;
 using NuGet.Packaging;
 
 namespace NuGet.Protocol
 {
     internal static class PackageIdValidator
     {
-        private const string DisableValidationEnvVar = "NUGET_DISABLE_PACKAGEID_VALIDATION";
-
-        private static readonly Lazy<bool> IsValidationDisabled = new Lazy<bool>(() =>
-            IsPackageIdValidationDisabled(EnvironmentVariableWrapper.Instance));
-
         /// <summary>
         /// Validates the package ID content.
         /// </summary>
@@ -22,22 +15,12 @@ namespace NuGet.Protocol
         /// <exception cref="InvalidPackageIdException">
         /// Thrown if <paramref name="packageId"/> is not a valid NuGet package ID.
         /// </exception>
-        internal static void Validate(string packageId, IEnvironmentVariableReader? env = null)
+        internal static void Validate(string packageId)
         {
-            bool isDisabled = env == null
-                ? IsValidationDisabled.Value
-                : IsPackageIdValidationDisabled(env);
-
-            if (!isDisabled)
+            if (!Packaging.PackageIdValidator.IsValidPackageId(packageId))
             {
-                if (!Packaging.PackageIdValidator.IsValidPackageId(packageId))
-                {
-                    throw new InvalidPackageIdException(string.Format(CultureInfo.CurrentCulture, Strings.Error_Invalid_package_id, packageId));
-                }
+                throw new InvalidPackageIdException(string.Format(CultureInfo.CurrentCulture, Strings.Error_Invalid_package_id, packageId));
             }
         }
-
-        private static bool IsPackageIdValidationDisabled(IEnvironmentVariableReader env) =>
-            string.Equals(env.GetEnvironmentVariable(DisableValidationEnvVar), bool.TrueString, StringComparison.OrdinalIgnoreCase);
     }
 }
