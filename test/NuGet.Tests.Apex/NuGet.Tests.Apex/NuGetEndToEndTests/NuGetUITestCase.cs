eac426be7268faba18d67cce9c4ab25d61352603
Nigusu Solomon Yenework <59111203+Nigusu-Allehu@users.noreply.github.com>
2026-06-30 15:06:36 -0700

    Prepare tests for canary promotion

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NuGetUITestCase.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NuGetUITestCase.cs
@@ -374,6 +374,7 @@ namespace NuGet.Tests.Apex
         [TestMethod]
         [Timeout(DefaultTimeout)]
         [TestCategory("StagingGate")]
+        [TestCatagory("CanaryGate")]
         public async Task VerifyDeletedAssetsFileIsBackByReloadingProject()
         {
             // Arrange
