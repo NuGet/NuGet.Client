3750e3df223e75a1c5d615c02d6e0e4c1c918590
Nigusu Solomon Yenework <59111203+Nigusu-Allehu@users.noreply.github.com>
2026-06-30 15:08:10 -0700

    Add second test

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NetCoreProjectTestCase.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NetCoreProjectTestCase.cs
@@ -179,6 +179,7 @@ namespace NuGet.Tests.Apex
         [TestMethod]
         [Timeout(DefaultTimeout)]
         [TestCategory("StagingGate")]
+        [TestCatagory("CanaryGate")]
         public async Task InstallAndUpdatePackageFromUI_NetCoreProject_Succeeds()
         {
             // Arrange
