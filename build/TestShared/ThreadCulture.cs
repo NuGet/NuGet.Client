6414029b61838f651af722113df2d61e03841ce2
gekka <10055381+gekka@users.noreply.github.com>
2026-07-25 14:01:14 +0900

    set child process culture from test functions through environment variables

--- build/TestShared/ThreadCulture.cs
+++ build/TestShared/ThreadCulture.cs
@@ -1,15 +1,24 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Globalization;
+using System.Threading;
+
 internal static class CommonThreadCultureModule
 {
     [System.Runtime.CompilerServices.ModuleInitializer]
     public static void Initialize()
     {
-        var enus = System.Globalization.CultureInfo.GetCultureInfo("en-us");
-        System.Globalization.CultureInfo.DefaultThreadCurrentCulture = enus;
-        System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = enus;
-        System.Threading.Thread.CurrentThread.CurrentCulture = enus;
-        System.Threading.Thread.CurrentThread.CurrentUICulture = enus;
+        var enus = CultureInfo.GetCultureInfo("en-us");
+        if (enus.LCID != CultureInfo.DefaultThreadCurrentCulture?.LCID)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = enus;
+            CultureInfo.DefaultThreadCurrentUICulture = enus;
+        }
+        if (enus.LCID != Thread.CurrentThread.CurrentCulture.LCID)
+        {
+            Thread.CurrentThread.CurrentCulture = enus;
+            Thread.CurrentThread.CurrentUICulture = enus;
+        }
     }
 }
