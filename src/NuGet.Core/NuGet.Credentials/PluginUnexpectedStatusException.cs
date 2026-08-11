0f781ba852a302b8f7fa7d67435edae22a446a86
Nikolche Kolev <nikolce1kolev@gmail.com>
2026-08-11 21:04:42 +0300

    Enable nullable annotations in NuGet.Credentials (#7604)

--- src/NuGet.Core/NuGet.Credentials/PluginUnexpectedStatusException.cs
+++ src/NuGet.Core/NuGet.Credentials/PluginUnexpectedStatusException.cs
@@ -1,8 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-#nullable disable
-
 using System;
 using System.Globalization;
 
@@ -29,14 +27,14 @@ namespace NuGet.Credentials
         /// Initializes a new instance of the <see cref="PluginUnexpectedStatusException"/> class with a specified error message.
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
-        public PluginUnexpectedStatusException(string message) : base(message) { }
+        public PluginUnexpectedStatusException(string? message) : base(message) { }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PluginUnexpectedStatusException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
         /// </summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
         /// <param name="inner">The exception that is the cause of the current exception.</param>
-        public PluginUnexpectedStatusException(string message, Exception inner) : base(message, inner) { }
+        public PluginUnexpectedStatusException(string? message, Exception? inner) : base(message, inner) { }
 
 #if IS_DESKTOP
         /// <summary>
@@ -59,6 +57,11 @@ namespace NuGet.Credentials
         public static PluginException CreateUnexpectedStatusMessage(
             string path, PluginCredentialResponseExitCode status)
         {
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
             return new PluginUnexpectedStatusException(
                 string.Format(CultureInfo.CurrentCulture, Resources.PluginException_UnexpectedStatus_Format, path, status));
         }
