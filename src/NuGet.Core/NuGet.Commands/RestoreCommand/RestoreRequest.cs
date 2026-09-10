9c354c6376972a50ea07f104add008cb9b466434
Andy Zivkovic <zivkan@users.noreply.github.com>
2026-09-11 08:34:38 +0930

    Fail restore when floating version needs cooldown

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/RestoreRequest.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/RestoreRequest.cs
@@ -184,6 +184,11 @@ namespace NuGet.Commands
 
         public PackageSourceMapping PackageSourceMapping { get; }
 
+        /// <summary>
+        /// Gets or sets the package ID patterns that are exempt from package source minimum publish age restrictions.
+        /// </summary>
+        public MinPublishAgeExceptions MinPublishAgeExceptions { get; set; }
+
         /// <remarks>
         /// This property should only be used to override the default verifier on tests.
         /// </remarks>
